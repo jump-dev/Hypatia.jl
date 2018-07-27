094108bf10b6131f33f4d94c6b8dad4456d63682
Chris Coey <coey.chris@gmail.com>
2018-07-27 19:25:02 -0400

    turn off printing by default (very slow), add goldstein-price example
    Former-commit-id: 75d5452724a5264b4657cd5458edda57512c0712

--- examples/namedpoly/namedpoly.jl
+++ examples/namedpoly/namedpoly.jl
@@ -16,18 +16,21 @@ using LinearAlgebra
 
 # select the SOS degree and named polynomial to minimize
 d = 10
-polyname = :robinson
+polyname = :goldsteinprice
 
 
 # TODO add more named polys from https://github.com/dpapp-github/alfonso/blob/master/polyOpt.m
 # TODO use nicer way to eval the pts, maybe using MultivariatePolynomials
 polys = Dict{Symbol,NamedTuple}(
     :robinson => (n=2, lbs=[-1.0,-1.0], ubs=[1.0,1.0], deg=6,
-        evalpts=(pts -> begin p1 = pts[:,1]; p2 = pts[:,2]; 1.0 .+ p1.^6 .+ p2.^6 .- (p1.^4).*(p2.^2) .+ p1.^4 .- (p2.^4).*(p1.^2) .+ p2.^4 .- p1.^2 .+ p2.^2 .+ 3*(p1.^2).*(p2.^2) end)),
+        feval=((x,y) -> 1+x^6+y^6-x^4*y^2+x^4-x^2*y^4+y^4-x^2+y^2+3x^2*y^2)
+        ),
+    :goldsteinprice => (n=2, lbs=[-2.0,-2.0], ubs=[2.0,2.0], deg=8,
+        feval=((x,y) -> (1+(x+y+1)^2*(19-14*x+3*x^2-14*y+6*x*y+3*y^2))*(30+(2*x-3*y)^2*(18-32*x+12*x^2+48*y-36*x*y+27*y^2)))
+        ),
 )
 
-(n, lbs, ubs, polyDeg, evalpts) = polys[polyname]
-
+(n, lbs, ubs, polyDeg, feval) = polys[polyname]
 if d < ceil(Int, polyDeg/2)
     error("requires d >= $(ceil(Int, polyDeg/2))")
 end
@@ -49,7 +52,7 @@ PWts = [Diagonal(sqrt.(wtVals[:,j]))*P0[:,1:LWts[j]] for j in 1:n]
 
 A = ones(1, U)
 b = [1.0,]
-c = evalpts(pts)
+c = [feval(pts[j,1], pts[j,2]) for j in 1:U]
 
 cones = ConeData[SumOfSqrData(U, P0, PWts),]
 coneidxs = AbstractUnitRange[1:U]
