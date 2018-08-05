c33e3b22787872d917d838a5ccd062bfa7e203b9
Chris Coey <coey.chris@gmail.com>
2018-08-05 02:49:12 -0400

    add new test files for examples
    Former-commit-id: ea4730f31d613d9165c2faff6f4f825a192fd416

---
+++ test/moiexamples.jl
@@ -0,0 +1,42 @@
+
+@testset "envelope example" begin
+    opt = build_envelope(2, 5, 1, 5, use_data=true, native=false)
+    MOI.optimize!(opt)
+    @test MOI.get(opt, MOI.TerminationStatus()) == MOI.Success
+    @test MOI.get(opt, MOI.ObjectiveValue()) ≈ -25.502777 rtol=1e-4
+    @test MOI.get(opt, MOI.ObjectiveBound()) ≈ -25.502777 rtol=1e-4
+end
+
+@testset "lp example" begin
+    opt = build_lp(500, 1000, use_data=true, native=false)
+    MOI.optimize!(opt)
+    @test MOI.get(opt, MOI.TerminationStatus()) == MOI.Success
+    @test MOI.get(opt, MOI.ObjectiveValue()) ≈ 2055.807 rtol=1e-4
+    @test MOI.get(opt, MOI.ObjectiveBound()) ≈ 2055.807 rtol=1e-4
+end
+
+@testset "namedpoly examples" begin
+    @testset "Goldstein Price" begin
+        opt = build_namedpoly(:goldsteinprice, 7, native=false)
+        MOI.optimize!(opt)
+        @test MOI.get(opt, MOI.TerminationStatus()) == MOI.Success
+        @test MOI.get(opt, MOI.ObjectiveValue()) ≈ 3 rtol=1e-4
+        @test MOI.get(opt, MOI.ObjectiveBound()) ≈ 3 rtol=1e-4
+    end
+
+    @testset "Robinson" begin
+        opt = build_namedpoly(:robinson, 8, native=false)
+        MOI.optimize!(opt)
+        @test MOI.get(opt, MOI.TerminationStatus()) == MOI.Success
+        @test MOI.get(opt, MOI.ObjectiveValue()) ≈ 0.814814 rtol=1e-4
+        @test MOI.get(opt, MOI.ObjectiveBound()) ≈ 0.814814 rtol=1e-4
+    end
+
+    @testset "Lotka Volterra" begin
+        opt = build_namedpoly(:lotkavolterra, 3, native=false)
+        MOI.optimize!(opt)
+        @test MOI.get(opt, MOI.TerminationStatus()) == MOI.Success
+        @test MOI.get(opt, MOI.ObjectiveValue()) ≈ -20.8 rtol=1e-4
+        @test MOI.get(opt, MOI.ObjectiveBound()) ≈ -20.8 rtol=1e-4
+    end
+end
