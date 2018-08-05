85e722d0429d64b6864026cd70381bba715fe3d5
Chris Coey <coey.chris@gmail.com>
2018-08-04 20:28:38 -0400

    implement most of MOI.copy!
    Former-commit-id: 54c55abff6fc0ca853d312bd6c4ba7c0f35b7755

--- examples/lp/lp.jl
+++ examples/lp/lp.jl
@@ -2,19 +2,19 @@
 Copyright 2018, David Papp, Sercan Yildiz, and contributors
 
 modified from https://github.com/dpapp-github/alfonso/blob/master/random_lp.m
-solves a simple LP min c'x s.t. Ax >= b
+solves a simple LP min c'x s.t. Ax = b, x >= 0
 =#
 
 using Alfonso
 import MathOptInterface
 const MOI = MathOptInterface
+import JuMP
 using SparseArrays
 using LinearAlgebra
 using DelimitedFiles
 using Random
 
 function solve_lp(m, n; use_data=false, dense=false, nzfrac=1/sqrt(n), rseed=1)
-    # set up MOI problem data
     if use_data
         # use provided data in data folder
         datapath = joinpath(@__DIR__, "data")
