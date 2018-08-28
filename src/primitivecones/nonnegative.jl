b77c0443387ad4469841475ceac63324878eb0df
Chris Coey <coey.chris@gmail.com>
2018-08-28 18:55:05 -0400

    start building exp gradient and inverse hessian
    Former-commit-id: ebad56d46226a49810c61da8d1df79d27f6170de

--- src/primitivecones/nonnegative.jl
+++ src/primitivecones/nonnegative.jl
@@ -13,8 +13,8 @@ end
 
 dimension(prm::NonnegativeCone) = prm.dim
 barrierpar_prm(prm::NonnegativeCone) = prm.dim
-getintdir_prm!(arr::AbstractVector{Float64}, prm::NonnegativeCone) = (arr .= 1.0)
+getintdir_prm!(arr::AbstractVector{Float64}, prm::NonnegativeCone) = (arr .= 1.0; arr)
 loadpnt_prm!(prm::NonnegativeCone, pnt::AbstractVector{Float64}) = (prm.pnt = pnt)
 incone_prm(prm::NonnegativeCone) = all(x -> (x > 0.0), prm.pnt)
-calcg_prm!(g::AbstractVector{Float64}, prm::NonnegativeCone) = (g .= inv.(prm.pnt) .* -1.0)
-calcHiarr_prm!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prm::NonnegativeCone) = (prod .= abs2.(prm.pnt) .* arr)
+calcg_prm!(g::AbstractVector{Float64}, prm::NonnegativeCone) = (g .= inv.(prm.pnt) .* -1.0; g)
+calcHiarr_prm!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prm::NonnegativeCone) = (prod .= abs2.(prm.pnt) .* arr; prod)
