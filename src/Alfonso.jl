d06938eeb65ffd971f048f5fee94e275b7354906
Chris Coey <coey.chris@gmail.com>
2018-08-28 16:34:41 -0400

    separate primitive cones into separate files

--- src/Alfonso.jl
+++ src/Alfonso.jl
@@ -5,8 +5,10 @@ module Alfonso
     using LinearAlgebra
 
     include("interpolation.jl")
-    include("primitivecones.jl")
     include("cone.jl")
+    for primcone in ["nonnegative", "secondorder", "sumofsquares"]
+        include(joinpath(@__DIR__, "primitivecones", primcone * ".jl"))
+    end
     include("nativeinterface.jl")
     include("mathoptinterface.jl")
 end
---
+++ src/primitivecones/nonnegative.jl
@@ -0,0 +1,20 @@
+
+# nonnegative orthant cone
+mutable struct NonnegativeCone <: PrimitiveCone
+    dim::Int
+    pnt::AbstractVector{Float64}
+
+    function NonnegativeCone(dim::Int)
+        prm = new()
+        prm.dim = dim
+        return prm
+    end
+end
+
+dimension(prm::NonnegativeCone) = prm.dim
+barrierpar_prm(prm::NonnegativeCone) = prm.dim
+getintdir_prm!(arr::AbstractVector{Float64}, prm::NonnegativeCone) = (arr .= 1.0)
+loadpnt_prm!(prm::NonnegativeCone, pnt::AbstractVector{Float64}) = (prm.pnt = pnt)
+incone_prm(prm::NonnegativeCone) = all(x -> (x > 0.0), prm.pnt)
+calcg_prm!(g::AbstractVector{Float64}, prm::NonnegativeCone) = (g .= inv.(prm.pnt) .* -1.0)
+calcHiarr_prm!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prm::NonnegativeCone) = (prod .= abs2.(prm.pnt) .* arr)
