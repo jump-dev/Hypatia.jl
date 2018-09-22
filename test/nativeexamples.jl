80225911f6937d5790844e07a984a412d4a9592e
Chris Coey <coey.chris@gmail.com>
2018-09-22 15:39:44 -0400

    refactor direction finding
    Former-commit-id: 430ead219f23cbf277f97e853d912768c090055f

--- test/nativeexamples.jl
+++ test/nativeexamples.jl
@@ -51,39 +51,39 @@ end
     @test Alfonso.get_dobj(s_alf) ≈ -25.502777 atol=1e-4 rtol=1e-4
 end
 
-@testset "2D poly envelope example (dense vs sparse A)" begin
-    # dense methods
-    d_alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-    build_envelope!(d_alf, 2, 4, 2, 7, dense=true)
-    @time Alfonso.solve!(d_alf)
-    @test Alfonso.get_status(d_alf) == :Optimal
-
-    # sparse methods
-    s_alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-    build_envelope!(s_alf, 2, 4, 2, 7, dense=false)
-    @time Alfonso.solve!(s_alf)
-    @test Alfonso.get_status(s_alf) == :Optimal
-
-    @test Alfonso.get_pobj(d_alf) ≈ Alfonso.get_pobj(s_alf) atol=1e-4 rtol=1e-4
-    @test Alfonso.get_dobj(d_alf) ≈ Alfonso.get_dobj(s_alf) atol=1e-4 rtol=1e-4
-end
-
-@testset "3D poly envelope example (sparse A)" begin
-    alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-    build_envelope!(alf, 2, 3, 3, 5, dense=false)
-    @time Alfonso.solve!(alf)
-    @test Alfonso.get_status(alf) == :Optimal
-    @test Alfonso.get_pobj(alf) ≈ Alfonso.get_dobj(alf) atol=1e-4 rtol=1e-4
-end
-
-# @testset "4D poly envelope example (sparse A)" begin
+# @testset "2D poly envelope example (dense vs sparse A)" begin
+#     # dense methods
+#     d_alf = Alfonso.AlfonsoOpt(verbose=verbflag)
+#     build_envelope!(d_alf, 2, 4, 2, 7, dense=true)
+#     @time Alfonso.solve!(d_alf)
+#     @test Alfonso.get_status(d_alf) == :Optimal
+#
+#     # sparse methods
+#     s_alf = Alfonso.AlfonsoOpt(verbose=verbflag)
+#     build_envelope!(s_alf, 2, 4, 2, 7, dense=false)
+#     @time Alfonso.solve!(s_alf)
+#     @test Alfonso.get_status(s_alf) == :Optimal
+#
+#     @test Alfonso.get_pobj(d_alf) ≈ Alfonso.get_pobj(s_alf) atol=1e-4 rtol=1e-4
+#     @test Alfonso.get_dobj(d_alf) ≈ Alfonso.get_dobj(s_alf) atol=1e-4 rtol=1e-4
+# end
+#
+# @testset "3D poly envelope example (sparse A)" begin
 #     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-#     build_envelope!(alf, 2, 3, 4, 4, dense=false)
+#     build_envelope!(alf, 2, 3, 3, 5, dense=false)
 #     @time Alfonso.solve!(alf)
 #     @test Alfonso.get_status(alf) == :Optimal
 #     @test Alfonso.get_pobj(alf) ≈ Alfonso.get_dobj(alf) atol=1e-4 rtol=1e-4
 # end
-
+#
+# # @testset "4D poly envelope example (sparse A)" begin
+# #     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
+# #     build_envelope!(alf, 2, 3, 4, 4, dense=false)
+# #     @time Alfonso.solve!(alf)
+# #     @test Alfonso.get_status(alf) == :Optimal
+# #     @test Alfonso.get_pobj(alf) ≈ Alfonso.get_dobj(alf) atol=1e-4 rtol=1e-4
+# # end
+#
 # most values taken from https://people.sc.fsu.edu/~jburkardt/py_src/polynomials/polynomials.html
 @testset "Butcher" begin
     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
@@ -103,70 +103,70 @@ end
     @test Alfonso.get_dobj(alf) ≈ -3.1800966258 atol=1e-4 rtol=1e-4
 end
 
-# @testset "Goldstein-Price" begin
+# # @testset "Goldstein-Price" begin
+# #     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
+# #     build_namedpoly!(alf, :goldsteinprice, 7)
+# #     @time Alfonso.solve!(alf)
+# #     @test Alfonso.get_status(alf) == :Optimal
+# #     @test Alfonso.get_pobj(alf) ≈ 3 atol=1e-4 rtol=1e-4
+# #     @test Alfonso.get_dobj(alf) ≈ 3 atol=1e-4 rtol=1e-4
+# # end
+#
+# # out of memory during interpolation calculations
+# # @testset "Heart" begin
+# #     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
+# #     build_namedpoly!(alf, :heart, 2)
+# #     @time Alfonso.solve!(alf)
+# #     @test Alfonso.get_status(alf) == :Optimal
+# #     @test Alfonso.get_pobj(alf) ≈ -1.36775 atol=1e-4 rtol=1e-4
+# #     @test Alfonso.get_dobj(alf) ≈ -1.36775 atol=1e-4 rtol=1e-4
+# # end
+#
+# @testset "Lotka-Volterra" begin
 #     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-#     build_namedpoly!(alf, :goldsteinprice, 7)
+#     build_namedpoly!(alf, :lotkavolterra, 3)
 #     @time Alfonso.solve!(alf)
 #     @test Alfonso.get_status(alf) == :Optimal
-#     @test Alfonso.get_pobj(alf) ≈ 3 atol=1e-4 rtol=1e-4
-#     @test Alfonso.get_dobj(alf) ≈ 3 atol=1e-4 rtol=1e-4
+#     @test Alfonso.get_pobj(alf) ≈ -20.8 atol=1e-4 rtol=1e-4
+#     @test Alfonso.get_dobj(alf) ≈ -20.8 atol=1e-4 rtol=1e-4
 # end
-
-# out of memory during interpolation calculations
-# @testset "Heart" begin
-#     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-#     build_namedpoly!(alf, :heart, 2)
+#
+# # out of memory during interpolation calculations
+# # @testset "Magnetism-7" begin
+# #     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
+# #     build_namedpoly!(alf, :magnetism7, 2)
+# #     @time Alfonso.solve!(alf)
+# #     @test Alfonso.get_status(alf) == :Optimal
+# #     @test Alfonso.get_pobj(alf) ≈ -0.25 atol=1e-4 rtol=1e-4
+# #     @test Alfonso.get_dobj(alf) ≈ -0.25 atol=1e-4 rtol=1e-4
+# # end
+#
+# @testset "Motzkin" begin
+#     alf = Alfonso.AlfonsoOpt(verbose=verbflag, tolrelopt=1e-5, tolabsopt=1e-6, tolfeas=1e-6)
+#     build_namedpoly!(alf, :motzkin, 7)
 #     @time Alfonso.solve!(alf)
 #     @test Alfonso.get_status(alf) == :Optimal
-#     @test Alfonso.get_pobj(alf) ≈ -1.36775 atol=1e-4 rtol=1e-4
-#     @test Alfonso.get_dobj(alf) ≈ -1.36775 atol=1e-4 rtol=1e-4
+#     @test Alfonso.get_pobj(alf) ≈ 0 atol=1e-4 rtol=1e-4
+#     @test Alfonso.get_dobj(alf) ≈ 0 atol=1e-4 rtol=1e-4
 # end
-
-@testset "Lotka-Volterra" begin
-    alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-    build_namedpoly!(alf, :lotkavolterra, 3)
-    @time Alfonso.solve!(alf)
-    @test Alfonso.get_status(alf) == :Optimal
-    @test Alfonso.get_pobj(alf) ≈ -20.8 atol=1e-4 rtol=1e-4
-    @test Alfonso.get_dobj(alf) ≈ -20.8 atol=1e-4 rtol=1e-4
-end
-
-# out of memory during interpolation calculations
-# @testset "Magnetism-7" begin
+#
+# @testset "Reaction-diffusion" begin
 #     alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-#     build_namedpoly!(alf, :magnetism7, 2)
+#     build_namedpoly!(alf, :reactiondiffusion, 4)
 #     @time Alfonso.solve!(alf)
 #     @test Alfonso.get_status(alf) == :Optimal
-#     @test Alfonso.get_pobj(alf) ≈ -0.25 atol=1e-4 rtol=1e-4
-#     @test Alfonso.get_dobj(alf) ≈ -0.25 atol=1e-4 rtol=1e-4
+#     @test Alfonso.get_pobj(alf) ≈ -36.71269068 atol=1e-4 rtol=1e-4
+#     @test Alfonso.get_dobj(alf) ≈ -36.71269068 atol=1e-4 rtol=1e-4
 # end
-
-@testset "Motzkin" begin
-    alf = Alfonso.AlfonsoOpt(verbose=verbflag, tolrelopt=1e-5, tolabsopt=1e-6, tolfeas=1e-6)
-    build_namedpoly!(alf, :motzkin, 7)
-    @time Alfonso.solve!(alf)
-    @test Alfonso.get_status(alf) == :Optimal
-    @test Alfonso.get_pobj(alf) ≈ 0 atol=1e-4 rtol=1e-4
-    @test Alfonso.get_dobj(alf) ≈ 0 atol=1e-4 rtol=1e-4
-end
-
-@testset "Reaction-diffusion" begin
-    alf = Alfonso.AlfonsoOpt(verbose=verbflag)
-    build_namedpoly!(alf, :reactiondiffusion, 4)
-    @time Alfonso.solve!(alf)
-    @test Alfonso.get_status(alf) == :Optimal
-    @test Alfonso.get_pobj(alf) ≈ -36.71269068 atol=1e-4 rtol=1e-4
-    @test Alfonso.get_dobj(alf) ≈ -36.71269068 atol=1e-4 rtol=1e-4
-end
-
-@testset "Robinson" begin
-    alf = Alfonso.AlfonsoOpt(verbose=verbflag, tolrelopt=1e-5, tolabsopt=1e-6, tolfeas=1e-6)
-    build_namedpoly!(alf, :robinson, 8)
-    @time Alfonso.solve!(alf)
-    @test Alfonso.get_status(alf) == :Optimal
-    @test Alfonso.get_pobj(alf) ≈ 0.814814 atol=1e-4 rtol=1e-4
-    @test Alfonso.get_dobj(alf) ≈ 0.814814 atol=1e-4 rtol=1e-4
-end
+#
+# @testset "Robinson" begin
+#     alf = Alfonso.AlfonsoOpt(verbose=verbflag, tolrelopt=1e-5, tolabsopt=1e-6, tolfeas=1e-6)
+#     build_namedpoly!(alf, :robinson, 8)
+#     @time Alfonso.solve!(alf)
+#     @test Alfonso.get_status(alf) == :Optimal
+#     @test Alfonso.get_pobj(alf) ≈ 0.814814 atol=1e-4 rtol=1e-4
+#     @test Alfonso.get_dobj(alf) ≈ 0.814814 atol=1e-4 rtol=1e-4
+# end
 
 # # tolerances not satisfied
 # @testset "Rosenbrock" begin
