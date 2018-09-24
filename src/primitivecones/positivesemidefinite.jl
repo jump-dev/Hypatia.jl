6f447bd38c285a7b234cd799618474bedb80635a
Chris Coey <coey.chris@gmail.com>
2018-09-24 15:21:19 -0400

    psd cone hessian and examples working
    Former-commit-id: 00312828d81b09e16c2d23247892f475574e293c

--- src/primitivecones/positivesemidefinite.jl
+++ src/primitivecones/positivesemidefinite.jl
@@ -4,19 +4,19 @@ mutable struct PositiveSemidefiniteCone <: PrimitiveCone
     dim::Int
     side::Int
     pnt::AbstractVector{Float64}
-    g::Vector{Float64}
     mat::Matrix{Float64}
     mat2::Matrix{Float64}
     matpnt::Matrix{Float64}
+    matinv::Matrix{Float64}
 
     function PositiveSemidefiniteCone(dim::Int)
         prm = new()
         prm.dim = dim
         prm.side = round(Int, sqrt(0.25 + dim + dim) - 0.5)
-        prm.g = Vector{Float64}(undef, dim)
         prm.mat = Matrix{Float64}(undef, prm.side, prm.side)
         prm.mat2 = copy(prm.mat)
         prm.matpnt = copy(prm.mat)
+        prm.matinv = copy(prm.mat)
         return prm
     end
 end
@@ -33,13 +33,12 @@ function incone_prm(prm::PositiveSemidefiniteCone)
         return false
     end
 
-    grad = -inv(F) # TODO reduce allocs
-    mattovec!(prm.g, grad)
+    prm.matinv = -inv(F) # TODO reduce allocs
     vectomat!(prm.matpnt, prm.pnt)
     return true
 end
 
-calcg_prm!(g::AbstractVector{Float64}, prm::PositiveSemidefiniteCone) = (g .= prm.g; g)
+calcg_prm!(g::AbstractVector{Float64}, prm::PositiveSemidefiniteCone) = (mattovec!(g, prm.matinv); g)
 
 function calcHiarr_prm!(prod::AbstractVector{Float64}, arr::AbstractVector{Float64}, prm::PositiveSemidefiniteCone)
     vectomat!(prm.mat, arr)
@@ -54,6 +53,24 @@ function calcHiarr_prm!(prod::AbstractMatrix{Float64}, arr::AbstractMatrix{Float
         vectomat!(prm.mat, view(arr, :, j))
         mul!(prm.mat2, prm.mat, prm.matpnt)
         mul!(prm.mat, prm.matpnt, prm.mat2)
+        mattovec!(view(prod, :, j), prm.mat)
+    end
+    return prod
+end
+
+function calcHarr_prm!(prod::AbstractVector{Float64}, arr::AbstractVector{Float64}, prm::PositiveSemidefiniteCone)
+    vectomat!(prm.mat, arr)
+    mul!(prm.mat2, prm.mat, prm.matinv)
+    mul!(prm.mat, prm.matinv, prm.mat2)
+    mattovec!(prod, prm.mat)
+    return prod
+end
+
+function calcHarr_prm!(prod::AbstractMatrix{Float64}, arr::AbstractMatrix{Float64}, prm::PositiveSemidefiniteCone)
+    for j in 1:size(arr, 2)
+        vectomat!(prm.mat, view(arr, :, j))
+        mul!(prm.mat2, prm.mat, prm.matinv)
+        mul!(prm.mat, prm.matinv, prm.mat2)
         mattovec!(view(prod, :, j), prm.mat)
     end
     return prod
