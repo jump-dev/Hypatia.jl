970cdcffa9b6643ec999946b803c257a71de86cf
Chris Coey <coey.chris@gmail.com>
2018-09-26 20:31:52 -0400

    reduced allocs during iteration to essentially zero
    Former-commit-id: b9784120609d2b6780f2bac301f1ed79d34ecaac

--- src/primitivecones/nonnegative.jl
+++ src/primitivecones/nonnegative.jl
@@ -17,15 +17,15 @@ end
 
 dimension(prm::NonnegativeCone) = prm.dim
 barrierpar_prm(prm::NonnegativeCone) = prm.dim
-getintdir_prm!(arr::AbstractVector{Float64}, prm::NonnegativeCone) = (arr .= 1.0; arr)
+getintdir_prm!(arr::AbstractVector{Float64}, prm::NonnegativeCone) = (@. arr = 1.0; arr)
 loadpnt_prm!(prm::NonnegativeCone, pnt::AbstractVector{Float64}) = (prm.pnt = pnt)
 incone_prm(prm::NonnegativeCone) = all(x -> (x > 0.0), prm.pnt)
 
 function calcg_prm!(g::AbstractVector{Float64}, prm::NonnegativeCone)
-    prm.invpnt = inv.(prm.pnt)
-    g .= -1.0 * prm.invpnt
+    @. prm.invpnt = inv(prm.pnt)
+    @. g = -prm.invpnt
     return g
 end
 
-calcHiarr_prm!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prm::NonnegativeCone) = (prod .= abs2.(prm.pnt) .* arr; prod)
-calcHarr_prm!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prm::NonnegativeCone) = (prod .= abs2.(prm.invpnt) .* arr; prod)
+calcHiarr_prm!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prm::NonnegativeCone) = (@. prod = abs2(prm.pnt)*arr; prod)
+calcHarr_prm!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prm::NonnegativeCone) = (@. prod = abs2(prm.invpnt)*arr; prod)
