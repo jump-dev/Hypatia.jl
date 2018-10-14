158725691f2283ac11257eac7da36b9942ca764a
Chris Coey <coey.chris@gmail.com>
2018-10-14 01:46:24 -0400

    get native tests and most examples tests passing
    Former-commit-id: 985329384d274d1002bb99e9820a03e570280b8d

--- test/runtests.jl
+++ test/runtests.jl
@@ -44,33 +44,35 @@ function fullsolve(opt::Hypatia.Optimizer, c, A, b, G, h, cone, lscachetype)
 end
 
 
-# native interface tests
-include(joinpath(@__DIR__, "native.jl"))
-verbose = true # test verbosity
-lscachetype = Hypatia.QRSymmCache # linear system cache type
-
-@testset "native interface tests" begin
-    for testfun in (
-        # _consistent1,
-        # _inconsistent1,
-        # _inconsistent2,
-        # _orthant1,
-        # _orthant2,
-        _orthant3,
-        _orthant4,
-        # _ellinf1,
-        # _ellinf2,
-        # _soc1,
-        # _rsoc1,
-        # _rsoc2,
-        # _psd1,
-        # _psd2,
-        # _exp1,
-        # _power1,
-        )
-        testfun(verbose, lscachetype)
-    end
-end
+# # native interface tests
+# include(joinpath(@__DIR__, "native.jl"))
+# verbose = true # test verbosity
+# lscachetype = Hypatia.QRSymmCache # linear system cache type
+#
+# @testset "native interface tests" begin
+#     for testfun in (
+#         _consistent1,
+#         _inconsistent1,
+#         _inconsistent2,
+#         _orthant1,
+#         _orthant2,
+#         _orthant3,
+#         _orthant4,
+#         _ellinf1,
+#         _ellinf2,
+#         _ellinfdual1,
+#         _ellinfdual2,
+#         _soc1,
+#         _rsoc1,
+#         _rsoc2,
+#         _psd1,
+#         _psd2,
+#         _exp1,
+#         _power1,
+#         )
+#         testfun(verbose, lscachetype)
+#     end
+# end
 
 # # examples in src/examples/ folder
 # egs_dir = joinpath(@__DIR__, "../examples")
@@ -83,35 +85,35 @@ end
 #     run_lp()
 #     run_namedpoly()
 # end
-#
-# include(joinpath(@__DIR__, "examples.jl"))
-# verbose = false # test verbosity
-# lscachetype = Hypatia.QRSymmCache # linear system cache type
-#
-# @testset "varied examples" begin
-#     for testfun in (
-#         _envelope1,
-#         _envelope2,
-#         _envelope3,
-#         # _envelope4,
-#         _lp1,
-#         _lp2,
-#         _lp3,
-#         _namedpoly1,
-#         _namedpoly2,
-#         _namedpoly3,
-#         # _namedpoly4,
-#         _namedpoly5,
-#         # _namedpoly6,
-#         _namedpoly7,
-#         _namedpoly8,
-#         _namedpoly9,
-#         _namedpoly10,
-#         _namedpoly11,
-#         )
-#         testfun(verbose, lscachetype)
-#     end
-# end
+
+include(joinpath(@__DIR__, "examples.jl"))
+verbose = true # test verbosity
+lscachetype = Hypatia.QRSymmCache # linear system cache type
+
+@testset "varied examples" begin
+    for testfun in (
+        _envelope1,
+        _envelope2,
+        # _envelope3,
+        # # _envelope4,
+        # _lp1,
+        # _lp2,
+        # _lp3,
+        # _namedpoly1,
+        # _namedpoly2,
+        # _namedpoly3,
+        # # _namedpoly4,
+        _namedpoly5,
+        # # _namedpoly6,
+        # _namedpoly7,
+        # _namedpoly8,
+        # _namedpoly9,
+        _namedpoly10,
+        _namedpoly11,
+        )
+        testfun(verbose, lscachetype)
+    end
+end
 #
 #
 # # MathOptInterface tests
