d796f95cbcf64f7219b355ec0df047d6f3f6e54f
Chris Coey <coey.chris@gmail.com>
2018-11-12 18:46:55 -0500

    for grad/hess calculations, scale point by mu first, to improve numerics for factorizations
    Former-commit-id: c5deccc1bee1ef6fb2e6d065430111643a50921a

--- src/primitivecones/epiperpower.jl
+++ src/primitivecones/epiperpower.jl
@@ -22,6 +22,8 @@ mutable struct EpiPerPower <: PrimitiveCone
     F
     barfun::Function
     diffres
+    scal::Float64
+    iscal::Float64
 
     function EpiPerPower(alpha::Float64, isdual::Bool)
         @assert alpha > 1.0
@@ -54,22 +56,29 @@ getintdir_prmtv!(arr::AbstractVector{Float64}, prmtv::EpiPerPower) = (arr[1] = 1
 loadpnt_prmtv!(prmtv::EpiPerPower, pnt::AbstractVector{Float64}) = (prmtv.pnt = pnt)
 
 function incone_prmtv(prmtv::EpiPerPower)
-    u = prmtv.pnt[1]
-    v = prmtv.pnt[2]
-    w = prmtv.pnt[3]
+    newpnt = copy(prmtv.pnt)
+    prmtv.scal = norm(newpnt)
+    prmtv.iscal = inv(prmtv.scal)
+    lmul!(prmtv.iscal, newpnt)
+    @show newpnt
+    @show prmtv.scal
+
+    u = newpnt[1]
+    v = newpnt[2]
+    w = newpnt[3]
     alpha = prmtv.alpha
-    if u <= 0.0 || v <= 0.0 || u < v*(abs(w/v))^alpha
+    if u <= 0.0 || v <= 0.0 || u <= v*(abs(w/v))^alpha
         return false
     end
 
     # TODO check allocations, check with Jarrett if this is most efficient way to use DiffResults
-    prmtv.diffres = ForwardDiff.hessian!(prmtv.diffres, prmtv.barfun, prmtv.pnt)
+    prmtv.diffres = ForwardDiff.hessian!(prmtv.diffres, prmtv.barfun, newpnt)
     prmtv.g .= DiffResults.gradient(prmtv.diffres)
     prmtv.H .= DiffResults.hessian(prmtv.diffres)
 
     return factH(prmtv)
 end
 
-calcg_prmtv!(g::AbstractVector{Float64}, prmtv::EpiPerPower) = (@. g = prmtv.g; g)
-calcHiarr_prmtv!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prmtv::EpiPerPower) = ldiv!(prod, prmtv.F, arr)
-calcHarr_prmtv!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prmtv::EpiPerPower) = mul!(prod, prmtv.H, arr)
+# calcg_prmtv!(g::AbstractVector{Float64}, prmtv::EpiPerPower) = (@. g = prmtv.g; lmul!(prmtv.iscal, g); g)
+# calcHiarr_prmtv!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prmtv::EpiPerPower) = (ldiv!(prod, prmtv.F, arr); lmul!(prmtv.scal, prod); lmul!(prmtv.scal, prod); prod)
+# calcHarr_prmtv!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, prmtv::EpiPerPower) = (mul!(prod, prmtv.H, arr); lmul!(prmtv.iscal, prod); lmul!(prmtv.iscal, prod); prod)
