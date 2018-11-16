a477290c154e75405cfe833745281848631f986a
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-11-16 11:08:41 -0500

    rely on hypatia's function for multivariate chebyshev polynomials after sampling

--- examples/namedpoly/jump.jl
+++ examples/namedpoly/jump.jl
@@ -117,7 +117,7 @@ end
 # end
 
 
-get_domain(dom::Domain, x) = error("")
+get_bss(dom::Domain, x) = error("")
 
 function get_bss(dom::Box, x)
     bss = BasicSemialgebraicSet{Float64,Polynomial{true,Float64}}()
@@ -140,26 +140,60 @@ function get_bss(dom::Ball, x)
 end
 
 # assumes we built the box ourselves for now, with known order of equations
-# TODO try only one equation, product_i{(u_i-x_i)(x_i-l_i)}
-function get_weight_equations(dom::Box, bss::BasicSemialgebraicSet{Float64,Polynomial{true,Float64}})
+function get_weights(::Box, bss::BasicSemialgebraicSet{Float64,Polynomial{true,Float64}}, pts)
     m = length(bss.p)
+    U = size(pts, 2)
     @assert mod(m, 2) == 0
-    g = Vector{Polynomial{true,Float64}}(undef, div(m, 2))
-    for i in eachindex(g)
-        g[i] = bss.p[2i-1] * bss.p[2i]
+    gpolys = Vector{Polynomial{true,Float64}}(undef, div(m, 2))
+    for i in eachindex(gpolys)
+        gpolys[i] = bss.p[2i-1] * bss.p[2i]
+    end
+    g = Vector{Vector{Float64}}(undef, length(gpolys))
+    for i in eachindex(gpolys)
+        g[i] = gpolys[i].(pts[i,:])
+        @assert all(g[i] .> -1e-6)
     end
     return g
 end
 
+function get_P(ipts, d::Int, U::Int)
+    (n, npts) = size(ipts)
+    L = binomial(n+d,n)
+    u = Hypatia.calc_u(n, 2d, ipts')
+    m = Vector{Float64}(undef, U)
+    m[1] = 2^n
+    M = Matrix{Float64}(undef, npts, U)
+    M[:,1] .= 1.0
+
+    col = 1
+    for t in 1:2d
+        for xp in Combinatorics.multiexponents(n, t)
+            col += 1
+            if any(isodd, xp)
+                m[col] = 0.0
+            else
+                m[col] = m[1]/prod(1.0 - abs2(xp[j]) for j in 1:n)
+            end
+            @. @views M[:,col] = u[1][:,xp[1]+1]
+            for j in 2:n
+                @. @views M[:,col] *= u[j][:,xp[j]+1]
+            end
+        end
+    end
+    P0 = M[:,1:L]
+    P = Array(qr(P0).Q)
+    return (P0, P)
+end
+
 function build_JuMP_namedpoly_SDP(
     x,
-    f::DynamicPolynomials.Polynomial{true,Float64},
+    f::DynamicPolynomials.Polynomial, #{true,Float64},
     dom::Domain,
+    d::Int = div(length(x), 2),
     )
-    @show typeof(x)
 
     # build domain BasicSemialgebraicSet representation
-    bss = get_domain(dom, x)
+    bss = get_bss(dom, x)
 
     # build JuMP model
     model = SOSModel(with_optimizer(Hypatia.Optimizer, verbose=true))
@@ -172,44 +206,33 @@ end
 
 function build_JuMP_namedpoly_WSOS(
     x,
-    f::DynamicPolynomials.Polynomial{true,Float64},
+    f::DynamicPolynomials.Polynomial, #{true,Float64},
     dom::Domain,
+    d::Int = div(length(x), 2),
     )
 
-    # generate interpolation
     n = nvariables(f) # number of polyvars
-    d =  div(length(x), 2) # let degree of interpolating polynomials match degree of given named polynomial
-    L = binomial(n+d, n)
     U = binomial(n+2d, n)
-    pts = sample(dom, U)
 
-    u = Hypatia.calc_u(n, d, pts')
-    P0 = Matrix{Float64}(undef, U, L)
-    P0[:,1] .= 1
-    col = 1
-    for t in 1:d
-        for xp in Combinatorics.multiexponents(2, t)
-            col += 1
-            @show size(P0), size(u[1]), size(u[2]), size(xp)
-            P0[:,col] .= u[1][:,xp[1]+1] .* u[2][:,xp[2]+1]
-        end
+    # toggle between sampling and current Hypatia method
+    sample_pts = true
+
+    if sample_pts
+        pts = sample(dom, U)
+        (P0, P) = get_P(pts, d, U)
+    else
+        L = binomial(n+d, n)
+        (L, U, pts, P0, P, w) = Hypatia.interpolate(n, d, calc_w=false)
+        pts = pts'
+        pts .*= 2.0
     end
-    P = Array(qr(P0).Q)
 
     P0sub = view(P0, :, 1:binomial(n+d-1, n))
 
     bss = get_bss(dom, x)
 
-    # ignores mismatch between dimension of x and dimension of domain, need to check and deal with this correctly
-    gpolys = get_weight_equations(dom, bss)
-    g = Vector{Vector{Float64}}(undef, length(gpolys))
-    for i in eachindex(gpolys)
-        g[i] = gpolys[i].(pts[i,:])
-        @show gpolys[i]
-        @assert all(g[i] .> 0)
-    end
+    g = get_weights(dom, bss, pts)
     PWts = [sqrt.(g[i]) .* P0sub for i in 1:n]
-    # PWts = [P0sub for i in 1:n]
 
     wsos_cone = WSOSPolyInterpCone(U, [P, PWts...])
 
@@ -233,9 +256,9 @@ function run_JuMP_namedpoly()
     polyname =
         # :butcher
         # :caprasse
-        # :goldsteinprice
+        :goldsteinprice
         # :heart
-        :lotkavolterra
+        # :lotkavolterra
         # :magnetism7
         # :motzkin
         # :reactiondiffusion
@@ -244,10 +267,10 @@ function run_JuMP_namedpoly()
         # :schwefel
 
     # get data for named polynomial
-    (x, f, dom, truemin) = getpolydata(polyname)
+    (x, f, dom, truemin, d) = getpolydata(polyname)
 
     println("solving model with PSD cones")
-    model = build_JuMP_namedpoly_SDP(x, f, dom)
+    model = build_JuMP_namedpoly_SDP(x, f, dom, d)
     JuMP.optimize!(model)
 
     println("done")
@@ -264,7 +287,7 @@ function run_JuMP_namedpoly()
     @test pobj ≈ truemin atol=1e-4 rtol=1e-4
 
     println("solving model with WSOS interpolation cones")
-    model = build_JuMP_namedpoly_WSOS(x, f, dom)
+    model = build_JuMP_namedpoly_WSOS(x, f, dom, d)
     JuMP.optimize!(model)
 
     println("done")
@@ -296,61 +319,72 @@ function getpolydata(polyname::Symbol)
         f = x[6]*x[2]^2+x[5]*x[3]^2-x[1]*x[4]^2+x[4]^3+x[4]^2-1/3*x[1]+4/3*x[4]
         dom = Box([-1,-0.1,-0.1,-1,-0.1,-0.1], [0,0.9,0.5,-0.1,-0.05,-0.03])
         truemin = -1.4393333333
+        d = 2
     elseif polyname == :caprasse
         @polyvar x[1:4]
         f = -x[1]*x[3]^3+4x[2]*x[3]^2*x[4]+4x[1]*x[3]*x[4]^2+2x[2]*x[4]^3+4x[1]*x[3]+4x[3]^2-10x[2]*x[4]-10x[4]^2+2
         dom = Box(fill(-0.5, 4), fill(0.5, 4))
         truemin = -3.1800966258
+        d = 4
     elseif polyname == :goldsteinprice
         @polyvar x[1:2]
         f = (1+(x[1]+x[2]+1)^2*(19-14x[1]+3x[1]^2-14x[2]+6x[1]*x[2]+3x[2]^2))*(30+(2x[1]-3x[2])^2*(18-32x[1]+12x[1]^2+48x[2]-36x[1]*x[2]+27x[2]^2))
         dom = Box(fill(-2, 2), fill(2, 2))
         truemin = 3
+        d = 7
     elseif polyname == :heart
         @polyvar x[1:8]
         f = x[1]*x[6]^3-3x[1]*x[6]*x[7]^2+x[3]*x[7]^3-3x[3]*x[7]*x[6]^2+x[2]*x[5]^3-3*x[2]*x[5]*x[8]^2+x[4]*x[8]^3-3x[4]*x[8]*x[5]^2
         dom = Box([-0.1,0.4,-0.7,-0.7,0.1,-0.1,-0.3,-1.1], [0.4,1,-0.4,0.4,0.2,0.2,1.1,-0.3])
         truemin = -1.36775
+        d = 2
     elseif polyname == :lotkavolterra
         @polyvar x[1:4]
         f = x[1]*(x[2]^2+x[3]^2+x[4]^2-1.1)+1
         dom = Box(fill(-2, 4), fill(2, 4))
         truemin = -20.8
+        d = 3
     elseif polyname == :magnetism7
         @polyvar x[1:7]
         f = x[1]^2+2x[2]^2+2x[3]^2+2x[4]^2+2x[5]^2+2x[6]^2+2x[7]^2-x[1]
         dom = Box(fill(-1, 7), fill(1, 7))
         truemin = -0.25
+        d = 2
     elseif polyname == :motzkin
         @polyvar x[1:2]
         f = 1-48x[1]^2*x[2]^2+64x[1]^2*x[2]^4+64x[1]^4*x[2]^2
         dom = Box(fill(-1, 2), fill(1, 2))
         truemin = 0
+        d = 7
     elseif polyname == :reactiondiffusion
         @polyvar x[1:3]
         f = -x[1]+2x[2]-x[3]-0.835634534x[2]*(1+x[2])
         dom = Box(fill(-5, 3), fill(5, 3))
         truemin = -36.71269068
+        d = 3
     elseif polyname == :robinson
         @polyvar x[1:2]
         f = 1+x[1]^6+x[2]^6-x[1]^4*x[2]^2+x[1]^4-x[1]^2*x[2]^4+x[2]^4-x[1]^2+x[2]^2+3x[1]^2*x[2]^2
         dom = Box(fill(-1, 2), fill(1, 2))
         truemin = 0.814814
+        d = 8
     elseif polyname == :rosenbrock
         @polyvar x[1:2]
         f = (1-x[1])^2+100*(x[1]^2-x[2])^2
         dom = Box(fill(-5, 2), fill(10, 2))
         truemin = 0
+        d = 4
     elseif polyname == :schwefel
         @polyvar x[1:3]
         f = (x[1]-x[2]^2)^2+(x[2]-1)^2+(x[1]-x[3]^2)^2+(x[3]-1)^2
         dom = Box(fill(-10, 3), fill(10, 3))
         truemin = 0
+        d = 3
     else
         error("poly $polyname not recognized")
     end
 
-    return (x, f, dom, truemin)
+    return (x, f, dom, truemin, d)
 end
 
 
