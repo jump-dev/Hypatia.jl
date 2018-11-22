7dd759acf4a9b56e48aa0574cd618113913d5010
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-11-22 13:28:11 -0500

    delete old code, take d out of function data, adjust optimal values for some polys
    Former-commit-id: 67faa31ddc4aac16e44bb7008653d87ae9848878

--- examples/namedpoly/jump.jl
+++ examples/namedpoly/jump.jl
@@ -12,7 +12,7 @@ function build_JuMP_namedpoly_SDP(
     x,
     f::DynamicPolynomials.Polynomial, #{true,Float64},
     dom::InterpDomain;
-    d::Int = div(maxdegree(f), 2),
+    d::Int = div(maxdegree(f) + 1, 2),
     )
 
     # build domain BasicSemialgebraicSet representation
@@ -31,7 +31,7 @@ function build_JuMP_namedpoly_WSOS(
     x,
     f::DynamicPolynomials.Polynomial, #{true,Float64},
     dom::InterpDomain;
-    d::Int = div(maxdegree(f), 2),
+    d::Int = div(maxdegree(f) + 1, 2),
     pts_factor = nvariables(f),
     )
 
@@ -90,64 +90,74 @@ function run_JuMP_namedpoly()
     # select the named polynomial to minimize
     polynames = [
         :butcher
+        :butcher_ball
+        :butcher_ellipsoid
         :caprasse
-        # :caprasse_ball # incorrect solution
-        :caprasse_ellipsoid
+        :caprasse_ball
         :goldsteinprice
-        # :goldsteinprice_ball # predictor/corrector failures in SDP
+        :goldsteinprice_ball
+        :goldsteinprice_ellipsoid # it is just a ball
         :heart
-        # :heart_ellipsoid
+        :heart_ellipsoid #TODO tighter ellispoid
         :lotkavolterra
         :lotkavolterra_ball
         :magnetism7
         :magnetism7_ball
         :motzkin
         :motzkin_ball1
-        :motzkin_ball2  # runs into issues
+        # :motzkin_ball2  # runs into issues
         :reactiondiffusion
-        # :reactiondiffusion_ball # incorrect solution
+        :reactiondiffusion_ball
         :robinson
+        :robinson_ball
         :rosenbrock
         :rosenbrock_ball
         :schwefel
         :schwefel_ball
     ]
 
+    # some polynomials run into predictor/corrector failusres with SDP but not WSOS formulation
+    sdp_skip = [:butcher_ball, :goldsteinprice_ball, :goldsteinprice_ellipsoid, :heart, :heart_ellipsoid]
+
     for polyname in polynames
 
         println(polyname)
 
         # get data for named polynomial
-        (x, f, dom, truemin, d) = getpolydata(polyname)
+        (x, f, dom, truemin) = getpolydata(polyname)
 
-        println("solving model with PSD cones")
+        # some polynomials run into predictor/corrector failusres with SDP but not WSOS formulation, skipping these with SDP only
+        if !(polyname in sdp_skip)
+            println("solving model with PSD cones")
 
-        if polyname in [:butcher, :lotkavolterra, :lotkavolterra_ball, :caprasse_ball]
-            model = build_JuMP_namedpoly_SDP(x, f, dom, d=d)
-        else
-            model = build_JuMP_namedpoly_SDP(x, f, dom)
+            if polyname in [:butcher, :butcher_ball, :caprasse_ball, :lotkavolterra, :lotkavolterra_ball]
+                d = default_degrees[polyname]
+                model = build_JuMP_namedpoly_SDP(x, f, dom, d=d)
+            else
+                model = build_JuMP_namedpoly_SDP(x, f, dom)
+            end
+            JuMP.optimize!(model)
+
+            println("done")
+            term_status = JuMP.termination_status(model)
+            pobj = JuMP.objective_value(model)
+            dobj = JuMP.objective_bound(model)
+            pr_status = JuMP.primal_status(model)
+            du_status = JuMP.dual_status(model)
+
+            @test term_status == MOI.Success
+            @test pr_status == MOI.FeasiblePoint
+            @test du_status == MOI.FeasiblePoint
+            @test pobj ≈ dobj atol=1e-4 rtol=1e-4
+            @test pobj ≈ truemin atol=1e-4 rtol=1e-4
         end
 
-        println("done")
-        term_status = JuMP.termination_status(model)
-        pobj = JuMP.objective_value(model)
-        dobj = JuMP.objective_bound(model)
-        pr_status = JuMP.primal_status(model)
-        du_status = JuMP.dual_status(model)
-
-        @test term_status == MOI.Success
-        @test pr_status == MOI.FeasiblePoint
-        @test du_status == MOI.FeasiblePoint
-        @test pobj ≈ dobj atol=1e-4 rtol=1e-4
-        @test pobj ≈ truemin atol=1e-4 rtol=1e-4
-
         println("solving model with WSOS interpolation cones")
+        d = default_degrees[polyname]
         if polyname == :heart
-            model = build_JuMP_namedpoly_WSOS(x, f, dom, d=d, pts_factor=1)
-        elseif polyname in [:schwefel, :schwefel_ball, :caprasse_ellipsoid, :reactiondiffusion, :reactiondiffusion_ball]
-            model = build_JuMP_namedpoly_WSOS(x, f, dom, d=d, pts_factor=2*length(x))
+            model = build_JuMP_namedpoly_WSOS(x, f, dom, d=d, pts_factor=3)
         else
-            model = build_JuMP_namedpoly_WSOS(x, f, dom, d=d, pts_factor=length(x))
+            model = build_JuMP_namedpoly_WSOS(x, f, dom, d=d, pts_factor=4*length(x))
         end
         JuMP.optimize!(model)
 
@@ -170,8 +180,32 @@ end
 
 
 
-
-
+default_degrees = Dict(
+    :butcher => 2,
+    :butcher_ball => 2,
+    :butcher_ellipsoid => 2,
+    :caprasse => 4,
+    :caprasse_ball => 4,
+    :goldsteinprice => 7,
+    :goldsteinprice_ball => 7,
+    :goldsteinprice_ellipsoid => 7,
+    :heart => 2,
+    :heart_ellipsoid => 2,
+    :lotkavolterra => 3,
+    :lotkavolterra_ball => 3,
+    :magnetism7 => 2,
+    :magnetism7_ball => 2,
+    :motzkin => 7,
+    :motzkin_ball1 => 7,
+    :reactiondiffusion => 3,
+    :reactiondiffusion_ball => 3,
+    :robinson => 8,
+    :robinson_ball => 8,
+    :rosenbrock => 4,
+    :rosenbrock_ball => 4,
+    :schwefel => 3,
+    :schwefel_ball => 3,
+)
 
 
 
@@ -181,148 +215,145 @@ function getpolydata(polyname::Symbol)
         f = x[6]*x[2]^2+x[5]*x[3]^2-x[1]*x[4]^2+x[4]^3+x[4]^2-1/3*x[1]+4/3*x[4]
         dom = Box([-1,-0.1,-0.1,-1,-0.1,-0.1], [0,0.9,0.5,-0.1,-0.05,-0.03])
         truemin = -1.4393333333
-        d = 2
+    elseif polyname == :butcher_ball
+        @polyvar x[1:6]
+        f = x[6]*x[2]^2+x[5]*x[3]^2-x[1]*x[4]^2+x[4]^3+x[4]^2-1/3*x[1]+4/3*x[4]
+        radius = maximum(0.5 * (-[-1,-0.1,-0.1,-1,-0.1,-0.1] + [0,0.9,0.5,-0.1,-0.05,-0.03])) * sqrt(6)
+        centers = 0.5 * ([-1,-0.1,-0.1,-1,-0.1,-0.1] + [0,0.9,0.5,-0.1,-0.05,-0.03])
+        dom = Ball(centers, radius)
+        truemin = -4.10380
+    elseif polyname == :butcher_ellipsoid
+        @polyvar x[1:6]
+        f = x[6]*x[2]^2+x[5]*x[3]^2-x[1]*x[4]^2+x[4]^3+x[4]^2-1/3*x[1]+4/3*x[4]
+        # heuristically attained enclosing ellipsoid
+        semiradii_sqrd = 6 * (0.5 * (-[-1,-0.1,-0.1,-1,-0.1,-0.1] + [0,0.9,0.5,-0.1,-0.05,-0.03])).^2
+        centers = 0.5 * ([-1,-0.1,-0.1,-1,-0.1,-0.1] + [0,0.9,0.5,-0.1,-0.05,-0.03])
+        Q = Diagonal(semiradii_sqrd)
+        dom = Ellipsoid(centers, Q)
+        truemin = -16.7378208
     elseif polyname == :caprasse
         @polyvar x[1:4]
         f = -x[1]*x[3]^3+4x[2]*x[3]^2*x[4]+4x[1]*x[3]*x[4]^2+2x[2]*x[4]^3+4x[1]*x[3]+4x[3]^2-10x[2]*x[4]-10x[4]^2+2
         dom = Box(fill(-0.5, 4), fill(0.5, 4))
         truemin = -3.1800966258
-        d = 4
     elseif polyname == :caprasse_ball
         @polyvar x[1:4]
         f = -x[1]*x[3]^3+4x[2]*x[3]^2*x[4]+4x[1]*x[3]*x[4]^2+2x[2]*x[4]^3+4x[1]*x[3]+4x[3]^2-10x[2]*x[4]-10x[4]^2+2
         # 0.5*sqrt(4) = 1
         dom = Ball(fill(0.0, 4), 1.0)
-        truemin = -3.1800966258
-        d = 4
-    elseif polyname == :caprasse_ellipsoid
-        @polyvar x[1:4]
-        f = -x[1]*x[3]^3+4x[2]*x[3]^2*x[4]+4x[1]*x[3]*x[4]^2+2x[2]*x[4]^3+4x[1]*x[3]+4x[3]^2-10x[2]*x[4]-10x[4]^2+2
-        dom = Ellipsoid(fill(0.0, 4), Matrix{Float64}(I, 4, 4))
-        truemin = -3.1800966258
-        d = 4
+        truemin = -9.47843346
     elseif polyname == :goldsteinprice
         @polyvar x[1:2]
         f = (1+(x[1]+x[2]+1)^2*(19-14x[1]+3x[1]^2-14x[2]+6x[1]*x[2]+3x[2]^2))*(30+(2x[1]-3x[2])^2*(18-32x[1]+12x[1]^2+48x[2]-36x[1]*x[2]+27x[2]^2))
         dom = Box(fill(-2, 2), fill(2, 2))
         truemin = 3
-        d = 7
     elseif polyname == :goldsteinprice_ball
         @polyvar x[1:2]
         f = (1+(x[1]+x[2]+1)^2*(19-14x[1]+3x[1]^2-14x[2]+6x[1]*x[2]+3x[2]^2))*(30+(2x[1]-3x[2])^2*(18-32x[1]+12x[1]^2+48x[2]-36x[1]*x[2]+27x[2]^2))
         dom = Ball(fill(0.0, 2), 2.0 * sqrt(2))
         truemin = 3
-        d = 7
+    elseif polyname == :goldsteinprice_ellipsoid
+        @polyvar x[1:2]
+        f = (1+(x[1]+x[2]+1)^2*(19-14x[1]+3x[1]^2-14x[2]+6x[1]*x[2]+3x[2]^2))*(30+(2x[1]-3x[2])^2*(18-32x[1]+12x[1]^2+48x[2]-36x[1]*x[2]+27x[2]^2))
+        centers = fill(0, 2)
+        semiradii = fill(0.25, 2)
+        Q = Diagonal(semiradii)
+        dom = Ellipsoid(centers, Q)
+        truemin = 3
     elseif polyname == :heart
         @polyvar x[1:8]
         f = x[1]*x[6]^3-3x[1]*x[6]*x[7]^2+x[3]*x[7]^3-3x[3]*x[7]*x[6]^2+x[2]*x[5]^3-3*x[2]*x[5]*x[8]^2+x[4]*x[8]^3-3x[4]*x[8]*x[5]^2+0.9563453
         dom = Box([-0.1,0.4,-0.7,-0.7,0.1,-0.1,-0.3,-1.1], [0.4,1,-0.4,0.4,0.2,0.2,1.1,-0.3])
         truemin = -1.36775
-        d = 2
     elseif polyname == :heart_ellipsoid
         @polyvar x[1:8]
         f = x[1]*x[6]^3-3x[1]*x[6]*x[7]^2+x[3]*x[7]^3-3x[3]*x[7]*x[6]^2+x[2]*x[5]^3-3*x[2]*x[5]*x[8]^2+x[4]*x[8]^3-3x[4]*x[8]*x[5]^2+0.9563453
-        semiradii = sqrt(8) * (0.5 * (-[-0.1,0.4,-0.7,-0.7,0.1,-0.1,-0.3,-1.1] + [0.4,1,-0.4,0.4,0.2,0.2,1.1,-0.3])).^2
+        # heuristically attained enclosing ellipsoid
+        semiradii_sqrd = 8.0 * (0.5 * (-[-0.1,0.4,-0.7,-0.7,0.1,-0.1,-0.3,-1.1] + [0.4,1,-0.4,0.4,0.2,0.2,1.1,-0.3])).^2
         centers = 0.5 * ([-0.1,0.4,-0.7,-0.7,0.1,-0.1,-0.3,-1.1] + [0.4,1,-0.4,0.4,0.2,0.2,1.1,-0.3])
-        Q = Diagonal(semiradii)
+        Q = Diagonal(semiradii_sqrd)
         dom = Ellipsoid(centers, Q)
-        truemin = -1.36775
-        d = 2
+        truemin = -306.46395
     elseif polyname == :lotkavolterra
         @polyvar x[1:4]
         f = x[1]*(x[2]^2+x[3]^2+x[4]^2-1.1)+1
         dom = Box(fill(-2, 4), fill(2, 4))
         truemin = -20.8
-        d = 3
     elseif polyname == :lotkavolterra_ball
         @polyvar x[1:4]
         f = x[1]*(x[2]^2+x[3]^2+x[4]^2-1.1)+1
         dom = Ball(fill(0.0, 4), 4.0)
         truemin = -21.13744
-        d = 3
     elseif polyname == :magnetism7
         @polyvar x[1:7]
         f = x[1]^2+2x[2]^2+2x[3]^2+2x[4]^2+2x[5]^2+2x[6]^2+2x[7]^2-x[1]
         dom = Box(fill(-1, 7), fill(1, 7))
         truemin = -0.25
-        d = 2
     elseif polyname == :magnetism7_ball
         @polyvar x[1:7]
         f = x[1]^2+2x[2]^2+2x[3]^2+2x[4]^2+2x[5]^2+2x[6]^2+2x[7]^2-x[1]
         dom = Ball(fill(0.0, 7), sqrt(7))
         truemin = -0.25
-        d = 2
     elseif polyname == :motzkin
         @polyvar x[1:2]
         f = 1-48x[1]^2*x[2]^2+64x[1]^2*x[2]^4+64x[1]^4*x[2]^2
         dom = Box(fill(-1, 2), fill(1, 2))
         truemin = 0
-        d = 7
     elseif polyname == :motzkin_ball1
         @polyvar x[1:2]
         f = 1-48x[1]^2*x[2]^2+64x[1]^2*x[2]^4+64x[1]^4*x[2]^2
         dom = Ball([0.0; 0.0], sqrt(2.0))
         truemin = 0
-        d = 7
     elseif polyname == :motzkin_ball2
         @polyvar x[1:2]
         f = 1-48x[1]^2*x[2]^2+64x[1]^2*x[2]^4+64x[1]^4*x[2]^2
         dom = Ball([0.0; 0.0], 0.5*sqrt(0.5))
         truemin = 0.84375
-        d = 7
     elseif polyname == :reactiondiffusion
         @polyvar x[1:3]
         f = -x[1]+2x[2]-x[3]-0.835634534x[2]*(1+x[2])
         dom = Box(fill(-5, 3), fill(5, 3))
         truemin = -36.71269068
-        d = 3
     elseif polyname == :reactiondiffusion_ball
         @polyvar x[1:3]
         f = -x[1]+2x[2]-x[3]-0.835634534x[2]*(1+x[2])
         dom = Ball(fill(0.0, 3), 5*sqrt(3))
-        truemin = -36.71269068
-        d = 5
+        truemin = -73.31
     elseif polyname == :robinson
         @polyvar x[1:2]
         f = 1+x[1]^6+x[2]^6-x[1]^4*x[2]^2+x[1]^4-x[1]^2*x[2]^4+x[2]^4-x[1]^2+x[2]^2+3x[1]^2*x[2]^2
         dom = Box(fill(-1, 2), fill(1, 2))
         truemin = 0.814814
-        d = 8
     elseif polyname == :robinson_ball
         @polyvar x[1:2]
         f = 1+x[1]^6+x[2]^6-x[1]^4*x[2]^2+x[1]^4-x[1]^2*x[2]^4+x[2]^4-x[1]^2+x[2]^2+3x[1]^2*x[2]^2
-        dom = Box(fill(0.0, 2), sqrt(2.0))
+        dom = Ball(fill(0.0, 2), sqrt(2.0))
         truemin = 0.814814
-        d = 8
     elseif polyname == :rosenbrock
         @polyvar x[1:2]
         f = (1-x[1])^2+100*(x[1]^2-x[2])^2
         dom = Box(fill(-5, 2), fill(10, 2))
         truemin = 0
-        d = 4
     elseif polyname == :rosenbrock_ball
         @polyvar x[1:2]
         f = (1-x[1])^2+100*(x[1]^2-x[2])^2
         dom = Ball(fill(2.5, 2), 7.5*sqrt(2.0))
         truemin = 0
-        d = 4
     elseif polyname == :schwefel
         @polyvar x[1:3]
         f = (x[1]-x[2]^2)^2+(x[2]-1)^2+(x[1]-x[3]^2)^2+(x[3]-1)^2
         dom = Box(fill(-10, 3), fill(10, 3))
         truemin = 0
-        d = 3
     elseif polyname == :schwefel_ball
         @polyvar x[1:3]
         f = (x[1]-x[2]^2)^2+(x[2]-1)^2+(x[1]-x[3]^2)^2+(x[3]-1)^2
         dom = Ball(fill(0.0, 3), 10.0 * sqrt(3))
         truemin = 0
-        d = 3
     else
         error("poly $polyname not recognized")
     end
 
-    return (x, f, dom, truemin, d)
+    return (x, f, dom, truemin)
 end
 
 
