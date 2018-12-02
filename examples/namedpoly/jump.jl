0d050c06375ec5efa7c7a7f788b65c84e6c62c42
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-12-02 18:33:10 -0500

    rename get_bss
    Former-commit-id: b43c9052b4548f246836588a1733d71d73e9f0d5

--- examples/namedpoly/jump.jl
+++ examples/namedpoly/jump.jl
@@ -25,7 +25,7 @@ function build_JuMP_namedpoly_PSD(
     model = SOSModel(with_optimizer(Hypatia.Optimizer, verbose=true))
     @variable(model, a)
     @objective(model, Max, a)
-    @constraint(model, fnn, f >= a, domain=get_bss(dom, x), maxdegree=2d)
+    @constraint(model, fnn, f >= a, domain=get_domain_inequalities(dom, x), maxdegree=2d)
 
     return model
 end
