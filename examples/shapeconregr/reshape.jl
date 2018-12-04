09120d7e8bbf73b462b593f8fc23abfcd188a268
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-12-04 11:31:02 -0500

    make files for plots
    Former-commit-id: dfcd9a9f6e935f7c734bd3badbb6f57a2c16b4bd

---
+++ examples/shapeconregr/reshape.jl
@@ -0,0 +1,12 @@
+using DataFrames
+using CSV
+
+for i in [1, 3, 5]
+    longres = CSV.read(joinpath(@__DIR__(), "synthetic$(i).csv"), comment="#")
+    categorical!(longres, :signal_ratio)
+    longres[:use_wsos] = string.(longres[:use_wsos])
+    longres[:use_wsos][longres[:use_wsos] .== "true"] .= "wsos"
+    longres[:use_wsos][longres[:use_wsos] .== "false"] .= "sdp"
+    longres_tm = longres[[:signal_ratio, :deg, :use_wsos, :tm]]
+    wideres = unstack(longres_tm, :use_wsos, :tm) |> CSV.write("syn$(i).csv")
+end
