ccc45e5ce87469d0ec9371301135b13fcfcbc02f
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-12-09 23:38:18 -0500

    use matrix trace

--- src/primitivecones/wsospolyinterpmat.jl
+++ src/primitivecones/wsospolyinterpmat.jl
@@ -41,35 +41,38 @@ mutable struct WSOSPolyInterpMat <: PrimitiveCone
         prmtv.g = similar(ipwt[1], dim)
         prmtv.H = similar(ipwt[1], dim, dim)
         prmtv.H2 = similar(prmtv.H)
-        prmtv.barfun = (x -> barfun(x, ipwt, r, u))
+        prmtv.barfun = (x -> barfun(x, ipwt, u))
         prmtv.diffres = DiffResults.HessianResult(prmtv.g)
         return prmtv
     end
 end
 
 # naively separate calculating barrier from calculating in cone
-function barfun(matpnt, ipwt::Vector{Matrix{Float64}}, r::Int, u::Int)
+function barfun(matpnt, ipwt::Vector{Matrix{Float64}}, u::Int)
     ret = 0.0
     for ipwtj in ipwt
         l = size(ipwtj, 2)
-        mat = similar(matpnt, l*r, l*r)
+        # mat = similar(matpnt, l*r, l*r)
+        mat = similar(matpnt, l, l)
         mat .= 0.0
         for j in 1:l, i in 1:l
-            mat[(i-1)*r+1:i*r, (j-1)*r+1:j*r] .+= sum(ipwtj[ui,i] * ipwtj[ui,j] * matpnt[:,:,ui] for ui in 1:u)
+            mat[i,j] = sum(sum(ipwtj[ui,i] * ipwtj[ui,j] * matpnt[:,:,ui]) for ui in 1:u)
+            # mat[(i-1)*r+1:i*r, (j-1)*r+1:j*r] .+= sum(ipwtj[ui,i] * ipwtj[ui,j] * matpnt[:,:,ui] for ui in 1:u)
         end
         # @show logdet(mat)
         ret -= logdet(mat)
     end
     return ret
 end
-function inconefun(matpnt, ipwt::Vector{Matrix{Float64}}, r::Int, u::Int)
+function inconefun(matpnt, ipwt::Vector{Matrix{Float64}}, u::Int)
     ret = true
     for ipwtj in ipwt
         l = size(ipwtj, 2)
-        mat = zeros(l*r, l*r)
+        # mat = zeros(l*r, l*r)
+        mat = zeros(l, l)
         for j in 1:l, i in 1:l
-            # @show [ipwtj[ui,i] * ipwtj[ui,j] for ui in 1:u]
-            mat[(i-1)*r+1:i*r, (j-1)*r+1:j*r] .+= sum(ipwtj[ui,i] * ipwtj[ui,j] * matpnt[:,:,ui] for ui in 1:u)
+            mat[i,j] = sum(sum(ipwtj[ui,i] * ipwtj[ui,j] * matpnt[:,:,ui]) for ui in 1:u)
+            # mat[(i-1)*r+1:i*r, (j-1)*r+1:j*r] .+= sum(ipwtj[ui,i] * ipwtj[ui,j] * matpnt[:,:,ui] for ui in 1:u)
         end
         @show mat
         if !isposdef(mat)
@@ -86,8 +89,8 @@ dimension(prmtv::WSOSPolyInterpMat) = prmtv.dim
 barrierpar_prmtv(prmtv::WSOSPolyInterpMat) = prmtv.r * sum(size(ipwtj, 2) for ipwtj in prmtv.ipwt)
 # sum of diagonal matrices with interpolant polynomial repeating on the diagonal
 function getintdir_prmtv!(arr::AbstractVector{Float64}, prmtv::WSOSPolyInterpMat)
-    # arr .= 1.0
-    arr .= vcat([Matrix{Float64}(I, prmtv.r, prmtv.r)[:] for ui in 1:prmtv.u]...) # TODO tidy
+    arr .= 1.0
+    # arr .= vcat([Matrix{Float64}(I, prmtv.r, prmtv.r)[:] for ui in 1:prmtv.u]...) # TODO tidy
     return arr
 end
 loadpnt_prmtv!(prmtv::WSOSPolyInterpMat, pnt::AbstractVector{Float64}) = (prmtv.pnt = pnt)
@@ -96,7 +99,7 @@ function incone_prmtv(prmtv::WSOSPolyInterpMat, scal::Float64)
     prmtv.scal = scal
     @. prmtv.scalpnt = prmtv.pnt/prmtv.scal
     prmtv.matpnt = reshape(prmtv.scalpnt, prmtv.r, prmtv.r, prmtv.u)
-    if !inconefun(prmtv.matpnt, prmtv.ipwt, prmtv.r, prmtv.u)
+    if !inconefun(prmtv.matpnt, prmtv.ipwt, prmtv.u)
         return false
     end
     # prmtv.mat = makemat(prmtv.matpnt, prmtv.ipwt, prmtv.r, prmtv.u)
