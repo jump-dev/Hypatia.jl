edfa0231cf6e26690f26459ac8202a23e8c3c338
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-12-12 09:48:07 -0500

    record refmse
    Former-commit-id: fe2b29c8ef114cd2f1ad44dd2173967609821564

--- examples/shapeconregr/reshape.jl
+++ examples/shapeconregr/reshape.jl
@@ -3,21 +3,21 @@ using CSV
 
 isdir("noise10") || mkdir("noise10")
 
-for i in [1, 3, 5]
+for i in 1:6
     # longres = CSV.read("C:/Users/lkape/.julia/dev/Hypatia/examples/shapeconregr/shapeconregr_154402512.csv", comment="#")
     longres = CSV.read(joinpath(@__DIR__(), "synthetic$(i).csv"), comment="#")
-    longres = longres[[:fold, :signal_ratio, :deg, :use_wsos, :tm, :tr_rmse, :ts_rmse, :ignore_mono]]
+    longres = longres[[:fold, :signal_ratio, :deg, :use_wsos, :tm, :tr_rmse, :ts_rmse, :ignore_mono, :refrmse]]
     categorical!(longres, [:signal_ratio])
 
     longres_tm = longres[longres.ignore_mono .== false, :]
-    longres_tm = longres_tm[[:fold, :signal_ratio, :deg, :use_wsos, :tm]]
+    longres_tm = longres_tm[[:fold, :signal_ratio, :deg, :use_wsos, :tm, :refrmse]]
     for subdf in groupby(longres_tm, :use_wsos)
         wsos = subdf[:use_wsos][1]
         smry_res = aggregate(subdf, [:deg, :signal_ratio], [median, minimum, maximum]) |> CSV.write("noise10/syn_$(i)_wsos_$(wsos).csv")
     end
 
     longres_er = longres[longres.use_wsos .== true, :]
-    longres_er = longres_er[[:fold, :signal_ratio, :deg, :tr_rmse, :ts_rmse, :ignore_mono]]
+    longres_er = longres_er[[:fold, :signal_ratio, :deg, :tr_rmse, :ts_rmse, :ignore_mono, :refrmse]]
     for subdf in groupby(longres_er, :ignore_mono)
         mono = subdf[:ignore_mono][1]
         smry_res = aggregate(subdf, [:deg, :signal_ratio], [median, minimum, maximum]) |> CSV.write("noise10/syn_$(i)_mono_$(mono).csv")
