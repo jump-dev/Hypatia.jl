07770f0ed8b6afae752ba66333986871a9d87b4b
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-12-13 13:27:29 -0500

    remaining MOI.success -> MOI.optimal
    Former-commit-id: d81a8fada955484a765dd3a03c7fe3d43b0c597a

--- examples/shapeconregr/jump.jl
+++ examples/shapeconregr/jump.jl
@@ -196,7 +196,7 @@ function run_JuMP_shapeconregr(use_wsos::Bool; usedense::Bool=true)
     pr_status = JuMP.primal_status(model)
     du_status = JuMP.dual_status(model)
 
-    @test term_status == MOI.Success
+    @test term_status == MOI.Optimal
     @test pr_status == MOI.FeasiblePoint
     @test du_status == MOI.FeasiblePoint
     @test pobj ≈ dobj atol=1e-4 rtol=1e-4
