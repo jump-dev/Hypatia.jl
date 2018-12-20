16a0b6e6ae02e03d9783c93f204b294bf2f5a5b4
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-12-20 00:11:48 -0500

    add wsos mat example
    Former-commit-id: f940b2491f4af764c7ccb0597ea5e064ef6edbe7

--- examples/sumofsquares/choi.jl
+++ examples/sumofsquares/choi.jl
@@ -37,5 +37,28 @@ function run_JuMP_choi_scalarwsos()
     @constraint(model, [AffExpr(conv_condition(pts[i, :])) for i in 1:U] in scalar_wsos_cone)
 
     JuMP.optimize!(model)
-    @test JuMP.dual_status(model) == MOI.InfeasibilityCertificate
+    @test JuMP.dual_status(model) == MOI.INFEASIBILITY_CERTIFICATE
+end
+
+function run_JuMP_choi_matwsos()
+    model = Model(with_optimizer(Hypatia.Optimizer, verbose=true))
+    @polyvar x y z
+
+    # TODO get around equal lengths of variables correctly
+    C = [x^2+2y^2+0*z -x*y+0*z -x*z+0*y;
+         -x*y+0*z y^2+2z^2+0*x -y*z+0*x;
+         -x*z+0*y -y*z+0*x z^2+2x^2+0*y]
+
+    n = 3
+    d = maximum(DynamicPolynomials.maxdegree.(C))
+
+    dom = Hypatia.FreeDomain(n)
+    (U, pts, P0, _, _) = Hypatia.interpolate(dom, d, sample_factor=10, sample=true)
+
+    mat_wsos_cone = WSOSPolyInterpMatCone(n, U, [P0])
+
+    @constraint(model, [AffExpr(C[i,j](pts[u, :])) for j in 1:n, i in 1:n, u in 1:U if i >= j] in mat_wsos_cone)
+
+    JuMP.optimize!(model)
+    @test JuMP.dual_status(model) == MOI.INFEASIBILITY_CERTIFICATE
 end
