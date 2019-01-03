83a01bd15cd15c46df97e723ed7aa777cdaefd31
Chris Coey <coey.chris@gmail.com>
2019-01-03 15:44:46 +1000

    improve sosmat4 example

--- examples/sumofsquares/sosmat4.jl
+++ examples/sumofsquares/sosmat4.jl
@@ -17,7 +17,12 @@ using Test
 
 const rt2 = sqrt(2)
 
-function run_JuMP_sosmat4_matrix(x::Vector, H::Matrix, use_wsos::Bool)
+function run_JuMP_sosmat4_matrix(
+    x::Vector,
+    H::Matrix,
+    use_wsos::Bool,
+    is_SOS::Bool,
+    )
     if use_wsos
         n = nvariables(x)
         d = div(maximum(maxdegree.(H)), 2)
@@ -34,37 +39,57 @@ function run_JuMP_sosmat4_matrix(x::Vector, H::Matrix, use_wsos::Bool)
 
     JuMP.optimize!(model)
 
-    return (JuMP.termination_status(model) == MOI.OPTIMAL)
+    if is_SOS
+        @test JuMP.termination_status(model) == MOI.OPTIMAL
+        @test JuMP.primal_status(model) == MOI.FEASIBLE_POINT
+    else
+        @test JuMP.termination_status(model) == MOI.INFEASIBLE
+        @test JuMP.dual_status(model) == MOI.INFEASIBILITY_CERTIFICATE
+    end
+    return
 end
+
+run_JuMP_sosmat4_poly(x::Vector, poly, use_wsos::Bool, is_SOS::Bool) = run_JuMP_sosmat4_matrix(x, differentiate(poly, x, 2), use_wsos, is_SOS)
 
 function run_JuMP_sosmat4_matrix_a(use_wsos::Bool)
     @polyvar x[1:1]
     M = [x[1]+2x[1]^3 1; -x[1]^2+2 3x[1]^2-x[1]+1]
     MM = M'*M
-    @show MM
-    return run_JuMP_sosmat4_matrix(x, MM, use_wsos)
+
+    run_JuMP_sosmat4_matrix(x, MM, use_wsos, true)
+    run_JuMP_sosmat4_matrix(x, -MM, use_wsos, false)
 end
 
 function run_JuMP_sosmat4_matrix_b(use_wsos::Bool; rseed::Int=1)
-    n = 2
-    m = 2
+    n = 3
+    m = 3
     d = 1
 
     @polyvar x[1:n]
     Z = monomials(x, 0:d)
-    # Random.seed!(rseed)
+    Random.seed!(rseed)
     M = [sum(rand() * Z[l] for l in 1:length(Z)) for i in 1:m, j in 1:m]
-
     MM = M'*M
     MM = 0.5*(MM + MM')
 
-    return run_JuMP_sosmat4_matrix(x, MM, use_wsos)
+    run_JuMP_sosmat4_matrix(x, MM, use_wsos, true)
+    run_JuMP_sosmat4_matrix(x, -MM, use_wsos, false)
 end
 
-run_JuMP_sosmat4_poly(x::Vector, poly, use_wsos::Bool) = run_JuMP_sosmat4_matrix(x, differentiate(poly, x, 2), use_wsos)
+function run_JuMP_sosmat4_poly_a(use_wsos::Bool)
+    @polyvar x[1:1]
+    poly = x[1]^4 + 2x[1]^2
 
-run_JuMP_sosmat4_poly_a(use_wsos::Bool) = (@polyvar x[1:1]; run_JuMP_sosmat4_poly(x, x[1]^4+2x[1]^2, use_wsos))
-run_JuMP_sosmat4_poly_b(use_wsos::Bool) = (@polyvar x[1:1]; run_JuMP_sosmat4_poly(x, -x[1]^4-2x[1]^2, use_wsos))
-run_JuMP_sosmat4_poly_c(use_wsos::Bool) = (@polyvar x[1:2]; run_JuMP_sosmat4_poly(x, (x[1]*x[2]-x[1]+2x[2]-x[2]^2)^2, use_wsos))
-run_JuMP_sosmat4_poly_d(use_wsos::Bool) = (@polyvar x[1:2]; run_JuMP_sosmat4_poly(x, (x[1]+x[2])^4 + (x[1]+x[2])^2, use_wsos))
-run_JuMP_sosmat4_poly_e(use_wsos::Bool) = (@polyvar x[1:2]; run_JuMP_sosmat4_poly(x, -(x[1]+x[2])^4 + (x[1]+x[2])^2, use_wsos))
+    run_JuMP_sosmat4_poly(x, poly, use_wsos, true)
+    run_JuMP_sosmat4_poly(x, -poly, use_wsos, false)
+end
+
+function run_JuMP_sosmat4_poly_b(use_wsos::Bool)
+    @polyvar x[1:2]
+    poly = (x[1]+x[2])^4 + (x[1]+x[2])^2
+
+    run_JuMP_sosmat4_poly(x, poly, use_wsos, true)
+    run_JuMP_sosmat4_poly(x, -poly, use_wsos, false)
+end
+
+# run_JuMP_sosmat4_poly_c(use_wsos::Bool) = (@polyvar x[1:2]; run_JuMP_sosmat4_poly(x, (x[1]*x[2]-x[1]+2x[2]-x[2]^2)^2, use_wsos)) # TODO SOS but not detected?
