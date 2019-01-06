a474587fa406af7435734f2765e22bce3cd4deb6
Chris Coey <coey.chris@gmail.com>
2019-01-06 11:05:48 +1000

    added default example tests for wsosmatrix

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,6 +19,11 @@ include(joinpath(egs_dir, "namedpoly/native.jl"))
 include(joinpath(egs_dir, "namedpoly/jump.jl"))
 include(joinpath(egs_dir, "shapeconregr/jump.jl"))
 include(joinpath(egs_dir, "densityest/jump.jl"))
+include(joinpath(egs_dir, "wsosmatrix/sosmatrix.jl"))
+include(joinpath(egs_dir, "wsosmatrix/muconvexity.jl"))
+include(joinpath(egs_dir, "wsosmatrix/sosmat1.jl"))
+include(joinpath(egs_dir, "wsosmatrix/sosmat2.jl"))
+include(joinpath(egs_dir, "wsosmatrix/sosmat3.jl"))
 include(joinpath(@__DIR__, "examples.jl"))
 
 
@@ -214,33 +219,47 @@ testfuns = [
     # _shapeconregr14_JuMP, # throws out-of-memory error
     # _shapeconregr15_JuMP, # throws out-of-memory error
     ]
-@testset "JuMP examples: $testfun" for testfun in testfuns
-    testfun()
-end
+# @testset "JuMP examples: $testfun" for testfun in testfuns
+#     testfun()
+# end
 
 
 @info("starting verbose default examples tests")
 testfuns = [
-    run_JuMP_expdesign,
-    run_linearopt,
-    run_namedpoly,
-    run_JuMP_namedpoly_PSD, # final objective doesn't match
-    run_JuMP_namedpoly_WSOS_primal,
-    run_JuMP_namedpoly_WSOS_dual,
-    run_envelope_primal_dense,
-    run_envelope_dual_dense,
-    run_envelope_primal_sparse,
-    run_envelope_dual_sparse,
-    run_JuMP_envelope_boxinterp,
-    run_JuMP_envelope_sampleinterp_box,
-    run_JuMP_envelope_sampleinterp_ball,
-    run_JuMP_shapeconregr_PSD,
-    run_JuMP_shapeconregr_WSOS,
-    run_JuMP_densityest,
+    # run_JuMP_expdesign,
+    # run_linearopt,
+    # run_namedpoly,
+    # run_JuMP_namedpoly_PSD, # final objective doesn't match
+    # run_JuMP_namedpoly_WSOS_primal,
+    # run_JuMP_namedpoly_WSOS_dual,
+    # run_envelope_primal_dense,
+    # run_envelope_dual_dense,
+    # run_envelope_primal_sparse,
+    # run_envelope_dual_sparse,
+    # run_JuMP_envelope_boxinterp,
+    # run_JuMP_envelope_sampleinterp_box,
+    # run_JuMP_envelope_sampleinterp_ball,
+    # run_JuMP_shapeconregr_PSD,
+    # run_JuMP_shapeconregr_WSOS,
+    # run_JuMP_densityest,
+    run_JuMP_sosmat4_matrix_rand,
+    run_JuMP_sosmat4_matrix_a,
+    run_JuMP_sosmat4_poly_a,
+    run_JuMP_sosmat4_poly_b,
+    run_JuMP_muconvexity_rand,
+    run_JuMP_muconvexity_a,
+    run_JuMP_muconvexity_b,
+    run_JuMP_muconvexity_c,
+    run_JuMP_muconvexity_d,
+    run_JuMP_sosmat1,
+    run_JuMP_sosmat2_scalar,
+    run_JuMP_sosmat2_matrix,
+    run_JuMP_sosmat2_matrix_dual,
+    run_JuMP_sosmat3,
     ]
-# @testset "default examples: $testfun" for testfun in testfuns
-#     testfun()
-# end
+@testset "default examples: $testfun" for testfun in testfuns
+    testfun()
+end
 
 
 @info("starting MathOptInterface tests")
