38ed298c7439d37a24d11063963d57075614188a
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-02-12 13:29:27 -0500

    rename tmps

--- src/Cones/wsospolyinterpmat.jl
+++ src/Cones/wsospolyinterpmat.jl
@@ -19,9 +19,9 @@ mutable struct WSOSPolyInterpMat <: Cone
     F
     mat::Vector{Matrix{Float64}}
     matfact::Vector{CholeskyPivoted{Float64,Array{Float64,2}}}
-    tmp2::Vector{Matrix{Float64}}
+    tmp1::Vector{Matrix{Float64}}
+    tmp2::Matrix{Float64}
     tmp3::Matrix{Float64}
-    tmp4::Matrix{Float64}
 
     function WSOSPolyInterpMat(r::Int, u::Int, ipwt::Vector{Matrix{Float64}}, is_dual::Bool)
         for ipwtj in ipwt
@@ -40,9 +40,9 @@ mutable struct WSOSPolyInterpMat <: Cone
         cone.H2 = similar(cone.H)
         cone.mat = [similar(ipwt[1], size(ipwtj, 2) * r, size(ipwtj, 2) * r) for ipwtj in ipwt]
         cone.matfact = Vector{CholeskyPivoted{Float64,Array{Float64,2}}}(undef, length(ipwt))
-        cone.tmp2 = [similar(ipwt[1], size(ipwtj, 2), u) for ipwtj in ipwt]
+        cone.tmp1 = [similar(ipwt[1], size(ipwtj, 2), u) for ipwtj in ipwt]
+        cone.tmp2 = similar(ipwt[1], u, u)
         cone.tmp3 = similar(ipwt[1], u, u)
-        cone.tmp4 = similar(ipwt[1], u, u)
         return cone
     end
 end
@@ -51,10 +51,10 @@ blockrange(i::Int, o::Int) = (o * (i - 1) + 1):(o * i)
 
 function buildmat!(cone::WSOSPolyInterpMat, point::AbstractVector{Float64})
     (R, U) = (cone.r, cone.u)
-    tmp3 = cone.tmp3
+    tmp2 = cone.tmp2
     for j in eachindex(cone.ipwt)
         ipwtj = cone.ipwt[j]
-        tmp2j = cone.tmp2[j]
+        tmp1j = cone.tmp1[j]
         L = size(ipwtj, 2)
         mat = cone.mat[j]
         mat .= 0.0
@@ -64,8 +64,8 @@ function buildmat!(cone::WSOSPolyInterpMat, point::AbstractVector{Float64})
             (p == q) ? fact = 1.0 : fact = rt2i
             rinds = blockrange(p, L)
             cinds = blockrange(q, L)
-            @. tmp2j = ipwtj' * cone.point[uo + 1:uo + U]' * fact
-            mul!(view(mat, rinds, cinds), tmp2j, ipwtj)
+            @. tmp1j = ipwtj' * cone.point[uo + 1:uo + U]' * fact
+            mul!(view(mat, rinds, cinds), tmp1j, ipwtj)
             uo += U
         end
         cone.matfact[j] = cholesky!(Symmetric(mat, :L), Val(true), check=false)
@@ -78,9 +78,9 @@ end
 
 function update_gradient_hessian!(cone::WSOSPolyInterpMat, j::Int, Winv::Matrix{Float64})
     ipwtj = cone.ipwt[j]
-    tmp2j = cone.tmp2[j]
+    tmp1j = cone.tmp1[j]
+    tmp2 = cone.tmp2
     tmp3 = cone.tmp3
-    tmp4 = cone.tmp4
 
     L = size(ipwtj, 2)
 
@@ -91,9 +91,9 @@ function update_gradient_hessian!(cone::WSOSPolyInterpMat, j::Int, Winv::Matrix{
         rinds = blockrange(p, L)
         cinds = blockrange(q, L)
         idxs = blockrange(uo, cone.u)
-        mul!(tmp2j, view(Winv, rinds, cinds), ipwtj')
-        mul!(tmp3, ipwtj, tmp2j)
-        cone.g[idxs] .-= diag(tmp3) * fact
+        mul!(tmp1j, view(Winv, rinds, cinds), ipwtj')
+        mul!(tmp2, ipwtj, tmp1j)
+        cone.g[idxs] .-= diag(tmp2) * fact
         uo2 = 0
         for p2 in 1:cone.r, q2 in 1:p2
             uo2 += 1
@@ -102,21 +102,19 @@ function update_gradient_hessian!(cone::WSOSPolyInterpMat, j::Int, Winv::Matrix{
             cinds2 = blockrange(q2, L)
             idxs2 = blockrange(uo2, cone.u)
 
-            mul!(tmp2j, view(Winv, rinds, rinds2), ipwtj')
-            mul!(tmp3, ipwtj, tmp2j)
-            mul!(tmp2j, view(Winv, cinds, cinds2), ipwtj')
-            mul!(tmp4, ipwtj, tmp2j)
+            mul!(tmp1j, view(Winv, rinds, rinds2), ipwtj')
+            mul!(tmp2, ipwtj, tmp1j)
+            mul!(tmp1j, view(Winv, cinds, cinds2), ipwtj')
+            mul!(tmp3, ipwtj, tmp1j)
             xor(p == q, p2 == q2) ? fact = rt2i : fact = 1.0
-            @. cone.H[idxs, idxs2] += tmp3 * tmp4 * fact
+            @. cone.H[idxs, idxs2] += tmp2 * tmp3 * fact
 
             if (p != q) || (p2 != q2)
-                mul!(tmp2j, view(Winv, rinds, cinds2), ipwtj')
-                mul!(tmp3, ipwtj, tmp2j)
-                mul!(tmp2j, view(Winv, cinds, rinds2), ipwtj')
-                mul!(tmp4, ipwtj, tmp2j)
-                @. cone.H[idxs, idxs2] += tmp3 * tmp4 * fact
-                # prod34 = (ipwtj * Winv[rinds, cinds2] * ipwtj') .* (ipwtj * Winv[cinds, rinds2] * ipwtj')
-                # cone.H[idxs, idxs2] += prod34 * fact
+                mul!(tmp1j, view(Winv, rinds, cinds2), ipwtj')
+                mul!(tmp2, ipwtj, tmp1j)
+                mul!(tmp1j, view(Winv, cinds, rinds2), ipwtj')
+                mul!(tmp3, ipwtj, tmp1j)
+                @. cone.H[idxs, idxs2] += tmp2 * tmp3 * fact
             end
         end
     end
