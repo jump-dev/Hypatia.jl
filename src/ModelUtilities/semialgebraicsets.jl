deb36667698b288c3cb29435c3bb1641c239efd6
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-02-18 14:31:16 -0500

    write psd version of univariate problem
    Former-commit-id: eac46a789a3ee482cabec00c98eda788bbdbecc7

--- src/ModelUtilities/semialgebraicsets.jl
+++ src/ModelUtilities/semialgebraicsets.jl
@@ -6,7 +6,7 @@ utilities for Hypatia domains and SemialgebraicSets.jl
 
 # construct domain inequalities for SumOfSquares models from Hypatia domains
 
-function get_domain_inequalities(dom::Box, x)
+function get_domain_inequalities(dom::Box, x::Vector{DP.PolyVar{true}})
     bss = SAS.BasicSemialgebraicSet{Float64, DynamicPolynomials.Polynomial{true, Float64}}()
     for i in 1:dimension(dom)
         SAS.addinequality!(bss, (-x[i] + dom.u[i]) * (x[i] - dom.l[i]))
@@ -14,8 +14,13 @@ function get_domain_inequalities(dom::Box, x)
     return bss
 end
 
+function get_domain_inequalities(dom::Box, x::DP.PolyVar{true})
+    @assert dimension(dom) == 1
+    return SAS.@set((x - dom.l[1]) * (dom.u[1] - x) <= 0)
+end
+
 get_domain_inequalities(dom::Ball, x) = SAS.@set(sum((x - dom.c) .^ 2) <= dom.r^2)
 
-get_domain_inequalities(dom::Ellipsoid, x) = SAS.@set((x - dom.c)' * dom.Q * (x - dom.c) <= 1.0)
+get_domain_inequalities(dom::Ellipsoid, x) = SAS.@set((x - dom.c)' * dom.Q * (x - dom.c) <= 1)
 
 get_domain_inequalities(dom::SemiFreeDomain, x) = get_domain_inequalities(dom.sampling_region, x)
