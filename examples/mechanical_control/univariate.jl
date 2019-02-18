eac46a789a3ee482cabec00c98eda788bbdbecc7
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-02-18 14:31:16 -0500

    write psd version of univariate problem

--- examples/mechanical_control/univariate.jl
+++ examples/mechanical_control/univariate.jl
@@ -11,6 +11,7 @@ const MO = HYP.Models
 const MU = HYP.ModelUtilities
 
 import JuMP
+import SumOfSquares
 import MathOptInterface
 const MOI = MathOptInterface
 import PolyJuMP
@@ -22,7 +23,23 @@ using Test
 
 T = 100.0
 
-function build_univariate(deg::Int)
+function get_cones_WSOS(dom1, dom2, dom3, deg)
+    (U2, pts2, P02, PWts2, _) = MU.interpolate(dom2, div(deg, 2) + 1, sample = false)
+    (U3, pts3, P03, PWts3, _) = MU.interpolate(dom3, div(deg, 2), sample = false)
+    wsos_cone2 = HYP.WSOSPolyInterpCone(U2, [P02, PWts2...])
+    wsos_cone3 = HYP.WSOSPolyInterpCone(U3, [P03, PWts3...])
+    return (wsos_cone2, wsos_cone3, U2, U3, pts2, pts3)
+end
+
+function get_bss(dom1, dom2, dom3, x, t)
+    bss1 = MU.get_domain_inequalities(dom1, x)
+    bss2 = MU.get_domain_inequalities(dom2, [x; t])
+    bss3 = MU.get_domain_inequalities(dom3, x)
+    return (bss1, bss2, bss2)
+end
+
+
+function build_univariate(deg::Int; use_wsos::Bool = true)
     DynamicPolynomials.@polyvar x
     DynamicPolynomials.@polyvar t
     f = x * (x - 0.5) * (x + 0.5) * T
@@ -31,13 +48,9 @@ function build_univariate(deg::Int)
     dom3 = MU.Box([-0.01], [0.01]) # state at the end
 
     (U1, pts1, P01, PWts1, quad_weights) = MU.interpolate(dom1, div(deg, 2) + 1, sample = false, calc_w = true)
-    (U2, pts2, P02, PWts2, _) = MU.interpolate(dom2, div(deg, 2) + 1, sample = false)
-    (U3, pts3, P03, PWts3, _) = MU.interpolate(dom3, div(deg, 2), sample = false)
     wsos_cone1 = HYP.WSOSPolyInterpCone(U1, [P01, PWts1...])
-    wsos_cone2 = HYP.WSOSPolyInterpCone(U2, [P02, PWts2...])
-    wsos_cone3 = HYP.WSOSPolyInterpCone(U3, [P03, PWts3...])
 
-    model = JuMP.Model(JuMP.with_optimizer(Hypatia.Optimizer, verbose=true))
+    model = SumOfSquares.SOSModel(JuMP.with_optimizer(Hypatia.Optimizer, verbose=true))
     JuMP.@variables(model, begin
         v, PolyJuMP.Poly(DynamicPolynomials.monomials([x; t], 0:deg)) # TODO issue reverse order
         w, PolyJuMP.Poly(DynamicPolynomials.monomials(x, 0:deg))
@@ -50,19 +63,30 @@ function build_univariate(deg::Int)
     dvdt = delvdelt + delvdelx * f
     diffwv = w - DynamicPolynomials.subs(v, t => 0.0) - 1.0
     vT = DynamicPolynomials.subs(v, t => 1.0)
-    JuMP.@constraints(model, begin
-        [-dvdt(pts2[u, :]) for u in 1:U2] in wsos_cone2
-        [diffwv(pts1[u, :]) for u in 1:U1] in wsos_cone1
-        [vT(pts3[u, :]) for u in 1:U3] in wsos_cone3
-        [w(pts1[u, :]) for u in 1:U1] in wsos_cone1
-    end)
+
+    if use_wsos
+        (wsos_cone2, wsos_cone3, U2, U3, pts2, pts3) = get_cones_WSOS(dom1, dom2, dom3, deg)
+        JuMP.@constraints(model, begin
+            [-dvdt(pts2[u, :]) for u in 1:U2] in wsos_cone2
+            [diffwv(pts1[u, :]) for u in 1:U1] in wsos_cone1
+            [vT(pts3[u, :]) for u in 1:U3] in wsos_cone3
+            [w(pts1[u, :]) for u in 1:U1] in wsos_cone1
+        end)
+    else
+        (bss1, bss2, bss2) = get_bss(dom1, dom2, dom3, x, t)
+        # JuMP.@constraint(model, -dvdt in JuMP.PSDCone(), domain = bss2)
+        JuMP.@constraint(model, diffwv in JuMP.PSDCone(), domain = bss1)
+        JuMP.@constraint(model, vT in JuMP.PSDCone(), domain = bss3)
+        JuMP.@constraint(model, w in JuMP.PSDCone(), domain = bss1)
+    end
+
     return model
 end
 
 function run_JuMP_univariate_roa()
-    model = build_univariate(4)
+    model = build_univariate(4, use_wsos = false)
     JuMP.optimize!(model)
-    
+
     term_status = JuMP.termination_status(model)
     primal_obj = JuMP.objective_value(model)
     dual_obj = JuMP.objective_bound(model)
--- src/ModelUtilities/ModelUtilities.jl
+++ src/ModelUtilities/ModelUtilities.jl
@@ -12,10 +12,10 @@ using LinearAlgebra
 import FFTW
 import Combinatorics
 import GSL: sf_gamma_inc_Q
-import DynamicPolynomials: Monomial, Polynomial, monomials, @polyvar
+import DynamicPolynomials
+const DP = DynamicPolynomials
 include("interpolate.jl")
 
-import DynamicPolynomials
 import SemialgebraicSets
 const SAS = SemialgebraicSets
 include("semialgebraicsets.jl")
