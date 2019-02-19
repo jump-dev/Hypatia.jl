3668e8ef1028e4a7dfe6a42109f8d88207f0846c
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-02-19 10:56:22 -0500

    address latest review comments
    Former-commit-id: e3dfd18e8fe7c7a4f45f085bfccc1cecd6fcde57

--- examples/regionofattraction/univariate.jl
+++ examples/regionofattraction/univariate.jl
@@ -20,16 +20,16 @@ import MathOptInterface
 const MOI = MathOptInterface
 import PolyJuMP
 import DynamicPolynomials
+const DP = DynamicPolynomials
 using LinearAlgebra
-import Random
 import Distributions
 using Test
 
 function univariate_WSOS(deg::Int)
     T = 100.0
 
-    DynamicPolynomials.@polyvar x
-    DynamicPolynomials.@polyvar t
+    DP.@polyvar x
+    DP.@polyvar t
     f = x * (x - 0.5) * (x + 0.5) * T
     dom1 = MU.Box([-1.0], [1.0]) # just state
     dom2 = MU.Box([-1.0, 0.0], [1.0, 1.0]) # state and time
@@ -44,15 +44,15 @@ function univariate_WSOS(deg::Int)
 
     model = SumOfSquares.Model(JuMP.with_optimizer(Hypatia.Optimizer, verbose = true))
     JuMP.@variables(model, begin
-        v, PolyJuMP.Poly(DynamicPolynomials.monomials([x; t], 0:deg)) # TODO issue reverse order
-        w, PolyJuMP.Poly(DynamicPolynomials.monomials(x, 0:deg))
+        v, PolyJuMP.Poly(DP.monomials([x; t], 0:deg))
+        w, PolyJuMP.Poly(DP.monomials(x, 0:deg))
     end)
 
     JuMP.@objective(model, Min, sum(quad_weights[u] * w(pts1[u, :]) for u in 1:U1))
 
-    dvdt = DynamicPolynomials.differentiate(v, t) + DynamicPolynomials.differentiate(v, x) * f
-    diffwv = w - DynamicPolynomials.subs(v, t => 0.0) - 1.0
-    vT = DynamicPolynomials.subs(v, t => 1.0)
+    dvdt = DP.differentiate(v, t) + DP.differentiate(v, x) * f
+    diffwv = w - DP.subs(v, t => 0.0) - 1.0
+    vT = DP.subs(v, t => 1.0)
 
     JuMP.@constraints(model, begin
         [-dvdt(pts2[u, :]) for u in 1:U2] in wsos_cone2
@@ -67,26 +67,24 @@ end
 function univariate_PSD(deg::Int)
     T = 100.0
 
-    DynamicPolynomials.@polyvar x
-    DynamicPolynomials.@polyvar t
+    DP.@polyvar x
+    DP.@polyvar t
     f = x * (x - 0.5) * (x + 0.5) * T
-    dom1 = MU.Box([-1.0], [1.0]) # just state
-    dom2 = MU.Box([-1.0, 0.0], [1.0, 1.0]) # state and time
-    dom3 = MU.Box([-0.01], [0.01]) # state at the end
 
-    (U1, pts1, _, _, quad_weights) = MU.interpolate(dom1, div(deg, 2) + 1, sample = false, calc_w = true)
+    int_box_mon(mon) = prod(1 / (p + 1) - (-1)^(p + 1) / (p + 1) for p in DP.exponents(mon))
+    int_box(pol) = sum(DP.coefficient(t) * int_box_mon(t) for t in DP.terms(pol))
 
     model = SumOfSquares.SOSModel(JuMP.with_optimizer(Hypatia.Optimizer, verbose = true))
     JuMP.@variables(model, begin
-        v, PolyJuMP.Poly(DynamicPolynomials.monomials([x; t], 0:deg)) # TODO issue reverse order
-        w, PolyJuMP.Poly(DynamicPolynomials.monomials(x, 0:deg))
+        v, PolyJuMP.Poly(DP.monomials([x; t], 0:deg))
+        w, PolyJuMP.Poly(DP.monomials(x, 0:deg))
     end)
 
-    JuMP.@objective(model, Min, sum(quad_weights[u] * w(pts1[u, :]) for u in 1:U1))
+    JuMP.@objective(model, Min, int_box(w))
 
-    dvdt = DynamicPolynomials.differentiate(v, t) + DynamicPolynomials.differentiate(v, x) * f
-    diffwv = w - DynamicPolynomials.subs(v, t => 0.0) - 1.0
-    vT = DynamicPolynomials.subs(v, t => 1.0)
+    dvdt = DP.differentiate(v, t) + DP.differentiate(v, x) * f
+    diffwv = w - DP.subs(v, t => 0.0) - 1.0
+    vT = DP.subs(v, t => 1.0)
 
     JuMP.@constraint(model, -dvdt >= 0, domain = (SAS.@set -1 <= x  && x <= 1 && 0 <= t && t <= 1))
     JuMP.@constraint(model, diffwv >= 0, domain = (SAS.@set -1 <= x && x <= 1))
@@ -97,22 +95,27 @@ function univariate_PSD(deg::Int)
 end
 
 
-function run_JuMP_univariate()
-    for func in [univariate_WSOS, univariate_PSD]
-        model = func(4)
-        JuMP.optimize!(model)
-
-        term_status = JuMP.termination_status(model)
-        primal_obj = JuMP.objective_value(model)
-        dual_obj = JuMP.objective_bound(model)
-        pr_status = JuMP.primal_status(model)
-        du_status = JuMP.dual_status(model)
-
-        @test term_status == MOI.OPTIMAL
-        @test pr_status == MOI.FEASIBLE_POINT
-        @test du_status == MOI.FEASIBLE_POINT
-        @test primal_obj ≈ dual_obj atol = 1e-4 rtol = 1e-4
+function run_JuMP_univariate(deg::Int; use_WSOS::Bool = true)
+    if use_WSOS
+        model = univariate_WSOS(deg)
+    else
+        model = univariate_PSD(deg)
     end
+    JuMP.optimize!(model)
+
+    term_status = JuMP.termination_status(model)
+    primal_obj = JuMP.objective_value(model)
+    dual_obj = JuMP.objective_bound(model)
+    pr_status = JuMP.primal_status(model)
+    du_status = JuMP.dual_status(model)
+
+    @test term_status == MOI.OPTIMAL
+    @test pr_status == MOI.FEASIBLE_POINT
+    @test du_status == MOI.FEASIBLE_POINT
+    @test primal_obj ≈ dual_obj atol = 1e-4 rtol = 1e-4
 
     return nothing
 end
+
+run_JuMP_univariate_WSOS() = run_JuMP_univariate(4, use_WSOS = true)
+run_JuMP_univariate_PSD() = run_JuMP_univariate(4, use_WSOS = false)
