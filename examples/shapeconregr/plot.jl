1bb31f055ef11018e6982fbfc40ad38797f016c2
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-02-25 11:37:47 -0500

    plot of raw data
    Former-commit-id: c32b94da888e60f775a1de1a0989a952a03d445a

--- examples/shapeconregr/plot.jl
+++ examples/shapeconregr/plot.jl
@@ -4,6 +4,8 @@ Copyright 2018, Chris Coey, Lea Kapelevich and contributors
 
 using PlotlyJS
 # using ORCA
+# using Plots
+# plotlyjs()
 import Distributions
 
 function get_data_trace3d(X, y)
@@ -21,16 +23,14 @@ function get_data_trace3d(X, y)
 end
 
 function get_model_trace3d(regressor, n)
-    s = 5
+    s = 20
     a = collect(range(-1, stop = 1, length = s))
     x = zeros(s^n, n)
     q = 0
-    for i in 1:s, j in 1:s, k in 1:s, l in 1:s
+    for i in 1:s, j in 1:s
         q += 1
         x[q, 1] = a[i]
         x[q, 2] = a[j]
-        x[q, 3] = a[k]
-        x[q, 4] = a[l]
     end
     z = [regressor(x[i, :]) for i in 1:s^2]
     model_trace = mesh3d(
@@ -88,24 +88,33 @@ end
 
 function makeplot2d(regressor, X, y)
     data_trace = get_data_trace2d(X, y)
-    model_trace = get_model_trace2d(regressor, X)
-    layout = Layout(margin = attr(l = 0, r = 0, t = 0, b = 0))
+    layout = Layout(margin = attr(l = 0, r = 0, t = 0, b = 0), title = "Value of Shipment")
 
-    pl = plot([data_trace, model_trace], layout)
+    pl = plot([data_trace], layout)
 
     return pl
 end
 
 # plot for real dataset
 function makeplot_production()
-    (X, y, _) = production_data(n = 2)
+    (X, y) = production_data()
     model = SumOfSquares.SOSModel(JuMP.with_optimizer(HYP.Optimizer, verbose = true))
-    deg = 4
+    # X = X[:, 1:1]
+    deg = 3
     n = size(X, 2)
     shapedata = ShapeData(MU.FreeDomain(n), MU.FreeDomain(n), zeros(n), 1)
-    poly = build_shapeconregr_WSOS(model, X[:, 1:n], y, deg, shapedata, use_naive = true)
+    poly = build_shapeconregr_WSOS(model, X[:, 1:n], y, deg, shapedata, use_naive = false)
     JuMP.optimize!(model)
     pl = makeplot2d(JuMP.value(poly), X, y)
 end
 
-# 8.281059404049783e-6x₁³ + 6.882751952330422e-5x₁²x₂ + 0.00013183157318747492x₁x₂² + 6.461202451160328e-5x₂³ - 0.04670076489051557
+function makeplot_exp()
+    (X, y) = expfunction_data(n = 2, num_points = 200)
+    model = SumOfSquares.SOSModel(JuMP.with_optimizer(HYP.Optimizer, verbose = true))
+    deg = 6
+    n = size(X, 2)
+    shapedata = ShapeData(MU.FreeDomain(n), MU.FreeDomain(n), ones(n), 0)
+    poly = build_shapeconregr_WSOS(model, X[:, 1:n], y, deg, shapedata)
+    JuMP.optimize!(model)
+    pl = makeplot3d(JuMP.value(poly), X, y)
+end
