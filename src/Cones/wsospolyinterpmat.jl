0f623f1911aadc75255513e89dabe2d36272cf5b
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-03-06 20:23:41 -0500

    get math working inside code
    Former-commit-id: 61ad0ad3b107b88f251d0ac25f441a20fd14635c

--- src/Cones/wsospolyinterpmat.jl
+++ src/Cones/wsospolyinterpmat.jl
@@ -19,7 +19,7 @@ mutable struct WSOSPolyInterpMat <: Cone
     Hi::Matrix{Float64}
     F
     mat::Vector{Matrix{Float64}}
-    matfact::Vector{CholeskyPivoted{Float64, Matrix{Float64}}}
+    matfact #::Vector{CholeskyPivoted{Float64, Matrix{Float64}}}
     tmp1::Vector{Matrix{Float64}}
     tmp2::Matrix{Float64}
     tmp3::Matrix{Float64}
@@ -41,7 +41,7 @@ mutable struct WSOSPolyInterpMat <: Cone
         cone.H2 = similar(cone.H)
         cone.Hi = similar(cone.H)
         cone.mat = [similar(ipwt[1], size(ipwtj, 2) * R, size(ipwtj, 2) * R) for ipwtj in ipwt]
-        cone.matfact = Vector{CholeskyPivoted{Float64, Matrix{Float64}}}(undef, length(ipwt))
+        cone.matfact = Vector{Cholesky{Float64,Array{Float64,2}}}(undef, length(ipwt)) # {CholeskyPivoted{Float64, Matrix{Float64}}}(undef, length(ipwt))
         cone.tmp1 = [similar(ipwt[1], size(ipwtj, 2), U) for ipwtj in ipwt]
         cone.tmp2 = similar(ipwt[1], U, U)
         cone.tmp3 = similar(cone.tmp2)
@@ -62,8 +62,6 @@ function set_initial_point(arr::AbstractVector{Float64}, cone::WSOSPolyInterpMat
     end
     return arr
 end
-
-_blockrange(inner::Int, outer::Int) = (outer * (inner - 1) + 1):(outer * inner)
 
 # TODO all views can be allocated just once in the cone definition (delete _blockrange too)
 function check_in_cone(cone::WSOSPolyInterpMat)
@@ -89,7 +87,7 @@ function check_in_cone(cone::WSOSPolyInterpMat)
             uo += cone.U
         end
 
-        cone.matfact[j] = cholesky!(Symmetric(mat, :L), Val(true), check = false)
+        cone.matfact[j] = cholesky!(Symmetric(mat, :L), Val(false), check = false) # TODO turn pivoting back on
         if !isposdef(cone.matfact[j])
             return false
         end
@@ -111,23 +109,27 @@ function check_in_cone(cone::WSOSPolyInterpMat)
 
         L = size(ipwtj, 2)
 
-        kron_ipwtj = kron(Matrix(I, cone.R, cone.R), ipwtj')
-        kron_Winv = cone.matfact[j] \ kron_ipwtj
+        # kron_ipwtj = kron(Matrix(I, cone.R, cone.R), ipwtj')
+        # kron_Winv = cone.matfact[j] \ kron_ipwtj
         # big_PLambdaP = kron(Matrix(I, cone.R, cone.R), ipwtj) * cone.matfact[j] \ kron(Matrix(I, cone.R, cone.R), ipwtj')
         # big_PLambdaP = kron(Matrix(I, cone.R, cone.R), ipwtj) * _block_uppertrisolve(cone.matfact[j].U, _block_lowertrisolve(cone.matfact[j].L, ipwtj, cone.R, L, cone.U), cone.R, L, cone.U)
-        big_PLambdaP = PLmabdaP(cone.matfact[j], ipwtj, cone.R, cone.L, cone.U)
+        big_PLambdaP = PLmabdaP(cone.matfact[j], ipwtj, cone.R, L, cone.U)
+        # @show kron(Matrix(I, cone.R, cone.R), ipwtj) * (cone.matfact[j] \ kron(Matrix(I, cone.R, cone.R), ipwtj'))
+        # @show big_PLambdaP ./ (kron(Matrix(I, cone.R, cone.R), ipwtj) * (cone.matfact[j] \ kron(Matrix(I, cone.R, cone.R), ipwtj')))
 
         uo = 0
         for p in 1:cone.R, q in 1:p
             uo += 1
             fact = (p == q) ? 1.0 : rt2
-            rinds = _blockrange(p, L)
-            cinds = _blockrange(q, L)
+            rinds = _blockrange(p, cone.U)
+            cinds = _blockrange(q, cone.U)
             idxs = _blockrange(uo, cone.U)
 
-            for i in 1:cone.U
-                cone.g[idxs[i]] -= ipwtj[i, :]' * view(W_inv_j, rinds, cinds) * ipwtj[i, :] * fact
-            end
+            # for i in 1:cone.U
+            #     cone.g[idxs[i]] -= ipwtj[i, :]' * view(W_inv_j, _blockrange(p, L), _blockrange(q, L)) * ipwtj[i, :] * fact
+            # end
+            # @show cone.g[idxs] ./ (-diag(big_PLambdaP[rinds, cinds]) .* fact)
+            cone.g[idxs] -= diag(big_PLambdaP[rinds, cinds]) .* fact
 
             uo2 = 0
             for p2 in 1:cone.R, q2 in 1:p2
@@ -136,24 +138,26 @@ function check_in_cone(cone::WSOSPolyInterpMat)
                     continue
                 end
 
-                rinds2 = _blockrange(p2, L)
-                cinds2 = _blockrange(q2, L)
+                rinds2 = _blockrange(p2, cone.U)
+                cinds2 = _blockrange(q2, cone.U)
                 idxs2 = _blockrange(uo2, cone.U)
 
-                mul!(tmp1j, view(W_inv_j, rinds, rinds2), ipwtj')
-                mul!(tmp2, ipwtj, tmp1j)
-                @show tmp2 ./ big_PLambdaP[_blockrange(p, cone.U), _blockrange(p2, cone.U)]
-                mul!(tmp1j, view(W_inv_j, cinds, cinds2), ipwtj')
-                mul!(tmp3, ipwtj, tmp1j)
+                # mul!(tmp1j, view(W_inv_j, rinds, rinds2), ipwtj')
+                # mul!(tmp2, ipwtj, tmp1j)
+                # @show tmp2 ./ big_PLambdaP[_blockrange(p, cone.U), _blockrange(p2, cone.U)]
+                # mul!(tmp1j, view(W_inv_j, cinds, cinds2), ipwtj')
+                # mul!(tmp3, ipwtj, tmp1j)
                 fact = xor(p == q, p2 == q2) ? rt2i : 1.0
-                @. cone.H[idxs, idxs2] += tmp2 * tmp3 * fact
+                # @. cone.H[idxs, idxs2] += tmp2 * tmp3 * fact
+                @. cone.H[idxs, idxs2] += big_PLambdaP[rinds, rinds2] * big_PLambdaP[cinds, cinds2] * fact
 
                 if (p != q) || (p2 != q2)
-                    mul!(tmp1j, view(W_inv_j, rinds, cinds2), ipwtj')
-                    mul!(tmp2, ipwtj, tmp1j)
-                    mul!(tmp1j, view(W_inv_j, cinds, rinds2), ipwtj')
-                    mul!(tmp3, ipwtj, tmp1j)
-                    @. cone.H[idxs, idxs2] += tmp2 * tmp3 * fact
+                    # mul!(tmp1j, view(W_inv_j, rinds, cinds2), ipwtj')
+                    # mul!(tmp2, ipwtj, tmp1j)
+                    # mul!(tmp1j, view(W_inv_j, cinds, rinds2), ipwtj')
+                    # mul!(tmp3, ipwtj, tmp1j)
+                    # @. cone.H[idxs, idxs2] += tmp2 * tmp3 * fact
+                    @. cone.H[idxs, idxs2] += big_PLambdaP[rinds, cinds2] * big_PLambdaP[cinds, rinds2] * fact
                 end
             end
         end
@@ -181,7 +185,6 @@ function _block_lowertrisolve(Lmat, ipwt, blocknum, R, L, U)
     tmp = zeros(L, U)
     for r in 1:R
         if r == blocknum
-            @show size(LowerTriangular(Lmatij(r, r)) \ ipwt'), size(resvec[_blockrange(r, L), :])
             resvec[_blockrange(r, L), :] = LowerTriangular(Lmatij(r, r)) \ ipwt'
         elseif r > blocknum
             tmp .= 0.0
@@ -201,6 +204,7 @@ function _block_lowertrisolve(Lmat, ipwt, R, L, U)
     return resmat
 end
 # left hand side always upper triangular block matrix
+# this function is not used and actually not needed
 function _block_uppertrisolve(Umat, rhs, blocknum, R, L, U)
     resvec = zeros(R * L, U)
     resi(i) = resvec[_blockrange(i, L), :]
@@ -220,6 +224,7 @@ function _block_uppertrisolve(Umat, rhs, blocknum, R, L, U)
     end
     return resvec
 end
+# this function is not used and actually not needed
 function _block_uppertrisolve(Umat, rhs, R, L, U)
     resmat = zeros(R * L, R * U)
     for r in 1:R
@@ -227,7 +232,7 @@ function _block_uppertrisolve(Umat, rhs, R, L, U)
     end
     return resmat
 end
-function mul_ipwtkron(ipwt, x, R)
+function mul_ipwtkron(ipwt, x, R, L, U)
     res = Matrix(undef, R * U, R * U)
     for r in 1:R
         for s in 1:R # will actually be symmetric
@@ -236,10 +241,35 @@ function mul_ipwtkron(ipwt, x, R)
     end
     return res
 end
-function PLmabdaP(fact, ipwtj, R, L, U)
-    mul_ipwtkron(ipwt, _block_uppertrisolve(fact.U, _block_lowertrisolve(fact.L, ipwtj, R, L, U), R, L, U), R)
+
+# because Ux only has blocks on the lower triangle, don't need to multiply all combinations for blocks
+# This is the same thing as BLAS.syrk!('U', 'T', 1.0, Ux, 0.0, res)
+# a lot could improve. for one the elements on the diagonal are symmetric so don't need to compute entire block.
+function mulblocks(Ux, R, L, U)
+    res = Matrix{Float64}(undef, R * U, R * U)
+    tmp = zeros(U, U)
+    for i in 1:R
+        rinds = _blockrange(i, U)
+        for j in i:R
+            cinds = _blockrange(j, U)
+            tmp .= 0.0
+            # since Ux is block lower triangular rows only from max(i,j) start making a nonzero contribution to the product
+            for k = j:R
+                tmp += Ux[_blockrange(k, L), _blockrange(i, U)]' * Ux[_blockrange(k, L), _blockrange(j, U)]
+            end
+            res[rinds, cinds] = tmp
+        end
+    end
+    return Symmetric(res)
 end
 
+function PLmabdaP(fact, ipwtj, R, L, U)
+    # mul_ipwtkron(ipwtj, _block_uppertrisolve(fact.U, _block_lowertrisolve(fact.L, ipwtj, R, L, U), R, L, U), R, L, U)
+    ux = _block_lowertrisolve(fact.L, ipwtj, R, L, U)
+    return mulblocks(ux, R, L, U)
+end
+
+_blockrange(inner::Int, outer::Int) = (outer * (inner - 1) + 1):(outer * inner)
 
 # using Test
 # using LinearAlgebra
@@ -251,9 +281,13 @@ end
 # F = cholesky(blocklambda)
 # Ux = _block_lowertrisolve(F.L, ipwt, R, L, U)
 # @test Ux ≈ F.L \ kron_ipwt'
-# x = _block_uppertrisolve(F.U, Ux, R, L, U)
-# @test x ≈ F \ kron_ipwt'
-# @test kron_ipwt * inv(F) * kron_ipwt' ≈ mul_ipwtkron(ipwt, x, R)
+# # x = _block_uppertrisolve(F.U, Ux, R, L, U) # this is not being used, not needed
+# # @test x ≈ F \ kron_ipwt' # this is not being used, not needed
+# # # @test kron_ipwt * inv(F) * kron_ipwt' ≈ mul_ipwtkron(ipwt, x, R, L, U) # this is not being used, not needed
+#
+# res = zeros(U * R, U * R)
+# BLAS.syrk!('U', 'T', 1.0, Ux, 0.0, res)
+# @test Symmetric(res) ≈ Symmetric(PLmabdaP(F, ipwt, R, L, U))
 
 
 inv_hess_prod!(prod::AbstractArray{Float64}, arr::AbstractArray{Float64}, cone::WSOSPolyInterpMat) = mul!(prod, Symmetric(cone.Hi, :U), arr)
