f6b914cb9e05a164c7b0621d1884396285ddc54d
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-04-08 21:53:58 -0400

    test problem
    Former-commit-id: 5c62801826cb507d05966020dbe5d55966023863

--- src/ModelUtilities/decompositions.jl
+++ src/ModelUtilities/decompositions.jl
@@ -4,22 +4,23 @@ Copyright 2018, Chris Coey, Lea Kapelevich and contributors
 
 import JuMP
 import DynamicPolynomials
+DP = DynamicPolynomials
 import Combinatorics
 
 function get_decomposition(
-        primal_var,
         constraint,
         lambda_oracle,
         hessian_oracle,
         n,
         d,
         )
-    s = JuMP.value(primal_var)
-    x = JuMP.dual(constraint)
-    w = hessian_oracle(x) \ s
-    lambda_inv = inv(lambda(x))
-    S = lambda_inv * lambda_oracle(w) * lambda_inv
-    f = cholesky(f)
+    s = JuMP.value(constraint)
+    x = JuMP.dual(constraint) # check sign
+    w = Symmetric(hessian_oracle(x), :U) \ s
+    lambda_inv = inv(Symmetric(lambda_oracle(x), :U))
+    @show eigen(lambda_oracle(w)).values
+    S = Symmetric(lambda_inv * lambda_oracle(w) * lambda_inv, :U)
+    f = cholesky(S)
     basis = build_basis(n, d)
     decomposition = f.U * basis
     return decomposition
@@ -30,7 +31,7 @@ function calc_u(d, monovec)
     u = Vector{Vector}(undef, n) # TODO type properly
     for j in 1:n
         uj = u[j] = Vector(undef, d+1) # TODO type properly
-        uj[1] = Monomial(1)
+        uj[1] = DP.Monomial(1)
         uj[2] = monovec[j]
         for t in 3:(d + 1)
             uj[t] = 2.0 * uj[2] * uj[t-1] - uj[t-2]
@@ -41,13 +42,13 @@ end
 
 # returns the basis dynamic polynomials
 function build_basis(n, d)
-    @polyvar x[1:n]
+    DP.@polyvar x[1:n]
     u = calc_u(d, x)
     L = binomial(n + d, n)
     m = Vector{Float64}(undef, L)
     m[1] = 2^n
     M = Vector(undef, L)
-    M[1] = Monomial(1)
+    M[1] = DP.Monomial(1)
 
     col = 1
     for t in 1:d
@@ -66,3 +67,50 @@ function build_basis(n, d)
     end
     return M
 end
+
+
+using Test
+import Hypatia
+HYP = Hypatia
+import JuMP
+using PolyJuMP
+import DynamicPolynomials
+DP = DynamicPolynomials
+MU = HYP.ModelUtilities
+using LinearAlgebra
+
+n = 2
+DP.@polyvar x[1:n]
+DP.@polyvar y[1:n]
+d = 3
+monos = PolyJuMP.monomials([x; y], 0:d)
+random_poly = JuMP.dot(rand(length(monos)), monos)
+random_poly_sqr = random_poly^2
+(U, pts, P0, _, _) = MU.interpolate(MU.FreeDomain(2n), d, sample_factor = 20, sample = true)
+model = JuMP.Model(JuMP.with_optimizer(HYP.Optimizer, verbose = true))
+cone = HYP.WSOSPolyInterpCone(U, [P0])
+sqrconstr = JuMP.@constraint(model, [random_poly_sqr(pts[u, :]) for u in 1:U] in cone)
+JuMP.optimize!(model)
+
+model = JuMP.Model(JuMP.with_optimizer(HYP.Optimizer, verbose = true))
+cone = HYP.WSOSPolyInterpCone(U, [P0])
+JuMP.@variable(model, dummy[1:U])
+sqrconstr2 = JuMP.@constraint(model, dummy in cone)
+JuMP.optimize!(model)
+
+lambda_oracle(point) = Symmetric(P0' * Diagonal(point) * P0, :U)
+function hessian_oracle(point)
+    cone = model.moi_backend.optimizer.model.optimizer.cones[1]
+    cone.point .= point
+    HYP.Cones.check_in_cone(cone)
+    return cone.H
+end
+
+get_decomposition(sqrconstr, lambda_oracle, hessian_oracle, 2n, d)
+
+
+
+
+
+# conobj = JuMP.constraint_object(sqrconstr)
+# @show random_poly
