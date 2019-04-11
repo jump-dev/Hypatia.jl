2f22152027e32abdcb0b988c754c0a97583a8be4
Chris Coey <coey.chris@gmail.com>
2019-04-10 20:02:19 -0400

    more trials

--- examples/complexsos/interp2.jl
+++ examples/complexsos/interp2.jl
@@ -8,12 +8,14 @@ d = 5
 
 # full vandermonde
 V_basis = [x -> x^i * conj(x)^j for j in 0:d for i in 0:d] # TODO columns are dependent if not doing j in 0:i
+# V_basis = [x -> x^i * conj(x)^j for j in 0:d for i in 0:j]
 U = length(V_basis)
 # @show U
 # @show div(d * (d + 1), 2)
-points = randn(ComplexF64, (d + 1)^2) # TODO try roots of unity
+points = 2 * rand(ComplexF64, U) .- (1 + im)# TODO try roots of unity
 V = [b(p) for p in points, b in V_basis]
 # @show rank(V)
+@test rank(V) == U
 
 # # points are the roots of unity
 # points = [cospi(2k / U) + sinpi(2k / U) * im for k = 0:(U - 1)]
@@ -24,7 +26,8 @@ V = [b(p) for p in points, b in V_basis]
 
 # rand real poly coefs
 fh = randn(ComplexF64, d + 1, d + 1)
-f = Hermitian(fh * fh')
+# f = Hermitian(fh * fh')
+f = Hermitian(fh)
 
 # values at points given coefs
 vals = [sum(f[i+1, j+1] * p^i * conj(p)^j for i in 0:d, j in 0:d) for p in points]
@@ -36,11 +39,29 @@ fvec = vec(f)
 # from values at points, recover coefs
 test_coefs = V \ vals
 @test test_coefs ≈ fvec
+@show fvec
+@show test_coefs
 
+# Vi = inv(V)
+# Vi' *
 
-y = randn((d + 1)^2)
+# P'*P
+#
+# P*P'
 
-P = V[:, 1:d+1] # [1, z, ..., z^d]
-@test ishermitian(P' * Diagonal(y) * P)
-Lam = Hermitian(P' * Diagonal(y) * P)
-@show isposdef(Lam)
+# y = randn((d + 1)^2)
+#
+# P = V[:, 1:d+1] # [1, z, ..., z^d]
+# # @test ishermitian(P' * Diagonal(y) * P)
+# Lam = Hermitian(P' * Diagonal(y) * P)
+# @show norm(Lam - P' * Diagonal(y) * P)
+# @show isposdef(Lam)
+#
+# Lam = P' * Diagonal(y) * P
+# Lam2 = transpose(P) * Diagonal(y) * conj(P)
+#
+# println(Lam)
+# println(Lam2)
+# @show norm(Lam - Lam')
+
+;
