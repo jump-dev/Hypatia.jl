6628a594410cad5c4a4114cafa100eae91821cb9
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-04-22 18:23:24 -0400

    analytic gradient and hessian for spectral norm cone

--- src/Cones/epinormspectral.jl
+++ src/Cones/epinormspectral.jl
@@ -63,10 +63,64 @@ function check_in_cone(cone::EpiNormSpectral)
         return false
     end
 
+    # -logdet(u * I - W * W' / u) - log(u)
+    n = cone.n
+    m = cone.m
+    W = reshape(cone.point[2:end], cone.n, cone.m)
+    u = cone.point[1]
+    ui = 1 / u
+    X = Symmetric(W * W')
+    Z = Symmetric(u * I - X * ui)
+    Zi = inv(Z)
+    Eu = I + X * ui^2
+    cone.g[1] = -sum(Zi .* Eu) - ui
+    cone.g[2:end] = vec(2 * Zi * W * ui)
+
+    H11 = sum((Zi * Eu * Zi) .* Eu) + sum(-Zi .* X * (-2 * inv(u^3))) + inv(u^2)
+    H1W = -2 * Zi * Eu * Zi * W * inv(u) - 2 * Zi * W * inv(u^2) # correct
+    HWW = zeros(cone.m * cone.n, cone.m * cone.n)
+    function dzdw(k, l)
+        ret = zeros(cone.n, cone.n)
+        ret[k, :] -= -inv(u) * W[:, l]
+        ret[:, k] -= -inv(u) * W[:, l]
+        return ret
+    end
+    function d2zdw2(ki, li, mi, ni)
+        ret = zeros(cone.n, cone.n)
+        if li == ni
+            ret[ki, mi] -= inv(u)
+            ret[mi, ki] -= inv(u)
+        end
+        return ret
+    end
+    idx1 = 0
+    for j in 1:m, i in 1:n
+        idx1 += 1
+        idx2 = 0
+        for l in 1:m, k in 1:n
+            idx2 += 1
+            HWW[idx1, idx2] = sum((Zi * dzdw(i, j) * Zi)' .* dzdw(k, l)) - sum(Zi .* d2zdw2(i, j, k, l))
+        end
+    end
+
+    # HWW = 2 * inv(u) * (Zi .* Zi .* )
+
     # TODO check allocations, check with Jarrett if this is most efficient way to use DiffResults
-    cone.diffres = ForwardDiff.hessian!(cone.diffres, cone.barfun, cone.point)
-    cone.g .= DiffResults.gradient(cone.diffres)
-    cone.H .= DiffResults.hessian(cone.diffres)
+    # cone.diffres = ForwardDiff.hessian!(cone.diffres, cone.barfun, cone.point)
+    # cone.g .= DiffResults.gradient(cone.diffres) # [g1; gW...]
+    # cone.H .= DiffResults.hessian(cone.diffres)
+    # cone.g = [g1; gW...]
+    cone.H = [H11 vec(H1W)'; vec(H1W) HWW]
+
+
+    # @show H11
+    # @show cone.H[1, 1] / H11
+    # if !isapprox(cone.H, H)
+    #     # @show HWW
+    #     # @show cone.H[2:end, 2:end]
+    #     @show cone.H[2:end, 2:end] ./ HWW
+    # end
+    @assert isapprox(cone.H * cone.point, -cone.g, atol = 1e-7, rtol = 1e-7)
 
     return factorize_hess(cone)
 end
