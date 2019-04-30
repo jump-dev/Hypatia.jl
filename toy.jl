98f79a7801c0a0bbf6193e8a0f25b6ae263aa8fe
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-04-30 19:23:31 -0400

    recover the sums of squares
    Former-commit-id: ca95962af2c977933c4e49a4cb017cb20d795b78

--- toy.jl
+++ toy.jl
@@ -7,6 +7,7 @@ const MO = Hypatia.Models
 const CO = Hypatia.Cones
 const SO = Hypatia.Solvers
 using LinearAlgebra
+using DynamicPolynomials
 Random.seed!(1234)
 
 motzkin = ((x,y) -> 1-48x^2*y^2+64x^2*y^4+64x^4*y^2)
@@ -43,6 +44,7 @@ g1vec = [sqrt(g1(pts1[i, :])) for i in 1:U]
 L1 = binomial(n + d - 1, n)
 P1 = Diagonal(g1vec) * M[keep_pts, 1:L1]
 
+# either this
 pts2 = zeros(size(pts_kept))
 pts2 .= pts_kept
 pts2[:, 2] = pts_kept[:, 2] * (ubs[2] - lbs[2]) / 2
@@ -65,4 +67,40 @@ s2 = solver.point.s
 z2 = solver.point.z
 # @test SO.get_status(solver) == :Optimal
 
-# make P0 from pts
+
+
+
+
+lagrange_polys = MU.recover_lagrange_polys(pts_kept, deg)
+dot(lagrange_polys, solver.point.s) # yep motzkin
+@polyvar x[1:n]
+
+ds = [3; 2; 2]
+cheb_polys = [MU.get_chebyshev_polys(x, d) for d in ds]
+
+get_lambda(pt, P) = P' * Diagonal(pt) * P
+ipwt = [P0, P1, P2]
+sprimal = solver.point.s
+sdual = solver.point.z
+cone = cones[1]
+cone.point .= sdual
+CO.check_in_cone(cone)
+H = Symmetric(cones[1].H, :U)
+nwts = 3
+w = H \ sprimal
+gram_matrices = Vector{Matrix{Float64}}(undef, nwts)
+for p in 1:nwts
+    lambda_inv = inv(Symmetric(get_lambda(sdual, ipwt[p]), :U))
+    lambdaw = get_lambda(w, ipwt[p]) #+ 1e-6I
+    S = Symmetric(lambda_inv * lambdaw * lambda_inv, :U)
+    gram_matrices[p] = S
+end
+weight_funs = [
+    1;
+    (ubs[1] - x[1] * (ubs[1] - lbs[1]) / 2) * (x[1] * (ubs[1] - lbs[1]) / 2 - lbs[1]);
+    (ubs[2] - x[2]) * (x[2] - lbs[2])
+    ]
+# weight_funs = [1; (ubs[1] - x[1]) * (x[1] - lbs[1]); (ubs[2] - x[2]) * (x[2] - lbs[2])]
+@show sum(cheb_polys[p]' * gram_matrices[p] * cheb_polys[p] * weight_funs[p] for p in 1:nwts)
+
+;
