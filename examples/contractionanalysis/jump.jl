ac005d91cadad91b927a82736c85e3eb09658d77
Chris Coey <coey.chris@gmail.com>
2019-05-12 16:19:35 -0400

    simplify contractionanalysis
    Former-commit-id: fe218e673ff00e763b3d8d645b2c3ba0ec7c3418

--- examples/contractionanalysis/jump.jl
+++ examples/contractionanalysis/jump.jl
@@ -26,14 +26,13 @@ using Test
 
 const rt2 = sqrt(2)
 
-# dynamics according to the Moore-Greitzer model
-function build_dynamics(x)
-    dx1dt = -x[2] - 1.5 * x[1]^2 - 0.5 * x[1]^3
-    dx2dt = 3 * x[1] - x[2]
-    return [dx1dt; dx2dt]
-end
-
-function build_contraction_JuMP(model::JuMP.Model, beta::Float64, deg_M::Int, delta::Float64 = 1e-3; use_wsos::Bool = true)
+function contraction_JuMP(
+    beta::Float64,
+    deg_M::Int,
+    delta::Float64 = 1e-3;
+    use_wsos::Bool = true,
+    use_dense::Bool = true,
+    )
     n = 2
     dom = MU.FreeDomain(n)
 
@@ -43,13 +42,18 @@ function build_contraction_JuMP(model::JuMP.Model, beta::Float64, deg_M::Int, de
 
     polyjump_basis = PJ.FixedPolynomialBasis(lagrange_polys)
     x = DP.variables(lagrange_polys[1])
-    dynamics = build_dynamics(x)
 
+    # dynamics according to the Moore-Greitzer model
+    dx1dt = -x[2] - 1.5 * x[1]^2 - 0.5 * x[1]^3
+    dx2dt = 3 * x[1] - x[2]
+    dynamics = [dx1dt; dx2dt]
+
+    model = JuMP.Model(JuMP.with_optimizer(Hypatia.Optimizer, verbose = true, tol_feas = 1e-4, tol_rel_opt = 1e-6, tol_abs_opt = 1e-6, use_dense = use_dense))
     JuMP.@variable(model, polys[1:3], PJ.Poly(polyjump_basis))
 
-    dfdx = DP.differentiate(dynamics, x)'
     M = [polys[1] polys[2]; polys[2] polys[3]]
     dMdt = [JuMP.dot(DP.differentiate(M[i, j], x), dynamics) for i in 1:n, j in 1:n]
+    dfdx = DP.differentiate(dynamics, x)'
     Mdfdx = [sum(M[i, k] * dfdx[k, j] for k in 1:n) for i in 1:n, j in 1:n]
     R = Mdfdx + Mdfdx' + dMdt + beta * M
 
@@ -58,37 +62,25 @@ function build_contraction_JuMP(model::JuMP.Model, beta::Float64, deg_M::Int, de
         d_R = div(deg_R + 1, 2)
         (U_R, pts_R, P0_R, _, _) = MU.interpolate(dom, d_R, sample = true)
         JuMP.@constraint(model, [M[i, j](pts_M[u, :]) * (i == j ? 1.0 : rt2) - (i == j ? delta : 0.0) for i in 1:n for j in 1:i for u in 1:U_M] in HYP.WSOSPolyInterpMatCone(n, U_M, [P0_M]))
-        JuMP.@constraint(model, [-1 * R[i, j](pts_R[u, :]) * (i == j ? 1.0 : rt2) - (i == j ? delta : 0.0) for i in 1:n for j in 1:i for u in 1:U_R] in HYP.WSOSPolyInterpMatCone(n, U_R, [P0_R]))
+        JuMP.@constraint(model, [-R[i, j](pts_R[u, :]) * (i == j ? 1.0 : rt2) - (i == j ? delta : 0.0) for i in 1:n for j in 1:i for u in 1:U_R] in HYP.WSOSPolyInterpMatCone(n, U_R, [P0_R]))
     else
         PJ.setpolymodule!(model, SumOfSquares)
-        JuMP.@constraint(model, M - delta * Matrix{Float64}(I, n, n) in JuMP.PSDCone())
-        JuMP.@constraint(model, -R - delta * Matrix{Float64}(I, n, n) in JuMP.PSDCone())
+        JuMP.@constraint(model, M - Matrix(delta * I, n, n) in JuMP.PSDCone())
+        JuMP.@constraint(model, -R - Matrix(delta * I, n, n) in JuMP.PSDCone())
     end
 
     return model
 end
 
-function contraction_JuMP(beta::Float64, deg_M::Int; use_wsos::Bool = true, use_dense::Bool = true)
-    model = JuMP.Model(JuMP.with_optimizer(Hypatia.Optimizer, verbose = true, tol_feas = 1e-4, tol_rel_opt = 1e-6, tol_abs_opt = 1e-6, use_dense = use_dense))
-    if !use_wsos
-        PJ.setpolymodule!(model, SumOfSquares)
-    end
-    return build_contraction_JuMP(model, beta, deg_M, use_wsos = use_wsos)
-end
-contraction1_JuMP(; use_dense::Bool = true) = contraction_JuMP(0.77, 4, use_wsos = true, use_dense = use_dense)
-contraction2_JuMP(; use_dense::Bool = true) = contraction_JuMP(0.77, 4, use_wsos = false, use_dense = use_dense)
+contraction1_JuMP(; use_dense::Bool = true) = contraction_JuMP(0.77, 4, 1e-3, use_wsos = true, use_dense = use_dense)
+contraction2_JuMP(; use_dense::Bool = true) = contraction_JuMP(0.77, 4, 1e-3, use_wsos = false, use_dense = use_dense)
 
 function test_contraction_JuMP(instance::Function)
     model = instance()
     JuMP.optimize!(model)
-    term_status = JuMP.termination_status(model)
-    pr_status = JuMP.primal_status(model)
-    du_status = JuMP.dual_status(model)
-
-    @test term_status == MOI.OPTIMAL
-    @test pr_status == MOI.FEASIBLE_POINT
-    @test du_status == MOI.FEASIBLE_POINT
-
+    @test JuMP.termination_status(model) == MOI.OPTIMAL
+    @test JuMP.primal_status(model) == MOI.FEASIBLE_POINT
+    @test JuMP.dual_status(model) == MOI.FEASIBLE_POINT
     return
 end
 
