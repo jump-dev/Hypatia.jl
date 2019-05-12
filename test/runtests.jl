14de6926b5e19bd9baf07f2bebf74c8661155f1a
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-05-11 22:19:45 -0400

    refactor shapeconregr in the same way as polymin

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,6 +24,7 @@ include(joinpath(examples_dir, "envelope/native.jl"))
 include(joinpath(examples_dir, "linearopt/native.jl"))
 include(joinpath(examples_dir, "polymin/real.jl"))
 include(joinpath(examples_dir, "polymin/complex.jl"))
+include(joinpath(examples_dir, "polymin/instances.jl"))
 
 include(joinpath(@__DIR__, "native.jl"))
 
@@ -33,7 +34,6 @@ include(joinpath(examples_dir, "envelope/jump.jl"))
 include(joinpath(examples_dir, "expdesign/jump.jl"))
 include(joinpath(examples_dir, "polymin/jump.jl"))
 include(joinpath(examples_dir, "shapeconregr/jump.jl"))
-include(joinpath(examples_dir, "shapeconregr/instances.jl"))
 include(joinpath(examples_dir, "densityest/jump.jl"))
 include(joinpath(examples_dir, "wsosmatrix/sosmatrix.jl"))
 include(joinpath(examples_dir, "wsosmatrix/muconvexity.jl"))
@@ -139,64 +139,64 @@ include(joinpath(@__DIR__, "JuMP.jl"))
 #     t(s, m, verbose)
 # end
 
-@info("starting default native examples tests")
-testfuns = [
-    run_envelope_primal_dense,
-    run_envelope_dual_dense,
-    run_envelope_primal_sparse,
-    run_envelope_dual_sparse,
-    run_linearopt,
-    run_polymin,
-    run_complexpolymin_primal,
-    run_complexpolymin_dual,
-    ]
-@testset "default examples: $t" for t in testfuns
-    t()
-end
+# @info("starting default native examples tests")
+# testfuns = [
+#     run_envelope_primal_dense,
+#     run_envelope_dual_dense,
+#     run_envelope_primal_sparse,
+#     run_envelope_dual_sparse,
+#     run_linearopt,
+#     run_polymin,
+#     run_complexpolymin_primal,
+#     run_complexpolymin_dual,
+#     ]
+# @testset "default examples: $t" for t in testfuns
+#     t()
+# end
 
-@info("starting additional native examples tests")
-verbose = true
-system_solvers = [
-    # SO.NaiveCombinedHSDSystemSolver,
-    SO.QRCholCombinedHSDSystemSolver,
-    ]
-linear_models = [
-    # MO.RawLinearModel,
-    MO.PreprocessedLinearModel,
-    ]
-testfuns = [
-    # TODO test primal and dual formulations of envelope
-    envelope1,
-    envelope2,
-    envelope3,
-    envelope4,
-    linearopt1,
-    linearopt2,
-    polymin1,
-    polymin2,
-    polymin3,
-    polymin4,
-    polymin5,
-    polymin6,
-    polymin7,
-    polymin8,
-    polymin9,
-    polymin10,
-    polymin11,
-    complexpolymin1,
-    complexpolymin2,
-    complexpolymin3,
-    complexpolymin4,
-    complexpolymin5,
-    complexpolymin6,
-    complexpolymin7,
-    ]
-@testset "native examples: $t, $s, $m" for t in testfuns, s in system_solvers, m in linear_models
-    if s == SO.QRCholCombinedHSDSystemSolver && m == MO.RawLinearModel
-        continue # QRChol linear system solver needs preprocessed model
-    end
-    t(s, m, verbose)
-end
+# @info("starting additional native examples tests")
+# verbose = true
+# system_solvers = [
+#     # SO.NaiveCombinedHSDSystemSolver,
+#     SO.QRCholCombinedHSDSystemSolver,
+#     ]
+# linear_models = [
+#     # MO.RawLinearModel,
+#     MO.PreprocessedLinearModel,
+#     ]
+# testfuns = [
+#     # TODO test primal and dual formulations of envelope
+#     envelope1,
+#     envelope2,
+#     envelope3,
+#     envelope4,
+#     linearopt1,
+#     linearopt2,
+#     polymin1,
+#     polymin2,
+#     polymin3,
+#     polymin4,
+#     polymin5,
+#     polymin6,
+#     polymin7,
+#     polymin8,
+#     polymin9,
+#     polymin10,
+#     polymin11,
+#     test_complexpolymin1,
+#     test_complexpolymin2,
+#     test_complexpolymin3,
+#     test_complexpolymin4,
+#     test_complexpolymin5,
+#     test_complexpolymin6,
+#     test_complexpolymin7,
+#     ]
+# @testset "native examples: $t, $s, $m" for t in testfuns, s in system_solvers, m in linear_models
+#     if s == SO.QRCholCombinedHSDSystemSolver && m == MO.RawLinearModel
+#         continue # QRChol linear system solver needs preprocessed model
+#     end
+#     t(s, m, verbose)
+# end
 
 # @info("starting MathOptInterface tests")
 # verbose = false
@@ -211,70 +211,47 @@ end
 #     test_moi(d, s, m, verbose)
 # end
 
-@info("starting default JuMP examples tests")
-testfuns = [
-    run_JuMP_envelope_boxinterp,
-    run_JuMP_envelope_sampleinterp_box,
-    run_JuMP_envelope_sampleinterp_ball,
-    run_JuMP_expdesign,
-    # run_JuMP_polymin_PSD, # too slow TODO check: final objective doesn't match
-    run_JuMP_polymin_WSOS_primal,
-    run_JuMP_polymin_WSOS_dual,
-    run_JuMP_shapeconregr_PSD,
-    run_JuMP_shapeconregr_WSOS,
-    run_JuMP_shapeconregr_WSOS_PolyJuMP,
-    run_JuMP_densityest,
-    run_JuMP_sosmatrix_rand,
-    run_JuMP_sosmatrix_a,
-    run_JuMP_sosmatrix_poly_a,
-    run_JuMP_sosmatrix_poly_b,
-    run_JuMP_muconvexity_rand,
-    run_JuMP_muconvexity_a,
-    run_JuMP_muconvexity_b,
-    run_JuMP_muconvexity_c,
-    run_JuMP_muconvexity_d,
-    run_JuMP_sosmat1,
-    run_JuMP_sosmat2_scalar,
-    run_JuMP_sosmat2_matrix,
-    run_JuMP_sosmat2_matrix_dual,
-    run_JuMP_sosmat3_primal, # numerically unstable
-    run_JuMP_sosmat3_dual, # numerically unstable
-    run_JuMP_univariate_roa_WSOS,
-    run_JuMP_univariate_roa_PSD,
-    run_JuMP_contraction_PSD,
-    run_JuMP_contraction_WSOS,
-    ]
-@testset "default examples: $t" for t in testfuns
-    t()
-end
+# @info("starting default JuMP examples tests")
+# testfuns = [
+#     run_JuMP_envelope_boxinterp,
+#     run_JuMP_envelope_sampleinterp_box,
+#     run_JuMP_envelope_sampleinterp_ball,
+#     run_JuMP_expdesign,
+#     # run_JuMP_polymin_PSD, # too slow TODO check: final objective doesn't match
+#     run_JuMP_polymin_WSOS_primal,
+#     run_JuMP_polymin_WSOS_dual,
+#     run_JuMP_shapeconregr_PSD,
+#     run_JuMP_shapeconregr_WSOS,
+#     run_JuMP_shapeconregr_WSOS_PolyJuMP,
+#     run_JuMP_densityest,
+#     run_JuMP_sosmatrix_rand,
+#     run_JuMP_sosmatrix_a,
+#     run_JuMP_sosmatrix_poly_a,
+#     run_JuMP_sosmatrix_poly_b,
+#     run_JuMP_muconvexity_rand,
+#     run_JuMP_muconvexity_a,
+#     run_JuMP_muconvexity_b,
+#     run_JuMP_muconvexity_c,
+#     run_JuMP_muconvexity_d,
+#     run_JuMP_sosmat1,
+#     run_JuMP_sosmat2_scalar,
+#     run_JuMP_sosmat2_matrix,
+#     run_JuMP_sosmat2_matrix_dual,
+#     run_JuMP_sosmat3_primal, # numerically unstable
+#     run_JuMP_sosmat3_dual, # numerically unstable
+#    # run_JuMP_univariate_roa_WSOS,
+#    # run_JuMP_univariate_roa_PSD,
+#    # run_JuMP_contraction_PSD,
+#    # run_JuMP_contraction_WSOS,
+#     ]
+# @testset "default examples: $t" for t in testfuns
+#     t()
+# end
 
 @info("starting additional JuMP examples tests")
 testfuns = [
-    polymin1_JuMP,
-    polymin2_JuMP,
-    polymin3_JuMP,
-    polymin4_JuMP,
-    polymin5_JuMP,
-    polymin6_JuMP,
-    polymin7_JuMP,
-    polymin8_JuMP,
-    polymin9_JuMP,
-    polymin10_JuMP,
-    test_JuMP_shapeconregr1,
-    test_JuMP_shapeconregr2,
-    test_JuMP_shapeconregr3_JuMP,
-    test_JuMP_shapeconregr4_JuMP,
-    test_JuMP_shapeconregr5_JuMP,
-    test_JuMP_shapeconregr6_JuMP,
-    test_JuMP_shapeconregr7_JuMP,
-    test_JuMP_shapeconregr8_JuMP,
-    test_JuMP_shapeconregr9_JuMP,
-    test_JuMP_shapeconregr10_JuMP,
-    test_JuMP_shapeconregr11_JuMP,
-    test_JuMP_shapeconregr12_JuMP, # numerically unstable
-    test_JuMP_shapeconregr13_JuMP,
-    # test_JuMP_shapeconregr14_JuMP, # throws out-of-memory error
-    # test_JuMP_shapeconregr15_JuMP, # throws out-of-memory error
+    test_polyminj_many,
+    test_shapeconregrj_many,
     ]
 @testset "JuMP examples: $t" for t in testfuns
     t()
