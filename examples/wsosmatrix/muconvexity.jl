4be36bf2bab01ca0f159f3cbf3526e2dbf971fbd
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-05-13 14:20:24 -0400

    refactor muconvexity examples

--- examples/wsosmatrix/muconvexity.jl
+++ examples/wsosmatrix/muconvexity.jl
@@ -16,6 +16,7 @@ const MOI = MathOptInterface
 import JuMP
 import MultivariatePolynomials
 import DynamicPolynomials
+const DP = DynamicPolynomials
 import SumOfSquares
 import PolyJuMP
 using Test
@@ -23,10 +24,18 @@ import Random
 
 const rt2 = sqrt(2)
 
-function run_JuMP_muconvexity(x::Vector, poly, dom, use_wsos::Bool)
-    Random.seed!(1)
+function muconvexity_JuMP(
+    polyfun::Function,
+    dom::MU.Domain;
+    use_wsos::Bool = true,
+    rseed::Int = 1,
+    )
+    Random.seed!(rseed)
 
-    model = JuMP.Model(JuMP.with_optimizer(HYP.Optimizer, verbose = true))
+    n = MU.get_dimension(dom)
+    DynamicPolynomials.@polyvar x[1:n]
+    poly = polyfun(x)
+    model = JuMP.Model()
     JuMP.@variable(model, mu)
     JuMP.@objective(model, Max, mu)
 
@@ -36,7 +45,7 @@ function run_JuMP_muconvexity(x::Vector, poly, dom, use_wsos::Bool)
     if use_wsos
         n = DynamicPolynomials.nvariables(x)
         d = div(maximum(DynamicPolynomials.maxdegree.(H)) + 1, 2)
-        (U, pts, P0, PWts, _) = MU.interpolate(dom, d, sample = true)
+        (U, pts, P0, PWts, _) = MU.interpolate(dom, d, sample = true, sample_factor = 200)
         mat_wsos_cone = HYP.WSOSPolyInterpMatCone(n, U, [P0, PWts...])
 
         JuMP.@constraint(model, [H[i, j](x => pts[u, :]) * (i == j ? 1.0 : rt2) for i in 1:n for j in 1:i for u in 1:U] in mat_wsos_cone)
@@ -50,85 +59,112 @@ function run_JuMP_muconvexity(x::Vector, poly, dom, use_wsos::Bool)
         end
     end
 
-    JuMP.optimize!(model)
-
-    return (JuMP.termination_status(model), JuMP.primal_status(model), JuMP.value(mu))
+    return (model = model, mu = mu)
 end
 
-function run_JuMP_muconvexity_rand(; rseed::Int = 1)
-    Random.seed!(rseed)
+muconvexity1_JuMP() = muconvexity_JuMP(x -> (x[1] + 1)^2 * (x[1] - 1)^2, MU.FreeDomain(1), use_wsos = true)
+muconvexity2_JuMP() = muconvexity_JuMP(x -> sum(x.^4) - sum(x.^2), MU.FreeDomain(3), use_wsos = true)
+muconvexity3_JuMP() = muconvexity_JuMP(x -> (x[1] + 1)^2 * (x[1] - 1)^2, MU.Box([-1.0], [1.0]), use_wsos = true)
+muconvexity4_JuMP() = muconvexity_JuMP(x -> sum(x.^4) - sum(x.^2), MU.Ball([5.0, 5.0], 1.0), use_wsos = true) # TODO giving incorrect solution
+muconvexity5_JuMP() = muconvexity_JuMP(x -> (x[1] + 1)^2 * (x[1] - 1)^2, MU.FreeDomain(1), use_wsos = false)
+muconvexity6_JuMP() = muconvexity_JuMP(x -> sum(x.^4) - sum(x.^2), MU.FreeDomain(3), use_wsos = false)
+muconvexity7_JuMP() = muconvexity_JuMP(x -> (x[1] + 1)^2 * (x[1] - 1)^2, MU.Box([-1.0], [1.0]), use_wsos = false)
+muconvexity8_JuMP() = muconvexity_JuMP(x -> sum(x.^4) - sum(x.^2), MU.Ball([5.0, 5.0], 1.0), use_wsos = false) # TODO giving incorrect solution
 
-    n = 2
-    d = 4
-    DynamicPolynomials.@polyvar x[1:n]
-
-    poly = sum(rand() * z for z in DynamicPolynomials.monomials(x, 0:d))
-
-    dom = MU.FreeDomain(n)
-    (term1, prim1, mu1) = run_JuMP_muconvexity(x, poly, dom, true)
-    (term2, prim2, mu2) = run_JuMP_muconvexity(x, poly, dom, false)
-    @test term1 == term2
-    @test prim1 == prim2
-    if term1 == MOI.OPTIMAL || term2 == MOI.OPTIMAL
-        @test mu1 ≈ mu2 atol = 1e-4 rtol = 1e-4
-        mufree = mu1
-    else
-        mufree = -Inf
-    end
-
-    dom = MU.Ball(zeros(n), 1.0)
-    (term1, prim1, mu1) = run_JuMP_muconvexity(x, poly, dom, true)
-    (term2, prim2, mu2) = run_JuMP_muconvexity(x, poly, dom, false)
-    @test term1 == term2 == MOI.OPTIMAL
-    @test prim1 == prim2 == MOI.FEASIBLE_POINT
-    @test mu1 ≈ mu2 atol = 1e-4 rtol = 1e-4
-    muball = mu1
-
-    @test mufree - muball <= 1e-4
+function test_muconvexity_JuMP(instance::Tuple{Function,Number}; options)
+    (builder, true_mu) = instance
+    data = builder()
+    JuMP.optimize!(data.model, JuMP.with_optimizer(Hypatia.Optimizer; options...))
+    @test JuMP.value(data.mu) ≈ true_mu atol = 1e-4 rtol = 1e-4
 end
 
-function run_JuMP_muconvexity_a(; use_wsos::Bool = true)
-    DynamicPolynomials.@polyvar x[1:1]
-    poly = (x[1] + 1)^2 * (x[1] - 1)^2
-    dom = MU.FreeDomain(1)
+test_muconvexity_JuMP(; options...) = test_muconvexity_JuMP.([
+    (muconvexity1_JuMP, -4),
+    (muconvexity2_JuMP, -2),
+    (muconvexity3_JuMP, -4),
+    (muconvexity4_JuMP, -2),
+    (muconvexity5_JuMP, -4),
+    (muconvexity6_JuMP, -2),
+    (muconvexity7_JuMP, -4),
+    (muconvexity8_JuMP, -2),
+    ], options = options)
 
-    (term, prim, mu) = run_JuMP_muconvexity(x, poly, dom, use_wsos)
-    @test term == MOI.OPTIMAL
-    @test prim == MOI.FEASIBLE_POINT
-    @test mu ≈ -4 atol = 1e-4 rtol = 1e-4
-end
+# TODO do we want to keep a random example?
+# muconvexity_JuMP() = muconvexity_JuMP(2, sum(rand() * z for z in DynamicPolynomials.monomials(x, 0:4)), MU.FreeDomain(n), true)
+# function run_JuMP_muconvexity_rand(; rseed::Int = 1)
+#     Random.seed!(rseed)
+#
+#     n = 2
+#     d = 4
+#     DynamicPolynomials.@polyvar x[1:n]
+#
+#     poly = sum(rand() * z for z in DynamicPolynomials.monomials(x, 0:d))
+#
+#     dom = MU.FreeDomain(n)
+#     (term1, prim1, mu1) = run_JuMP_muconvexity(x, poly, dom, true)
+#     (term2, prim2, mu2) = run_JuMP_muconvexity(x, poly, dom, false)
+#     @test term1 == term2
+#     @test prim1 == prim2
+#     if term1 == MOI.OPTIMAL || term2 == MOI.OPTIMAL
+#         @test mu1 ≈ mu2 atol = 1e-4 rtol = 1e-4
+#         mufree = mu1
+#     else
+#         mufree = -Inf
+#     end
+#
+#     dom = MU.Ball(zeros(n), 1.0)
+#     (term1, prim1, mu1) = run_JuMP_muconvexity(x, poly, dom, true)
+#     (term2, prim2, mu2) = run_JuMP_muconvexity(x, poly, dom, false)
+#     @test term1 == term2 == MOI.OPTIMAL
+#     @test prim1 == prim2 == MOI.FEASIBLE_POINT
+#     @test mu1 ≈ mu2 atol = 1e-4 rtol = 1e-4
+#     muball = mu1
+#
+#     @test mufree - muball <= 1e-4
+# end
 
-function run_JuMP_muconvexity_b(; use_wsos::Bool = true)
-    n = 3
-    DynamicPolynomials.@polyvar x[1:n]
-    poly = sum(x.^4) - sum(x.^2)
-    dom = MU.FreeDomain(n)
+# function run_JuMP_muconvexity_a(; use_wsos::Bool = true)
+#     DynamicPolynomials.@polyvar x[1:1]
+#     poly = (x[1] + 1)^2 * (x[1] - 1)^2
+#     dom = MU.FreeDomain(1)
+#
+#     (term, prim, mu) = run_JuMP_muconvexity(x, poly, dom, use_wsos)
+#     @test term == MOI.OPTIMAL
+#     @test prim == MOI.FEASIBLE_POINT
+#     @test mu ≈ -4 atol = 1e-4 rtol = 1e-4
+# end
 
-    (term, prim, mu) = run_JuMP_muconvexity(x, poly, dom, use_wsos)
-    @test term == MOI.OPTIMAL
-    @test prim == MOI.FEASIBLE_POINT
-    @test mu ≈ -2 atol = 1e-4 rtol = 1e-4
-end
-
-function run_JuMP_muconvexity_c(; use_wsos::Bool = true)
-    DynamicPolynomials.@polyvar x[1:1]
-    poly = (x[1] + 1)^2 * (x[1] - 1)^2
-    dom = MU.Box([-1.0], [1.0])
-
-    (term, prim, mu) = run_JuMP_muconvexity(x, poly, dom, use_wsos)
-    @test term == MOI.OPTIMAL
-    @test prim == MOI.FEASIBLE_POINT
-    @test mu ≈ -4 atol = 1e-4 rtol = 1e-4
-end
-
-function run_JuMP_muconvexity_d(; use_wsos::Bool = true)
-    n = 3
-    DynamicPolynomials.@polyvar x[1:n]
-    poly = sum(x.^4) - sum(x.^2)
-    dom = MU.Ball([5.0, 5.0], 1.0)
-
-    (term, prim, mu) = run_JuMP_muconvexity(x, poly, dom, use_wsos)
-    @test term == MOI.OPTIMAL
-    @test prim == MOI.FEASIBLE_POINT
-    @test mu ≈ -2 atol = 1e-4 rtol = 1e-4
-end
+# function run_JuMP_muconvexity_b(; use_wsos::Bool = true)
+#     n = 3
+#     DynamicPolynomials.@polyvar x[1:n]
+#     poly = sum(x.^4) - sum(x.^2)
+#     dom = MU.FreeDomain(n)
+#
+#     (term, prim, mu) = run_JuMP_muconvexity(x, poly, dom, use_wsos)
+#     @test term == MOI.OPTIMAL
+#     @test prim == MOI.FEASIBLE_POINT
+#     @test mu ≈ -2 atol = 1e-4 rtol = 1e-4
+# end
+#
+# function run_JuMP_muconvexity_c(; use_wsos::Bool = true)
+#     DynamicPolynomials.@polyvar x[1:1]
+#     poly = (x[1] + 1)^2 * (x[1] - 1)^2
+#     dom = MU.Box([-1.0], [1.0])
+#
+#     (term, prim, mu) = run_JuMP_muconvexity(x, poly, dom, use_wsos)
+#     @test term == MOI.OPTIMAL
+#     @test prim == MOI.FEASIBLE_POINT
+#     @test mu ≈ -4 atol = 1e-4 rtol = 1e-4
+# end
+#
+# function run_JuMP_muconvexity_d(; use_wsos::Bool = true)
+#     n = 3 # why was this working? the ball had dimension 2
+#     DynamicPolynomials.@polyvar x[1:n]
+#     poly = sum(x.^4) - sum(x.^2)
+#     dom = MU.Ball([5.0, 5.0], 1.0)
+#
+#     (term, prim, mu) = run_JuMP_muconvexity(x, poly, dom, use_wsos)
+#     @test term == MOI.OPTIMAL
+#     @test prim == MOI.FEASIBLE_POINT
+#     @test mu ≈ -2 atol = 1e-4 rtol = 1e-4
+# end
