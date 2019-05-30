8c3fe3a8adfd9cd128c5a4bb53aa55052251627a
Chris Coey <coey.chris@gmail.com>
2019-05-30 19:43:44 -0400

    update more examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,21 +18,21 @@ const MU = HYP.ModelUtilities
 # include(joinpath(@__DIR__, "MathOptInterface.jl"))
 
 examples_dir = joinpath(@__DIR__, "../examples")
-# include(joinpath(examples_dir, "envelope/native.jl"))
-# include(joinpath(examples_dir, "linearopt/native.jl"))
-# include(joinpath(examples_dir, "polymin/native.jl"))
-# include(joinpath(examples_dir, "contraction/JuMP.jl"))
-# include(joinpath(examples_dir, "densityest/JuMP.jl"))
+include(joinpath(examples_dir, "envelope/native.jl"))
+include(joinpath(examples_dir, "linearopt/native.jl"))
+include(joinpath(examples_dir, "polymin/native.jl"))
+include(joinpath(examples_dir, "contraction/JuMP.jl"))
+include(joinpath(examples_dir, "densityest/JuMP.jl"))
 include(joinpath(examples_dir, "envelope/JuMP.jl"))
 include(joinpath(examples_dir, "expdesign/JuMP.jl"))
-# include(joinpath(examples_dir, "lotkavolterra/JuMP.jl"))
+include(joinpath(examples_dir, "lotkavolterra/JuMP.jl"))
 include(joinpath(examples_dir, "muconvexity/JuMP.jl"))
 include(joinpath(examples_dir, "polymin/JuMP.jl"))
-# include(joinpath(examples_dir, "polynorm/JuMP.jl"))
-# include(joinpath(examples_dir, "regionofattr/JuMP.jl"))
-# include(joinpath(examples_dir, "secondorderpoly/JuMP.jl"))
-# include(joinpath(examples_dir, "shapeconregr/JuMP.jl"))
-# include(joinpath(examples_dir, "semidefinitepoly/JuMP.jl"))
+include(joinpath(examples_dir, "polynorm/JuMP.jl"))
+include(joinpath(examples_dir, "regionofattr/JuMP.jl"))
+include(joinpath(examples_dir, "secondorderpoly/JuMP.jl"))
+include(joinpath(examples_dir, "shapeconregr/JuMP.jl"))
+include(joinpath(examples_dir, "semidefinitepoly/JuMP.jl"))
 
 
 # @info("starting Hypatia tests")
@@ -194,9 +194,9 @@ JuMP_options = (
         ) end
     # @testset "polynorm" begin test_polynormJuMP(; JuMP_options...,
     #     ) end
-    # @testset "regionofattr" begin test_regionofattrJuMP(; JuMP_options...,
-    #     tol_abs_opt = 1e-6, tol_rel_opt = 1e-6, tol_feas = 1e-6,
-    #     ) end
+    @testset "regionofattr" begin test_regionofattrJuMP(; JuMP_options...,
+        tol_abs_opt = 1e-6, tol_rel_opt = 1e-6, tol_feas = 1e-6,
+        ) end
     # @testset "secondorderpoly" begin test_secondorderpolyJuMP(; JuMP_options...,
     #     ) end
     # @testset "semidefinitepoly" begin test_semidefinitepolyJuMP(; JuMP_options...,
