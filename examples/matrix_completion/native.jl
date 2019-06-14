09f2044595dee755fd7d51ecc593374244813521
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-06-15 07:56:54 +1200

    try to make matrix building more intuitive

--- examples/matrix_completion/native.jl
+++ examples/matrix_completion/native.jl
@@ -25,6 +25,7 @@ function matrix_completion(
     known_rows::Vector{Int} = Int[],
     known_cols::Vector{Int} = Int[],
     known_vals::Vector{Float64} = Float64[],
+    use_3dim::Bool = false,
     )
 
     if num_known < 0
@@ -49,39 +50,94 @@ function matrix_completion(
     A = zeros(0, dimx)
     b = Float64[]
 
-    # spectral norm cone for entire matrix
-    G1 = zeros(1, dimx) # TODO remove
-    G1[1, 1] = -1
-    G2 = zeros(m * n, dimx)
-    # hypogeomean for values to be filled
-    G3 = zeros(num_unknown + 1, dimx)
+    # epinormspectral cone- get vec(X) in G and h
+    G1 = zeros(m * n, num_unknown)
+    h1 = zeros(m * n)
 
-    h = zeros(m * n + 2 + num_unknown)
-    # first component of the vector in the in geomean cone, elements multiply to one
-    h[m * n + 2] = 1
-
+    is_known = fill(false, n * m)
     for (k, (i, j)) in enumerate(zip(known_rows, known_cols))
         known_idx = cart_to_single(i, j)
-        h[1 + known_idx] = known_vals[k]
+        h1[known_idx] = known_vals[k]
+        is_known[known_idx] = true
     end
 
     total_idx = 1
     unknown_idx = 1
     for j in 1:n, i in 1:m
-        if !((i, j) in known_pairs) # TODO redo this logic and remove helper array
-            G2[total_idx, unknown_idx + 1] = -1
-            G3[unknown_idx + 1, unknown_idx + 1] = -1
+        if !is_known[cart_to_single(i, j)]
+            G1[total_idx, unknown_idx] = -1
             unknown_idx += 1
         end
         total_idx += 1
     end
-    @assert total_idx - 1 == m * n
-    @assert unknown_idx - 1 == num_unknown
 
-    G = vcat(G1, G2, G3)
+    # add first row and column for epigraph variable
+    G1 = [-1 zeros(1, num_unknown); zeros(m * n) G1]
+    h1 = [0, h1...]
 
-    cone_idxs = [1:(m * n + 1), (m * n + 2):(m * n + 2 + num_unknown)]
-    cones = [CO.EpiNormSpectral{Float64}(n, m), CO.HypoGeomean{Float64}(ones(num_unknown) / num_unknown)]
+    cones = CO.Cone[CO.EpiNormSpectral{Float64}(n, m)]
+    cone_idxs = [1:(m * n + 1)]
+
+    if !use_3dim
+        # hypogeomean for values to be filled
+        G2 = zeros(num_unknown + 1, num_unknown + 1)
+        total_idx = 1
+        unknown_idx = 1
+        for j in 1:n, i in 1:m
+            if !is_known[cart_to_single(i, j)]
+                G2[unknown_idx + 1, unknown_idx + 1] = -1
+                unknown_idx += 1
+            end
+            total_idx += 1
+        end
+        # first component of the vector in the in geomean cone, elements multiply to one
+        h2 = [1, zeros(num_unknown)...]
+        h = vcat(h1, h2)
+        @assert total_idx - 1 == m * n
+        @assert unknown_idx - 1 == num_unknown
+
+        G = vcat(G1, G2)
+
+        push!(cone_idxs, (m * n + 2):(m * n + 2 + num_unknown))
+        push!(cones, CO.HypoGeomean{Float64}(ones(num_unknown) / num_unknown))
+    end
+
+    if use_3dim
+        dimx += num_unknown - 2
+        # number of 3-dimensional power cones needed is num_unknown - 1
+        G3 = zeros(3 * (num_unknown - 1), dimx)
+        # first cone is a special case since two of the original variables participate in it
+        # TODO offset columns by 1 due to spectral variable
+        G3[3, 1] = -1
+        G3[2, 2] = -1
+        G3[1, num_unknown + 1] = -1
+        push!(cones, CO.HypoGeomean{Float64}([0.5, 0.5]))
+        push!(cone_idxs, spectral_dim + 1:3)
+        offset = 4
+        # loop over new vars
+        for i in 1:(num_unknown - 3)
+            G3[offset, num_unknown + i + 1] = -1
+            G3[offset + 1, num_unknown + i] = -1
+            G3[offset + 2, i + 2] = -1
+            push!(cones, CO.HypoGeomean{Float64}([(i + 1) / (i + 2), 1 / (i + 2)]))
+            push!(cone_idxs, spectral_dim + (3 * i + 1):(3 * (i + 1)))
+            offset += 3
+        end
+        # last row also special becuase hypograph variable is fixed
+        G3[offset + 2, num_unknown] = -1
+        G3[offset + 1, 2 * num_unknown - 2] = -1
+        @show G3
+        push!(cones, CO.HypoGeomean{Float64}([(num_unknown - 1) / num_unknown, 1 / num_unknown]))
+        push!(cone_idxs, spectral_dim + (num_unknown - 5):(num_unknown - 3))
+        h = vcat(h1, zeros(3 * (num_unknown - 2)), [1, 0, 0])
+        @show h
+
+        G3 = [zeros(3 * (num_unknown - 1)) G3]
+        G = vcat([G1 zeros(1, num_unknown - 1)], [G2, zeros(num_unknown - 1)], G3)
+
+    end
+
+    @show A, G, h cone_idxs, cones
 
     return (c = c, A = A, b = b, G = G, h = h, cones = cones, cone_idxs = cone_idxs)
 end
