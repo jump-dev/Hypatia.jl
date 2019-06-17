45d85e2268d6e3d5b5f7c27fa7b2b2c036c6903b
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-06-18 11:33:39 +1200

    add multiplier constatns to logdet cone, parameterize examples

--- examples/expdesign/native.jl
+++ examples/expdesign/native.jl
@@ -23,35 +23,36 @@ function expdesign(
     n::Int,
     nmax::Int;
     use_logdet::Bool = true,
+    T::DataType = Float64,
     )
     @assert (p > q) && (n > q) && (nmax <= n)
     V = randn(q, p)
 
     # hypograph variable and number of trials of each experiment
-    A = ones(1, p)
-    b = Float64[n]
+    A = ones(T, 1, p)
+    b = T[n]
 
     # nonnegativity
-    G_nonneg = -Matrix{Float64}(I, p, p)
-    h_nonneg = zeros(p)
+    G_nonneg = -Matrix{T}(I, p, p)
+    h_nonneg = zeros(T, p)
     # do <= nmax experiments
-    G_nmax = Matrix{Float64}(I, p, p)
-    h_nmax = fill(nmax, p)
+    G_nmax = Matrix{T}(I, p, p)
+    h_nmax = fill(T(nmax), p)
 
-    cones = CO.Cone[CO.Nonnegative{Float64}(p), CO.Nonnegative{Float64}(p)]
+    cones = CO.Cone[CO.Nonnegative{T}(p), CO.Nonnegative{T}(p)]
     cone_idxs = [1:p, (p + 1):(2 * p)]
 
     if use_logdet
         # pad with hypograph variable
-        A = [0 A]
-        G_nonneg = [zeros(p) G_nonneg]
-        G_nmax = [zeros(p) G_nmax]
+        A = T[0 A]
+        G_nonneg = [zeros(T, p) G_nonneg]
+        G_nmax = [zeros(T, p) G_nmax]
         # maximize the hypograph variable of the logdet cone
-        c = vcat(-1, zeros(p))
+        c = vcat(T(-1), zeros(T, p))
 
         # dimension of vectorized matrix V*diag(np)*V'
-        dimvec = Int(q * (q + 1) / 2)
-        G_logdet = zeros(dimvec, p)
+        dimvec = div(q * (q + 1), 2)
+        G_logdet = zeros(T, dimvec, p)
         l = 1
         for i in 1:q, j in 1:i
             for k in 1:p
@@ -61,29 +62,29 @@ function expdesign(
         end
         @assert l - 1 == dimvec
         # pad with hypograph variable and perspective variable
-        h_logdet = [0, 1, zeros(size(G_logdet, 1))...]
-        G_logdet = [-1 zeros(1, p); zeros(1, p + 1); zeros(dimvec) G_logdet]
-        push!(cones, CO.HypoPerLogdet{Float64}(dimvec + 2))
+        h_logdet = [T(0), T(1), zeros(T, size(G_logdet, 1))...]
+        G_logdet = [T(-1) zeros(T, 1, p); zeros(T, 1, p + 1); zeros(T, dimvec) G_logdet]
+        push!(cones, CO.HypoPerLogdet{T}(dimvec + 2))
         push!(cone_idxs, (2 * p + 1):(2 * p + dimvec + 2))
 
         G = vcat(G_nonneg, G_nmax, G_logdet)
         h = vcat(h_nonneg, h_nmax, h_logdet)
     else
         # requires an upper triangular matrix of additional variables, ordered row wise
-        num_trivars = Int(q * (q + 1) / 2)
+        num_trivars = div(q * (q + 1), 2)
         # pad with triangle matrix variables and q hypopoerlog cone hypograph variables
-        A = [A zeros(1, num_trivars) zeros(1, q)]
-        G_nonneg = [G_nonneg zeros(p, num_trivars) zeros(p, q)]
-        G_nmax = [G_nmax zeros(p, num_trivars) zeros(p, q)]
+        A = [A zeros(T, 1, num_trivars) zeros(T, 1, q)]
+        G_nonneg = [G_nonneg zeros(T, p, num_trivars) zeros(T, p, q)]
+        G_nmax = [G_nmax zeros(T, p, num_trivars) zeros(T, p, q)]
         # maximize the sum of hypograph variables of all hypoperlog cones
-        c = vcat(zeros(p), zeros(num_trivars), -ones(q))
+        c = vcat(zeros(T, p), zeros(T, num_trivars), -ones(T, q))
 
         # number of experiments, upper triangular matrix, hypograph variables
         dimx = p + num_trivars + q
 
         # vectorized dimension of psd matrix
         dimvec = q * (2 * q + 1)
-        G_psd = zeros(dimvec, dimx)
+        G_psd = zeros(T, dimvec, dimx)
 
         # variables in upper triangular matrix numbered row-wise
         diag_idx(i::Int) = (i == 1 ? 1 : 1 + sum(q - j for j in 0:(i - 2)))
@@ -114,12 +115,12 @@ function expdesign(
             l += 1
         end
 
-        h_psd = zeros(dimvec)
+        h_psd = zeros(T, dimvec)
         push!(cone_idxs, (2 * p + 1):(2 * p + dimvec))
-        push!(cones, CO.PosSemidef{Float64, Float64}(dimvec))
+        push!(cones, CO.PosSemidef{T, T}(dimvec))
 
-        G_log = zeros(3 * q, dimx)
-        h_log = zeros(3 * q)
+        G_log = zeros(T, 3 * q, dimx)
+        h_log = zeros(T, 3 * q)
         offset = 1
         for i in 1:q
             # hypograph variable
@@ -130,7 +131,7 @@ function expdesign(
             G_log[offset + 2, p + diag_idx(i)] = -1
             cone_offset = 2 * p + dimvec + offset
             push!(cone_idxs, cone_offset:(cone_offset + 2))
-            push!(cones, CO.HypoPerLog{Float64}())
+            push!(cones, CO.HypoPerLog{T}())
             offset += 3
         end
 
@@ -143,11 +144,13 @@ end
 
 function test_expdesign(instance::Function; options, rseed::Int = 1)
     Random.seed!(rseed)
+    T = Float64
     d = instance()
-    model = MO.PreprocessedLinearModel{Float64}(d.c, d.A, d.b, d.G, d.h, d.cones, d.cone_idxs)
-    solver = SO.HSDSolver{Float64}(model; options...)
-    SO.solve(solver)
-    r = SO.get_certificates(solver, model, test = true, atol = 1e-4, rtol = 1e-4)
+    model = MO.PreprocessedLinearModel{T}(d.c, d.A, d.b, d.G, d.h, d.cones, d.cone_idxs)
+    solver = SO.HSDSolver{T}(model; options...)
+    @time SO.solve(solver)
+    @show solver.num_iters
+    r = SO.get_certificates(solver, model, test = false, atol = 1e-4, rtol = 1e-4)
     @test r.status == :Optimal
     return
 end
@@ -163,7 +166,10 @@ expdesign8() = expdesign(5, 15, 25, 5, use_logdet = false)
 expdesign9() = expdesign(4, 8, 12, 3, use_logdet = false)
 expdesign10() = expdesign(3, 5, 7, 2, use_logdet = false)
 
-test_expdesign_all(; options...) = test_expdesign.([
+expdesign11() = expdesign(50, 100, 125, 5, use_logdet = true)
+expdesign12() = expdesign(50, 100, 125, 5, use_logdet = false)
+
+test_expdesign_all(; real_type::DataType = Float64, options...) = test_expdesign.([
     expdesign1,
     expdesign2,
     expdesign3,
@@ -174,13 +180,57 @@ test_expdesign_all(; options...) = test_expdesign.([
     expdesign9,
     expdesign9,
     expdesign10,
-    ], options = options)
+    ], real_type = real_type, options = options)
 
 test_expdesign(; options...) = test_expdesign.([
-    expdesign5,
-    expdesign10,
     expdesign3,
-    expdesign8,
-    expdesign1,
-    expdesign6,
+    # expdesign6,
+    # expdesign3,
+    # expdesign8,
+    # expdesign4,
+    # expdesign9,
+    # expdesign5,
+    # expdesign10,
     ], options = options)
+
+@testset "" begin
+    test_expdesign(verbose = true, tol_abs_opt = 1e-5, tol_rel_opt = 1e-5)
+end
+
+
+
+
+# n_range = [4, 8, 12]
+# p_range = [4, 8, 16]
+
+# q_range = [4, 6, 8]
+# nmax = 5
+# tf = [true]
+# seeds = 1:2
+# real_types = [Float64]
+#
+# io = open("expdesign.csv", "w")
+# println(io, "uselogdet,real,seed,q,p,n,dimx,dimy,dimz,time,bytes,numiters,status,pobj,dobj,xfeas,yfeas,zfeas")
+# for q in q_range, T in real_types, seed in seeds
+#     p = 2 * q
+#     n = 2 * q
+#     Random.seed!(seed)
+#     for use_logdet in tf
+#         d = expdesign(q, p, n, nmax, use_logdet = use_logdet, T = T)
+#         model = MO.PreprocessedLinearModel{T}(d.c, d.A, d.b, d.G, d.h, d.cones, d.cone_idxs)
+#         solver = SO.HSDSolver{T}(model, tol_abs_opt = 1e-5, tol_rel_opt = 1e-5)
+#         t = @timed SO.solve(solver)
+#         r = SO.get_certificates(solver, model, test = false, atol = 1e-4, rtol = 1e-4)
+#         dimx = size(d.G, 2)
+#         dimy = size(d.A, 1)
+#         dimz = size(d.G, 1)
+#         println(io, "$use_logdet,$T,$seed,$q,$p,$n,$dimx,$dimy,$dimz,$(t[2]),$(t[3])," *
+#             "$(solver.num_iters),$(r.status),$(r.primal_obj),$(r.dual_obj),$(solver.x_feas)," *
+#             "$(solver.y_feas),$(solver.z_feas)"
+#             )
+#     end
+# end
+# close(io)
+#
+#
+# ;
--- src/Cones/hypoperlogdet.jl
+++ src/Cones/hypoperlogdet.jl
@@ -5,8 +5,9 @@ Copyright 2018, Chris Coey and contributors
 (smat space) (u in R, v in R_+, w in S_+) : u <= v*logdet(W/v)
 (see equivalent MathOptInterface LogDetConeConeTriangle definition)
 
-barrier (guessed, based on analogy to hypoperlog barrier)
--log(v*logdet(W/v) - u) - logdet(W) - log(v)
+barrier based on the conic hull of the hypograph of the logdet function
+see http://webdoc.sub.gwdg.de/ebook/serien/e/CORE/dp2006_30.pdf
+gamma * (-log(v*logdet(W/v) - u) - logdet(W) - log(v) * k)
 
 TODO remove allocations
 =#
@@ -15,6 +16,8 @@ mutable struct HypoPerLogdet{T <: HypReal} <: Cone{T}
     use_dual::Bool
     dim::Int
     side::Int
+    k::Float64
+    gamma::Float64
 
     point::AbstractVector{T}
     mat::Matrix{T}
@@ -28,6 +31,10 @@ mutable struct HypoPerLogdet{T <: HypReal} <: Cone{T}
         cone.use_dual = is_dual
         cone.dim = dim
         cone.side = round(Int, sqrt(0.25 + 2 * (dim - 2)) - 0.5)
+        k = cone.side + 2
+        n = cone.side + 1
+        cone.k = k
+        cone.gamma = (k^(3 / 2) / (k - n)^(3 / 2) + (1 + k / (k - n))^(3 / 2) / sqrt(k))^2
         return cone
     end
 end
@@ -36,7 +43,6 @@ HypoPerLogdet{T}(dim::Int) where {T <: HypReal} = HypoPerLogdet{T}(dim, false)
 
 function setup_data(cone::HypoPerLogdet{T}) where {T <: HypReal}
     dim = cone.dim
-    side = round(Int, sqrt(0.25 + 2 * (dim - 2)) - 0.5)
     side = cone.side
     cone.mat = Matrix{T}(undef, side, side)
     cone.g = Vector{T}(undef, dim)
@@ -45,7 +51,7 @@ function setup_data(cone::HypoPerLogdet{T}) where {T <: HypReal}
     return
 end
 
-get_nu(cone::HypoPerLogdet) = cone.side + 2
+get_nu(cone::HypoPerLogdet) = cone.k * cone.gamma
 
 function set_initial_point(arr::AbstractVector{T}, cone::HypoPerLogdet{T}) where {T <: HypReal}
     arr[1] = -one(T)
@@ -79,16 +85,19 @@ function check_in_cone(cone::HypoPerLogdet{T}) where {T <: HypReal}
     vzi = v / z
 
     cone.g[1] = inv(z)
-    cone.g[2] = (T(n) - L) / z - inv(v)
+    cone.g[2] = (T(n) - L) / z - inv(v) * (cone.k - cone.side - 1)
     gwmat = -Wi * (one(T) + vzi)
     smat_to_svec!(view(cone.g, 3:dim), gwmat)
+    cone.g .*= cone.gamma
 
     cone.H[1, 1] = inv(z) / z
     cone.H[1, 2] = (T(n) - L) / z / z
     Huwmat = -vzi * Wi / z
     smat_to_svec!(view(cone.H, 1, 3:dim), Huwmat)
 
-    cone.H[2, 2] = abs2(T(-n) + L) / z / z + T(n) / (v * z) + inv(v) / v
+    cone.k = cone.side + 2
+
+    cone.H[2, 2] = abs2(T(-n) + L) / z / z + T(n) / (v * z) + inv(v) / v * (cone.k - cone.side - 1)
     Hvwmat = ((T(-n) + L) * vzi - one(T)) * Wi / z
     smat_to_svec!(view(cone.H, 2, 3:dim), Hvwmat)
 
@@ -110,6 +119,108 @@ function check_in_cone(cone::HypoPerLogdet{T}) where {T <: HypReal}
         end
         k += 1
     end
+    cone.H .*= cone.gamma
 
     return factorize_hess(cone)
 end
+
+#=
+Copyright 2018, Chris Coey and contributors
+
+(closure of) hypograph of perspective of (natural) log of determinant of a (row-wise lower triangle i.e. svec space) symmetric positive define matrix
+(smat space) (u in R, v in R_+, w in S_+) : u <= v*logdet(W/v)
+(see equivalent MathOptInterface LogDetConeConeTriangle definition)
+
+barrier (guessed, based on analogy to hypoperlog barrier)
+-log(v*logdet(W/v) - u) - logdet(W) - log(v)
+
+TODO remove allocations
+=#
+#
+# mutable struct HypoPerLogdet{T <: HypReal} <: Cone{T}
+#     use_dual::Bool
+#     dim::Int
+#     side::Int
+#
+#     point::AbstractVector{T}
+#     mat::Matrix{T}
+#     g::Vector{T}
+#     H::Matrix{T}
+#     H2::Matrix{T}
+#     F
+#     barfun::Function
+#     diffres
+#
+#     function HypoPerLogdet{T}(dim::Int, is_dual::Bool) where {T <: HypReal}
+#         cone = new{T}()
+#         cone.use_dual = is_dual
+#         cone.dim = dim
+#         cone.side = round(Int, sqrt(0.25 + 2 * (dim - 2)) - 0.5)
+#         return cone
+#     end
+# end
+#
+# HypoPerLogdet{T}(dim::Int) where {T <: HypReal} = HypoPerLogdet{T}(dim, false)
+#
+# gammafun(nu, k) =  (k^(3 / 2) / (k - nu)^(3 / 2) + (1 + k / (k - nu))^(3 / 2) / sqrt(k))^2
+# function nubar(nu)
+#     toa = 4
+#     k = toa * nu
+#     gamma = gammafun(nu, k)
+#     return gamma * k
+# end
+#
+# function setup_data(cone::HypoPerLogdet{T}) where {T <: HypReal}
+#     dim = cone.dim
+#     side = round(Int, sqrt(0.25 + 2 * (dim - 2)) - 0.5)
+#     side = cone.side
+#     cone.mat = Matrix{T}(undef, side, side)
+#     cone.g = Vector{T}(undef, dim)
+#     cone.H = zeros(T, dim, dim)
+#     cone.H2 = similar(cone.H)
+#     function barfun(point)
+#         u = point[1]
+#         v = point[2]
+#         W = similar(point, side, side)
+#         svec_to_smat!(W, view(point, 3:dim))
+#         nu = cone.side + 1
+#         gamma = (8 + 1 / 2 / sqrt(nu) * 7^(3 / 2))^2 / 27
+#         return -log(v * logdet(W / v) - u) - logdet(W) - log(v) #* side
+#         # return (-log(logdet(W / v) - u / v) - logdet(W / v) - log(v) * (4 * nu)) * gamma
+#     end
+#     cone.barfun = barfun
+#     cone.diffres = DiffResults.HessianResult(cone.g)
+#     return
+# end
+#
+# get_nu(cone::HypoPerLogdet) = cone.side + 2 # 2 * cone.side + 1
+# # get_nu(cone::HypoPerLogdet) = (16 * sqrt(cone.side + 1) + 7^(3 / 2))^2 / 27
+#
+# function set_initial_point(arr::AbstractVector{T}, cone::HypoPerLogdet{T}) where {T <: HypReal}
+#     arr[1] = -one(T)
+#     arr[2] = one(T)
+#     smat_to_svec!(view(arr, 3:cone.dim), Matrix(one(T) * I, cone.side, cone.side)) # TODO remove allocs
+#     return arr
+# end
+#
+# # TODO remove allocs
+# function check_in_cone(cone::HypoPerLogdet{T}) where {T <: HypReal}
+#     u = cone.point[1]
+#     v = cone.point[2]
+#     if v <= zero(T)
+#         return false
+#     end
+#     W = cone.mat
+#     svec_to_smat!(W, view(cone.point, 3:cone.dim))
+#     F = hyp_chol!(Symmetric(W))
+#     ldW = logdet(F)
+#     if !isposdef(F) || u >= v * (ldW - cone.side * log(v))
+#         return false
+#     end
+#
+#     cone.diffres = ForwardDiff.hessian!(cone.diffres, cone.barfun, cone.point)
+#     cone.g .= DiffResults.gradient(cone.diffres)
+#     cone.H .= DiffResults.hessian(cone.diffres)
+#
+#     return factorize_hess(cone)
+# end
