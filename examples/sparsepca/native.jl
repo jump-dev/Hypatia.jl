cb2d91bdcb1f80479b97a359006251a72c79f461
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-06-19 22:50:11 +1200

    l1 for portfolio

--- examples/sparsepca/native.jl
+++ examples/sparsepca/native.jl
@@ -24,17 +24,23 @@ const rt2 = sqrt(2)
 #     JuMP.@variable(model, X[i in 1:n, j in 1:i])
 #     @show vcat(1, [X[i, j] * (i == j ? 1 : 2) for i in 1:n for j in 1:i])
 #     JuMP.@constraints(model, begin
-#         vcat(1, [X[i, j] * (i == j ? 1 : 2) for i in 1:n for j in 1:i]) in CO.EpiNormInf{Float64}(1 + div(n * (n + 1), 2), true)
+#         vcat(k, [X[i, j] * (i == j ? 1 : 2) for i in 1:n for j in 1:i]) in CO.EpiNormInf{Float64}(1 + div(n * (n + 1), 2), true)
 #         [X[i, j] * (i == j ? 1 : rt2) for i in 1:n for j in 1:i] in MOI.PositiveSemidefiniteConeTriangle(n)
 #         sum(X[i, i] for i in 1:n) == 1
 #     end)
 #     JuMP.@objective(model, Max, sum(X[i, j] * A[i, j] * (i == j ? 1 : 2) for i in 1:n for j in 1:i))
 #     return (model = model,)
 # end
-function sparsepca(n::Int, k::Int; T = Float64)
+function sparsepca(n::Int, k::Int; T = Float64, mat = zeros(T, 0, 0))
     dimx = div(n * (n + 1), 2)
-    c = randn(dimx)
-    b = T[10]
+    if isempty(mat)
+        mat = randn(n, n)
+        mat = mat + mat'
+    else
+        @assert size(mat) == (n, n)
+    end
+    c = [mat[i, j] for i in 1:n for j in 1:i]
+    b = T[1]
     A = zeros(T, 1, dimx)
     # l1 cone
     G1 = -Matrix{T}(I, dimx, dimx) * 2
@@ -42,12 +48,12 @@ function sparsepca(n::Int, k::Int; T = Float64)
     G2 = -Matrix{T}(I, dimx, dimx) * rt2
     for i in 1:n
         s = sum(1:i)
-        A[s] = -1
+        A[s] = 1
         G1[s, s] = -1
         G2[s, s] = -1
     end
     G = vcat(zeros(T, 1, dimx), G1, G2)
-    h = vcat(1, zeros(2 * dimx))
+    h = vcat(k, zeros(2 * dimx))
     cones = [CO.EpiNormInf{T}(1 + dimx, true), CO.PosSemidef{T, T}(dimx)]
     cone_idxs = [1:(dimx + 1), (dimx + 2):(2 * dimx + 1)]
     @show (A), (G), cone_idxs, size(h), size(c)
@@ -55,6 +61,7 @@ function sparsepca(n::Int, k::Int; T = Float64)
 end
 
 sparsepca1(; T::THR = Float) = sparsepca(3, 3, T = T)
+# sparsepca2(; T::THR = Float) = sparsepca(3, 3, T = T, mat = Matrix{Float64}(I, 3, 3))
 
 function test_sparsepca(instance::Function; T::THR = Float64, rseed::Int = 1, options)
     Random.seed!(rseed)
@@ -64,6 +71,7 @@ function test_sparsepca(instance::Function; T::THR = Float64, rseed::Int = 1, op
     SO.solve(solver)
     r = SO.get_certificates(solver, model, test = false, atol = 1e-4, rtol = 1e-4)
     @test r.status == :Optimal
+    @show r.x
     return
 end
 
@@ -71,6 +79,6 @@ test_sparsepca_all(; options...) = test_sparsepca.([
     sparsepca1,
     ], options = options)
 
-test_sparsepca(; options...) = test_sparsepca.([
+test_sparsepca1(; options...) = test_sparsepca.([
     sparsepca1,
     ], options = options)
