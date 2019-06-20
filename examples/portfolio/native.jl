b276d2d3456cb9e55d8ee0bdc1b05a00e473cd37
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-06-21 10:14:52 +1200

    sumlog for expdesign, l1 in sparsepca, l1 in portfolio

--- examples/portfolio/native.jl
+++ examples/portfolio/native.jl
@@ -26,16 +26,19 @@ function portfolio(
     sigma_half::AbstractMatrix{Float64} = Matrix{Float64}(undef, 0, 0),
     gamma::Float64 = -1.0,
     risk_measure::Symbol = :entropic,
-    use_l1::Bool = true,
+    use_l1ball::Bool = true,
     )
     if isempty(returns)
         returns = rand(num_stocks)
     end
     if isempty(sigma_half)
-        sigma_half = randn(num_stocks, num_stocks)
+        sigma = randn(num_stocks, num_stocks)
+        sigma = sigma * sigma'
+        sigma_half = cholesky(Symmetric(sigma)).U
     end
     if gamma < 0
-        gamma = rand()
+        x = randn(num_stocks)
+        gamma = sum(abs, sigma_half * x)
     end
 
     c = returns
@@ -46,7 +49,7 @@ function portfolio(
     cone_idxs = [1:num_stocks]
     cones = CO.Cone[CO.Nonnegative{Float64}(num_stocks)]
 
-    if risk_measure == :quadratic || (risk_measure == :l1 && use_l1)
+    if risk_measure == :quadratic || (risk_measure == :l1 && use_l1ball)
         G2 = vcat(zeros(1, num_stocks), -sigma_half)
         h2 = [gamma; zeros(num_stocks)]
         cone_idxs = vcat(cone_idxs, [(num_stocks + 1):(2 * num_stocks + 1)])
@@ -58,20 +61,21 @@ function portfolio(
             cones = vcat(cones, CO.EpiNormInf{Float64}(num_stocks + 1, true))
         end
 
-    elseif risk_measure == :l1 && !use_l1
+    elseif risk_measure == :l1 && !use_l1ball
         c = vcat(c, zeros(2 * num_stocks))
         id = Matrix{Float64}(I, num_stocks, num_stocks)
-        A_slacks = [sigma_half -id; -sigma_half, -id]
+        id2 = Matrix{Float64}(I, 2 * num_stocks, 2 * num_stocks)
+        A_slacks = [sigma_half -id id]
         A_l1 = [zeros(1, num_stocks) ones(1, 2 * num_stocks)]
         A = [A zeros(1, 2 * num_stocks); A_slacks; A_l1]
-        b = vcat(b, zeros(2 * num_stocks), gamma)
+        b = vcat(b, zeros(num_stocks), gamma)
         G = [
-            G1 zeros(num_stocks, 2 * num_stocks);
-            zeros(2 * num_stocks, num_stocks) Matrix{Float64}(I, 2 * num_stocks, 2 * num_stocks)
+            G1 zeros(num_stocks, 2 * num_stocks)
+            zeros(2 * num_stocks, num_stocks) -id2
             ]
-        h = vcat(h, zeros(2 * num_stocks))
+        h = vcat(h1, zeros(2 * num_stocks))
         cones = vcat(cones, CO.Nonnegative{Float64}(2 * num_stocks))
-        cone_idxs = vcat(cone_idxs, (num_stocks + 1):(3 * num_stocks))
+        cone_idxs = vcat(cone_idxs, [(num_stocks + 1):(3 * num_stocks)])
 
     elseif risk_measure == :entropic
         # sigma_half = abs.(sigma_half) TODO will this always be feasible?
@@ -100,7 +104,7 @@ function portfolio(
         error("unknown risk measure: $risk_measure")
     end
 
-    return (c = c, A = A, b = b, G = G, h = h, cones = cones, cone_idxs = cone_idxs)
+    return (c = c, A = A, b = b, G = G, h = h, cones = cones, cone_idxs = cone_idxs, sigma_half = sigma_half, gamma = gamma)
 end
 
 function test_portfolio(instance::Function; options, rseed::Int = 1)
@@ -111,10 +115,11 @@ function test_portfolio(instance::Function; options, rseed::Int = 1)
     SO.solve(solver)
     r = SO.get_certificates(solver, model, test = true, atol = 1e-4, rtol = 1e-4)
     @test r.status == :Optimal
+    @assert sum(abs, d.sigma_half * r.x) <= d.gamma
     return
 end
 
-portfolio_ex(risk_measure::Symbol) = portfolio(
+portfolio_ex(risk_measure::Symbol; kwargs...) = portfolio(
     3,
     returns = -[0.0254,	0.0190,	0.0045],
     sigma_half = cholesky([
@@ -122,12 +127,15 @@ portfolio_ex(risk_measure::Symbol) = portfolio(
         0.0012	0.0020	0.0002
         0.0001	0.0002	0.0019
         ]).U,
-    gamma = 0.033,
-    risk_measure = risk_measure,
+    gamma = 0.05, # 0.033,
+    risk_measure = risk_measure;
+    kwargs...,
     )
 
 portfolio1() = portfolio_ex(:quadratic)
 portfolio2() = portfolio_ex(:entropic)
+portfolio3() = portfolio(4, risk_measure = :l1, use_l1ball = true)
+portfolio4() = portfolio(4, risk_measure = :l1, use_l1ball = false)
 
 test_portfolio_all(; options...) = test_portfolio.([
     portfolio1,
@@ -137,4 +145,40 @@ test_portfolio_all(; options...) = test_portfolio.([
 test_portfolio(; options...) = test_portfolio.([
     portfolio1,
     portfolio2,
+    portfolio3,
+    portfolio4,
     ], options = options)
+
+
+
+
+n_range = [100, 200, 400, 600]
+tf = [true, false]
+seeds = 1:2
+real_types = [Float64, Float32]
+T = Float64
+
+io = open("portfoliol1.csv", "w")
+println(io, "usel1ball,seed,n,dimx,dimy,dimz,time,bytes,numiters,status,pobj,dobj,xfeas,yfeas,zfeas")
+for n in n_range, seed in seeds
+    for use_l1ball in tf
+
+        Random.seed!(seed)
+        d = portfolio(n, risk_measure = :l1, use_l1ball = use_l1ball)
+        model = MO.PreprocessedLinearModel{T}(d.c, d.A, d.b, d.G, d.h, d.cones, d.cone_idxs)
+        solver = SO.HSDSolver{T}(model, tol_abs_opt = 1e-5, tol_rel_opt = 1e-5, time_limit = 600)
+        t = @timed SO.solve(solver)
+        r = SO.get_certificates(solver, model, test = false, atol = 1e-4, rtol = 1e-4)
+        dimx = size(d.G, 2)
+        dimy = size(d.A, 1)
+        dimz = size(d.G, 1)
+        println(io, "$use_l1ball,$seed,$n,$dimx,$dimy,$dimz,$(t[2]),$(t[3])," *
+            "$(solver.num_iters),$(r.status),$(r.primal_obj),$(r.dual_obj),$(solver.x_feas)," *
+            "$(solver.y_feas),$(solver.z_feas)"
+            )
+
+    end
+end
+close(io)
+
+;
