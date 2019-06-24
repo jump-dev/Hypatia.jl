d590b71ea335783612e382830ebe3e1abdf24fce
Chris Coey <coey.chris@gmail.com>
2019-06-24 01:36:51 -0400

    rewrite epinormeucl barrier derivatives to fix bug where H is 2 * inv(Hinv)
    Former-commit-id: 1df6d25bc107d826fafd77df625234d0e0aede2e

--- test/barriers.jl
+++ test/barriers.jl
@@ -9,24 +9,27 @@ import Hypatia.HypReal
 const CO = Hypatia.Cones
 using Test
 
-function pass_through_cone(cone::CO.Cone{T}; num_checks::Int = 2) where {T <: HypReal}
+function test_barrier_oracles(cone::CO.Cone{T}) where {T <: HypReal}
     CO.setup_data(cone)
-    for _ in 1:num_checks
-        point = Vector{T}(undef, CO.dimension(cone))
-        CO.set_initial_point(point, cone)
-        CO.load_point(cone, point)
-        @test CO.check_in_cone(cone)
-        tol = max(1e-12, sqrt(eps(T)))
-        @test -dot(cone.point, CO.grad(cone)) ≈ CO.get_nu(cone) atol=tol rtol=tol
-        @test CO.hess(cone) * cone.point ≈ -CO.grad(cone) atol=tol rtol=tol
-    end
+    point = Vector{T}(undef, CO.dimension(cone))
+    CO.set_initial_point(point, cone)
+    CO.load_point(cone, point)
+
+    @test cone.point == point
+    @test CO.check_in_cone(cone)
+
+    tol = max(1e-12, sqrt(eps(T)))
+    @test -dot(point, CO.grad(cone)) ≈ CO.get_nu(cone) atol=tol rtol=tol
+    @test CO.hess(cone) * point ≈ -CO.grad(cone) atol=tol rtol=tol
+    @test CO.hess(cone) * CO.inv_hess(cone) ≈ I atol=tol rtol=tol
+
     return
 end
 
 function test_epinormeucl_barrier(T::Type{<:HypReal})
     for dim in [2, 3, 5]
         cone = CO.EpiNormEucl{T}(dim)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -34,7 +37,7 @@ end
 function test_epinorinf_barrier(T::Type{<:HypReal})
     for dim in [3, 5, 8]
         cone = CO.EpiNormInf{T}(dim)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -42,7 +45,7 @@ end
 function test_epinormspectral_barrier(T::Type{<:HypReal})
     for (n, m) in [(1, 3), (2, 4)]
         cone = CO.EpiNormSpectral{T}(n, m)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -50,7 +53,7 @@ end
 function test_epiperpower_barrier(T::Type{<:HypReal})
     for alpha in [1.5, 2.5]
         cone = CO.EpiPerPower{T}(alpha)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -58,7 +61,7 @@ end
 function test_epipersquare_barrier(T::Type{<:HypReal})
     for dim in [3, 5, 8]
         cone = CO.EpiPerSquare{T}(dim)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -66,7 +69,7 @@ end
 function test_epipersumexp_barrier(T::Type{<:HypReal})
     for dim in [3, 5, 8]
         cone = CO.EpiPerSumExp{T}(dim)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -77,21 +80,21 @@ function test_hypogeomean_barrier(T::Type{<:HypReal})
         alpha = rand(T, dim - 1)
         alpha ./= sum(alpha)
         cone = CO.HypoGeomean{T}(alpha)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
 
 function test_hypoperlog_barrier(T::Type{<:HypReal})
     cone = CO.HypoPerLog{T}()
-    pass_through_cone(cone)
+    test_barrier_oracles(cone)
     return
 end
 
 function test_hypoperlogdet_barrier(T::Type{<:HypReal})
     for dim in [3, 5, 8]
         cone = CO.HypoPerLogdet{T}(dim)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -99,7 +102,7 @@ end
 function test_hypopersumlog_barrier(T::Type{<:HypReal})
     for dim in [3, 5, 8]
         cone = CO.HypoPerSumLog{T}(dim)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -107,11 +110,11 @@ end
 function test_semidefinite_barrier(T::Type{<:HypReal})
     for dim in [1, 3, 6]
         cone = CO.PosSemidef{T, T}(dim) # real
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     for dim in [1, 4, 9]
         cone = CO.PosSemidef{T, Complex{T}}(dim) # complex
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -122,7 +125,7 @@ function test_wsospolyinterp_barrier(T::Type{<:HypReal})
         (U, _, P0, _, _) = MU.interpolate(MU.FreeDomain(n), halfdeg, sample = false)
         P0 = convert(Matrix{T}, P0)
         cone = CO.WSOSPolyInterp{T, T}(U, [P0], true)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     # TODO also test complex case CO.WSOSPolyInterp{T, Complex{T}} - need complex MU interp functions first
     return
@@ -134,7 +137,7 @@ function test_wsospolyinterpmat_barrier(T::Type{<:HypReal})
         (U, _, P0, _, _) = MU.interpolate(MU.FreeDomain(n), halfdeg, sample = false)
         P0 = convert(Matrix{T}, P0)
         cone = CO.WSOSPolyInterpMat{T}(R, U, [P0], true)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
@@ -145,7 +148,7 @@ function test_wsospolyinterpsoc_barrier(T::Type{<:HypReal})
         (U, _, P0, _, _) = MU.interpolate(MU.FreeDomain(n), halfdeg, sample = false)
         P0 = convert(Matrix{T}, P0)
         cone = CO.WSOSPolyInterpSOC{T}(R, U, [P0], true)
-        pass_through_cone(cone)
+        test_barrier_oracles(cone)
     end
     return
 end
