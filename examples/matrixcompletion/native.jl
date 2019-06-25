689d2be0064af0352fab7c9b85899105728c8e78
Chris Coey <coey.chris@gmail.com>
2019-06-25 15:48:42 -0400

    cleanup mat compl

--- examples/matrixcompletion/native.jl
+++ examples/matrixcompletion/native.jl
@@ -6,50 +6,35 @@ modified to use spectral norm
 =#
 
 using LinearAlgebra
-using SparseArrays
 import Random
 using Test
-import DynamicPolynomials
 import Hypatia
+import Hypatia.HypReal
 const HYP = Hypatia
 const MO = HYP.Models
-const MU = HYP.ModelUtilities
 const CO = HYP.Cones
 const SO = HYP.Solvers
 
-THR = Type{<: HYP.HypReal}
-
 function matrixcompletion(
+    T::Type{<:HypReal},
     m::Int,
     n::Int;
-    num_known::Int = -1,
-    known_rows::Vector{Int} = Int[],
-    known_cols::Vector{Int} = Int[],
-    known_vals::Vector{Float64} = Float64[],
     use_geomean::Bool = true,
     use_epinorm::Bool = true,
-    T::THR = Float64,
     )
     @assert m <= n
+    rt2 = sqrt(T(2))
 
-    if num_known < 0
-        num_known = round(Int, m * n * 0.1)
-    end
-    if isempty(known_rows)
-        known_rows = rand(1:m, num_known)
-    end
-    if isempty(known_cols)
-        known_cols = rand(1:n, num_known)
-    end
-    if isempty(known_vals)
-        known_vals = randn(num_known)
-    end
+    num_known = round(Int, m * n * 0.1)
+    known_rows = rand(1:m, num_known)
+    known_cols = rand(1:n, num_known)
+    known_vals = rand(T, num_known) .- T(0.5)
 
     mat_to_vec_idx(i::Int, j::Int) = (j - 1) * m + i
 
     is_known = fill(false, m * n)
     # h for the rows that X (the matrix and not epigraph variable) participates in
-    h_norm_x = zeros(m * n)
+    h_norm_x = zeros(T, m * n)
     for (k, (i, j)) in enumerate(zip(known_rows, known_cols))
         known_idx = mat_to_vec_idx(i, j)
         # if not using the epinorminf cone, indices relate to X'
@@ -59,12 +44,12 @@ function matrixcompletion(
 
     num_known = sum(is_known) # if randomly generated, some indices may repeat
     num_unknown = m * n - num_known
-    c = vcat(1, zeros(num_unknown))
-    b = Float64[]
+    c = vcat(one(T), zeros(T, num_unknown))
+    b = T[]
 
     # epinormspectral cone- get vec(X) in G and h
     if use_epinorm
-        G_norm = zeros(m * n, num_unknown)
+        G_norm = zeros(T, m * n, num_unknown)
         total_idx = 1
         unknown_idx = 1
         for j in 1:n, i in 1:m
@@ -76,8 +61,11 @@ function matrixcompletion(
         end
 
         # add first row and column for epigraph variable
-        G_norm = [-1 zeros(1, num_unknown); zeros(m * n) G_norm]
-        h_norm_x = vcat(0, h_norm_x)
+        G_norm = [
+            -one(T)    zeros(T, 1, num_unknown);
+            zeros(T, m * n)    G_norm;
+            ]
+        h_norm_x = vcat(zero(T), h_norm_x)
         h_norm = h_norm_x
 
         cones = CO.Cone[CO.EpiNormSpectral{T}(m, n)]
@@ -85,8 +73,8 @@ function matrixcompletion(
         cone_offset = m * n + 1
     else
         num_rows = div(m * (m + 1), 2) + m * n + div(n * (n + 1), 2)
-        G_norm = zeros(num_rows, num_unknown + 1)
-        h_norm = zeros(num_rows)
+        G_norm = zeros(T, num_rows, num_unknown + 1)
+        h_norm = zeros(T, num_rows)
         # first block epigraph variable * I
         for i in 1:m
             G_norm[sum(1:i), 1] = -1
@@ -123,7 +111,7 @@ function matrixcompletion(
 
     if use_geomean
         # hypogeomean for values to be filled
-        G_geo = zeros(num_unknown + 1, num_unknown + 1)
+        G_geo = zeros(T, num_unknown + 1, num_unknown + 1)
         total_idx = 1
         unknown_idx = 1
         for j in 1:n, i in 1:m
@@ -134,18 +122,18 @@ function matrixcompletion(
             total_idx += 1
         end
         # first component of the vector in the in geomean cone, elements multiply to one
-        h2 = vcat(1, zeros(num_unknown))
+        h2 = vcat(one(T), zeros(T, num_unknown))
         h = vcat(h_norm, h2)
         @assert total_idx - 1 == m * n
         @assert unknown_idx - 1 == num_unknown
 
-        A = zeros(0, 1 + num_unknown)
+        A = zeros(T, 0, 1 + num_unknown)
         push!(cone_idxs, (cone_offset + 1):(cone_offset + num_unknown + 1))
         push!(cones, CO.HypoGeomean{T}(ones(num_unknown) / num_unknown))
     else
         # number of 3-dimensional power cones needed is num_unknown - 1, number of new variables is num_unknown - 2
         # first num_unknown columns overlap with G_norm, column for the epigraph variable of the spectral cone added later
-        G_geo = zeros(3 * (num_unknown - 1), 2 * num_unknown - 2)
+        G_geo = zeros(T, 3 * (num_unknown - 1), 2 * num_unknown - 2)
         # first cone is a special case since two of the original variables participate in it
         G_geo[3, 1] = -1
         G_geo[2, 2] = -1
@@ -168,54 +156,43 @@ function matrixcompletion(
         G_geo[offset + 1, 2 * num_unknown - 2] = -1
         push!(cones, CO.HypoGeomean{T}([(num_unknown - 1) / num_unknown, 1 / num_unknown]))
         push!(cone_idxs, (cone_offset + 3 * num_unknown - 5):(cone_offset + 3 * num_unknown - 3))
-        h = vcat(h_norm, zeros(3 * (num_unknown - 2)), [1, 0, 0])
+        h = vcat(h_norm, zeros(T, 3 * (num_unknown - 2)), T[1, 0, 0])
 
         # G_norm needs to be post-padded with columns for 3dim cone vars
-        G_norm = [G_norm zeros(m * n + 1, num_unknown - 2)]
+        G_norm = hcat(G_norm, zeros(T, m * n + 1, num_unknown - 2))
         # G_geo needs to be pre-padded with the epigraph variable for the spectral norm cone
-        G_geo = [zeros(3 * (num_unknown - 1)) G_geo]
-        c = vcat(c, zeros(num_unknown - 2))
-        A = zeros(0, size(G_geo, 2))
+        G_geo = hcat(zeros(T, 3 * (num_unknown - 1)), G_geo)
+        c = vcat(c, zeros(T, num_unknown - 2))
+        A = zeros(T, 0, size(G_geo, 2))
     end
     G = vcat(G_norm, G_geo)
 
     return (c = c, A = A, b = b, G = G, h = h, cones = cones, cone_idxs = cone_idxs)
 end
 
-matrixcompletion_ex(use_geomean::Bool) = matrixcompletion(
-    3,
-    3,
-    num_known = 5,
-    known_rows = [1, 3, 2, 3, 1],
-    known_cols = [1, 1, 2, 2, 3],
-    known_vals = [1.0, 3.2, 0.8, 5.9, 1.9];
-    use_geomean = true,
-    use_epinorm = true,
-    )
-matrixcompletion1(; T::THR = Float64) = matrixcompletion_ex()
-matrixcompletion2(; T::THR = Float64) = matrixcompletion_ex(use_geomean = false, use_epinorm = false)
-matrixcompletion3(; T::THR = Float64) = matrixcompletion(5, 6)
-matrixcompletion4(; T::THR = Float64) = matrixcompletion(5, 6, use_geomean = false)
-matrixcompletion5(; T::THR = Float64) = matrixcompletion(5, 6, use_epinorm = false)
-matrixcompletion6(; T::THR = Float64) = matrixcompletion(6, 8)
-matrixcompletion7(; T::THR = Float64) = matrixcompletion(6, 8, use_geomean = false)
-matrixcompletion8(; T::THR = Float64) = matrixcompletion(6, 8, use_epinorm = false)
-matrixcompletion9(; T::THR = Float64) = matrixcompletion(8, 12)
-matrixcompletion10(; T::THR = Float64) = matrixcompletion(8, 12, use_geomean = false)
-matrixcompletion11(; T::THR = Float64) = matrixcompletion(8, 12, use_epinorm = false)
+matrixcompletion1(T::Type{<:HypReal}) = matrixcompletion(T, 5, 6)
+matrixcompletion2(T::Type{<:HypReal}) = matrixcompletion(T, 5, 6, use_geomean = false)
+matrixcompletion3(T::Type{<:HypReal}) = matrixcompletion(T, 5, 6, use_epinorm = false)
+matrixcompletion4(T::Type{<:HypReal}) = matrixcompletion(T, 6, 8)
+matrixcompletion5(T::Type{<:HypReal}) = matrixcompletion(T, 6, 8, use_geomean = false)
+matrixcompletion6(T::Type{<:HypReal}) = matrixcompletion(T, 6, 8, use_epinorm = false)
+matrixcompletion7(T::Type{<:HypReal}) = matrixcompletion(T, 8, 8)
+matrixcompletion8(T::Type{<:HypReal}) = matrixcompletion(T, 8, 8, use_geomean = false)
+matrixcompletion9(T::Type{<:HypReal}) = matrixcompletion(T, 8, 8, use_epinorm = false)
 
-function test_matrixcompletion(instance::Function; T::THR = Float64, rseed::Int = 1, options)
+function test_matrixcompletion(T::Type{<:HypReal}, instance::Function; options, rseed::Int = 1)
     Random.seed!(rseed)
-    d = instance(T = T)
+    d = instance(T)
     model = MO.PreprocessedLinearModel{T}(d.c, d.A, d.b, d.G, d.h, d.cones, d.cone_idxs)
     solver = SO.HSDSolver{T}(model; options...)
     SO.solve(solver)
-    r = SO.get_certificates(solver, model, test = true, atol = 1e-4, rtol = 1e-4)
+    tol = max(1e-5, sqrt(sqrt(eps(T))))
+    r = SO.get_certificates(solver, model, test = true, atol = tol, rtol = tol)
     @test r.status == :Optimal
     return
 end
 
-test_matrixcompletion_all(; T::THR = Float64, options...) = test_matrixcompletion.([
+test_matrixcompletion_all(T::Type{<:HypReal}; options...) = test_matrixcompletion.(T, [
     matrixcompletion1,
     matrixcompletion2,
     matrixcompletion3,
@@ -225,12 +202,10 @@ test_matrixcompletion_all(; T::THR = Float64, options...) = test_matrixcompletio
     matrixcompletion7,
     matrixcompletion8,
     matrixcompletion9,
-    matrixcompletion10,
-    matrixcompletion11,
-    ], T = T, options = options)
+    ], options = options)
 
-test_matrixcompletion(; T::THR = Float64, options...) = test_matrixcompletion.([
+test_matrixcompletion(T::Type{<:HypReal}; options...) = test_matrixcompletion.(T, [
+    matrixcompletion1,
+    matrixcompletion2,
     matrixcompletion3,
-    matrixcompletion4,
-    matrixcompletion5,
-    ], T = T, options = options)
+    ], options = options)
