b717d317666c6847a59aa3d788747134a9bece30
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-08-19 06:53:12 -0400

    add power cone
    Former-commit-id: d618a1b5c5de698fcae20ec8bf8e0f828c525549

---
+++ src/Cones/generalizedpower.jl
@@ -0,0 +1,9 @@
+#=
+Copyright 2019, Chris Coey, Lea Kapelevich and contributors
+
+generalized power cone parametrized by alpha in R_+^n on unit simplex
+(u in R, w in R_+^n) : |u| <= prod_i(w_i^alpha_i)
+where sum_i(alpha_i) = 1, alpha_i >= 0
+
+barrier from "On self-concordant barriers for generalized power cones" by Roy & Xiao 2018
+=#
