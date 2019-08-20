6d98d81172d61dc361621117460046288b73c35c
lkapelevich <leakapelevich@gmail.com>
2019-08-20 10:58:16 -0400

    generalize to norm in the hypograph

--- src/Cones/generalizedpower.jl
+++ src/Cones/generalizedpower.jl
@@ -2,17 +2,18 @@
 Copyright 2019, Chris Coey, Lea Kapelevich and contributors
 
 generalized power cone parametrized by alpha in R_+^n on unit simplex
-(u in R, w in R_+^n) : |u| <= prod_i(w_i^alpha_i)
+(u in R^m, w in R_+^n) : norm_2(u) <= prod_i(w_i^alpha_i)
 where sum_i(alpha_i) = 1, alpha_i >= 0
 
 barrier from "On self-concordant barriers for generalized power cones" by Roy & Xiao 2018
--log(prod_i((w_i)^(2 * alpha_i)) - norm_2(u)) - sum_i((1 - alpha_i)*log(w_i))
+-log(prod_i((w_i)^(2 * alpha_i)) - norm_2(u)^2) - sum_i((1 - alpha_i)*log(w_i))
 =#
 
 mutable struct GeneralizedPower{T <: Real} <: Cone{T}
     use_dual::Bool
     dim::Int
     alpha::Vector{T}
+    m::Int
     point::Vector{T}
 
     feas_updated::Bool
@@ -31,13 +32,14 @@ mutable struct GeneralizedPower{T <: Real} <: Cone{T}
     tmp_hess::Symmetric{T, Matrix{T}}
     hess_fact # TODO prealloc
 
-    function GeneralizedPower{T}(alpha::Vector{T}, is_dual::Bool) where {T <: Real}
-        dim = length(alpha) + 1
+    function GeneralizedPower{T}(alpha::Vector{T}, m::Int, is_dual::Bool) where {T <: Real}
+        dim = length(alpha) + m
         @assert dim >= 3
         @assert all(ai > 0 for ai in alpha)
         tol = 1e3 * eps(T)
         @assert sum(alpha) ≈ 1 atol=tol rtol=tol
         cone = new{T}()
+        cone.m = m
         cone.use_dual = is_dual
         cone.dim = dim
         cone.alpha = alpha
@@ -45,7 +47,7 @@ mutable struct GeneralizedPower{T <: Real} <: Cone{T}
     end
 end
 
-GeneralizedPower{T}(alpha::Vector{T}) where {T <: Real} = GeneralizedPower{T}(alpha, false)
+GeneralizedPower{T}(alpha::Vector{T}, m::Int) where {T <: Real} = GeneralizedPower{T}(alpha, m, false)
 
 function setup_data(cone::GeneralizedPower{T}) where {T <: Real}
     reset_data(cone)
@@ -62,7 +64,7 @@ get_nu(cone::GeneralizedPower) = cone.dim
 
 function set_initial_point(arr::AbstractVector, cone::GeneralizedPower)
     arr .= 1
-    arr[1] = 0
+    arr[1:cone.m] .= 0
     return arr
 end
 
@@ -71,7 +73,7 @@ function update_feas(cone::GeneralizedPower)
     u = cone.point[1]
     w = view(cone.point, 2:cone.dim)
     if all(wi -> wi > 0, w)
-        cone.is_feas = (sum(cone.alpha[i] * log(w[i]) for i in eachindex(cone.alpha)) > log(abs(u)))
+        cone.is_feas = (sum(cone.alpha[i] * log(w[i]) for i in eachindex(cone.alpha)) > log(sum(abs2, u)))
     else
         cone.is_feas = false
     end
@@ -81,44 +83,58 @@ end
 
 function update_grad(cone::GeneralizedPower)
     @assert cone.is_feas
-    u = cone.point[1]
-    w = view(cone.point, 2:cone.dim)
+    m = cone.m
+    u = cone.point[1:cone.m]
+    w = view(cone.point, (m + 1):cone.dim)
     @. cone.alphawi = 2 * cone.alpha / w
-    # prod_i((w_i)^(2 * alpha_i)) - norm_2(u))
+    # prod_i((w_i)^(2 * alpha_i))
     cone.wi2a = exp(sum(2 * cone.alpha[i] * log(w[i]) for i in eachindex(cone.alpha)))
     # violation term
-    wi2au = cone.wi2a - abs2(u)
-    cone.grad[1] = 2 * u / wi2au
-    @. cone.grad[2:end] = -cone.alphawi * cone.wi2a / wi2au
-    @. cone.grad[2:end] -= (1 - cone.alpha) / w
+    wi2au = cone.wi2a - sum(abs2, u)
+    @. cone.grad[1:m] = u * 2 / wi2au
+    @. cone.grad[(m + 1):end] = -cone.alphawi * cone.wi2a / wi2au
+    @. cone.grad[(m + 1):end] -= (1 - cone.alpha) / w
     cone.grad_updated = true
     return cone.grad
 end
 
 function update_hess(cone::GeneralizedPower)
     @assert cone.grad_updated
-    u = cone.point[1]
-    w = view(cone.point, 2:cone.dim)
+    m = cone.m
+    u = cone.point[1:m]
+    w = view(cone.point, (m + 1):cone.dim)
     H = cone.hess.data
     alpha = cone.alpha
     alphawi = cone.alphawi
 
     wi2a = cone.wi2a
-    wi2au = cone.wi2a - abs2(u)
+    wi2au = cone.wi2a - sum(abs2, u)
     # ratio of product and violation
     wi2a_wi2au = wi2a / wi2au
 
-    H[1, 1] = 2 / wi2au + 4 * u^2 / wi2au^2
+    H .= 0
+    # double derivative wrt u
+    scal = 4 / wi2au / wi2au
+    offset = 2 / wi2au
+    for i in 1:m
+        H[i, i] = offset + scal * u[i]^2
+    end
     for j in eachindex(w)
-        j1 = j + 1
-        H[1, j1] = -4 * cone.alpha[j] * u * wi2a_wi2au / wi2au / w[j]
+        jm = j + m
+        # derivative wrt u and w
+        scal = -4 * cone.alpha[j] * wi2a_wi2au / wi2au / w[j]
+        for i in 1:m
+            H[i, jm] = scal * u[i]
+        end
+        # double derivative wrt w
         awj_ratio = alphawi[j] * wi2a_wi2au
         awj_ratio_ratio = awj_ratio * (wi2a_wi2au - 1)
         for i in 1:(j - 1)
-            H[i + 1, j1] = alphawi[i] * awj_ratio_ratio
+            im = i + m
+            H[im, jm] = alphawi[i] * awj_ratio_ratio
         end
-        H[j1, j1] = awj_ratio_ratio * alphawi[j] + (awj_ratio + (1 - alpha[j]) / w[j]) / w[j]
-        # H[j1, j1] = -awj_ratio_ratio * cone.grad[j1] + (1 - cone.alpha[j]) / w[j] / w[j]
+        H[jm, jm] = awj_ratio_ratio * alphawi[j] + (awj_ratio + (1 - alpha[j]) / w[j]) / w[j]
+        # H[jm, jm] = -awj_ratio_ratio * cone.grad[jm] + (1 - cone.alpha[j]) / w[j] / w[j]
     end
 
 
