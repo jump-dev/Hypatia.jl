99dbf473d30aa48385588ba5b9ffee7dc5fa84ab
Chris Coey <coey.chris@gmail.com>
2019-08-23 12:43:16 -0400

    remove ForwardDiff and DiffResults from src; remove references to epiperpower; slight refac in epiperexp
    Former-commit-id: ff9ebe7d605a93c43e1f39a94d90a5e092d813fd

--- src/Cones/epiperexp.jl
+++ src/Cones/epiperexp.jl
@@ -1,15 +1,11 @@
 #=
-Copyright 2018, Chris Coey and contributors
+Copyright 2018, Chris Coey, Lea Kapelevich and contributors
 
 (closure of) epigraph of perspective of sum of exponentials (n-dimensional exponential cone)
 (u in R, v in R_+, w in R^n) : u >= v*sum(exp.(w/v))
 
 barrier (guessed)
--log(v*log(u/v) - v*logsumexp(w/v)) - log(u) - log(v)
-= -log(log(u) - log(v) - logsumexp(w/v)) - log(u) - 2*log(v)
-
-TODO use the numerically safer way to evaluate LSE function
-TODO compare alternative barrier -log(u - v*sum(wi -> exp(wi/v), w)) - log(u) - log(v)
+-log(u - v*sum(wi -> exp(wi/v), w)) - log(u) - log(v)
 =#
 
 mutable struct EpiPerExp{T <: Real} <: Cone{T}
@@ -54,9 +50,8 @@ function setup_data(cone::EpiPerExp{T}) where {T <: Real}
     cone.point = zeros(T, dim)
     cone.grad = zeros(T, dim)
     cone.hess = Symmetric(zeros(T, dim, dim), :U)
-    cone.tmp_hess = Symmetric(zeros(T, dim, dim), :U)
-
     cone.dzdw = zeros(T, dim - 2)
+    cone.tmp_hess = Symmetric(zeros(T, dim, dim), :U)
     cone.hess_fact_cache = nothing
     return
 end
@@ -65,7 +60,7 @@ get_nu(cone::EpiPerExp) = 3
 
 function set_initial_point(arr::AbstractVector{T}, cone::EpiPerExp{T}) where {T <: Real}
     @. arr = -log(T(cone.dim - 2))
-    arr[1] = 2
+    arr[1] = exp(1)
     arr[2] = 1
     return arr
 end
@@ -97,16 +92,11 @@ function update_grad(cone::EpiPerExp)
     sumwexp = sum(wi -> wi * exp(wi / v), w)
     cone.sumwexp = sumwexp
     uvlse = cone.uvlse
-
-    # derivative of uvlse wrt v
-    dzdv = (sumwexp / v / sumexp - 1) / v
-    cone.dzdv = dzdv
-
+    cone.dzdv = (sumwexp / v / sumexp - 1) / v # derivative of uvlse wrt v
     cone.grad[1] = -(inv(uvlse) + 1) / u
-    cone.grad[2] = -dzdv / uvlse - 2 / v
+    cone.grad[2] = -cone.dzdv / uvlse - 2 / v
     @. cone.dzdw = exp(w / v) / v / sumexp
     @. cone.grad[3:end] = cone.dzdw / uvlse
-
     cone.grad_updated = true
     return cone.grad
 end
@@ -122,15 +112,15 @@ function update_hess(cone::EpiPerExp)
     dzdv = cone.dzdv
     dzdw = cone.dzdw
     sumexp = cone.sumexp
-    sumwexpvv = sumwexp / v / v
-    sumwsqrexpvv = sum(wi -> abs2(wi) * exp(wi / v), w) / v / v
-
     H = cone.hess.data
-    H[1, 1] = (inv(abs2(uvlse)) + inv(uvlse) + 1) / u / u
+
+    H[1, 1] = ((inv(uvlse) + 1) / uvlse + 1) / u / u
     H[1, 2] = dzdv / uvlse / uvlse / u
     @views @. H[1, 3:cone.dim] = -dzdw / u / uvlse / uvlse
 
     # the derivative of sum(w / v ^ 2 .* exp.(w / v)) with respect to v, unscaled by inv(v ^ 2)
+    sumwexpvv = sumwexp / v / v
+    sumwsqrexpvv = sum(wi -> abs2(wi) * exp(wi / v), w) / v / v
     dsumwexpvvdv = -(sumwsqrexpvv + 2 * sumwexp / v)
     H[2, 2] = -(dzdv / uvlse + 1 / v) / v
     H[2, 2] += (sumwexp * (dzdv / uvlse - sumwexpvv / sumexp) - dsumwexpvvdv) / v / v / sumexp
@@ -152,7 +142,7 @@ function update_hess(cone::EpiPerExp)
     for j in eachindex(w)
         j2 = j + 2
         for i in 1:j
-            H[2 + i, j2] = dzdw[i] * dzdw[j] * (inv(abs2(uvlse)) - inv(uvlse))
+            H[2 + i, j2] = dzdw[i] * dzdw[j] * (inv(uvlse) - 1) / uvlse
         end
         H[j2, j2] += dzdw[j] / v / uvlse
     end
--- test/runbarriertests.jl
+++ test/runbarriertests.jl
@@ -9,7 +9,7 @@ include(joinpath(@__DIR__, "barrier.jl"))
 real_types = [
     Float64,
     Float32,
-    BigFloat, # NOTE can only use BLAS floats with ForwardDiff barriers, see https://github.com/JuliaDiff/DiffResults.jl/pull/9#issuecomment-497853361
+    BigFloat,
     ]
 
 barrier_testfuns = [
@@ -17,9 +17,8 @@ barrier_testfuns = [
     test_epinorminf_barrier,
     test_epinormeucl_barrier,
     test_epipersquare_barrier,
-    test_epiperpower_barrier, # fails with BigFloat
     test_hypoperlog_barrier,
-    test_epiperexp_barrier, # fails with BigFloat
+    test_epiperexp_barrier,
     test_power_barrier,
     test_hypogeomean_barrier,
     test_epinormspectral_barrier,
@@ -33,8 +32,5 @@ barrier_testfuns = [
     ]
 
 @testset "barrier functions tests: $t, $T" for t in barrier_testfuns, T in real_types
-    if T == BigFloat && t in (test_epiperpower_barrier, test_epiperexp_barrier) #, test_wsospolyinterpmat_barrier, test_wsospolyinterpsoc_barrier)
-        continue
-    end
     t(T)
 end
