6178d9a3b00fabea4eff5855ea54c2a9a668c05c
lkapelevich <leakapelevich@gmail.com>
2019-08-23 21:42:01 -0400

    uncomment tests
    Former-commit-id: b18abeb0790b3b1749d94b226d2b91affbc2eb8d

--- test/runnativetests.jl
+++ test/runnativetests.jl
@@ -7,17 +7,19 @@ include(joinpath(@__DIR__, "native.jl"))
 const MO = Hypatia.Models
 const SO = Hypatia.Solvers
 
+@info("starting native tests")
+
 real_types = [
     Float64,
-    # Float32,
-    # BigFloat,
+    Float32,
+    BigFloat,
     ]
 
 system_solvers = [
     SO.QRCholCombinedHSDSystemSolver,
-    # SO.SymIndefCombinedHSDSystemSolver,
-    # SO.NaiveElimCombinedHSDSystemSolver,
-    # SO.NaiveCombinedHSDSystemSolver,
+    SO.SymIndefCombinedHSDSystemSolver,
+    SO.NaiveElimCombinedHSDSystemSolver,
+    SO.NaiveCombinedHSDSystemSolver,
     ]
 
 testfuns_preproc = [
@@ -27,15 +29,14 @@ testfuns_preproc = [
     inconsistent2,
     ]
 
-# @info("starting preprocessing tests")
-# @testset "preprocessing tests: $t, $s, $T" for t in testfuns_preproc, s in system_solvers, T in real_types
-#     test_options = (
-#         linear_model = MO.PreprocessedLinearModel,
-#         system_solver = s,
-#         solver_options = (verbose = true,),
-#         )
-#     t(T, test_options)
-# end
+@testset "preprocessing tests: $t, $s, $T" for t in testfuns_preproc, s in system_solvers, T in real_types
+    test_options = (
+        linear_model = MO.PreprocessedLinearModel,
+        system_solver = s,
+        solver_options = (verbose = true,),
+        )
+    t(T, test_options)
+end
 
 linear_models = [
     MO.PreprocessedLinearModel,
@@ -48,48 +49,52 @@ use_infty_nbhd = [
     ]
 
 testfuns_raw = [
-    # orthant1,
-    # orthant2,
-    # orthant3,
-    # orthant4,
-    # epinorminf1,
-    # epinorminf2,
-    # epinorminf3,
-    # epinorminf4,
-    # epinorminf5,
-    # epinormeucl1,
-    # epinormeucl2,
-    # epipersquare1,
-    # epipersquare2,
-    # epipersquare3,
-    # hypoperlog1,
-    # # hypoperlog2,
-    # hypoperlog3,
-    # hypoperlog4,
-    # hypoperlog5,
-    # hypoperlog6,
+    orthant1,
+    orthant2,
+    orthant3,
+    orthant4,
+    epinorminf1,
+    epinorminf2,
+    epinorminf3,
+    epinorminf4,
+    epinorminf5,
+    epinormeucl1,
+    epinormeucl2,
+    epipersquare1,
+    epipersquare2,
+    epipersquare3,
+    hypoperlog1,
+    hypoperlog2,
+    hypoperlog3,
+    hypoperlog4,
+    hypoperlog5,
+    hypoperlog6,
+    epiperpower1,
+    epiperpower2,
     epiperexp1,
     epiperexp2,
     power1,
     power2,
     power3,
     power4,
-    # hypogeomean1,
-    # hypogeomean2,
-    # hypogeomean3,
-    # epinormspectral1,
-    # possemideftri1,
-    # possemideftri2,
-    # possemideftricomplex1,
-    # hypoperlogdettri1,
-    # hypoperlogdettri2,
-    # hypoperlogdettri3,
+    hypogeomean1,
+    hypogeomean2,
+    hypogeomean3,
+    epinormspectral1,
+    possemideftri1,
+    possemideftri2,
+    possemideftricomplex1,
+    hypoperlogdettri1,
+    hypoperlogdettri2,
+    hypoperlogdettri3,
     ]
 
-@info("starting native tests")
 @testset "native tests: $t, $s, $m, $n, $T" for t in testfuns_raw, s in system_solvers, m in linear_models, n in use_infty_nbhd, T in real_types
-    if T == BigFloat && t == epinormspectral1
-        continue # cannot get svdvals with BigFloat
+    if T == BigFloat && t in (epiperpower1, epiperpower2, epiperexp1, epiperexp2, epinormspectral1)
+        continue # ForwardDiff does not work with BigFloat, cannot get svdvals with BigFloat
+    end
+    if T == BigFloat && m == MO.RawLinearModel
+        continue # IterativeSolvers does not work with BigFloat
     end
     if s == SO.QRCholCombinedHSDSystemSolver && m == MO.RawLinearModel
         continue # QRChol linear system solver needs preprocessed model
@@ -105,8 +110,7 @@ testfuns_raw = [
     t(T, test_options)
 end
 
-@info("starting native tests (iterative linear system methods)")
-@testset "native tests (iterative linear system methods): $t, $T" for t in testfuns_raw, T in real_types
+@testset "native tests (iterative linear system solves): $t, $T" for t in testfuns_raw, T in real_types
     if T == BigFloat
         continue # IterativeSolvers does not work with BigFloat
     end
