becb1ccc90e706658cacc1456bad0f1332b7894e
Chris Coey <coey.chris@gmail.com>
2019-09-13 17:26:13 -0400

    update naiveelim and fix math descriptions
    Former-commit-id: 14d28003d133778f119e4ac7dc46cab24ee13ab0

--- test/runnativetests.jl
+++ test/runnativetests.jl
@@ -17,17 +17,17 @@ real_types = [
 system_solvers = [
     # SO.QRCholSystemSolver,
     # SO.SymIndefSystemSolver,
-    # SO.NaiveElimSystemSolver,
+    SO.NaiveElimSystemSolver,
     SO.NaiveSystemSolver,
     ]
 
 use_infty_nbhd = [
     true,
-    false,
+    # false,
     ]
 
 preprocess = [
-    true,
+    # true,
     false
     ]
 
@@ -83,10 +83,10 @@ testfuns_raw = [
     dualinfeas3,
     ]
 
-@info("starting preprocessing tests")
-@testset "preprocessing tests: $t, $s, $T" for t in testfuns_preproc, s in system_solvers, T in real_types
-    t(T, solver = SO.Solver{T}(verbose = true, system_solver = s{T}()))
-end
+# @info("starting preprocessing tests")
+# @testset "preprocessing tests: $t, $s, $T" for t in testfuns_preproc, s in system_solvers, T in real_types
+#     t(T, solver = SO.Solver{T}(verbose = true, system_solver = s{T}()))
+# end
 
 @info("starting miscellaneous tests")
 @testset "miscellaneous tests: $t, $s, $n, $p, $T" for t in testfuns_raw, s in system_solvers, n in use_infty_nbhd, p in preprocess, T in real_types
@@ -96,10 +96,10 @@ end
     t(T, solver = solver)
 end
 
-@info("starting iterative system solver tests")
-@testset "iterative system solver tests: $t, $T" for t in testfuns_raw, T in real_types
-    T == BigFloat && continue # IterativeSolvers does not work with BigFloat
-    solver = SO.Solver{T}(verbose = true, init_use_iterative = true, preprocess = false,
-        system_solver = SO.NaiveSystemSolver{T}(use_iterative = true))
-    t(T, solver = solver)
-end
+# @info("starting iterative system solver tests")
+# @testset "iterative system solver tests: $t, $T" for t in testfuns_raw, T in real_types
+#     T == BigFloat && continue # IterativeSolvers does not work with BigFloat
+#     solver = SO.Solver{T}(verbose = true, init_use_iterative = true, preprocess = false,
+#         system_solver = SO.NaiveSystemSolver{T}(use_iterative = true))
+#     t(T, solver = solver)
+# end
