547c2d0d4bf0464a80de14b807225e579360e23d
Chris Coey <coey.chris@gmail.com>
2019-09-16 17:33:07 -0400

    reenable tests

--- test/runnativetests.jl
+++ test/runnativetests.jl
@@ -28,7 +28,7 @@ use_infty_nbhd = [
 
 preprocess = [
     true,
-    # false
+    false
     ]
 
 testfuns_preproc = [
@@ -39,60 +39,60 @@ testfuns_preproc = [
     ]
 
 testfuns_raw = [
-    # orthant1,
-    # orthant2,
-    # orthant3,
-    # orthant4,
-    # epinorminf1,
-    # epinorminf2,
-    # epinorminf3,
-    # epinorminf4,
-    # epinorminf5,
-    # epinormeucl1,
-    # epinormeucl2,
-    # epipersquare1,
-    # epipersquare2,
-    # epipersquare3,
-    # hypoperlog1,
-    # hypoperlog2,
-    # hypoperlog3,
-    # hypoperlog4,
-    # hypoperlog5,
-    # hypoperlog6,
-    # epiperexp1,
-    # epiperexp2,
-    # power1,
-    # power2,
-    # power3,
-    # power4,
-    # hypogeomean1,
-    # hypogeomean2,
-    # hypogeomean3,
+    orthant1,
+    orthant2,
+    orthant3,
+    orthant4,
+    epinorminf1,
+    epinorminf2,
+    epinorminf3,
+    epinorminf4,
+    epinorminf5,
+    epinormeucl1,
+    epinormeucl2,
+    epipersquare1,
+    epipersquare2,
+    epipersquare3,
+    hypoperlog1,
+    hypoperlog2,
+    hypoperlog3,
+    hypoperlog4,
+    hypoperlog5,
+    hypoperlog6,
+    epiperexp1,
+    epiperexp2,
+    power1,
+    power2,
+    power3,
+    power4,
+    hypogeomean1,
+    hypogeomean2,
+    hypogeomean3,
     epinormspectral1,
-    # possemideftri1,
-    # possemideftri2,
-    # possemideftricomplex1,
-    # hypoperlogdettri1,
-    # hypoperlogdettri2,
-    # hypoperlogdettri3,
-    # primalinfeas1,
-    # primalinfeas2,
-    # primalinfeas3,
-    # dualinfeas1,
-    # dualinfeas2,
-    # dualinfeas3,
+    possemideftri1,
+    possemideftri2,
+    possemideftricomplex1,
+    hypoperlogdettri1,
+    hypoperlogdettri2,
+    hypoperlogdettri3,
+    primalinfeas1,
+    primalinfeas2,
+    primalinfeas3,
+    dualinfeas1,
+    dualinfeas2,
+    dualinfeas3,
     ]
 
-# @info("starting preprocessing tests")
-# @testset "preprocessing tests: $t, $s, $T" for t in testfuns_preproc, s in system_solvers, T in real_types
-#     t(T, solver = SO.Solver{T}(verbose = true, system_solver = s{T}()))
-# end
+@info("starting preprocessing tests")
+@testset "preprocessing tests: $t, $s, $T" for t in testfuns_preproc, s in system_solvers, T in real_types
+    t(T, solver = SO.Solver{T}(verbose = true, system_solver = s{T}()))
+end
 
 @info("starting miscellaneous tests")
 @testset "miscellaneous tests: $t, $s, $n, $p, $T" for t in testfuns_raw, s in system_solvers, n in use_infty_nbhd, p in preprocess, T in real_types
     T == BigFloat && t == epinormspectral1 && continue # Cannot get svdvals with BigFloat
     !p && s == SO.QRCholSystemSolver && continue # Must use preprocessing if using QRCholSystemSolver
-    solver = SO.Solver{T}(verbose = true, preprocess = p, use_infty_nbhd = n, system_solver = s{T}())
+    solver = SO.Solver{T}(verbose = false, preprocess = p, use_infty_nbhd = n, system_solver = s{T}())
     t(T, solver = solver)
 end
 
