7651e5d834be2f778bd6c650bf5de7a1cd72c9ae
Chris Coey <coey.chris@gmail.com>
2019-09-21 18:00:35 -0400

    update naiveelim for dispatch

--- src/Solvers/homogeneous_self_dual/naiveelim.jl
+++ src/Solvers/homogeneous_self_dual/naiveelim.jl
@@ -30,97 +30,10 @@ A'*y + G'*z + c*tau = xrhs
 (pr bar) -mu*H_k*G_k*x + z_k + mu*H_k*h_k*tau = mu*H_k*zrhs_k + srhs_k
 (du bar) -G_k*x + mu*H_k*z_k + h_k*tau = zrhs_k + srhs_k
 -c'x - b'y - h'z + mu/(taubar^2)*tau = taurhs + kaprhs
-
-TODO add iterative method
 =#
 
-mutable struct NaiveElimSystemSolver{T <: Real} <: SystemSolver{T}
-    use_indirect::Bool
-    use_sparse::Bool
+abstract type NaiveElimSystemSolver{T <: Real} <: SystemSolver{T} end
 
-    tau_row
-    lhs
-    lhs_copy
-    fact_cache
-
-    function NaiveElimSystemSolver{T}(; use_indirect::Bool = false, use_sparse::Bool = false) where {T <: Real}
-        system_solver = new{T}()
-        system_solver.use_indirect = use_indirect
-        system_solver.use_sparse = use_sparse
-        return system_solver
-    end
-end
-
-# create the system_solver cache
-function load(system_solver::NaiveElimSystemSolver{T}, solver::Solver{T}) where {T <: Real}
-    model = solver.model
-    (n, p, q) = (model.n, model.p, model.q)
-    system_solver.tau_row = n + p + q + 1
-
-    if system_solver.use_indirect
-        error("not implemented")
-    else
-        if system_solver.use_sparse
-            system_solver.lhs = T[
-                spzeros(T,n,n)  model.A'        model.G'              model.c;
-                -model.A        spzeros(T,p,p)  spzeros(T,p,q)        model.b;
-                -model.G        spzeros(T,q,p)  sparse(one(T)*I,q,q)  model.h;
-                -model.c'       -model.b'       -model.h'             one(T);
-                ]
-            dropzeros!(system_solver.lhs)
-            @assert issparse(system_solver.lhs)
-        else
-            system_solver.lhs_copy = T[
-                zeros(T,n,n)  model.A'      model.G'              model.c;
-                -model.A      zeros(T,p,p)  zeros(T,p,q)          model.b;
-                -model.G      zeros(T,q,p)  Matrix(one(T)*I,q,q)  model.h;
-                -model.c'     -model.b'     -model.h'             one(T);
-                ]
-            system_solver.lhs = similar(system_solver.lhs_copy)
-            # system_solver.fact_cache = HypLUSolveCache(system_solver.sol, system_solver.lhs, rhs)
-        end
-    end
-
-    return system_solver
-end
-
-# update the LHS factorization to prepare for solve
-function update_fact(system_solver::NaiveElimSystemSolver{T}, solver::Solver{T}) where {T <: Real}
-    system_solver.use_indirect && return system_solver
-
-    model = solver.model
-    (n, p, q) = (model.n, model.p, model.q)
-    lhs = system_solver.lhs
-
-    if !system_solver.use_sparse
-        copyto!(lhs, system_solver.lhs_copy)
-    end
-
-    lhs[end, end] = solver.mu / solver.tau / solver.tau
-    for (k, cone_k) in enumerate(model.cones)
-        idxs_k = model.cone_idxs[k]
-        z_rows_k = (n + p) .+ idxs_k
-        if Cones.use_dual(cone_k)
-            # -G_k*x + mu*H_k*z_k + h_k*tau = zrhs_k + srhs_k
-            lhs[z_rows_k, z_rows_k] .= Cones.hess(cone_k)
-        else
-            # -mu*H_k*G_k*x + z_k + mu*H_k*h_k*tau = mu*H_k*zrhs_k + srhs_k
-            @views Cones.hess_prod!(lhs[z_rows_k, 1:n], model.G[idxs_k, :], cone_k)
-            @. lhs[z_rows_k, 1:n] *= -1
-            @views Cones.hess_prod!(lhs[z_rows_k, end], model.h[idxs_k], cone_k)
-        end
-    end
-
-    if system_solver.use_sparse
-        system_solver.fact_cache = lu(system_solver.lhs)
-    else
-        system_solver.fact_cache = lu!(system_solver.lhs) # TODO use wrapped lapack function
-    end
-
-    return system_solver
-end
-
-# solve system without outer iterative refinement
 function solve_system(system_solver::NaiveElimSystemSolver{T}, solver::Solver{T}, sol, rhs) where {T <: Real}
     model = solver.model
     (n, p, q) = (model.n, model.p, model.q)
@@ -145,23 +58,7 @@ function solve_system(system_solver::NaiveElimSystemSolver{T}, solver::Solver{T}
     # -c'x - b'y - h'z + mu/(taubar^2)*tau = taurhs + kaprhs
     @. @views rhs4[end, :] += rhs[end, :]
 
-    if system_solver.use_indirect
-        error("not implemented")
-        # for j in 1:size(rhs, 2)
-        #     rhs_j = view(rhs4, :, j)
-        #     sol_j = view(sol4, :, j)
-        #     IterativeSolvers.gmres!(sol_j, system_solver.lhs, rhs_j, restart = tau_row)
-        # end
-    else
-        if system_solver.use_sparse
-            sol4 .= system_solver.fact_cache \ rhs4
-        else
-            # if !hyp_lu_solve!(system_solver.fact_cache, sol4, lhs, rhs4)
-            #     @warn("numerical failure: could not fix linear solve failure")
-            # end
-            ldiv!(sol4, system_solver.fact_cache, rhs4)
-        end
-    end
+    ldiv!(sol4, system_solver.fact_cache, rhs4)
 
     # lift to get s and kap
     tau = sol4[end:end, :]
@@ -178,4 +75,54 @@ function solve_system(system_solver::NaiveElimSystemSolver{T}, solver::Solver{T}
     @. @views sol[end:end, :] = -solver.mu / solver.tau * tau / solver.tau + rhs[end:end, :]
 
     return sol
+end
+
+#=
+direct dense
+=#
+
+mutable struct NaiveElimDenseSystemSolver{T <: Real} <: NaiveElimSystemSolver{T}
+    tau_row
+    lhs
+    lhs_copy
+    fact_cache
+    NaiveElimDenseSystemSolver{T}() where {T <: Real} = new{T}()
+end
+
+function load(system_solver::NaiveElimDenseSystemSolver{T}, solver::Solver{T}) where {T <: Real}
+    model = solver.model
+    (n, p, q) = (model.n, model.p, model.q)
+    system_solver.tau_row = n + p + q + 1
+    system_solver.lhs_copy = T[
+        zeros(T,n,n)  model.A'      model.G'              model.c;
+        -model.A      zeros(T,p,p)  zeros(T,p,q)          model.b;
+        -model.G      zeros(T,q,p)  Matrix(one(T)*I,q,q)  model.h;
+        -model.c'     -model.b'     -model.h'             one(T);
+        ]
+    system_solver.lhs = similar(system_solver.lhs_copy)
+    # system_solver.fact_cache = HypLUSolveCache(system_solver.sol, system_solver.lhs, rhs)
+    return system_solver
+end
+
+function update_fact(system_solver::NaiveElimDenseSystemSolver{T}, solver::Solver{T}) where {T <: Real}
+    model = solver.model
+    (n, p, q) = (model.n, model.p, model.q)
+    lhs = system_solver.lhs
+    copyto!(lhs, system_solver.lhs_copy)
+    lhs[end, end] = solver.mu / solver.tau / solver.tau
+    for (k, cone_k) in enumerate(model.cones)
+        idxs_k = model.cone_idxs[k]
+        z_rows_k = (n + p) .+ idxs_k
+        if Cones.use_dual(cone_k)
+            # -G_k*x + mu*H_k*z_k + h_k*tau = zrhs_k + srhs_k
+            lhs[z_rows_k, z_rows_k] .= Cones.hess(cone_k)
+        else
+            # -mu*H_k*G_k*x + z_k + mu*H_k*h_k*tau = mu*H_k*zrhs_k + srhs_k
+            @views Cones.hess_prod!(lhs[z_rows_k, 1:n], model.G[idxs_k, :], cone_k)
+            @. lhs[z_rows_k, 1:n] *= -1
+            @views Cones.hess_prod!(lhs[z_rows_k, end], model.h[idxs_k], cone_k)
+        end
+    end
+    system_solver.fact_cache = lu!(system_solver.lhs) # TODO use wrapped lapack function
+    return system_solver
 end
