e4aa66898270350f937909d98492444069a8cbc5
lkapelevich <leakapelevich@gmail.com>
2019-09-27 22:42:56 -0400

    move init code

--- src/Hypatia.jl
+++ src/Hypatia.jl
@@ -11,7 +11,12 @@ using LinearAlgebra
 include("linearalgebra/blockmatrix.jl")
 include("linearalgebra/dense.jl")
 include("linearalgebra/sparse.jl")
-include("init.jl")
+
+import Requires
+function __init__()
+    Requires.@require Pardiso = "46dd5b70-b6fb-5a00-ae2d-e8fea33afaf2" include(joinpath(@__DIR__(), "linearalgebra", "Pardiso.jl"))
+end
+
 
 # submodules
 include("ModelUtilities/ModelUtilities.jl")
