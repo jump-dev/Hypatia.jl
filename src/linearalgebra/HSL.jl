69279d44b9f63359397b6c0bf68e54a93a282eb4
lkapelevich <leakapelevich@gmail.com>
2019-09-29 19:18:33 -0400

    symmetric hsl cache passes tests

--- src/linearalgebra/HSL.jl
+++ src/linearalgebra/HSL.jl
@@ -2,19 +2,21 @@
 Copyright 2019, Chris Coey, Lea Kapelevich and contributors
 =#
 
-mutable struct HSLSymCache{T <: Union{Float32, Float64}} <: SparseSymCache{T}
+import LinearAlgebra.BlasReal
+
+mutable struct HSLSymCache{T <: BlasReal} <: SparseSymCache{T}
     analyzed::Bool
     ma57::HSL.Ma57{T}
     diag_pert
-    function HSLSymCache{T}(; diag_pert = sqrt(eps(T))) where {T <: Union{Float32, Float64}}
+    function HSLSymCache{T}(; diag_pert = zero(T)) where {T <: BlasReal}
         cache = new{T}()
         cache.analyzed = false
         cache.diag_pert = diag_pert
         return cache
     end
 end
-HSLSymCache{T}(; diag_pert = zero(T)) where {T <: Real} = error("HSL only works with real type Float64 or Float32")
-HSLSymCache(; diag_pert::Float64 = sqrt(eps(Float64))) = HSLSymCache{Float64}(diag_pert = diag_pert)
+HSLSymCache{T}() where {T <: Real} = error("HSL only works with real type Float64 or Float32")
+HSLSymCache(; diag_pert::Float64 = zero(Float64)) = HSLSymCache{Float64}(diag_pert = diag_pert)
 
 int_type(::HSLSymCache) = Int
 
@@ -22,23 +24,26 @@ function update_sparse_fact(cache::HSLSymCache, A::SparseMatrixCSC{<:HSL.Ma57Dat
     if !cache.analyzed
         cache.ma57 = HSL.Ma57(A)
         cache.analyzed = true
+    else
+        copyto!(cache.ma57.vals, A.nzval)
     end
     HSL.ma57_factorize(cache.ma57)
     return
 end
 
-function solve_sparse_system(cache::HSLSymCache, x::Matrix{Float64}, A::SparseMatrixCSC{<:HSL.Ma57Data, Int}, b::Matrix{Float64})
-    # TODO iterative refinement
-    ma57 = cache.ma57
-    # solve with 2 iterative refinement steps
-    HSL.ma57_solve!(ma57, b)
-    copyto!(x, b)
+using DataFrames, CSV
 
+function solve_sparse_system(cache::HSLSymCache, x::Matrix{Float64}, A::SparseMatrixCSC{<:HSL.Ma57Data, Int}, b::Matrix{Float64})
+    # NOTE MA57 only has the option to take iterative refinement steps for a single-column RHS
+    ma57 = cache.ma57
+    copyto!(x, b)
+    HSL.ma57_solve!(ma57, x)
+    
     # info we could print
     # ma57.info.backward_error1
     # ma57.info.backward_error2
     # ma57.info.matrix_inf_norm
-    # ma57.info.solution_inf_norm
+    # @show ma57.info.solution_inf_norm
     # ma57.info.scaled_residuals
     # ma57.info.cond1
     # ma57.info.cond2
