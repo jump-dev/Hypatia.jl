fd702d9de15d19c87b661efd86bbf0c4f891fbda
Chris Coey <coey.chris@gmail.com>
2019-10-04 21:00:53 -0400

    progress on reduction

--- test/runnativetests.jl
+++ test/runnativetests.jl
@@ -8,7 +8,7 @@ const SO = Hypatia.Solvers
 
 testfuns_no_preproc = [
     orthant1,
-    # epinorminf1,
+    epinorminf1,
     # epinormeucl1,
     # epipersquare1,
     # hypoperlog1,
@@ -77,10 +77,15 @@ testfuns = [
     dualinfeas3,
     ]
 
-real_types = [
+generic_reals = [
+    Float64,
+    # Float32,
+    # BigFloat,
+    ]
+
+blas_reals = [
     Float64,
     Float32,
-    BigFloat,
     ]
 
 options = (verbose = true,)
@@ -88,25 +93,30 @@ options = (verbose = true,)
 @info("starting native tests")
 @testset "native tests" begin
     # # test with and without preprocessing
-    # @testset "no preprocessing tests: $t, $T" for t in testfuns_no_preproc, T in real_types
-    #     t(T, solver = SO.Solver{T}(preprocess = false, system_solver = SO.SymIndefDenseSystemSolver{T}(); options...)) # TODO make default system solver depend on preprocess
+    # @testset "no preprocessing tests: $t, $T" for t in testfuns_no_preproc, T in generic_reals
+    #     t(T, solver = SO.Solver{T}(preprocess = false, init_use_indirect = false, reduce = false, system_solver = SO.SymIndefDenseSystemSolver{T}(); options...)) # TODO make default system solver depend on preprocess
     # end
-    # @testset "preprocessing tests: $t, $T" for t in testfuns_preproc, T in real_types
-    #     t(T, solver = SO.Solver{T}(preprocess = true; options...))
+    # @testset "preprocessing tests: $t, $T" for t in testfuns_preproc, T in generic_reals
+    #     t(T, solver = SO.Solver{T}(preprocess = true, init_use_indirect = false, reduce = false; options...))
+    # end
+
+    # # test indirect initial point method
+    # @testset "indirect initialization tests: $t, $T" for t in testfuns_no_preproc, T in blas_reals
+    #     t(T, solver = SO.Solver{T}(preprocess = false, init_use_indirect = true, reduce = false, system_solver = SO.SymIndefDenseSystemSolver{T}(); options...))
     # end
 
     # test with reduction (removing all primal equalities)
-    @testset "reduction tests: $t, $T" for t in testfuns_reduce, T in real_types
-        t(T, solver = SO.Solver{T}(preprocess = true, reduce = true; options...))
+    @testset "reduction tests: $t, $T" for t in testfuns_reduce, T in generic_reals
+        t(T, solver = SO.Solver{T}(preprocess = true, init_use_indirect = false, reduce = true, system_solver = SO.QRCholDenseSystemSolver{T}(); options...))
     end
 
     # # test with different neighborhood functions
-    # @testset "neighborhood function tests: $t, $T, $n" for t in testfuns, T in real_types, n in [true, false]
+    # @testset "neighborhood function tests: $t, $T, $n" for t in testfuns, T in generic_reals, n in [true, false]
     #     t(T, solver = SO.Solver{T}(use_infty_nbhd = n; options...))
     # end
     #
     # # test each system solver
-    # @testset "NaiveDense tests: $t, $T" for t in testfuns, T in real_types
+    # @testset "NaiveDense tests: $t, $T" for t in testfuns, T in generic_reals
     #     t(T, solver = SO.Solver{T}(system_solver = SO.NaiveDenseSystemSolver{T}(); options...))
     # end
     # @testset "NaiveSparse tests: $t" for t in testfuns
@@ -117,21 +127,21 @@ options = (verbose = true,)
     #     T = Float64
     #     t(T, solver = SO.Solver{T}(preprocess = false, init_use_indirect = true, system_solver = SO.NaiveIndirectSystemSolver{T}(); options...))
     # end
-    # @testset "NaiveElimDense tests: $t, $T, $h" for t in testfuns, T in real_types, h in [true, false]
+    # @testset "NaiveElimDense tests: $t, $T, $h" for t in testfuns, T in generic_reals, h in [true, false]
     #     t(T, solver = SO.Solver{T}(system_solver = SO.NaiveElimDenseSystemSolver{T}(use_inv_hess = h); options...))
     # end
     # @testset "NaiveElimSparse tests: $t" for t in testfuns
     #     T = Float64
     #     t(T, solver = SO.Solver{T}(system_solver = SO.NaiveElimSparseSystemSolver{T}(); options...))
     # end
-    # @testset "SymIndefDense tests: $t, $T, $h" for t in testfuns, T in real_types, h in [true, false]
+    # @testset "SymIndefDense tests: $t, $T, $h" for t in testfuns, T in generic_reals, h in [true, false]
     #     t(T, solver = SO.Solver{T}(system_solver = SO.SymIndefDenseSystemSolver{T}(use_inv_hess = h); options...))
     # end
     # @testset "SymIndefSparse tests: $t" for t in testfuns
     #     T = Float64
     #     t(T, solver = SO.Solver{T}(system_solver = SO.SymIndefSparseSystemSolver{T}(); options...))
     # end
-    # @testset "QRCholDense tests: $t, $T" for t in testfuns, T in real_types
+    # @testset "QRCholDense tests: $t, $T" for t in testfuns, T in generic_reals
     #     t(T, solver = SO.Solver{T}(system_solver = SO.QRCholDenseSystemSolver{T}(); options...))
     # end
 end
