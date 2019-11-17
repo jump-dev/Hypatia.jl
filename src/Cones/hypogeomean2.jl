7fd27a577a284cd6d7155972b54fd1d02596723c
lkapelevich <leakapelevich@gmail.com>
2019-11-17 16:29:14 -0500

    change what is cached
    Former-commit-id: 383acddda7b801a8778c98aa960836a581a3f188

--- src/Cones/hypogeomean2.jl
+++ src/Cones/hypogeomean2.jl
@@ -27,8 +27,8 @@ mutable struct HypoGeomean2{T <: Real} <: Cone{T}
     hess_fact_cache
 
     wprod::T
-    wwprodu::T
-    tmpnn::Matrix{T} # TODO why is this here?
+    wprodu::T
+    tmpn::Vector{T}
 
     function HypoGeomean2{T}(
         alpha::Vector{T},
@@ -59,7 +59,7 @@ function setup_data(cone::HypoGeomean2{T}) where {T <: Real}
     cone.hess = Symmetric(zeros(T, dim, dim), :U)
     cone.inv_hess = Symmetric(zeros(T, dim, dim), :U)
     load_matrix(cone.hess_fact_cache, cone.hess)
-    cone.tmpnn = zeros(T, dim - 1, dim - 1)
+    cone.tmpn = zeros(T, dim - 1)
     return
 end
 
@@ -91,15 +91,10 @@ function update_grad(cone::HypoGeomean2)
     u = cone.point[1]
     w = view(cone.point, 2:cone.dim)
     wprod = cone.wprod
-    wprodu = cone.wprod - u # TODO add field
+    wprodu = cone.wprodu = cone.wprod - u
     cone.grad[1] = inv(wprodu)
-    @. cone.grad[2:end] = -wprod * cone.alpha / w / wprodu - 1 / w
-    cone.wwprodu = wprod / wprodu
-
-    # cone.wiw = cone.wiaa / wiaau
-    # @. cone.a1ww = cone.alphaiw * (1 - cone.wiw)
-    # cone.grad[1] = -inv(wiaau) - inv(u)
-    # @. cone.grad[2:end] = cone.a1ww - inv(w)
+    @. cone.tmpn = wprod * cone.alpha / w / wprodu
+    @. cone.grad[2:end] = -cone.tmpn - 1 / w
     cone.grad_updated = true
     return cone.grad
 end
@@ -110,35 +105,21 @@ function update_hess(cone::HypoGeomean2)
     w = view(cone.point, 2:cone.dim)
     alpha = cone.alpha
     wprod = cone.wprod
-    wprodu = wprod - u
-    wwprodu = cone.wwprodu
+    wprodu = cone.wprodu
+    wwprodu = wprod / wprodu
+    tmpn = cone.tmpn
     H = cone.hess.data
 
     H[1, 1] = cone.grad[1] / wprodu
-    for j in eachindex(w)
+    @inbounds for j in eachindex(w)
         j1 = j + 1
-        fact = wwprodu * alpha[j] / w[j]
+        fact = tmpn[j]
         H[1, j1] = -fact / wprodu
-        for i in 1:(j - 1)
+        @inbounds for i in 1:(j - 1)
             H[i + 1, j1] = fact * alpha[i] / w[i] * (wwprodu - 1)
         end
         H[j1, j1] = fact * (1 - alpha[j] + alpha[j] * wwprodu) / w[j] + inv(w[j]) / w[j]
     end
-
-    # H .= 0
-
-    # wiaau = cone.wiaa + u
-    # H[1, 1] = inv(wiaau) / wiaau + inv(u) / u
-    # @inbounds for j in eachindex(w)
-    #     j1 = j + 1
-    #     wiwaw = -cone.wiw * cone.alpha[j] / w[j]
-    #     H[1, j1] = -wiwaw / wiaau
-    #     @inbounds for i in 1:(j - 1)
-    #         H[i + 1, j1] = wiwaw * cone.a1ww[i]
-    #     end
-    #     H[j1, j1] = wiwaw * cone.grad[j1] + (1 - cone.alpha[j]) / w[j] / w[j]
-    # end
-
     cone.hess_updated = true
     return cone.hess
 end
