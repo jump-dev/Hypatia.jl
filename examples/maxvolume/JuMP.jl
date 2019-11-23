862f544ddb77925bf6743ac499cbf4e99d0fd540
lkapelevich <leakapelevich@gmail.com>
2019-11-23 12:47:08 -0500

    clean
    Former-commit-id: f1ceb7f24952644c802049a4a4a49c908b3dc817

--- examples/maxvolume/JuMP.jl
+++ examples/maxvolume/JuMP.jl
@@ -30,20 +30,18 @@ function maxvolumeJuMP(
     elseif constr_cone == :soc
         # number of variables inside geometric mean is n
         # number of layers of variables
-        l = MathOptInterface.Bridges.Constraint.ilog2(n)
+        num_layers = MathOptInterface.Bridges.Constraint.ilog2(n)
         # number of new variables = 1 + 2 + ... + 2^(l - 1) = 2^l - 1
-        N = 2 ^ l
-        num_new_vars = N - 1
+        num_new_vars = 2 ^ num_layers - 1
         JuMP.@variable(model, new_vars[1:num_new_vars] >= 0)
-        # JuMP.@constraint(model, new_vars[1] >= 0)
-        rtN =√N
+        rtfact = sqrt(2 ^ num_layers)
         xl1 = new_vars[1]
-        JuMP.@constraint(model, t <= xl1 / rtN)
+        JuMP.@constraint(model, t <= xl1 / rtfact)
 
         function _getx(i)
             if i > n
                 # if we are beyond the number of variables in the actual geometric mean, we are adding the buffer variable
-                return xl1 / √N # bounds hypograph variable
+                return xl1 / rtfact # bounds hypograph variable
             else
                 # otherwise we are adding the (i+1)th variable
                 return end_pts[i]
@@ -52,15 +50,12 @@ function maxvolumeJuMP(
 
         offset = offset_next = 0
         # loop over layers, layer 1 describes hypograph variable
-        for i in 1:l
-            offset_next = offset + 2 ^ (i - 1) # **********************
+        for i in 1:num_layers
+            num_lvars = 2 ^ (i - 1)
+            offset_next = offset + num_lvars
             # loop over variables in each layer
-            @show i
-            for j in 1:(2 ^ (i - 1))
-                @show j
-                @show offset, offset_next
-                @show offset + j, offset_next + 2j - 1, offset_next + 2j
-                if i == l
+            for j in 1:num_lvars
+                if i == num_layers
                     # in the last layer, we use the original variables
                     a = _getx(2j - 1)
                     b = _getx(2j)
@@ -78,13 +73,11 @@ function maxvolumeJuMP(
         error("unknown cone $(constr_cone)")
     end
 
-    println(model)
-
     return (model = model,)
 end
 
-maxvolumeJuMP1() = maxvolumeJuMP(4, constr_cone = :geomean)
-maxvolumeJuMP2() = maxvolumeJuMP(4, constr_cone = :soc)
+maxvolumeJuMP1() = maxvolumeJuMP(8, constr_cone = :geomean)
+maxvolumeJuMP2() = maxvolumeJuMP(8, constr_cone = :soc)
 
 function test_maxvolumeJuMP(instance::Function; options, rseed::Int = 1)
     Random.seed!(rseed)
