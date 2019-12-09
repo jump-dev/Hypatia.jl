86ffc94d24fecdbcb5a0a1784000442ec1257f3b
Chris Coey <coey.chris@gmail.com>
2019-12-08 23:42:56 -0500

    update example
    Former-commit-id: e81a04a2e2f8abf4c7c618052c1a18eec4d66104

--- examples/matrixregression/native.jl
+++ examples/matrixregression/native.jl
@@ -321,6 +321,9 @@ end
 
 
 
+
+# matrixregression2(R::Type{<:RealOrComplex}) = matrixregression(R, 5, 3, 4)#, lam_fro = 0.1, lam_nuc = 0.1, lam_las = 0.1, lam_glc = 0.2, lam_glr = 0.2)
+
 # TODO delete
 T = Float64
 R = Complex{T}
@@ -328,8 +331,11 @@ R = Complex{T}
 options = (atol = 1e-2, solver = Hypatia.Solvers.Solver{T}(
     verbose = true, iter_limit = 250, time_limit = 12e2,
     tol_feas = 1e-5, tol_rel_opt = 1e-5, tol_abs_opt = 1e-5,
+    max_nbhd = 0.7,
     system_solver = Hypatia.Solvers.QRCholDenseSystemSolver{T}(),
+    # system_solver = Hypatia.Solvers.SymIndefDenseSystemSolver{T}(),
     ))
 
-instances = instances_matrixregression_all
+instances = instances_matrixregression_few
+# instances = [matrixregression1, matrixregression3, matrixregression5]
 @testset begin test_matrixregression.(instances, R = R, options = options) end
