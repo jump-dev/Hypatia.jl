c458bea339d8e8bb6d6ff6851d416059f1c4427d
Chris Coey <coey.chris@gmail.com>
2019-12-09 17:21:46 -0500

    add complex generalizations of Linf and spectral; add matrix regression example; taken from NTorthant branch

--- src/Cones/Cones.jl
+++ src/Cones/Cones.jl
@@ -211,4 +211,27 @@ function vec_to_mat_U!(mat::AbstractMatrix{Complex{T}}, vec::AbstractVector{T}) 
     return mat
 end
 
+# utilities for converting between real and complex vectors
+function rvec_to_cvec!(cvec::AbstractVector{Complex{T}}, rvec::AbstractVector{T}) where {T}
+    k = 1
+    # @inbounds for i in eachindex(cvec)
+    for i in eachindex(cvec)
+        cvec[i] = Complex(rvec[k], rvec[k + 1])
+        k += 2
+    end
+    return cvec
+end
+
+function cvec_to_rvec!(rvec::AbstractVector{T}, cvec::AbstractVector{Complex{T}}) where {T}
+    k = 1
+    # @inbounds for i in eachindex(cvec)
+    for i in eachindex(cvec)
+        ci = cvec[i]
+        rvec[k] = real(ci)
+        rvec[k + 1] = imag(ci)
+        k += 2
+    end
+    return rvec
+end
+
 end
