cfa93aeb12ee58eb29e356073e9f7a7bdd42412c
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2019-12-27 16:52:08 +1300

    extended formulation for rootdet expdesign in native
    Former-commit-id: cff9fbe2cd74631dccb4ffca255f2fcbcf560ac4

--- examples/expdesign/native.jl
+++ examples/expdesign/native.jl
@@ -87,7 +87,7 @@ function expdesign(
         G = vcat(G_nonneg, G_nmax, G_detcone)
         h = vcat(h_nonneg, h_nmax, h_detcone)
 
-        return (c = c, A = A, b = b, G = G, h = h, cones = cones)
+        return (c = c, A = A, b = b, G = G, h = h, cones = cones, p = p)
     end
 
     if geomean_obj
@@ -140,12 +140,75 @@ function expdesign(
             ]
         h = vcat(h_nonneg, h_nmax, zeros(2p + 1 + pq))
 
-        return (c = c, A = A, b = b, G = G, h = h, cones = cones)
+        return (c = c, A = A, b = b, G = G, h = h, cones = cones, p = p)
     end
 
+    if (rootdet_obj && !use_rootdet) || (logdet_obj && !use_logdet)
+        # requires an upper triangular matrix of additional variables, ordered row wise
+        num_trivars = CO.svec_length(q)
+
+        # vectorized dimension of psd matrix
+        dimvec = q * (2 * q + 1)
+        G_psd = zeros(T, dimvec, p + num_trivars)
+
+        # variables in upper triangular matrix numbered row-wise
+        diag_idx(i::Int) = (i == 1 ? 1 : 1 + sum(q - j for j in 0:(i - 2)))
+
+        # V*diag(np)*V
+        l = 1
+        for i in 1:q, j in 1:i
+            for k in 1:p
+                G_psd[l, k] = -V[i, k] * V[j, k]
+            end
+            l += 1
+        end
+        # [triangle' diag(triangle)]
+        tri_idx = 1
+        for i in 1:q
+            # triangle'
+            # skip zero-valued elements
+            l += i - 1
+            for j in i:q
+                G_psd[l, p + tri_idx] = -1
+                l += 1
+                tri_idx += 1
+            end
+            # diag(triangle)
+            # skip zero-valued elements
+            l += i - 1
+            G_psd[l, p + diag_idx(i)] = -1
+            l += 1
+        end
+        MU.vec_to_svec!(G_psd, rt2 = sqrt(T(2)))
+
+        h_psd = zeros(T, dimvec)
+        push!(cones, CO.PosSemidefTri{T, T}(dimvec))
+    end
+
+
     if rootdet_obj
-        # TODO extended
-    elseif logdet_obj
+        @assert !use_rootdet
+        c = vcat(zeros(T, p + num_trivars), -one(T))
+        A = [A zeros(T, 1, num_trivars + 1)]
+        h_geo = zeros(T, q + 1)
+        G_geo = zeros(T, q, num_trivars)
+        for i in 1:q
+            G_geo[i, sum(1:i)] = -1
+        end
+        push!(cones, CO.HypoGeomean{T}(fill(inv(T(q)), q)))
+        # all conic constraints
+        G = [
+            Matrix{T}(-I, p, p)    zeros(T, p, num_trivars + 1); # nonneg
+            Matrix{T}(-I, p, p)    zeros(T, p, num_trivars + 1); # nmax
+            G_psd    zeros(T, dimvec, 1); # psd
+            zeros(T, 1, p)    zeros(T, 1, num_trivars)    -one(T); # hypogeomean
+            zeros(T, q, p)    G_geo    zeros(T, q); # hypogeomean
+            ]
+        h = vcat(h_nonneg, h_nmax, h_psd, h_geo)
+        return (c = c, A = A, b = b, G = G, h = h, cones = cones, p = p)
+    end
+
+    if logdet_obj
         if use_logdet
             @assert use_linops
             A = BlockMatrix{T}(1, p + 1, [A], [1:1], [2:(p + 1)])
@@ -158,8 +221,6 @@ function expdesign(
                 )
             h = vcat(h_nonneg, h_nmax, h_detcone)
         else
-            # requires an upper triangular matrix of additional variables, ordered row wise
-            num_trivars = CO.svec_length(q)
             if use_sumlog
                 dimx = p + num_trivars + 1
                 padx = num_trivars + 1
@@ -172,43 +233,6 @@ function expdesign(
             end
             # maximize the sum of hypograph variables of all hypoperlog cones
             c = vcat(zeros(T, p + num_trivars), -ones(T, num_hypo))
-
-            # vectorized dimension of psd matrix
-            dimvec = q * (2 * q + 1)
-            G_psd = zeros(T, dimvec, dimx)
-
-            # variables in upper triangular matrix numbered row-wise
-            diag_idx(i::Int) = (i == 1 ? 1 : 1 + sum(q - j for j in 0:(i - 2)))
-
-            # V*diag(np)*V
-            l = 1
-            for i in 1:q, j in 1:i
-                for k in 1:p
-                    G_psd[l, k] = -V[i, k] * V[j, k]
-                end
-                l += 1
-            end
-            # [triangle' diag(triangle)]
-            tri_idx = 1
-            for i in 1:q
-                # triangle'
-                # skip zero-valued elements
-                l += i - 1
-                for j in i:q
-                    G_psd[l, p + tri_idx] = -1
-                    l += 1
-                    tri_idx += 1
-                end
-                # diag(triangle)
-                # skip zero-valued elements
-                l += i - 1
-                G_psd[l, p + diag_idx(i)] = -1
-                l += 1
-            end
-            MU.vec_to_svec!(G_psd, rt2 = sqrt(T(2)))
-
-            h_psd = zeros(T, dimvec)
-            push!(cones, CO.PosSemidefTri{T, T}(dimvec))
 
             if use_sumlog
                 G_logvars = zeros(T, q, num_trivars)
@@ -255,13 +279,18 @@ function expdesign(
                 G_nonneg = hcat(Matrix{T}(-I, p, p), zeros(T, p, padx))
                 G_nmax = hcat(Matrix{T}(-I, p, p), zeros(T, p, padx))
                 # all conic constraints
-                G = vcat(G_nonneg, G_nmax, G_psd, G_log)
+                G = [
+                    G_nonneg;
+                    G_nmax;
+                    G_psd    zeros(T, dimvec, num_hypo);
+                    G_log;
+                    ]
             end
             h = vcat(h_nonneg, h_nmax, h_psd, h_log)
         end
     end
 
-    return (c = c, A = A, b = b, G = G, h = h, cones = cones)
+    return (c = c, A = A, b = b, G = G, h = h, cones = cones, p = p)
 end
 
 expdesign1(T::Type{<:Real}) = expdesign(T, 25, 75, 125, 5, use_logdet = true, logdet_obj = true)
@@ -288,6 +317,11 @@ expdesign21(T::Type{<:Real}) = expdesign(T, 10, 30, 50, 5, geomean_obj = true)
 expdesign22(T::Type{<:Real}) = expdesign(T, 5, 15, 25, 5, geomean_obj = true)
 expdesign23(T::Type{<:Real}) = expdesign(T, 4, 8, 12, 3, geomean_obj = true)
 expdesign24(T::Type{<:Real}) = expdesign(T, 3, 5, 7, 2, geomean_obj = true)
+expdesign25(T::Type{<:Real}) = expdesign(T, 25, 75, 125, 5, use_rootdet = false, rootdet_obj = true)
+expdesign26(T::Type{<:Real}) = expdesign(T, 10, 30, 50, 5, use_rootdet = false, rootdet_obj = true)
+expdesign27(T::Type{<:Real}) = expdesign(T, 5, 15, 25, 5, use_rootdet = false, rootdet_obj = true)
+expdesign28(T::Type{<:Real}) = expdesign(T, 4, 8, 12, 3, use_rootdet = false, rootdet_obj = true)
+expdesign29(T::Type{<:Real}) = expdesign(T, 3, 5, 7, 2, use_rootdet = false, rootdet_obj = true)
 
 instances_expdesign_all = [
     expdesign1,
@@ -312,6 +346,11 @@ instances_expdesign_all = [
     expdesign22,
     expdesign23,
     expdesign24,
+    expdesign25,
+    expdesign26,
+    expdesign27,
+    expdesign28,
+    expdesign29,
     ]
 instances_expdesign_linops = [
     expdesign11,
@@ -323,14 +362,21 @@ instances_expdesign_few = [
     expdesign10,
     expdesign19,
     expdesign24,
+    expdesign29,
     ]
 
 function test_expdesign(instance::Function; T::Type{<:Real} = Float64, options::NamedTuple = NamedTuple(), rseed::Int = 1)
     Random.seed!(rseed)
     d = instance(T)
     r = Hypatia.Solvers.build_solve_check(d.c, d.A, d.b, d.G, d.h, d.cones; options...)
+    @show r.x[2:(d.p + 1)]
     @test r.status == :Optimal
     return
 end
 
 test_expdesign.(instances_expdesign_few)
+
+# [0.5892327034087503, 1.9848818350565813, 2.2756593473710542, 2.1502260256372683, 8.852634136857773e-8]
+# [1.9848817473638083, 2.2756593374286678, 2.150225974602002, 9.154905833419491e-8, 23.9316009078927]
+# [0.5892325002481775, 1.9848819598634755, 2.2756594566610353, 2.150226071709818, 1.151749451366868e-8]
+# [1.4000000006502067, 1.3999999992790264, 1.3999999998436734, 1.4000000000280783, 1.4000000001990138]
