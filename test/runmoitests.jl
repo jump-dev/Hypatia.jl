724180068b63e4ce5a081981fbe65b79ca044fca
Chris Coey <coey.chris@gmail.com>
2020-01-01 02:35:07 +1000

    start on testing cones defined for MOI
    Former-commit-id: 080503b8aee852b2847efe58bbd4b6a2d8dc6aed

--- test/runmoitests.jl
+++ test/runmoitests.jl
@@ -2,27 +2,47 @@
 Copyright 2019, Chris Coey and contributors
 =#
 
-include(joinpath(@__DIR__, "moi.jl"))
+# MOI wrapper Hypatia cone tests
 
-system_solvers = [
-    SO.NaiveDenseSystemSolver,
-    SO.NaiveSparseSystemSolver,
-    # SO.NaiveIndirectSystemSolver,
-    SO.NaiveElimDenseSystemSolver,
-    SO.NaiveElimSparseSystemSolver,
-    SO.SymIndefDenseSystemSolver,
-    SO.SymIndefSparseSystemSolver,
-    SO.QRCholDenseSystemSolver,
-    ]
+include(joinpath(@__DIR__, "moicones.jl"))
 
 real_types = [
     Float64,
-    # BigFloat, # TODO test when MOI allows
+    Float32,
     ]
 
-@info("starting MOI tests")
-@testset "MOI tests" begin
-    @testset "MOI tests: $s, $T" for s in system_solvers, T in real_types
-        test_moi(T, verbose = false, system_solver = s{T}())
+@info("starting MOI wrapper Hypatia cone tests")
+@testset "MOI wrapper Hypatia cone tests" begin
+    @testset "MOI wrapper Hypatia cone tests: $T" for T in real_types
+        test_moi_cones(T)
     end
 end
+
+# # MOI.Test linear and conic tests
+#
+# include(joinpath(@__DIR__, "moi.jl"))
+#
+# system_solvers = [
+#     SO.NaiveDenseSystemSolver,
+#     SO.NaiveSparseSystemSolver,
+#     # SO.NaiveIndirectSystemSolver,
+#     SO.NaiveElimDenseSystemSolver,
+#     SO.NaiveElimSparseSystemSolver,
+#     SO.SymIndefDenseSystemSolver,
+#     SO.SymIndefSparseSystemSolver,
+#     SO.QRCholDenseSystemSolver,
+#     ]
+#
+# real_types = [
+#     Float64,
+#     # TODO test when wrapper allows
+#     # Float32,
+#     # BigFloat,
+#     ]
+#
+# @info("starting MOI.Test tests")
+# @testset "MOI.Test tests" begin
+#     @testset "MOI.Test tests: $s, $T" for s in system_solvers, T in real_types
+#         test_moi(T, verbose = false, system_solver = s{T}())
+#     end
+# end
