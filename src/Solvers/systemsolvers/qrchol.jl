9d4189150cd243d9cce1e6b0ab60b705266d52b3
Chris Coey <coey.chris@gmail.com>
2019-12-31 11:08:19 +1000

    make qrchol more robust to bk failures
    Former-commit-id: cdf3fe0efd539dec5ce665c6cfd47cc0a63e315f

--- src/Solvers/systemsolvers/qrchol.jl
+++ src/Solvers/systemsolvers/qrchol.jl
@@ -241,9 +241,14 @@ function update_fact(system_solver::QRCholDenseSystemSolver{T}, solver::Solver{T
                 load_matrix(system_solver.fact_cache, system_solver.lhs1)
             else
                 system_solver.lhs1 += sqrt(eps(T)) * I # attempt recovery # TODO make more efficient
+                println("here")
             end
             if !update_fact(system_solver.fact_cache, system_solver.lhs1)
                 @warn("QRChol Bunch-Kaufman factorization failed after recovery")
+                system_solver.lhs1 += sqrt(eps(T)) * I # attempt recovery # TODO make more efficient
+                if !update_fact(system_solver.fact_cache, system_solver.lhs1)
+                    @warn("QRChol Bunch-Kaufman factorization failed twice after recovery")
+                end
             end
         end
     end
