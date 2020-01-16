078eac797f9d0790dfdebbffe0b88042427bc58c
lkapelevich <leakapelevich@gmail.com>
2020-01-16 13:21:37 -0500

    change constraints in conditionnum
    Former-commit-id: 247eb3169956b62183c4db90e7e2d56d29e33fba

--- examples/conditionnum/JuMP.jl
+++ examples/conditionnum/JuMP.jl
@@ -2,23 +2,24 @@
 Copyright 2020, Chris Coey, Lea Kapelevich and contributors
 
 minimize the condition number of positive definite matrix M(x) = M_0 + sum_i x_i*M_i
-subject to F(x) = F_0 + sum_i x_i*F_i in S_+
+subject to Ax >= b
 
-from section 3.2 "Linear Matrix Inequalities in System and Control Theory" by
+adapted from section 3.2 "Linear Matrix Inequalities in System and Control Theory" by
 S. Boyd, L. El Ghaoui, E. Feron, and V. Balakrishnan
 
 original formulation:
 min gamma
-F(x) in S_+
+Ax >= b
 mu >= 0
 M(x) - mu*I in S_+
 mu*gamma*I - M(x) in S_+
 
 introduce nu = inv(mu), y = x/mu:
 min gamma
-nu*F_0 + sum_i y_i*F_i in S_+
+Ay >= b * nu
 nu*M_0 + sum_i y_i*M_i - I in S_+
 gamma*I - nu*M_0 - sum_i y_i*M_i in S_+
+nu >= 0
 
 we make F_0 and M_0 positive definite to ensure existence of a feasible solution
 =#
@@ -44,30 +45,23 @@ function conditionnumJuMP(
     end
     M0 = randn(side, side)
     M0 = Symmetric(M0 * M0')
-    Fi = [Symmetric(randn(side, side)) for i in 1:len_y]
-    F0 = randn(side, side)
-    F0 = Symmetric(F0 * F0')
-    # choose to make some F_i matrices pd so several feasible solutions exists
-    pd_idxs = rand(1:len_y, max(1, div(len_y, 5)))
-    for i in pd_idxs
-        Fi[i] = Symmetric(Fi[i] * Fi[i]')
-    end
+    A = randn(div(len_y, 2), len_y)
+    b = randn(div(len_y, 2))
 
     model = JuMP.Model()
     JuMP.@variables(model, begin
         gamma
-        nu
+        nu >= 0
         y[1:len_y]
     end)
+    JuMP.@constraint(model, A * y .>= nu * b)
     if use_linmatrixineq
         JuMP.@constraints(model, begin
-            vcat(nu, y) in Hypatia.LinMatrixIneqCone{Float64}([F0, Fi...])
             vcat(-1, nu, y) in Hypatia.LinMatrixIneqCone{Float64}([I, M0, Mi...])
             vcat(gamma, -nu, -y) in Hypatia.LinMatrixIneqCone{Float64}([I, M0, Mi...])
         end)
     else
         JuMP.@constraints(model, begin
-            Symmetric(nu .* F0 + sum(y[i] .* Fi[i] for i in eachindex(y))) in JuMP.PSDCone()
             Symmetric(nu .* M0 + sum(y[i] .* Mi[i] for i in eachindex(y)) - I) in JuMP.PSDCone()
             Symmetric(gamma .* Matrix(I, side, side) - nu .* M0 - sum(y[i] .* Mi[i] for i in eachindex(y))) in JuMP.PSDCone()
         end)
@@ -81,8 +75,8 @@ conditionnumJuMP1() = conditionnumJuMP(5, 6, use_linmatrixineq = true)
 conditionnumJuMP2() = conditionnumJuMP(5, 6, use_linmatrixineq = false)
 conditionnumJuMP3() = conditionnumJuMP(10, 8, use_linmatrixineq = true)
 conditionnumJuMP4() = conditionnumJuMP(10, 8, use_linmatrixineq = false)
-conditionnumJuMP5() = conditionnumJuMP(50, 15, use_linmatrixineq = true)
-conditionnumJuMP6() = conditionnumJuMP(50, 15, use_linmatrixineq = false)
+conditionnumJuMP5() = conditionnumJuMP(50, 20, use_linmatrixineq = true)
+conditionnumJuMP6() = conditionnumJuMP(50, 20, use_linmatrixineq = false)
 
 function test_conditionnumJuMP(instance::Function; options, rseed::Int = 1)
     Random.seed!(rseed)
