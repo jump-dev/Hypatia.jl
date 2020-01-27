2b835912fabba1f08e6ce2cd391dd06e1dce900e
lkapelevich <leakapelevich@gmail.com>
2020-01-27 09:32:57 -0500

    add E term

--- examples/matquadratic/JuMP.jl
+++ examples/matquadratic/JuMP.jl
@@ -3,13 +3,13 @@ Copyright 2020, Chris Coey, Lea Kapelevich and contributors
 
 minimize the trace of the epigraph of a matrix quadratic function
 min tr(Y)
-s.t. Y - A*X*B*B'*X'*C - C*X*D - ( C*X*D)' in S_+
+s.t. Y - A*X*B*B'*X'*A' - C*X*D - (C*X*D)' - E in S_+
 X .>= Q
 
-Y - A*X*B*B'*X'*C - C*X*D - ( C*X*D)' in S_+ equivalent to
+Y - A*X*B*B'*X'*C - C*X*D - (C*X*D)' in S_+ equivalent to
 [
 I      (A*X*B)'
-A*X*B  Y-C*X*D-(C*X*D)'
+A*X*B  Y-C*X*D-(C*X*D)'-E
 ]
 in S_+
 reformulation from Lecture 4 Lectures on Convex Programming
@@ -33,13 +33,15 @@ function matquadraticJuMP(
     B = randn(W_rows, W_cols)
     C = randn(W_rows, W_rows)
     D = randn(W_rows, W_rows)
+    E = randn(W_rows, W_rows)
+    E = E + E'
 
     model = JuMP.Model()
     JuMP.@variable(model, X[1:W_rows, 1:W_rows])
     JuMP.@variable(model, Y[1:W_rows, 1:W_rows], Symmetric)
     JuMP.@objective(model, Min, tr(Y))
 
-    U = Symmetric(Y - C * X * D - (C * X * D)')
+    U = Symmetric(Y - C * X * D - (C * X * D)' - E)
     W = A * X * B
     if use_matrixepipersquare
         U_svec = CO.smat_to_svec!(zeros(eltype(U), CO.svec_length(W_rows)), U, sqrt(2))
@@ -82,6 +84,4 @@ test_matquadraticJuMP(; options...) = test_matquadraticJuMP.([
     matquadraticJuMP2,
     matquadraticJuMP3,
     matquadraticJuMP4,
-    matquadraticJuMP5,
-    matquadraticJuMP6,
     ], options = options)
