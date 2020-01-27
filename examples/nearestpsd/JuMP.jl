93f2fb0a361779e8607f0fbfe07d7151c548dc40
Chris Coey <coey.chris@gmail.com>
2020-01-27 15:59:14 +1000

    use chordal extension option
    Former-commit-id: 9790f37e2ec752b9b04f79788e8e4f8ccf78b2ea

--- examples/nearestpsd/JuMP.jl
+++ examples/nearestpsd/JuMP.jl
@@ -2,17 +2,16 @@
 Copyright 2020, Chris Coey, Lea Kapelevich and contributors
 
 let E be a symmetric matrix sparsity pattern:
-(1) find sparse PSD matrix with given sparsity pattern, "nearest" to A, fixing some indices F to B
+(1) find sparse PSD matrix with given sparsity pattern, "nearest" to A
     max_X tr(A, X) :
     tr(X) = 1
     X in intersect(S_+^n, S^n(E))
-    X_k = B_k, k in F
-(2) find sparse PSD-completable matrix with given sparsity pattern, "nearest" to A, fixing some indices F to B
+(2) find sparse PSD-completable matrix with given sparsity pattern, "nearest" to A
     max_X tr(A, X) :
     tr(X) = 1
     X in proj_E(S_+^n)
-    X_k = B_k, k in F
-modified from "Decomposition methods for sparse matrix nearness problems" (2015) by Sun & Vandenberghe
+
+adapted from "Decomposition methods for sparse matrix nearness problems" (2015) by Sun & Vandenberghe
 =#
 
 using Test
@@ -22,24 +21,28 @@ import Random
 import JuMP
 const MOI = JuMP.MOI
 import Hypatia
+import Hypatia.Cones
 
-# TODO maybe optionally fix some particular values in matrix
 function nearestpsdJuMP(
     use_completable::Bool,
+    use_chordal_sparsity::Bool,
     side::Int;
-    sparsity::Float64 = sqrt(inv(side)),
+    sparsity::Float64 = min(3 / side, 1.0),
     )
     # generate random symmetric A (indefinite) with sparsity pattern E (nonchordal, with diagonal)
     A = tril!(sprandn(side, side, sparsity)) + Diagonal(randn(side))
-    (row_idxs, col_idxs, A_vals) = findnz(A)
+    if use_chordal_sparsity
+        # compute a chordal extension of A using CHOLMOD functions
+        # TODO extend ModelUtilities to compute chordal extensions
+        copyto!(A, I)
+        A = sparse(cholesky(Symmetric(A, :L)).L)
+        (row_idxs, col_idxs, _) = findnz(A)
+        A_vals = randn(length(row_idxs))
+    else
+        (row_idxs, col_idxs, A_vals) = findnz(A)
+    end
     num_nz = length(row_idxs)
-    @show side
-    @show num_nz
 
-    # TODO generate random sample of indices to fix to known values
-    # TODO fraction option?
-
-    # setup model
     model = JuMP.Model()
     JuMP.@variable(model, X[1:num_nz])
     JuMP.@objective(model, Max, dot(A_vals, X)) # tr(A, X)
@@ -49,28 +52,30 @@ function nearestpsdJuMP(
     return (model = model,)
 end
 
-nearestpsdJuMP1() = nearestpsdJuMP(false, 1)
-nearestpsdJuMP2() = nearestpsdJuMP(true, 1)
-nearestpsdJuMP3() = nearestpsdJuMP(false, 2)
-nearestpsdJuMP4() = nearestpsdJuMP(true, 2)
-nearestpsdJuMP5() = nearestpsdJuMP(false, 5)
-nearestpsdJuMP6() = nearestpsdJuMP(true, 5)
-nearestpsdJuMP7() = nearestpsdJuMP(false, 10)
-nearestpsdJuMP8() = nearestpsdJuMP(true, 10)
-nearestpsdJuMP9() = nearestpsdJuMP(false, 15)
-nearestpsdJuMP10() = nearestpsdJuMP(true, 15)
-nearestpsdJuMP11() = nearestpsdJuMP(false, 25)
-nearestpsdJuMP12() = nearestpsdJuMP(true, 25)
-nearestpsdJuMP13() = nearestpsdJuMP(false, 50)
-nearestpsdJuMP14() = nearestpsdJuMP(true, 50)
-nearestpsdJuMP15() = nearestpsdJuMP(false, 100)
-nearestpsdJuMP16() = nearestpsdJuMP(true, 100)
-nearestpsdJuMP17() = nearestpsdJuMP(false, 250)
-nearestpsdJuMP18() = nearestpsdJuMP(true, 250)
-nearestpsdJuMP19() = nearestpsdJuMP(false, 500)
-nearestpsdJuMP20() = nearestpsdJuMP(true, 500)
-nearestpsdJuMP21() = nearestpsdJuMP(false, 1000)
-nearestpsdJuMP22() = nearestpsdJuMP(true, 1000)
+nearestpsdJuMP1() = nearestpsdJuMP(false, true, 1)
+nearestpsdJuMP2() = nearestpsdJuMP(true, true, 1)
+nearestpsdJuMP3() = nearestpsdJuMP(false, false, 1)
+nearestpsdJuMP4() = nearestpsdJuMP(true, false, 1)
+nearestpsdJuMP5() = nearestpsdJuMP(false, true, 5)
+nearestpsdJuMP6() = nearestpsdJuMP(true, true, 5)
+nearestpsdJuMP7() = nearestpsdJuMP(false, false, 5)
+nearestpsdJuMP8() = nearestpsdJuMP(true, false, 5)
+nearestpsdJuMP9() = nearestpsdJuMP(false, true, 20)
+nearestpsdJuMP10() = nearestpsdJuMP(true, true, 20)
+nearestpsdJuMP11() = nearestpsdJuMP(false, false, 20)
+nearestpsdJuMP12() = nearestpsdJuMP(true, false, 20)
+nearestpsdJuMP13() = nearestpsdJuMP(false, true, 50)
+nearestpsdJuMP14() = nearestpsdJuMP(true, true, 50)
+nearestpsdJuMP15() = nearestpsdJuMP(false, false, 50)
+nearestpsdJuMP16() = nearestpsdJuMP(true, false, 50)
+nearestpsdJuMP17() = nearestpsdJuMP(false, true, 200)
+nearestpsdJuMP18() = nearestpsdJuMP(true, true, 200)
+nearestpsdJuMP19() = nearestpsdJuMP(false, false, 200)
+nearestpsdJuMP20() = nearestpsdJuMP(true, false, 200)
+nearestpsdJuMP21() = nearestpsdJuMP(false, true, 1000)
+nearestpsdJuMP22() = nearestpsdJuMP(true, true, 1000)
+nearestpsdJuMP23() = nearestpsdJuMP(false, false, 1000)
+nearestpsdJuMP24() = nearestpsdJuMP(true, false, 1000)
 
 function test_nearestpsdJuMP(instance::Function; options, rseed::Int = 1)
     Random.seed!(rseed)
@@ -104,6 +109,8 @@ test_nearestpsdJuMP_all(; options...) = test_nearestpsdJuMP.([
     nearestpsdJuMP20,
     nearestpsdJuMP21,
     nearestpsdJuMP22,
+    nearestpsdJuMP23,
+    nearestpsdJuMP24,
     ], options = options)
 
 test_nearestpsdJuMP(; options...) = test_nearestpsdJuMP.([
@@ -113,10 +120,14 @@ test_nearestpsdJuMP(; options...) = test_nearestpsdJuMP.([
     nearestpsdJuMP4,
     nearestpsdJuMP5,
     nearestpsdJuMP6,
+    nearestpsdJuMP7,
+    nearestpsdJuMP8,
+    nearestpsdJuMP9,
+    nearestpsdJuMP10,
     nearestpsdJuMP11,
     nearestpsdJuMP12,
     nearestpsdJuMP13,
     nearestpsdJuMP14,
-    # nearestpsdJuMP15,
-    # nearestpsdJuMP16,
+    nearestpsdJuMP15,
+    nearestpsdJuMP16,
     ], options = options)
