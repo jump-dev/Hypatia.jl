09839dfad2f3577864666d1e3f3e74ffde3cb48c
Chris Coey <coey.chris@gmail.com>
2020-01-29 23:43:47 +1000

    allow constrained signomial instances

--- examples/signomialmin/JuMP.jl
+++ examples/signomialmin/JuMP.jl
@@ -12,9 +12,11 @@ with parameters
 takes values
     f(x) = sum_{i in [m]} c_i exp(A_{i, :} * x)
 
-the "unconstrained" signomial minimization SAGE relaxation problem is
-    max_{g in R} g :
-    c - (g, 0, ..., 0) in C_SAGE(A)
+for signomials f, (g_p)_{p in 1:q}, the signomial minimization problem is
+    inf_{x in R^n} f(x) : g_p(x) >= 0, p in 1:q
+
+the convex SAGE relaxation problem is
+    max_{γ in R, μ in R_+^q} γ : c - γ - sum_{p in 1:q} μ_p * g_p in C_SAGE(A)
 
 a vector d in R^m belongs to SAGE cone C_SAGE(A) if
     exists C in R^{m, m} :
@@ -37,22 +39,39 @@ import Random
 include(joinpath(@__DIR__, "data.jl"))
 
 function signomialminJuMP(
-    c::Vector{<:Real},
-    A::Matrix{<:Real};
+    fc::Vector{<:Real},
+    fA::Matrix{<:Real},
+    gc::Vector,
+    gA::Vector;
     x::Vector = [],
     obj_ub = NaN,
     )
-    (m, n) = size(A)
-    @assert length(c) == m
+    (mc, n) = size(fA)
+    @assert length(fc) == mc
     if isnan(obj_ub)
         @assert !isempty(x)
-        obj_ub = eval_signomial(c, A, x)
+        for (gc_p, gA_p) in zip(gc, gA)
+            @show eval_signomial(gc_p, gA_p, x)
+        end
+        @assert all(eval_signomial(gc_p, gA_p, x) >= 0 for (gc_p, gA_p) in zip(gc, gA))
+        obj_ub = eval_signomial(fc, fA, x)
     end
+    q = length(gc)
+    @assert length(gA) == q
+    @assert all(size(gA_p, 2) == n for gA_p in gA)
+    @assert all(size(gA_p, 1) == length(gc_p) for (gc_p, gA_p) in zip(gc, gA))
+
+    # let c and A contain all terms
+    # currently assuming terms in each g_p are unique among all terms in f and gs
+    # TODO merge like-terms
+    A = vcat(fA, gA...)
+    m = size(A, 1)
 
     model = JuMP.Model()
-    JuMP.@variable(model, g)
-    JuMP.@objective(model, Max, g)
-    d = c - vcat(g, zeros(m - 1))
+    JuMP.@variable(model, γ)
+    JuMP.@objective(model, Max, γ)
+    JuMP.@variable(model, μ[1:q] >= 0)
+    d = vcat(fc, zeros(m - mc)) - vcat(γ, zeros(m - 1)) - vcat(zeros(mc), vcat((μ .* gc)...))
 
     # setup SAGE constraints
     notk = [[l for l in 1:m if l != k] for k in 1:m]
@@ -66,13 +85,13 @@ function signomialminJuMP(
 end
 
 function signomialminJuMP(signomial_name::Symbol)
-    (c, A, x, obj_ub) = signomials[signomial_name]
-    return signomialminJuMP(c, A; x = x, obj_ub = obj_ub)
+    (fc, fA, gc, gA, x, obj_ub) = signomials[signomial_name]
+    return signomialminJuMP(fc, fA, gc, gA; x = x, obj_ub = obj_ub)
 end
 
 function signomialminJuMP(m::Int, n::Int)
-    (c, A, obj_ub) = random_signomial(m, n)
-    return signomialminJuMP(c, A; obj_ub = obj_ub)
+    (fc, fA, gc, gA, obj_ub) = random_signomial(m, n)
+    return signomialminJuMP(fc, fA, gc, gA; obj_ub = obj_ub)
 end
 
 signomialminJuMP1() = signomialminJuMP(:motzkin2)
@@ -80,10 +99,12 @@ signomialminJuMP2() = signomialminJuMP(:motzkin3)
 signomialminJuMP3() = signomialminJuMP(:CS16ex8_13)
 signomialminJuMP4() = signomialminJuMP(:CS16ex8_14)
 signomialminJuMP5() = signomialminJuMP(:CS16ex18)
-signomialminJuMP6() = signomialminJuMP(3, 2)
-signomialminJuMP7() = signomialminJuMP(4, 4)
-signomialminJuMP8() = signomialminJuMP(8, 4)
-signomialminJuMP9() = signomialminJuMP(12, 6)
+signomialminJuMP6() = signomialminJuMP(:CS16ex12)
+signomialminJuMP7() = signomialminJuMP(:CS16ex13)
+# signomialminJuMP6() = signomialminJuMP(3, 2)
+# signomialminJuMP7() = signomialminJuMP(4, 4)
+# signomialminJuMP8() = signomialminJuMP(8, 4)
+# signomialminJuMP9() = signomialminJuMP(12, 6)
 
 function test_signomialminJuMP(instance::Function; options, rseed::Int = 1)
     Random.seed!(rseed)
@@ -114,8 +135,8 @@ test_signomialminJuMP(; options...) = test_signomialminJuMP.([
     # signomialminJuMP3,
     # signomialminJuMP4,
     # signomialminJuMP5,
-    signomialminJuMP6,
+    # signomialminJuMP6,
     signomialminJuMP7,
-    signomialminJuMP8,
-    signomialminJuMP9,
+    # signomialminJuMP8,
+    # signomialminJuMP9,
     ], options = options)
