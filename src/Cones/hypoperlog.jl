ee60643501d5e14a4a11ca198834dfa4980f38a1
Chris Coey <coey.chris@gmail.com>
2020-02-16 18:50:34 -0500

    minor cleanup of hypoperlog code
    Former-commit-id: d387f9a260021ef4775500e3b36d36d8a4bd5a6b

--- src/Cones/hypoperlog.jl
+++ src/Cones/hypoperlog.jl
@@ -2,10 +2,10 @@
 Copyright 2019, Chris Coey, Lea Kapelevich and contributors
 
 (closure of) hypograph of perspective of sum of logarithms
-(u in R, v in R_+, w in R_+^n) : u <= v*sum(log.(w/v))
+(u in R, v in R_+, w in R_+^d) : u <= v*sum(log.(w/v))
 
-barrier (guessed, reduces to 3-dim exp cone self-concordant barrier)
--log(v*sum(log.(w/v)) - u) - sum(log.(w)) - log(v)
+barrier modified from "Primal-Dual Interior-Point Methods for Domain-Driven Formulations" by Karimi & Tuncel, 2019
+-log(sum_i v*log(w_i/v) - u) - sum_i log(w_i) - d*log(v)
 =#
 
 mutable struct HypoPerLog{T <: Real} <: Cone{T}
@@ -27,6 +27,7 @@ mutable struct HypoPerLog{T <: Real} <: Cone{T}
 
     lwv::T
     vlwvu::T
+    lvwnivlwvu::T
     vwivlwvu::Vector{T}
 
     function HypoPerLog{T}(
@@ -71,13 +72,15 @@ function update_feas(cone::HypoPerLog)
     u = cone.point[1]
     v = cone.point[2]
     w = view(cone.point, 3:cone.dim)
-    if v > 0 && all(wi -> wi > 0, w)
+
+    if v > 0 && all(>(zero(u)), w)
         cone.lwv = sum(log(wi / v) for wi in w)
         cone.vlwvu = v * cone.lwv - u
         cone.is_feas = (cone.vlwvu > 0)
     else
         cone.is_feas = false
     end
+
     cone.feas_updated = true
     return cone.is_feas
 end
@@ -87,10 +90,14 @@ function update_grad(cone::HypoPerLog)
     u = cone.point[1]
     v = cone.point[2]
     w = view(cone.point, 3:cone.dim)
-    cone.grad[1] = inv(cone.vlwvu)
-    cone.grad[2] = -(cone.lwv - (cone.dim - 2)) / cone.vlwvu - inv(v)
+    g = cone.grad
+
+    g[1] = inv(cone.vlwvu)
+    cone.lvwnivlwvu = (length(cone.vwivlwvu) - cone.lwv) / cone.vlwvu
+    g[2] = cone.lvwnivlwvu - inv(v)
     gden = -1 - inv(cone.lwv - u / v)
-    @. cone.grad[3:end] = gden / w
+    @. g[3:end] = gden / w
+
     cone.grad_updated = true
     return cone.grad
 end
@@ -100,23 +107,25 @@ function update_hess(cone::HypoPerLog)
     u = cone.point[1]
     v = cone.point[2]
     w = view(cone.point, 3:cone.dim)
-    d = cone.dim - 2
+    vwivlwvu = cone.vwivlwvu
+    lvwnivlwvu = cone.lvwnivlwvu
+    g = cone.grad
     H = cone.hess.data
 
-    @. cone.vwivlwvu = v / cone.vlwvu / w
-    H[1, 1] = abs2(cone.grad[1])
-    lvwnivlwvu = (cone.lwv - d) / cone.vlwvu
-    H[1, 2] = -lvwnivlwvu / cone.vlwvu
-    @. H[1, 3:end] = -cone.vwivlwvu / cone.vlwvu
-    H[2, 2] = abs2(lvwnivlwvu) + (d / cone.vlwvu + inv(v)) / v
-    hden = (v * lvwnivlwvu - 1) / cone.vlwvu
+    vivlwvu = v / cone.vlwvu
+    @. vwivlwvu = vivlwvu / w
+    H[1, 1] = abs2(g[1])
+    H[1, 2] = lvwnivlwvu / cone.vlwvu
+    @. H[1, 3:end] = -vwivlwvu / cone.vlwvu
+    H[2, 2] = abs2(lvwnivlwvu) + (length(vwivlwvu) / cone.vlwvu + inv(v)) / v
+    hden = (-v * lvwnivlwvu - 1) / cone.vlwvu
     @. H[2, 3:end] = hden / w
-    @inbounds for j in 1:d
+    @inbounds for j in eachindex(vwivlwvu)
         j2 = 2 + j
         @inbounds for i in 1:j
-            H[2 + i, j2] = cone.vwivlwvu[i] * cone.vwivlwvu[j]
+            H[2 + i, j2] = vwivlwvu[i] * vwivlwvu[j]
         end
-        H[j2, j2] -= cone.grad[j2] / w[j]
+        H[j2, j2] -= g[j2] / w[j]
     end
 
     cone.hess_updated = true
@@ -154,3 +163,90 @@ const central_rays_hypoperlog = [
     -0.287837755  0.600745276  1.083013006;
     -0.264242795  0.596018958  1.075868819;
     ]
+
+
+# TODO add hess prod, inv hess etc functions
+# NOTE old EpiPerExp code below may be useful (cone vector is reversed)
+
+# function update_feas(cone::EpiPerExp)
+#     @assert !cone.feas_updated
+#     (u, v, w) = (cone.point[1], cone.point[2], cone.point[3])
+#
+#     if u > 0 && v > 0
+#         cone.luv = log(u / v)
+#         cone.vluvw = v * cone.luv - w
+#         cone.is_feas = (cone.vluvw > 0)
+#     else
+#         cone.is_feas = false
+#     end
+#
+#     cone.feas_updated = true
+#     return cone.is_feas
+# end
+#
+# function update_grad(cone::EpiPerExp)
+#     @assert cone.is_feas
+#     (u, v, w) = (cone.point[1], cone.point[2], cone.point[3])
+#     vluvw = cone.vluvw
+#
+#     cone.g1a = -v / u / vluvw
+#     cone.grad[1] = cone.g1a - inv(u)
+#     cone.g2a = (1 - cone.luv) / vluvw
+#     cone.grad[2] = cone.g2a - inv(v)
+#     cone.grad[3] = inv(vluvw)
+#
+#     cone.grad_updated = true
+#     return cone.grad
+# end
+#
+# function update_hess(cone::EpiPerExp)
+#     @assert cone.grad_updated
+#     (u, v, w) = (cone.point[1], cone.point[2], cone.point[3])
+#     H = cone.hess.data
+#     vluvw = cone.vluvw
+#     g1a = cone.g1a
+#     g2a = cone.g2a
+#
+#     H[1, 3] = g1a / vluvw
+#     H[2, 3] = g2a / vluvw
+#     H[3, 3] = abs2(cone.grad[3])
+#     H[1, 1] = abs2(g1a) - cone.grad[1] / u
+#     H[1, 2] = -(v * cone.g2a + 1) / cone.vluvw / u
+#     H[2, 2] = abs2(g2a) + (inv(vluvw) + inv(v)) / v
+#
+#     cone.hess_updated = true
+#     return cone.hess
+# end
+
+# function update_inv_hess(cone::EpiPerExp)
+#     @assert cone.is_feas
+#     (u, v, w) = (cone.point[1], cone.point[2], cone.point[3])
+#     Hi = cone.inv_hess.data
+#     vluvw = cone.vluvw
+#     vluv = vluvw + w
+#     denom = vluvw + 2 * v
+#     uvdenom = u * v / denom
+#
+#     Hi[1, 1] = u * (vluvw + v) / denom * u
+#     Hi[2, 2] = v * (vluvw + v) / denom * v
+#     Hi[3, 3] = 2 * (abs2(vluv - v) + vluv * (v - w)) + abs2(w) - v / denom * abs2(vluv - 2 * v)
+#     Hi[1, 2] = uvdenom * v
+#     Hi[1, 3] = uvdenom * (2 * vluv - w)
+#     Hi[2, 3] = (abs2(vluv) + w * (v - vluv)) / denom * v
+#
+#     cone.inv_hess_updated = true
+#     return cone.inv_hess
+# end
+#
+# function update_inv_hess_prod(cone::EpiPerExp)
+#     if !cone.inv_hess_updated
+#         update_inv_hess(cone)
+#     end
+#     return
+# end
+#
+# function inv_hess_prod!(prod::AbstractVecOrMat, arr::AbstractVecOrMat, cone::EpiPerExp)
+#     update_inv_hess_prod(cone)
+#     mul!(prod, cone.inv_hess, arr)
+#     return prod
+# end
