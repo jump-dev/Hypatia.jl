5b647c080b5e1cfe8f5a32af22ac6bb63ec4a038
lkapelevich <leakapelevich@gmail.com>
2020-04-06 20:47:07 -0400

    Polynomial -> polynomial

--- src/ModelUtilities/interpolate.jl
+++ src/ModelUtilities/interpolate.jl
@@ -388,7 +388,7 @@ function recover_lagrange_polys(pts::Matrix{T}, deg::Int) where {T <: Real}
     DP.@polyvar x[1:n]
     monos = DP.monomials(x, 0:deg)
     vandermonde_inv = inv([monos[j](view(pts, i, :)) for i in 1:U, j in 1:U])
-    lagrange_polys = [DP.Polynomial(view(vandermonde_inv, :, i), monos) for i in 1:U]
+    lagrange_polys = [DP.polynomial(view(vandermonde_inv, :, i), monos) for i in 1:U]
     return lagrange_polys
 end
 
