2a6afc2704ce6f10162722970a1d0769f7da15de
lkapelevich <leakapelevich@gmail.com>
2020-04-11 15:25:35 -0400

    undo unintended changes
    Former-commit-id: 4849ef2df1c59c372ff88163195942a6a8a83f0a

--- examples/densityest/JuMP.jl
+++ examples/densityest/JuMP.jl
@@ -11,27 +11,20 @@ import PolyJuMP
 
 struct DensityEstJuMP{T <: Real} <: ExampleInstanceJuMP{T}
     dataset_name::Symbol
-    num_obs::Int
-    n::Int
     X::Matrix{T}
     true_obj::Float64
     deg::Int
-    use_monomial_space::Bool # use variables in monomial space, else interpolation space
     use_wsos::Bool # use WSOS cone formulation, else PSD formulation
 end
 function DensityEstJuMP{Float64}(
     dataset_name::Symbol,
     deg::Int,
-    use_monomial_space::Bool,
     use_wsos::Bool)
     X = DelimitedFiles.readdlm(joinpath(@__DIR__, "data", "$dataset_name.txt"))
-    (num_obs, n) = size(X)
-    true_obj = NaN
-    return DensityEstJuMP{Float64}(dataset_name, num_obs, n, X, true_obj, deg, use_monomial_space, use_wsos)
+    return DensityEstJuMP{Float64}(dataset_name, X, NaN, deg, use_wsos)
 end
 function DensityEstJuMP{Float64}(
     density_name::Symbol,
-    use_monomial_space::Bool,
     use_wsos::Bool)
     (density, cummulative_inv, num_obs, n, deg) = densityest_data(density_name)
     X = rand(num_obs, n)
@@ -40,14 +33,14 @@ function DensityEstJuMP{Float64}(
         X[i, :] .= map(cummulative_inv, X[i, :])
     end
     true_obj = exp(sum(log(density(X[i, :]...)) / num_obs for i in 1:num_obs))
-    return DensityEstJuMP{Float64}(density_name, num_obs, n, X, true_obj, deg, use_monomial_space, use_wsos)
+    return DensityEstJuMP{Float64}(density_name, X, true_obj, deg, use_wsos)
 end
 function DensityEstJuMP{Float64}(
     num_obs::Int,
     n::Int,
     args...)
     X = randn(num_obs, n)
-    return DensityEstJuMP{Float64}(:random, num_obs, n, X, NaN, args...)
+    return DensityEstJuMP{Float64}(:random, X, NaN, args...)
 end
 
 # support on [-1, 1]^n, so won't get rescaled in build
@@ -78,87 +71,87 @@ function densityest_data(density_name::Symbol)
 end
 
 example_tests(::Type{DensityEstJuMP{Float64}}, ::MinimalInstances) = [
-    # ((5, 1, 2, true, true),),
-    # ((:iris, 2, true, true),),
-    ((:density1, true, true),),
+    # ((5, 1, 2, true),),
+    # ((:iris, 2, true),),
+    ((:density1, true),),
     ]
 example_tests(::Type{DensityEstJuMP{Float64}}, ::FastInstances) = begin
     options = (tol_feas = 1e-7, tol_rel_opt = 1e-6, tol_abs_opt = 1e-6)
     return [
-    # ((50, 2, 2, true, true), nothing, options),
-    # ((50, 2, 2, true, false), nothing, options),
-    # ((50, 2, 2, false, true), nothing, options),
-    # ((50, 2, 2, false, false), nothing, options),
-    # ((100, 8, 2, true, true), nothing, options),
-    # ((100, 8, 2, true, false), nothing, options),
-    # ((100, 8, 2, false, true), nothing, options),
-    # ((100, 8, 2, false, false), nothing, options),
-    # ((250, 4, 4, true, true), nothing, options),
-    # ((250, 4, 4, true, false), nothing, options),
-    # ((250, 4, 4, false, true), nothing, options),
-    # ((:iris, 4, true, true), nothing, options),
-    # ((:iris, 5, true, true), nothing, options),
-    # ((:iris, 6, true, true), nothing, options),
-    # ((:iris, 4, true, false), nothing, options),
-    # ((:iris, 4, false, true), nothing, options),
-    # ((:iris, 6, false, true), nothing, options),
-    # ((:iris, 4, false, false), nothing, options),
-    # ((:cancer, 4, true, true), nothing, options),
-    # ((:cancer, 4, false, true), nothing, options),
-    ((:density2, true, true), nothing, options),
-    ((:density3, true, true), nothing, options),
+    # ((50, 2, 2, true), nothing, options),
+    # ((50, 2, 2, false), nothing, options),
+    # ((50, 2, 2, true), nothing, options),
+    # ((50, 2, 2, false), nothing, options),
+    # ((100, 8, 2, true), nothing, options),
+    # ((100, 8, 2, false), nothing, options),
+    # ((100, 8, 2, true), nothing, options),
+    # ((100, 8, 2, false), nothing, options),
+    # ((250, 4, 4, true), nothing, options),
+    # ((250, 4, 4, false), nothing, options),
+    # ((250, 4, 4, true), nothing, options),
+    # ((:iris, 4, true), nothing, options),
+    # ((:iris, 5, true), nothing, options),
+    # ((:iris, 6, true), nothing, options),
+    # ((:iris, 4, false), nothing, options),
+    # ((:iris, 4, true), nothing, options),
+    # ((:iris, 6, true), nothing, options),
+    # ((:iris, 4, false), nothing, options),
+    # ((:cancer, 4, true), nothing, options),
+    # ((:cancer, 4, true), nothing, options),
+    ((:density2, true), nothing, options),
+    ((:density3, true), nothing, options),
     ]
 end
 example_tests(::Type{DensityEstJuMP{Float64}}, ::SlowInstances) = begin
     options = (tol_feas = 1e-7, tol_rel_opt = 1e-6, tol_abs_opt = 1e-6)
     return [
-    ((200, 4, 4, false, false), nothing, options),
-    ((200, 4, 6, false, true), nothing, options),
-    ((200, 4, 6, false, false), nothing, options),
+    ((200, 2, 50, true), nothing, options),
+    ((200, 4, 4, false), nothing, options),
+    ((200, 4, 6, true), nothing, options),
+    ((200, 4, 6, false), nothing, options),
     ]
 end
 
 function build(inst::DensityEstJuMP{T}) where {T <: Float64} # TODO generic reals
-    (n, X) = (inst.n, inst.X)
+    X = inst.X
+    (num_obs, n) = size(X)
+    domain = ModelUtilities.Box{Float64}(-ones(n), ones(n)) # domain is unit box [-1,1]^n
 
-    domain = ModelUtilities.Box{Float64}(-ones(n), ones(n))
     # rescale X to be in unit box
     minX = minimum(X, dims = 1)
     maxX = maximum(X, dims = 1)
     X .-= (minX + maxX) / 2
     X ./= (maxX - minX) / 2
 
+    # setup interpolation
     halfdeg = div(inst.deg + 1, 2)
     (U, pts, Ps, w) = ModelUtilities.interpolate(domain, halfdeg, calc_w = true)
 
+    DynamicPolynomials.@polyvar x[1:n]
+    basis = ModelUtilities.get_chebyshev_polys(x, 2 * halfdeg)
+    # TODO try to get the V U*U sub-QR out from interpolate instead
+    V = [bj(x => pts_u) for pts_u in eachrow(pts), bj in basis]
+    F = qr!(V, Val(true))
+    V_X = [bj(x => X_i) for X_i in eachrow(X), bj in basis]
+    X_pts_polys = (V_X[:, F.p] / F.R) * F.Q'
+
     model = JuMP.Model()
-
-    if inst.use_monomial_space
-        DynamicPolynomials.@polyvar x[1:n]
-        PX = DynamicPolynomials.monomials(x, 0:(2 * halfdeg))
-        JuMP.@variable(model, f, PolyJuMP.Poly(PX))
-        f_pts = [f(pts_i) for pts_i in eachrow(pts)]
-        f_X = [f(X_i) for X_i in eachrow(X)]
-    else
-        lagrange_polys = ModelUtilities.recover_lagrange_polys(pts, 2 * halfdeg)
-        basis_evals = Matrix{Float64}(undef, inst.num_obs, U)
-        for i in 1:inst.num_obs, j in 1:U
-            basis_evals[i, j] = lagrange_polys[j](X[i, :])
-        end
-        JuMP.@variable(model, f_pts[1:U])
-        f_X = [dot(f_pts, b_i) for b_i in eachrow(basis_evals)]
-    end
-
-    JuMP.@constraint(model, dot(w, f_pts) == 1.0) # integrate to 1
-
     JuMP.@variable(model, z)
     JuMP.@objective(model, Max, z)
-    JuMP.@constraint(model, vcat(z, f_X) in MOI.GeometricMeanCone(1 + length(f_X)))
+    JuMP.@variable(model, f_pts[1:U])
+
+    # objective epigraph
+    JuMP.@constraint(model, vcat(z, X_pts_polys * f_pts) in MOI.GeometricMeanCone(1 + num_obs))
+
+    # density integrates to 1
+    JuMP.@constraint(model, dot(w, f_pts) == 1)
 
     # density nonnegative
     if inst.use_wsos
+        # WSOS formulation
         JuMP.@constraint(model, f_pts in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
     else
+        # PSD formulation
         psd_vars = []
         for (r, Pr) in enumerate(Ps)
             Lr = size(Pr, 2)
@@ -175,11 +168,22 @@ end
 
 function test_extra(inst::DensityEstJuMP{T}, model::JuMP.Model) where T
     @test JuMP.termination_status(model) == MOI.OPTIMAL
+    if JuMP.termination_status(model) == MOI.OPTIMAL && inst.dataset_name == :Uniform
+        error("TODO: finish this and add instances")
+        # check objective value is correct
+        tol = eps(T)^0.25
+        @test JuMP.objective_value(model) ≈ 1 atol = tol rtol = tol
+    end
+end
+
+function test_extra(inst::DensityEstJuMP{T}, model::JuMP.Model) where T
+    @test JuMP.termination_status(model) == MOI.OPTIMAL
     if JuMP.termination_status(model) == MOI.OPTIMAL && !isnan(inst.true_obj)
         # check objective value is correct
         tol = 0.1
         @test JuMP.objective_value(model) ≈ inst.true_obj atol = tol rtol = tol
     end
 end
+
 
 return DensityEstJuMP
