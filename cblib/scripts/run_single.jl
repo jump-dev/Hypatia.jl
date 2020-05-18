8fb254c73b5602d96f1ab4b1482c10e8e5b8cc86
lkapelevich <leakapelevich@gmail.com>
2020-05-18 15:44:25 -0400

    changes in run_single
    Former-commit-id: 7bc7f6d6a7a5c1dc221033a11061d7245ec343d0

--- cblib/scripts/run_single.jl
+++ cblib/scripts/run_single.jl
@@ -7,29 +7,12 @@ helper file that calls single_moi, factoring out any code that can be avoided du
 =#
 
 import Dates
-include(joinpath(@__DIR__, "read_instances.jl"))
-#
-@show newARGS
-nargs = length(ARGS)
-if !(nargs in [3, 4])
-    error("usage: julia run_single.jl instname csvfile solver system_solver_name")
-end
-#
-# open("mypid", "w") do fd
-#     print(fd, getpid())
-# end
 
-
-instname = ARGS[1]
-csvfile = ARGS[2]
-solver_name = ARGS[3]
-system_solver_name = (solver_name == "hypatia" ? ARGS[4] : "")
-# # bss1 solved with nbhds 1e-2
-# # demb782 solved with 1e-3
-# instname = "synthes1"
-# csvfile =  "dummy_results.txt"
-# solver_name = "hypatia"
-# system_solver_name = "qrchol_dense"
+# instname = "enpro56"
+instname = "syn10m04m"
+csvfile =  "dummy_results.txt"
+solver_name = "hypatia"
+system_solver_name = "qrchol_dense"
 # system_solver_name = "naive_dense"
 # model_type = BigFloat
 model_type = Float64
@@ -47,14 +30,6 @@ open(csvfile, "a") do fdcsv
     print(fdcsv, "\n$instname,$model_type,$solver_name,$system_solver_name,")
     flush(fdcsv)
 end
-# if solver_name == "hypatia"
-    # println("solving small instances to precompile...")
-    # for instname in read_instances(joinpath(@__DIR__, "../sets", "compile.txt"))
-    #     println("solving small instance: $instname...")
-    #     flush(stdout)
-    #     single_moi(instname, csvfile, system_solver_name, print_timer = false, precompiling = true)
-    # end
-# end
 single_moi(instname, csvfile, system_solver_name, print_timer = true, precompiling = false, out_type = model_type)
 
 println()
