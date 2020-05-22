ce68b8f907b52a3674b8d07d8780f04f99bb99d5
lkapelevich <leakapelevich@gmail.com>
2020-05-21 21:31:42 -0400

    add power corrector with bugs
    Former-commit-id: de9dec377df4e756e45026f9478675afce9de685

--- src/Cones/power.jl
+++ src/Cones/power.jl
@@ -8,6 +8,7 @@ where sum_i(alpha_i) = 1, alpha_i > 0
 barrier from "On self-concordant barriers for generalized power cones" by Roy & Xiao 2018
 -log(prod_i((u_i)^(2 * alpha_i)) - norm_2(w)^2) - sum_i((1 - alpha_i)*log(u_i))
 =#
+using ForwardDiff # TODO remove
 
 mutable struct Power{T <: Real} <: Cone{T}
     use_dual_barrier::Bool
@@ -17,6 +18,7 @@ mutable struct Power{T <: Real} <: Cone{T}
     alpha::Vector{T}
     n::Int
     point::Vector{T}
+    dual_point::Vector{T}
     timer::TimerOutput
 
     feas_updated::Bool
@@ -37,6 +39,9 @@ mutable struct Power{T <: Real} <: Cone{T}
     produuw::T
     aui::Vector{T}
     auiproduuw::Vector{T}
+
+    correction::Vector{T}
+    barrier::Function # TODO delete later
 
     function Power{T}(
         alpha::Vector{T},
@@ -59,6 +64,12 @@ mutable struct Power{T <: Real} <: Cone{T}
         cone.dim = dim
         cone.alpha = alpha
         cone.hess_fact_cache = hess_fact_cache
+        function barrier(s)
+            m = length(cone.alpha)
+            (u, w) = (s[1:m], s[(m + 1):end])
+            return -log(prod(u[j] ^ (2 * alpha[j]) for j in eachindex(alpha)) - sum(abs2, w)) - sum((1 - alpha[j]) * log(u[j]) for j in eachindex(alpha))
+        end
+        cone.barrier = barrier
         return cone
     end
 end
@@ -70,6 +81,7 @@ function setup_data(cone::Power{T}) where {T <: Real}
     reset_data(cone)
     dim = cone.dim
     cone.point = zeros(T, dim)
+    cone.dual_point = zeros(T, dim)
     cone.grad = zeros(T, dim)
     cone.hess = Symmetric(zeros(T, dim, dim), :U)
     cone.inv_hess = Symmetric(zeros(T, dim, dim), :U)
@@ -78,8 +90,11 @@ function setup_data(cone::Power{T}) where {T <: Real}
     load_matrix(cone.hess_fact_cache, cone.hess)
     cone.aui = zeros(length(cone.alpha))
     cone.auiproduuw = zeros(length(cone.alpha))
+    cone.correction = zeros(T, dim)
     return
 end
+
+use_correction(cone::Power) = true
 
 get_nu(cone::Power) = length(cone.alpha) + 1
 
@@ -106,6 +121,16 @@ function update_feas(cone::Power{T}) where {T <: Real}
 
     cone.feas_updated = true
     return cone.is_feas
+end
+
+function update_dual_feas(cone::Power{T}) where {T <: Real}
+    alpha = cone.alpha
+    if all(>(zero(T)), u)
+        p = exp(2 * sum(alpha[i] * log(u[i] / alpha[i]) for i in eachindex(alpha)))
+        return p - sum(abs2, w) > 0
+    else
+        return false
+    end
 end
 
 function update_grad(cone::Power)
@@ -158,6 +183,87 @@ function update_hess(cone::Power)
 
     cone.hess_updated = true
     return cone.hess
+end
+
+function correction(
+    cone::Power{T},
+    primal_dir::AbstractVector{T},
+    dual_dir::AbstractVector{T},
+    ) where {T <: Real}
+
+    m = length(cone.alpha)
+    u = cone.point[1:m]
+    w = view(cone.point, (m + 1):cone.dim)
+    w_idxs = (m + 1):cone.dim
+    alpha = cone.alpha
+
+    produ = cone.produ # = exp(2 * sum(cone.alpha[i] * log(u[i]) for i in eachindex(cone.alpha)))
+    produw = cone.produw # = cone.produ - sum(abs2, w)
+    produuw = cone.produuw # = cone.produ / cone.produw
+    produuw_tw = produuw * (produuw - 1)
+    aui = cone.aui # @. cone.aui = 2 * cone.alpha / u
+    # derivative of produuw wrt u
+    duuw_du = produuw .* aui * (1 - produuw)
+    # derivative of produuw * (produuw - 1) wrt u
+
+
+    third_order = zeros(T, cone.dim, cone.dim, cone.dim)
+    # ui
+    for i in 1:m
+        # ui uj
+        for j in 1:m
+            # ui uj uk
+            for k in 1:m
+                if i == j == k
+                    third_order[i, i, i] = produuw_tw * aui[i] * (-inv(u[i]) + aui[i] * (2 * produuw - 1)) - 2 * (1 - alpha[i]) / u[i] ^ 3 + produuw / u[i] *
+                        (4 * alpha[i] * (alpha[i] - 1) / u[i] ^ 2 - aui[i] / produuw)
+                elseif i == j
+                    third_order[i, i, k] = third_order[i, k, i] = third_order[k, i, i] =  aui[i] * aui[k] * produuw_tw * ((2 * produuw - 1) * aui[i] - inv(u[i]))
+                elseif i != k && j != k
+                    third_order[i, j, k] = third_order[i, k, j] = third_order[j, i, k] = third_order[j, k, i] =
+                        third_order[k, i, j] = third_order[k, j, i] = aui[i] * aui[j] * aui[k] * produuw_tw * (2 * produuw - 1)
+                end
+            end
+            # ui uj wk
+            for k in w_idxs
+                wk = k - m
+                if i == j
+                    third_order[i, i, k] = third_order[i, k, i] = third_order[k, i, i] = 2 * w[wk] * produuw / produw * aui[i] * (2 * produuw * aui[i] + inv(u[i]) - aui[i])
+                else
+                    third_order[i, j, k] = 2 * aui[i] * aui[j] * produuw * w[wk] / produw * (produuw + 1)
+                end
+            end
+        end
+        # ui wj wk
+        for j in w_idxs, k in w_idxs
+            (wj, wk) = (j, k) .- m
+            third_order[i, j, k] = -8 * aui[i] * w[wj] * w[wk] * produuw / produw / produw
+            if j == k
+                third_order[i, j, k] -= 2 * aui[i] * produuw / produw
+            end
+        end
+
+    end
+    for i in w_idxs, j in w_idxs, k in w_idxs
+        (wi, wj, wk) = (i, j, k) .- m
+        # TODO refactor common summand or stick to this everywhere else and cut loops
+        if i == j == k
+            third_order[i, i, i] = 12 * w[wi] / (produw) ^ 2 + 16 * w[wi] ^ 3 / (produw) ^ 3
+        elseif i == j
+            third_order[i, i, k] = third_order[i, k, i] = third_order[k, i, i] = 16 * abs2(w[wi]) * w[wk] / (produw) ^ 3 + 4 * w[wk] / (produw) ^ 2
+        elseif i != k && j != k
+            third_order[i, j, k] = third_order[i, k, j] = third_order[j, i, k] = third_order[j, k, i] =
+                third_order[k, i, j] = third_order[k, j, i] = 16 * w[wi] * w[wj] * w[wk] / (produw) ^ 3
+        end
+    end
+    third_order = reshape(third_order, cone.dim^2, cone.dim)
+
+    barrier = cone.barrier
+    FD_3deriv = ForwardDiff.jacobian(x -> ForwardDiff.hessian(barrier, x), cone.point)
+    # @show size()
+    @show third_order - FD_3deriv
+    @show FD_3deriv
+
 end
 
 # TODO update and benchmark to decide whether this improves speed/numerics
