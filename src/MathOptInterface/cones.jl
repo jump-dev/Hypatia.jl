1e1ce34e20a4365507f0614f7824994b201a4f24
lkapelevich <leakapelevich@gmail.com>
2020-06-12 22:07:30 -0400

    start updating soc cone, remove size from entropy barrier test, minor tweaks
    Former-commit-id: 1c79c3857256e89fc2556c1fb9aa1ee08fb07bf3

--- src/MathOptInterface/cones.jl
+++ src/MathOptInterface/cones.jl
@@ -48,7 +48,6 @@ function permute_affine(cone::MOI.RelativeEntropyCone, idxs::AbstractVector)
     dim = MOI.dimension(cone)
     first_idx = 1 # TODO
     w_dim = div(dim - 1, 2)
-    @show idxs
     new_idxs = collect(idxs)
     for (i, idx) in enumerate(idxs)
         if idx != first_idx
@@ -58,11 +57,11 @@ function permute_affine(cone::MOI.RelativeEntropyCone, idxs::AbstractVector)
             #     new_offset = 2 * (idx - w_dim) - first_idx
             # end
             new_idx = 2 * idx - first_idx - (idx <= (w_dim + first_idx) ? first_idx : 2 * w_dim)
-            new_idxs[i] = new_offset
+            new_idxs[i] = new_idx
         end
     end
-
     @show new_idxs
+
     return new_idxs
 end
 
