04b19525a32a81f01df776d2f7a44a15f183a1a9
Chris Coey <coey.chris@gmail.com>
2020-07-23 10:07:56 +1000

    add dual sage model for signomialmin
    Former-commit-id: 95287832f7164a4dfee62a7c490cd10705c273c6

--- examples/signomialmin/JuMP.jl
+++ examples/signomialmin/JuMP.jl
@@ -16,7 +16,7 @@ for signomials f, (g_p)_{p in 1:q}, the signomial minimization problem is
     inf_{x in R^n} f(x) : g_p(x) >= 0, p in 1:q
 
 the convex SAGE relaxation problem is
-    max_{γ in R, μ in R_+^q} γ : c - γ - sum_{p in 1:q} μ_p * g_p in C_SAGE(A)
+    max_{γ in R, μ in R_+^q} γ : c - γe₁ - sum_{p in 1:q} μ_p * g_p in C_SAGE(A)
 
 a vector d in R^m belongs to SAGE cone C_SAGE(A) if
     exists C in R^{m, m} :
@@ -48,29 +48,31 @@ example_tests(::Type{SignomialMinJuMP{Float64}}, ::MinimalInstances) = [
     ((2, 2), ClassicConeOptimizer),
     ]
 example_tests(::Type{SignomialMinJuMP{Float64}}, ::FastInstances) = begin
-    options = (tol_feas = 1e-7, tol_rel_opt = 1e-6, tol_abs_opt = 1e-6)
-    relaxed_options = (tol_feas = 1e-5, tol_rel_opt = 1e-4, tol_abs_opt = 1e-4)
+    options = (tol_feas = 1e-5, tol_rel_opt = 1e-5, tol_abs_opt = 1e-5)
+    # relaxed_options = (tol_feas = 1e-5, tol_rel_opt = 1e-5, tol_abs_opt = 1e-5)
+    # ext = nothing
+    ext = ClassicConeOptimizer
     return [
-    # # ((:motzkin2,), nothing, options),
-    # # # ((:motzkin2,), ClassicConeOptimizer, options),
-    # # ((:motzkin3,), nothing, options),
-    # # ((:CS16ex8_13,), nothing, options),
-    # # ((:CS16ex8_14,), nothing, options),
-    # # ((:CS16ex18,), nothing, options),
-    # # ((:CS16ex12,), nothing, options),
-    # # ((:CS16ex13,), nothing, options),
-    # # ((:MCW19ex1_mod,), nothing, options),
-    # # ((:MCW19ex8,), nothing, relaxed_options),
-    # # # ((:MCW19ex8,), ClassicConeOptimizer, relaxed_options),
-    # # ((3, 2), nothing, options),
-    # # # ((3, 2), ClassicConeOptimizer, options),
-    # ((6, 6), nothing, options),
-    # ((20, 3), nothing, options),
-    # ((10, 6), nothing, options),
-    # ((9, 9), nothing, options),
-    # ((15, 4), nothing, options),
-    # ((15, 5), nothing, options),
-    ((25, 3), nothing, options),
+    ((:motzkin2,), ext, options),
+    # # ((:motzkin2,), ClassicConeOptimizer, options),
+    ((:motzkin3,), ext, options),
+    ((:CS16ex8_13,), ext, options),
+    ((:CS16ex8_14,), ext, options),
+    ((:CS16ex18,), ext, options),
+    ((:CS16ex12,), ext, options),
+    ((:CS16ex13,), ext, options),
+    ((:MCW19ex1_mod,), ext, options),
+    ((:MCW19ex8,), ext, options),
+    # # ((:MCW19ex8,), ClassicConeOptimizer, options),
+    ((3, 2), ext, options),
+    # # ((3, 2), ClassicConeOptimizer, options),
+    ((6, 6), ext, options),
+    ((20, 3), ext, options),
+    ((10, 6), ext, options),
+    ((9, 9), ext, options),
+    ((15, 4), ext, options),
+    # ((15, 5), ext, options),
+    # ((25, 3), ext, options),
     # ((20, 3), ClassicConeOptimizer, options),
     ]
 end
@@ -114,44 +116,83 @@ function build(inst::SignomialMinJuMP{T}) where {T <: Float64} # TODO generic re
             end
         end
     end
-    A = vcat((row_k' for row_k in keys(unique_terms))...)
+
+    A = vcat((row_k' for row_k in keys(unique_terms) if !iszero(norm(row_k, Inf)))...)
+    A = vcat(zeros(1, n), A)
     m = size(A, 1)
 
-    model = JuMP.Model()
-    JuMP.@variable(model, γ)
-    JuMP.@objective(model, Max, γ)
-    JuMP.@variable(model, μ[1:q] >= 0)
-
-    d = zeros(JuMP.AffExpr, m)
-    const_found = false
+    # TODO sparse?
+    fc_new = zeros(m)
+    gc_new = [zeros(m) for p in 1:q]
     for k in 1:m
-        row_k = A[k, :]
-        (ks, pks) = unique_terms[row_k]
+        (ks, pks) = unique_terms[A[k, :]]
         for k2 in ks
-            d[k] += fc[k2]
+            fc_new[k] += fc[k2]
         end
         for (p, k2) in pks
-            d[k] -= μ[p] * gc[p][k2]
-        end
-        if iszero(norm(row_k))
-            # row is a constant term
-            d[k] -= γ
-            @assert !const_found
-            const_found = true
+            gc_new[p][k] += gc[p][k2]
         end
     end
-    if !const_found
-        A = vcat(A, zeros(A, 1, n))
-        d = vcat(d, -γ)
-    end
+    fc = fc_new
+    gc = gc_new
 
-    # setup SAGE constraints
-    notk = [[l for l in 1:m if l != k] for k in 1:m]
-    JuMP.@variable(model, C[1:m, 1:m])
-    JuMP.@variable(model, V[1:m, 1:(m - 1)])
-    JuMP.@constraint(model, [k in 1:m], d[k] == sum(C[:, k]))
-    JuMP.@constraint(model, [k in 1:m, i in 1:n], dot(A[notk[k], i] .- A[k, i], V[k, :]) == 0)
-    JuMP.@constraint(model, [k in 1:m], vcat(C[k, k] + sum(V[k, :]), C[k, notk[k]], V[k, :]) in MOI.RelativeEntropyCone(2m - 1))
+    model = JuMP.Model()
+
+    use_primal = true # TODO option
+    # use_primal = false # TODO option
+
+    if use_primal
+        JuMP.@variable(model, γ)
+        JuMP.@objective(model, Max, γ)
+        JuMP.@variable(model, μ[1:q] >= 0)
+
+        d = zeros(JuMP.AffExpr, m)
+        d[1] -= γ
+        d += fc
+        for p in 1:q
+            d -= μ[p] * gc[p]
+        end
+
+        # setup SAGE constraints
+        notk = [[l for l in 1:m if l != k] for k in 1:m]
+        JuMP.@variable(model, C[1:m, 1:m])
+        JuMP.@variable(model, V[1:m, 1:(m - 1)])
+        JuMP.@constraint(model, [k in 1:m], d[k] == sum(C[:, k]))
+        JuMP.@constraint(model, [k in 1:m, i in 1:n], dot(A[notk[k], i] .- A[k, i], V[k, :]) == 0)
+        JuMP.@constraint(model, [k in 1:m], vcat(C[k, k] + sum(V[k, :]), C[k, notk[k]], V[k, :]) in MOI.RelativeEntropyCone(2m - 1))
+    else
+        # TODO dual formulation
+        # primal is: min_{γ in R, μ in R^q} -γ : c - γe₁ - sum_{p in 1:q} μ_p * g_p in C_SAGE(A), μ in R_+^q
+        # dual is: max_{π in R^m, ψ in R^q} -c'π : -1 + π_1 = 0, g_1'π - ψ_1 = ... = g_q'π - ψ_q = 0, π in C_SAGE^*(A), ψ in R_+^q
+
+        # JuMP.@variable(model, π[1:m])
+        # JuMP.@constraint(model, π[1] == 1)
+        π = vcat(1, JuMP.@variable(model, [1:(m - 1)]))
+
+        JuMP.@objective(model, Min, dot(fc, π))
+
+        # JuMP.@variable(model, ψ[1:q] >= 0)
+        # JuMP.@constraint(model, [p in 1:q], dot(gc[p], π) - ψ[p] == 0)
+        JuMP.@constraint(model, [p in 1:q], dot(gc[p], π) >= 0) # TODO can include this in the SAGE cone definition (changes it a bit though - what's the analogy to dual WSOS?)
+
+        # setup dual SAGE constraints: π in C_SAGE^*(A)
+        JuMP.@variable(model, τ[1:m, 1:n])
+        for k in 1:m, l in 1:m
+            k == l && continue
+            # π_k*log(π_k/π_l) <= (A_k - A_l)'*τ_(k,:)
+            epi = dot(τ[k, :], A[k, :] - A[l, :])
+            JuMP.@constraint(model, [epi, π[l], π[k]] in MOI.RelativeEntropyCone(3))
+        end
+        # for k in 1:m
+        # notk = [l for l in 1:m if l != k]
+        #     vec_trans = zeros(JuMP.AffExpr, 2m - 1)
+        #     vec_trans[1] = π[k]
+        #     vec_trans[2:2:end] = π[notk]
+        #     τk = JuMP.@variable(model, [1:n])
+        #     vec_trans[3:2:end] = [dot(τk, A[k, :] - A[l, :]) - π[k] for l in notk]
+        #     JuMP.@constraint(model, vec_trans in Hypatia.EpiSumPerEntropyCone{Float64}(2m - 1, true)) # use higher dim RE dual cone
+        # end
+    end
 
     return model
 end
