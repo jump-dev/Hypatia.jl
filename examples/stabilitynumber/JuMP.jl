e820dc72b40537101c6faefbc4fab464345fc4ad
Chris Coey <coey.chris@gmail.com>
2020-08-07 13:20:21 +1000

    minor cleanup example
    Former-commit-id: 7b60818342cac6f90226de282f6025476aebc6da

--- examples/stabilitynumber/JuMP.jl
+++ examples/stabilitynumber/JuMP.jl
@@ -12,28 +12,29 @@ import Hypatia.ModelUtilities.vec_to_svec!
 
 struct StabilityNumber{T <: Real} <: ExampleInstanceJuMP{T}
     side::Int
-    use_dnn::Bool
+    use_doublynonnegative::Bool
 end
 
 function build(inst::StabilityNumber{T}) where {T <: Float64} # TODO generic reals
-    (side, use_dnn) = (inst.side, inst.use_dnn)
-    sparsity = 1 - 1.0 / side
-    inv_graph = tril!(sprandn(side, side, sparsity)) + Diagonal(ones(side))
-    (row_idxs, col_idxs, A_vals) = findnz(inv_graph)
-    diag_idxs = findall(row_idxs .== col_idxs)
+    side = inst.side
+    sparsity = 1 - inv(side)
+    inv_graph = tril!(sprand(Bool, side, side, sparsity) + I)
+    (row_idxs, col_idxs, _) = findnz(inv_graph)
+    diags = (row_idxs .== col_idxs)
 
     model = JuMP.Model()
     JuMP.@variable(model, X[1:length(row_idxs)])
-    JuMP.@objective(model, Max, 2 * sum(X) - sum(X[diag_idxs]))
-    JuMP.@constraint(model, sum(X[diag_idxs]) == 1)
+    X_diag = X[diags]
+    JuMP.@objective(model, Max, 2 * sum(X) - sum(X_diag))
+    JuMP.@constraint(model, sum(X_diag) == 1)
     X_lifted = sparse(row_idxs, col_idxs, X, side, side)
-    X_vec = [X_lifted[i, j] for i in 1:side for j in 1:i]
-    if inst.use_dnn
+    X_vec = JuMP.AffExpr[X_lifted[i, j] for i in 1:side for j in 1:i]
+    if inst.use_doublynonnegative
         cone_dim = length(X_vec)
-        JuMP.@constraint(model, [X_vec...] .* vec_to_svec!(ones(cone_dim)) in Hypatia.DoublyNonnegativeTriCone{T}(cone_dim))
+        JuMP.@constraint(model, X_vec .* vec_to_svec!(ones(cone_dim)) in Hypatia.DoublyNonnegativeTriCone{T}(cone_dim))
     else
-        JuMP.@constraint(model, [X_vec...] in MOI.PositiveSemidefiniteConeTriangle(side))
-        JuMP.@constraint(model, [X_vec...] .>= 0)
+        JuMP.@constraint(model, X_vec in MOI.PositiveSemidefiniteConeTriangle(side))
+        JuMP.@constraint(model, X[.!(diags)] .>= 0)
     end
 
     return model
