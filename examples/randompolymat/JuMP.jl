a0f182f5d2b230c22d77ca7e70acf460106e9f5a
lkapelevich <leakapelevich@gmail.com>
2020-09-05 10:39:58 -0400

    comment out diag version of rpm
    Former-commit-id: ac385e21894eb48552e102f9ed54947b120170d6

--- examples/randompolymat/JuMP.jl
+++ examples/randompolymat/JuMP.jl
@@ -143,62 +143,62 @@ function build(inst::RandomPolyMatJuMP{T}) where {T <: Float64} # TODO generic r
     return model
 end
 
-function build(inst::RandomPolyMatJuMP{T}) where {T <: Float64} # TODO generic reals
-    (n, halfdeg, R) = (inst.n, inst.halfdeg, inst.R)
-    U = binomial(n + 2 * halfdeg, n)
-    L = binomial(n + halfdeg, n)
-    svec_dim = div(R * (R + 1), 2)
-
-    domain = ModelUtilities.FreeDomain{Float64}(n)
-    # domain = ModelUtilities.Box{Float64}(-ones(n), ones(n))
-    (U, points, Ps, _, w) = ModelUtilities.interpolate(domain, halfdeg, calc_w = true)
-
-    model = JuMP.Model()
-    JuMP.@variable(model, q_poly[1:U, 1:R])
-
-    full_mat = Vector{T where T}[zeros(U) for _ in 1:R, _ in 1:R]
-    for j in 1:R
-        for i in 1:(j - 1)
-            full_mat[i, j] = full_mat[j, i] = vec(Ps[1] * rand(-9:9, L, 1))
-        end
-        full_mat[j, j] = q_poly[:, j]
-    end
-    full_coeffs = full_mat
-
-
-    if inst.formulation == :nat_wsos_mat
-        JuMP.@constraint(model, vcat([full_coeffs[i, j] * (i == j ? 1 : sqrt(2)) for j in 1:R for i in 1:j]...) in Hypatia.WSOSInterpPosSemidefTriCone{Float64}(R, U, Ps))
-    elseif inst.formulation == :nat_wsos
-        ypts = zeros(svec_dim, R)
-        idx = 1
-        for j in 1:R
-            for i in 1:(j - 1)
-                full_coeffs[i, j] .*= 2
-                full_coeffs[i, j] .+= full_coeffs[i, i] + full_coeffs[j, j]
-                ypts[idx, i] = ypts[idx, j] = 1
-                idx += 1
-            end
-            ypts[idx, j] = 1
-            idx += 1
-        end
-        new_Ps = Matrix{Float64}[]
-        for P in Ps
-            push!(new_Ps, kron(ypts, P))
-        end
-        JuMP.@constraint(model, vcat(Vector{JuMP.AffExpr}[full_coeffs[i, j] for j in 1:R for i in 1:j]...) in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U * svec_dim, new_Ps))
-    elseif inst.formulation == :ext
-        JuMP.@variable(model, psd_var[1:(L * R), 1:(L * R)], PSD)
-        for x2 in 1:R, x1 in 1:x2
-            coeffs_lhs = JuMP.@expression(model, [u in 1:U], sum(Ps[1][u, k] * Ps[1][u, l] * psd_var[(x1 - 1) * L + k, (x2 - 1) * L + l] for k in 1:L for l in 1:L))
-            JuMP.@constraint(model, coeffs_lhs .== full_coeffs[x1, x2])
-        end
-    else
-        error()
-    end
-    JuMP.@objective(model, Min, sum(dot(w, q_poly[:, i]) for i in 1:R))
-
-    return model
-end
+# function build(inst::RandomPolyMatJuMP{T}) where {T <: Float64} # TODO generic reals
+#     (n, halfdeg, R) = (inst.n, inst.halfdeg, inst.R)
+#     U = binomial(n + 2 * halfdeg, n)
+#     L = binomial(n + halfdeg, n)
+#     svec_dim = div(R * (R + 1), 2)
+#
+#     domain = ModelUtilities.FreeDomain{Float64}(n)
+#     # domain = ModelUtilities.Box{Float64}(-ones(n), ones(n))
+#     (U, points, Ps, _, w) = ModelUtilities.interpolate(domain, halfdeg, calc_w = true)
+#
+#     model = JuMP.Model()
+#     JuMP.@variable(model, q_poly[1:U, 1:R])
+#
+#     full_mat = Vector{T where T}[zeros(U) for _ in 1:R, _ in 1:R]
+#     for j in 1:R
+#         for i in 1:(j - 1)
+#             full_mat[i, j] = full_mat[j, i] = vec(Ps[1] * rand(-9:9, L, 1))
+#         end
+#         full_mat[j, j] = q_poly[:, j]
+#     end
+#     full_coeffs = full_mat
+#
+#
+#     if inst.formulation == :nat_wsos_mat
+#         JuMP.@constraint(model, vcat([full_coeffs[i, j] * (i == j ? 1 : sqrt(2)) for j in 1:R for i in 1:j]...) in Hypatia.WSOSInterpPosSemidefTriCone{Float64}(R, U, Ps))
+#     elseif inst.formulation == :nat_wsos
+#         ypts = zeros(svec_dim, R)
+#         idx = 1
+#         for j in 1:R
+#             for i in 1:(j - 1)
+#                 full_coeffs[i, j] .*= 2
+#                 full_coeffs[i, j] .+= full_coeffs[i, i] + full_coeffs[j, j]
+#                 ypts[idx, i] = ypts[idx, j] = 1
+#                 idx += 1
+#             end
+#             ypts[idx, j] = 1
+#             idx += 1
+#         end
+#         new_Ps = Matrix{Float64}[]
+#         for P in Ps
+#             push!(new_Ps, kron(ypts, P))
+#         end
+#         JuMP.@constraint(model, vcat(Vector{JuMP.AffExpr}[full_coeffs[i, j] for j in 1:R for i in 1:j]...) in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U * svec_dim, new_Ps))
+#     elseif inst.formulation == :ext
+#         JuMP.@variable(model, psd_var[1:(L * R), 1:(L * R)], PSD)
+#         for x2 in 1:R, x1 in 1:x2
+#             coeffs_lhs = JuMP.@expression(model, [u in 1:U], sum(Ps[1][u, k] * Ps[1][u, l] * psd_var[(x1 - 1) * L + k, (x2 - 1) * L + l] for k in 1:L for l in 1:L))
+#             JuMP.@constraint(model, coeffs_lhs .== full_coeffs[x1, x2])
+#         end
+#     else
+#         error()
+#     end
+#     JuMP.@objective(model, Min, sum(dot(w, q_poly[:, i]) for i in 1:R))
+#
+#     return model
+# end
 
 # instances[RandomPolyMatJuMP]["minimal"] = [
 #     ((1, 1, 2, true, false),),
