6429a67a7d7f7c1b4fa4870b48975cedd12c7851
Chris Coey <coey.chris@gmail.com>
2020-09-09 16:39:53 +1000

    cleanup line search

--- src/Solvers/linesearch.jl
+++ src/Solvers/linesearch.jl
@@ -2,127 +2,50 @@
 line search for s,z
 =#
 
-# # backtracking line search to find large distance to step in direction while remaining inside cones and inside a given neighborhood
-# function find_max_alpha(
-#     stepper::Stepper{T},
-#     solver::Solver{T};
-#     prev_alpha::T,
-#     min_alpha::T,
-#     ) where {T <: Real}
-#     cones = solver.model.cones
-#     cone_times = stepper.cone_times
-#     cone_order = stepper.cone_order
-#     z = solver.point.z
-#     s = solver.point.s
-#     tau = solver.point.tau
-#     kap = solver.point.kap
-#     z_dir = stepper.dir.z
-#     s_dir = stepper.dir.s
-#     tau_dir = stepper.dir[stepper.tau_row]
-#     kap_dir = stepper.dir[stepper.kap_row]
-#     z_ls = stepper.z_ls
-#     s_ls = stepper.s_ls
-#     primals_ls = stepper.primal_views_ls
-#     duals_ls = stepper.dual_views_ls
-#     timer = solver.timer
-#
-#     alpha = max(T(0.1), min(prev_alpha * T(1.4), one(T))) # TODO option for parameter
-#     if tau_dir < zero(T)
-#         alpha = min(alpha, -tau / tau_dir)
-#     end
-#     if kap_dir < zero(T)
-#         alpha = min(alpha, -kap / kap_dir)
-#     end
-#     alpha *= T(0.9999)
-#
-#     nup1 = solver.model.nu + 1
-#     while true
-#         in_nbhd = true
-#
-#         @. z_ls = z + alpha * z_dir
-#         @. s_ls = s + alpha * s_dir
-#         dot_s_z = zero(T)
-#         for k in cone_order
-#             dot_s_z_k = dot(primals_ls[k], duals_ls[k])
-#             if dot_s_z_k < eps(T)
-#                 in_nbhd = false
-#                 break
-#             end
-#             dot_s_z += dot_s_z_k
-#         end
-#
-#         if in_nbhd
-#             taukap_temp = (tau + alpha * tau_dir) * (kap + alpha * kap_dir)
-#             mu_temp = (dot_s_z + taukap_temp) / nup1
-#
-#             if mu_temp > eps(T) && abs(taukap_temp - mu_temp) < mu_temp * solver.max_nbhd
-#                 # order the cones by how long it takes to check neighborhood condition and iterate in that order, to improve efficiency
-#                 sortperm!(cone_order, cone_times, initialized = true)
-#
-#                 irt_mu_temp = inv(sqrt(mu_temp))
-#                 for k in cone_order
-#                     cone_k = cones[k]
-#                     time_k = time_ns()
-#                     Cones.load_point(cone_k, primals_ls[k], irt_mu_temp)
-#                     Cones.load_dual_point(cone_k, duals_ls[k])
-#                     Cones.reset_data(cone_k)
-#                     in_nbhd_k = (Cones.is_feas(cone_k) && Cones.is_dual_feas(cone_k) && Cones.in_neighborhood(cone_k, mu_temp))
-#                     cone_times[k] = time_ns() - time_k
-#
-#                     if !in_nbhd_k
-#                         in_nbhd = false
-#                         break
-#                     end
-#                 end
-#
-#                 if in_nbhd
-#                     break
-#                 end
-#             end
-#         end
-#
-#         if alpha < min_alpha
-#             # alpha is very small so finish
-#             alpha = zero(T)
-#             break
-#         end
-#
-#         # iterate is outside the neighborhood: decrease alpha
-#         alpha *= T(0.9) # TODO option for parameter
-#     end
-#
-#     return alpha
-# end
+mutable struct LineSearcher{T <: Real}
+    z::Vector{T}
+    s::Vector{T}
+    dual_views::Vector{SubArray{T, 1, Vector{T}, Tuple{UnitRange{Int}}, true}}
+    primal_views::Vector{SubArray{T, 1, Vector{T}, Tuple{UnitRange{Int}}, true}}
+    skzk::Vector{T}
+    nup1::T
+    cone_times::Vector{Float64}
+    cone_order::Vector{Int}
+
+    function LineSearcher{T}(model::Models.Model{T}) where {T <: Real}
+        cones = model.cones
+        line_searcher = new{T}()
+        z = line_searcher.z = zeros(T, model.q)
+        s = line_searcher.s = zeros(T, model.q)
+        line_searcher.dual_views = [view(Cones.use_dual_barrier(cone) ? s : z, idxs) for (cone, idxs) in zip(cones, model.cone_idxs)]
+        line_searcher.primal_views = [view(Cones.use_dual_barrier(cone) ? z : s, idxs) for (cone, idxs) in zip(cones, model.cone_idxs)]
+        line_searcher.skzk = zeros(T, length(cones))
+        line_searcher.nup1 = T(model.nu + 1)
+        line_searcher.cone_times = zeros(length(cones))
+        line_searcher.cone_order = collect(1:length(cones))
+        return line_searcher
+    end
+end
 
 # backtracking line search to find large distance to step in direction while remaining inside cones and inside a given neighborhood
 function find_max_alpha(
-    stepper::Stepper{T},
-    solver::Solver{T}; # TODO remove if not using
+    point::Point{T},
+    dir::Point{T},
+    line_searcher::LineSearcher{T},
+    model::Models.Model{T}; # TODO remove if not using
     prev_alpha::T,
     min_alpha::T,
     min_nbhd::T = T(0.01),
     # max_nbhd::T = one(T),
     max_nbhd::T = T(0.99),
     ) where {T <: Real}
-    cones = solver.model.cones
-    cone_times = stepper.cone_times
-    cone_order = stepper.cone_order
-    z = solver.point.z
-    s = solver.point.s
-    tau = solver.point.tau[1]
-    kap = solver.point.kap[1]
-    z_dir = stepper.dir.z
-    s_dir = stepper.dir.s
-    tau_dir = stepper.dir.tau[1]
-    kap_dir = stepper.dir.kap[1]
-    z_ls = stepper.z_ls
-    s_ls = stepper.s_ls
-    primals_ls = stepper.primal_views_ls
-    duals_ls = stepper.dual_views_ls
+    cones = model.cones
+    cone_order = line_searcher.cone_order
+    (tau, kap) = (point.tau[1], point.kap[1])
+    (tau_dir, kap_dir) = (dir.tau[1], dir.kap[1])
+    skzk = line_searcher.skzk
 
     alpha_reduce = T(0.95) # TODO tune, maybe try smaller for pred_alpha since heuristic
-    nup1 = solver.model.nu + 1
-    sz_ks = zeros(T, length(cone_order)) # TODO prealloc
 
     # TODO experiment with starting alpha (<1)
     # alpha = one(T)
@@ -150,22 +73,22 @@ function find_max_alpha(
         (taukap_ls < eps(T)) && continue
 
         # order the cones by how long it takes to check neighborhood condition and iterate in that order, to improve efficiency
-        sortperm!(cone_order, cone_times, initialized = true) # NOTE stochastic
+        sortperm!(cone_order, line_searcher.cone_times, initialized = true) # NOTE stochastic
 
-        @. z_ls = z + alpha * z_dir
-        @. s_ls = s + alpha * s_dir
+        @. line_searcher.z = point.z + alpha * dir.z
+        @. line_searcher.s = point.s + alpha * dir.s
 
         for k in cone_order
-            sz_ks[k] = dot(primals_ls[k], duals_ls[k])
+            skzk[k] = dot(line_searcher.primal_views[k], line_searcher.dual_views[k])
         end
-        any(<(eps(T)), sz_ks) && continue
+        any(<(eps(T)), skzk) && continue
 
-        mu_ls = (sum(sz_ks) + taukap_ls) / nup1
+        mu_ls = (sum(skzk) + taukap_ls) / line_searcher.nup1
         (mu_ls < eps(T)) && continue
 
         min_nbhd_mu = min_nbhd * mu_ls
         (taukap_ls < min_nbhd_mu) && continue
-        any(sz_ks[k] < min_nbhd_mu * Cones.get_nu(cones[k]) for k in cone_order) && continue
+        any(skzk[k] < min_nbhd_mu * Cones.get_nu(cones[k]) for k in cone_order) && continue
 
         # TODO experiment with SY nbhd for tau-kappa
         isfinite(max_nbhd) && (abs(taukap_ls - mu_ls) > max_nbhd * mu_ls) && continue
@@ -177,8 +100,8 @@ function find_max_alpha(
             cone_k = cones[k]
             time_k = time_ns()
 
-            Cones.load_point(cone_k, primals_ls[k], irtmu)
-            Cones.load_dual_point(cone_k, duals_ls[k])
+            Cones.load_point(cone_k, line_searcher.primal_views[k], irtmu)
+            Cones.load_dual_point(cone_k, line_searcher.dual_views[k])
             Cones.reset_data(cone_k)
 
             in_nbhd_k = (Cones.is_feas(cone_k) && Cones.is_dual_feas(cone_k) && Cones.in_neighborhood(cone_k, rtmu, max_nbhd))
@@ -186,7 +109,7 @@ function find_max_alpha(
             # TODO is_dual_feas function should fall back to a nbhd-like check (for ray maybe) if not using nbhd check
             # in_nbhd_k = (Cones.is_feas(cone_k) && Cones.is_dual_feas(cone_k))
 
-            cone_times[k] = time_ns() - time_k
+            line_searcher.cone_times[k] = time_ns() - time_k
             if !in_nbhd_k
                 in_nbhd = false
                 break
