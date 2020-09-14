4aebb4f46092a95f4f396a97a990853f8fc35db6
lkapelevich <leakapelevich@gmail.com>
2020-09-14 06:47:31 -0400

    add l1 for envelope
    Former-commit-id: bd24015d53aa078585e4ba19d32a838a48b903d2

--- examples/envelope/JuMP.jl
+++ examples/envelope/JuMP.jl
@@ -15,6 +15,7 @@ struct EnvelopeJuMP{T <: Real} <: ExampleInstanceJuMP{T}
     num_polys::Int
     env_halfdeg::Int
     formulation::Symbol
+    problem::Symbol
 end
 
 function build(inst::EnvelopeJuMP{T}) where {T <: Float64} # TODO generic reals
@@ -35,86 +36,93 @@ function build(inst::EnvelopeJuMP{T}) where {T <: Float64} # TODO generic reals
     model = JuMP.Model()
     JuMP.@variable(model, fpv[1:U]) # values at Fekete points
     JuMP.@objective(model, Min, dot(fpv, w)) # integral over domain (via quadrature)
-    #
-    # p_plus = []
-    # p_minus = []
-    # for i in 1:inst.num_polys
-    #     p_plus_i = JuMP.@variable(model, [1:U])
-    #     p_minus_i = JuMP.@variable(model, [1:U])
-    #     push!(p_plus, p_plus_i)
-    #     push!(p_minus, p_minus_i)
-    #     JuMP.@constraint(model, polys[:, i] .== p_plus_i - p_minus_i)
-    #     JuMP.@constraint(model, p_plus_i in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
-    #     JuMP.@constraint(model, p_minus_i in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
-    # end
-    # JuMP.@constraint(model, fpv - sum(p_plus) - sum(p_minus) in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
 
-    # JuMP.@constraint(model, [i in 1:inst.num_polys], polys[:, i] .- fpv in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
     R = num_polys + 1
-    if inst.formulation == :nat_wsos_soc
-        # JuMP.@constraint(model, vcat(fpv, [polys[:, i] for i in 1:inst.num_polys]...) in Hypatia.WSOSInterpEpiNormInfCone{Float64}(R, U, Ps))
-        JuMP.@constraint(model, vcat(fpv, [polys[:, i] for i in 1:inst.num_polys]...) in Hypatia.WSOSInterpEpiNormEuclCone{Float64}(R, U, Ps))
-    elseif inst.formulation == :nat_wsos # broken
-        # keep in mind this is not equivalent
-        svec_dim = div(R * (R + 1), 2)
-        ypts = zeros(svec_dim, R)
-        polyvec = Vector{JuMP.AffExpr}(undef, svec_dim * U)
-        # 11
-        polyvec[1:U] .= fpv
-        ypts[1, 1] = 1
-        idx = 2
-        for i in 2:R
-            # i1
-            polyvec[Cones.block_idxs(U, idx)] .= (2 * polys[:, i - 1] / sqrt(2) + 2 * fpv)
-            ypts[idx, i] = ypts[idx, 1] = 1
-            idx += 1
-            # ij
-            for j in 2:(i - 1)
-                polyvec[Cones.block_idxs(U, idx)] .= 0
-                ypts[idx, i] = ypts[idx, j] = 1
+
+    if inst.problem == :L1
+        if inst.formulation == :nat_wsos
+            p_plus = []
+            p_minus = []
+            for i in 1:inst.num_polys
+                p_plus_i = JuMP.@variable(model, [1:U])
+                p_minus_i = JuMP.@variable(model, [1:U])
+                push!(p_plus, p_plus_i)
+                push!(p_minus, p_minus_i)
+                JuMP.@constraint(model, polys[:, i] .== p_plus_i - p_minus_i)
+                JuMP.@constraint(model, p_plus_i in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
+                JuMP.@constraint(model, p_minus_i in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
+            end
+            JuMP.@constraint(model, fpv - sum(p_plus) - sum(p_minus) in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
+        elseif inst.formulation == :nat_wsos_l1
+            JuMP.@constraint(model, vcat(fpv, [polys[:, i] for i in 1:inst.num_polys]...) in Hypatia.WSOSInterpEpiNormInfCone{Float64}(R, U, Ps))
+        end
+    else
+
+        # JuMP.@constraint(model, [i in 1:inst.num_polys], polys[:, i] .- fpv in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(U, Ps))
+        if inst.formulation == :nat_wsos_soc
+            JuMP.@constraint(model, vcat(fpv, [polys[:, i] for i in 1:inst.num_polys]...) in Hypatia.WSOSInterpEpiNormEuclCone{Float64}(R, U, Ps))
+        elseif inst.formulation == :nat_wsos # broken
+            # keep in mind this is not equivalent
+            svec_dim = div(R * (R + 1), 2)
+            ypts = zeros(svec_dim, R)
+            polyvec = Vector{JuMP.AffExpr}(undef, svec_dim * U)
+            # 11
+            polyvec[1:U] .= fpv
+            ypts[1, 1] = 1
+            idx = 2
+            for i in 2:R
+                # i1
+                polyvec[Cones.block_idxs(U, idx)] .= (2 * polys[:, i - 1] / sqrt(2) + 2 * fpv)
+                ypts[idx, i] = ypts[idx, 1] = 1
+                idx += 1
+                # ij
+                for j in 2:(i - 1)
+                    polyvec[Cones.block_idxs(U, idx)] .= 0
+                    ypts[idx, i] = ypts[idx, j] = 1
+                    idx += 1
+                end
+                polyvec[Cones.block_idxs(U, idx)] .= fpv
+                ypts[idx, i] = 1
                 idx += 1
             end
-            polyvec[Cones.block_idxs(U, idx)] .= fpv
-            ypts[idx, i] = 1
-            idx += 1
-        end
-        new_Ps = Matrix{Float64}[]
-        for P in Ps
-            push!(new_Ps, kron(ypts, P))
-        end
-        JuMP.@constraint(model, polyvec in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(svec_dim * U, new_Ps))
-    elseif inst.formulation == :nat_wsos_mat
-        svec_dim = div(R * (R + 1), 2)
-        polyvec = Vector{JuMP.AffExpr}(undef, svec_dim * U)
-        polyvec[1:U] .= fpv
-        idx = 2
-        for j in 2:R
-            polyvec[Cones.block_idxs(U, idx)] .= polys[:, (j - 1)] * sqrt(2)
-            idx += 1
-            for i in 2:(j - 1)
-                polyvec[Cones.block_idxs(U, idx)] .= 0
+            new_Ps = Matrix{Float64}[]
+            for P in Ps
+                push!(new_Ps, kron(ypts, P))
+            end
+            JuMP.@constraint(model, polyvec in Hypatia.WSOSInterpNonnegativeCone{Float64, Float64}(svec_dim * U, new_Ps))
+        elseif inst.formulation == :nat_wsos_mat
+            svec_dim = div(R * (R + 1), 2)
+            polyvec = Vector{JuMP.AffExpr}(undef, svec_dim * U)
+            polyvec[1:U] .= fpv
+            idx = 2
+            for j in 2:R
+                polyvec[Cones.block_idxs(U, idx)] .= polys[:, (j - 1)] * sqrt(2)
+                idx += 1
+                for i in 2:(j - 1)
+                    polyvec[Cones.block_idxs(U, idx)] .= 0
+                    idx += 1
+                end
+                polyvec[Cones.block_idxs(U, idx)] .= fpv
                 idx += 1
             end
-            polyvec[Cones.block_idxs(U, idx)] .= fpv
-            idx += 1
-        end
-        JuMP.@constraint(model, polyvec in Hypatia.WSOSInterpPosSemidefTriCone{Float64}(R, U, Ps))
-    elseif inst.formulation == :ext
-        psd_vars = []
-        for (r, Pr) in enumerate(Ps)
-            Lr = size(Pr, 2)
-            psd_r = JuMP.@variable(model, [1:(Lr * R), 1:(Lr * R)], Symmetric)
-            push!(psd_vars, psd_r)
-            JuMP.@SDconstraint(model, psd_r >= 0)
-        end
-        Ls = [size(Pr, 2) for Pr in Ps]
-        JuMP.@constraint(model, [u in 1:U], fpv[u] .== sum(sum(Ps[r][u, k] * Ps[r][u, l] * psd_vars[r][(x1 - 1) * Ls[r] + k, (x1 - 1) * Ls[r] + l] for x1 in 1:R for k in 1:Ls[r] for l in 1:Ls[r]) for r in eachindex(Ls)))
-        for x1 in 2:R
-            # note that psd_vars[r][(x1 - 1) * Ls[r] + k, (x2 - 1) * Ls[r] + l] is not necessarily symmetric
-            coeffs_lhs = JuMP.@expression(model, [u in 1:U], sum(sum(Ps[r][u, k] * Ps[r][u, l] * (psd_vars[r][(x1 - 1) * Ls[r] + k, l] + psd_vars[r][(x1 - 1) * Ls[r] + l, k]) for k in 1:Ls[r] for l in 1:Ls[r]) for r in eachindex(Ls)))
-            JuMP.@constraint(model, coeffs_lhs .== polys[:, (x1 - 1)])
-        end
-    end
+            JuMP.@constraint(model, polyvec in Hypatia.WSOSInterpPosSemidefTriCone{Float64}(R, U, Ps))
+        elseif inst.formulation == :ext
+            psd_vars = []
+            for (r, Pr) in enumerate(Ps)
+                Lr = size(Pr, 2)
+                psd_r = JuMP.@variable(model, [1:(Lr * R), 1:(Lr * R)], Symmetric)
+                push!(psd_vars, psd_r)
+                JuMP.@SDconstraint(model, psd_r >= 0)
+            end
+            Ls = [size(Pr, 2) for Pr in Ps]
+            JuMP.@constraint(model, [u in 1:U], fpv[u] .== sum(sum(Ps[r][u, k] * Ps[r][u, l] * psd_vars[r][(x1 - 1) * Ls[r] + k, (x1 - 1) * Ls[r] + l] for x1 in 1:R for k in 1:Ls[r] for l in 1:Ls[r]) for r in eachindex(Ls)))
+            for x1 in 2:R
+                # note that psd_vars[r][(x1 - 1) * Ls[r] + k, (x2 - 1) * Ls[r] + l] is not necessarily symmetric
+                coeffs_lhs = JuMP.@expression(model, [u in 1:U], sum(sum(Ps[r][u, k] * Ps[r][u, l] * (psd_vars[r][(x1 - 1) * Ls[r] + k, l] + psd_vars[r][(x1 - 1) * Ls[r] + l, k]) for k in 1:Ls[r] for l in 1:Ls[r]) for r in eachindex(Ls)))
+                JuMP.@constraint(model, coeffs_lhs .== polys[:, (x1 - 1)])
+            end
+        end # formulation
+    end # problem type
 
     return model
 end
