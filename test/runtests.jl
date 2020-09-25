c3ef2e68ed9485875172a2e53aa4e12415a3b1e5
Chris Coey <coey.chris@gmail.com>
2020-09-25 20:27:04 +1000

    try barrier tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ using Test
 
 @info("starting minimal tests")
 @testset "minimal tests" begin
-    include(joinpath(@__DIR__, "runmodelutilitiestests.jl"))
-    # include(joinpath(@__DIR__, "runbarriertests.jl"))
+    # include(joinpath(@__DIR__, "runmodelutilitiestests.jl"))
+    include(joinpath(@__DIR__, "runbarriertests.jl"))
     # include(joinpath(@__DIR__, "runnativetests.jl"))
     # include(joinpath(@__DIR__, "runmoitests.jl"))
 end
