fe49a44813850703abfd5eb6bd1750a2d73bcf57
Chris Coey <coey.chris@gmail.com>
2020-09-25 20:18:29 +1000

    fix type bug

--- src/ModelUtilities/interpolate.jl
+++ src/ModelUtilities/interpolate.jl
@@ -233,7 +233,7 @@ function cheb2_data(
     if calc_w
         wa = T[2 / T(1 - j^2) for j in 0:2:(U - 1)]
         append!(wa, wa[div(U, 2):-1:2])
-        tmpconst = im * 2 * pi / length(wa)
+        tmpconst = pi / T(length(wa)) * 2 * im
         w = [abs(sum(wa[j] * exp(tmpconst * (i - 1) * j) for j in eachindex(wa)) / length(wa)) for i in eachindex(wa)] # inverse fft
         w[1] /= 2
         push!(w, w[1])
