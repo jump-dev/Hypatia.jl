218b01d7890de5e34d911bdd88e0f39f461050a9
Chris Coey <coey.chris@gmail.com>
2020-10-23 22:46:49 +1000

    try moi with looser tol

--- test/runmoitests.jl
+++ test/runmoitests.jl
@@ -27,7 +27,7 @@ include(joinpath(@__DIR__, "moi.jl"))
 default_options = (
     # verbose = true,
     verbose = false,
-    default_tol_relax = 5,
+    default_tol_relax = 10,
     )
 
 @testset "MOI.Test tests" begin
