cf0c33ee94290f18b6bb2cfbaf1973c3f6f03bea
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2020-10-23 16:13:02 -0400

    [ci skip] shorter plot code

--- examples/postproc/plotcsvs.jl
+++ examples/postproc/plotcsvs.jl
@@ -1,13 +1,11 @@
 # function make_plot_csv(ex)
 #     df = CSV.read(ex * "_wide.csv")
-#     # only plot converged instances
-#     # success_df = DataFrame(x = Int[], gr = Union{Int, String}[], nat = Union{Missing, Float64}[], ext = Union{Missing, Float64}[], mosek = Union{Missing, Float64}[])
 #     if ex == "MatrixCompletionJuMP"
-#         group_vals = [5, 10]
 #         transform!(df, [:d1, :d2] => ((x, y) -> round.(Int, y ./ x)) => :gr)
+#         x_var = :d1
 #     elseif ex in ["DensityEstJuMP", "PolyMinJuMP", "ShapeConRegrJuMP"]
-#         group_vals = [2, 3, 4]
 #         rename!(df, :m => :gr)
+#         x_var = :d
 #     else
 #         ex_short = ex[1:(match(r"JuMP", ex).offset + 3)] # in case of suffix
 #         x_var = params_map[ex_short].inst_keys[1]
@@ -17,20 +15,7 @@
 #     transform!(df, [:converged_ext_Hypatia, :solve_time_ext_Hypatia] => ((x, y) -> (!ismissing(x) && x ? y : missing)) => :ext)
 #     transform!(df, [:converged_ext_Mosek, :solve_time_ext_Mosek] => ((x, y) -> (!ismissing(x) && x ? y : missing)) => :mosek)
 #
-#     for r in eachrow(df)
-#         nat_time = (coalesce(r.converged_nat_Hypatia, false) ? r[:solve_time_nat_Hypatia] : missing)
-#         ext_time = (coalesce(r.converged_ext_Hypatia, false) ? r[:solve_time_ext_Hypatia] : missing)
-#         mosek_time = (coalesce(r.converged_ext_Mosek, false) ? r[:solve_time_ext_Mosek] : missing)
-#         if ex == "MatrixCompletionJuMP"
-#             push!(success_df, (r.d1, round(Int, r[:d2] / r[:d1]), nat_time, ext_time, mosek_time))
-#         elseif ex in ["DensityEstJuMP", "PolyMinJuMP", "ShapeConRegrJuMP"]
-#             push!(success_df, (r[:d], r[:m], nat_time, ext_time, mosek_time))
-#         else
-#             ex_short = ex[1:(match(r"JuMP", ex).offset + 3)] # in case of suffix
-#             x_var = params_map[ex_short].inst_keys[1]
-#             push!(success_df, (r[x_var], "", nat_time, ext_time, mosek_time))
-#         end
-#     end
+#     success_df = select(df, x_var, :gr, :nat, :ext, :mosek)
 #     for subdf in groupby(success_df, :gr)
 #         v = subdf.gr[1]
 #         CSV.write(ex * "$(v)_plot.csv", select(subdf, Not(:gr)))
@@ -42,11 +27,6 @@ function make_plot_csv(ex)
     df = CSV.read(ex * "_wide.csv")
     # only plot converged instances
     success_df = DataFrame(x = Int[], gr = Union{Int, String}[], nat = Union{Missing, Float64}[], ext = Union{Missing, Float64}[], mosek = Union{Missing, Float64}[])
-    if ex == "MatrixCompletionJuMP"
-        group_vals = [5, 10]
-    elseif ex in ["DensityEstJuMP", "PolyMinJuMP", "ShapeConRegrJuMP"]
-        group_vals = [2, 3, 4]
-    end
     for r in eachrow(df)
         nat_time = (coalesce(r.converged_nat_Hypatia, false) ? r.solve_time_nat_Hypatia : missing)
         ext_time = (coalesce(r.converged_ext_Hypatia, false) ? r.solve_time_ext_Hypatia : missing)
