0a6505509fa5c9594e3cfae3c7d74ffb9cf5735e
Chris Coey <coey.chris@gmail.com>
2020-11-07 22:51:35 +1000

    adjust expdesign benchmark sizes

--- examples/expdesign/JuMP_benchmark.jl
+++ examples/expdesign/JuMP_benchmark.jl
@@ -1,6 +1,6 @@
 
 expdesign_insts = [
-    [(q, 2q, 2q, 5, use_logdet, !use_logdet, false) for q in vcat(3, 20:20:300)] # includes compile run
+    [(q, 2q, 2q, 5, use_logdet, !use_logdet, false) for q in vcat(3, 25:25:300)] # includes compile run
     for use_logdet in (false, true)
     ]
 
