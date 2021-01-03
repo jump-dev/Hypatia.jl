e3d532b3346f9e718099ba2cfbcc296e1e82ffe4
Chris Coey <coey.chris@gmail.com>
2021-01-03 21:09:58 +1000

    fix failing examples

--- examples/conditionnum/JuMP_test.jl
+++ examples/conditionnum/JuMP_test.jl
@@ -6,15 +6,15 @@ insts["minimal"] = [
     ((3, 2, false),),
     ]
 insts["fast"] = [
-    ((3, 4, true), nothing),
-    ((3, 4, false), nothing),
-    ((10, 15, true), nothing),
-    ((10, 15, false), nothing),
+    ((3, 4, true),),
+    ((3, 4, false),),
+    ((10, 15, true),),
+    ((10, 15, false),),
     ((20, 10, true), nothing, relaxed_tols),
     ((100, 40, false), nothing, relaxed_tols),
     ]
 insts["slow"] = [
-    ((100, 10, true), nothing),
-    ((100, 40, true), nothing),
+    ((100, 10, true),),
+    ((100, 40, true),),
     ]
 return (ConditionNumJuMP, insts)
