5fd83a591e0c81b896713d7ce0e817c8b40e29a0
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2021-02-04 15:53:02 -1000

    add all_opt column

--- examples/processbenchmarks2.jl
+++ examples/processbenchmarks2.jl
@@ -14,17 +14,29 @@ function shifted_geomean_opt(metric, status; shift = 0)
     idxs = .!(ismissing.(status)) .& (status .== "Optimal")
     return exp(sum(log, metric[idxs] .+ shift) / length(idxs))
 end
+shifted_geomean_all_opt(metric, all_opt; shift = 0) = exp(sum(log, metric[all_opt] .+ shift) / count(all_opt))
+
+# comb_df = filter(:stepper => isequal("Hypatia.Solvers.CombinedStepper{Float64}"), dropmissing(all_df))
+# pc_df = filter(t -> t.stepper == "Hypatia.Solvers.PredOrCentStepper{Float64}" && t.use_curve_search == true, dropmissing(all_df))
+# comb_times = (comb_df[!, :solve_time])
+# pc_times = (pc_df[!, :solve_time])
+#
+# plot(comb_times, seriestype=:stephist)
+# plot!(pc_times, seriestype=:stephist)
 
 # aggregate stuff
 function post_process()
     all_df = CSV.read(bench_file, DataFrame)
-    select!(all_df, :stepper, :use_corr, :use_curve_search, :solve_time, :iters, :status)
+    all_df = combine(groupby(all_df, :inst_data), names(all_df), :status => (x -> all(isequal.("Optimal", x))) => :all_opt)
+    select!(all_df, :stepper, :use_corr, :use_curve_search, :solve_time, :iters, :status, :all_opt)
 
     df_agg = combine(groupby(all_df, [:stepper, :use_corr, :use_curve_search]),
         :solve_time => shifted_geomean_notmissing => :solve_time_geomean_notmissing,
         :iters => shifted_geomean_notmissing => :iters_geomean_notmissing,
-        [:solve_time, :status] => shifted_geomean_opt => :solve_time_geomean_opt,
-        [:iters, :status] => shifted_geomean_opt => :iters_geomean_opt,
+        [:solve_time, :status] => shifted_geomean_opt => :solve_time_geomean_this_opt,
+        [:iters, :status] => shifted_geomean_opt => :iters_geomean_this_opt,
+        [:solve_time, :all_opt] => shifted_geomean_all_opt => :solve_time_geomean_all_opt,
+        [:iters, :all_opt] => shifted_geomean_all_opt => :iters_geomean_all_opt,
         :solve_time => (x -> shifted_geomean_all(x, cap = MAX_TIME)) => :solve_time_geomean_all,
         :iters => (x -> shifted_geomean_all(x, cap = MAX_ITER)) => :iters_geomean_all,
         :status => (x -> count(isequal("Optimal"), x)) => :optimal,
