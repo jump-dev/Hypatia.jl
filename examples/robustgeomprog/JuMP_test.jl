ec77e0f3c7ea8e7e212fb7fa9419932b92140d72
Lea Kapelevich <lkape@LeaXPS-15.mit.edu>
2021-02-14 16:02:21 -1000

    correct robustgeomprog instances and relax tols

--- examples/robustgeomprog/JuMP_test.jl
+++ examples/robustgeomprog/JuMP_test.jl
@@ -22,15 +22,15 @@ insts["slow"] = [
     ((100, 200), nothing, relaxed_tols),
     ]
 insts["various"] = [
-    ((5, 10),),
-    ((5, 10), SOCExpPSDOptimizer),
-    ((10, 20),),
-    ((10, 20), SOCExpPSDOptimizer),
-    ((20, 40),),
-    ((20, 40), SOCExpPSDOptimizer),
-    ((40, 80),),
-    ((40, 80), SOCExpPSDOptimizer),
-    ((100, 150),),
-    ((100, 150), SOCExpPSDOptimizer),
+    ((5, 10), nothing, relaxed_tols),
+    ((5, 10), SOCExpPSDOptimizer, relaxed_tols),
+    ((10, 20), nothing, relaxed_tols),
+    ((10, 20), SOCExpPSDOptimizer, relaxed_tols),
+    ((20, 40), nothing, relaxed_tols),
+    ((20, 40), SOCExpPSDOptimizer, relaxed_tols),
+    ((40, 80), nothing, relaxed_tols),
+    ((40, 80), SOCExpPSDOptimizer, relaxed_tols),
+    ((100, 150), nothing, relaxed_tols),
+    ((100, 150), SOCExpPSDOptimizer, relaxed_tols),
     ]
 return (RobustGeomProgJuMP, insts)
