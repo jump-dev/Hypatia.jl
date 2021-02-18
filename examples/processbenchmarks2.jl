e75af7ed167012b29eaef89f41f0da8b485ddad9
Lea Kapelevich <lkape@LeaXPS-15.mit.edu>
2021-02-17 19:14:53 -1000

    updates to perf profiles

--- examples/processbenchmarks2.jl
+++ examples/processbenchmarks2.jl
@@ -2,7 +2,7 @@ using CSV
 using DataFrames
 using Plots
 
-MAX_TIME = 1800
+MAX_TIME = 3600
 MAX_ITER = 250
 
 nickname = "various"
@@ -17,46 +17,16 @@ end
 shifted_geomean_conv(metric, conv; shift = 0) = exp(sum(log, metric[conv] .+ shift) / count(conv))
 shifted_geomean_all_conv(metric, all_conv; shift = 0) = exp(sum(log, metric[all_conv] .+ shift) / count(all_conv))
 
-# comb_df = filter(:stepper => isequal("Hypatia.Solvers.CombinedStepper{Float64}"), dropmissing(all_df))
-# pc_df = filter(t -> t.stepper == "Hypatia.Solvers.PredOrCentStepper{Float64}" && t.use_curve_search == true, dropmissing(all_df))
-# comb_times = (comb_df[!, :solve_time])
-# pc_times = (pc_df[!, :solve_time])
-#
-# plot(comb_times, seriestype=:stephist)
-# plot!(pc_times, seriestype=:stephist)
-
-# missig instances
-# all_df = CSV.read(bench_file, DataFrame)
-# insts1 = filter(t -> t.stepper == "Hypatia.Solvers.PredOrCentStepper{Float64}" && t.use_curve_search == true, dropmissing(all_df))
-# insts2 = filter(t -> t.stepper == "Hypatia.Solvers.CombinedStepper{Float64}" && t.shift == 1, dropmissing(all_df))
-# @show setdiff(unique(insts2[!, :inst_data]), unique(insts1[!, :inst_data]))
-
-# boxplots
-# using StatsPlots
-# all_df = CSV.read(bench_file, DataFrame)
-# transform!(all_df, [:inst_data, :extender] => ((x, y) -> x .* y) => :inst_key)
-# all_df = combine(groupby(all_df, :inst_key), names(all_df), :status => (x -> all(isequal.("Optimal", x)) || all(isequal.("Infeasible", x))) => :all_conv)
-# filter!(t -> t.all_conv == true, all_df)
-# all_df = combine(groupby(all_df, :inst_key), names(all_df), :solve_time => sum => :time_sum)
-# sort!(all_df, order(:time_sum, rev = true))
-# all_df = all_df[1:(4 * 20), :]
-# select!(all_df,
-#     [:inst_data, :extender] => ((x, y) -> x .* y) => :k,
-#     [:stepper, :use_corr, :use_curve_search] => ((a, b, c) -> a .* "_" .* string.(b) .* "_" .* string.(c)) => :stepper,
-#     :solve_time => ByRow(log10) => :log_time,
-#     # :solve_time,
-#     )
-# timings = unstack(all_df, :stepper, :log_time)
-# # timings = unstack(all_df, :stepper, :solve_time)
-# boxplot(["pc_00" "pc_01" "pc_11" "comb"], Matrix(timings[:, 2:end]), leg = false)
-
 function post_process()
     all_df = CSV.read(bench_file, DataFrame)
     transform!(all_df,
         :status => ByRow(x -> !ismissing(x) && x in ["Optimal", "Infeasible"]) => :conv,
+        # each instance is identified by instance data + extender combination
         [:inst_data, :extender] => ((x, y) -> x .* y) => :inst_key,
         )
+    # assumes that nothing returned incorrect status, which is checked manually
     all_df = combine(groupby(all_df, :inst_key), names(all_df), :status => (x -> all(isequal.("Optimal", x)) || all(isequal.("Infeasible", x))) => :all_conv)
+    # remove precompile instances
     filter!(t -> t.inst_set == "various", all_df)
     return all_df
 end
@@ -91,52 +61,74 @@ end
 agg_stats()
 
 # performance profiles, currently hardcoded for corrector vs no corrector
-function perf_prof()
-    # feature = :use_corr
-    feature = :stepper
-    metric = :solve_time
-    # metric = :iters
-    # s1 = "TRUE"
-    # s2 = "FALSE"
-    s1 = "Hypatia.Solvers.PredOrCentStepper{Float64}"
-    s2 = "Hypatia.Solvers.CombinedStepper{Float64}"
-    # s1 = true
-    # s2 = false
+function perf_prof(; feature = :stepper, metric = :solve_time)
+    if feature == :stepper
+        s1 = "Hypatia.Solvers.PredOrCentStepper{Float64}"
+        s2 = "Hypatia.Solvers.CombinedStepper{Float64}"
+        use_corr = [true]
+        use_curve_search = [true]
+        stepper = [s1, s2]
+    else
+        # s1 = "FALSE"
+        # s2 = "TRUE"
+        s1 = false
+        s2 = true
+        stepper = ["Hypatia.Solvers.PredOrCentStepper{Float64}"]
+        if feature == :use_corr
+            use_curve_search = [false]
+            use_corr = [s1, s2]
+        elseif feature == :use_curve_search
+            use_corr = [true]
+            use_curve_search = [s1, s2]
+        end
+    end
 
     all_df = post_process()
     filter!(t ->
-        # t -> t.stepper == "Hypatia.Solvers.PredOrCentStepper{Float64}" && t.use_curve_search == false,
-        t.use_corr == true &&
-        t.use_curve_search == true &&
-        t.all_conv == true, # only include instances where all steppers converged (currently includes steppers not in the plot)
+        t.stepper in stepper &&
+        t.use_corr in use_corr &&
+        t.use_curve_search in use_curve_search,
         all_df,
         )
+
+    # remove instances where neither stepper being compared converged
+    all_df = combine(groupby(all_df, :inst_key), names(all_df), :status => (x -> any(isequal.("Optimal", x)) || any(isequal.("Infeasible", x))) => :any_conv)
+    filter!(t -> t.any_conv, all_df)
+
     select!(all_df,
         :inst_key,
         feature,
-        :solve_time => ByRow(x -> (ismissing(x) ? MAX_TIME : min(x, MAX_TIME))) => :solve_time,
-        :iters => ByRow(x -> (ismissing(x) ? MAX_TIME : min(x, MAX_ITER))) => :iters,
+        [:solve_time, :conv] => ByRow((x, y) -> (y ? x : NaN)) => :solve_time,
+        [:iters, :conv] => ByRow((x, y) -> (y ? x : NaN)) => :iters,
         )
+
     all_df = combine(groupby(all_df, :inst_key), names(all_df), metric => (x -> x ./ minimum(x)) => :ratios)
-    # transform!(all_df, metric => (x -> x ./ minimum(x)) => :ratios)
+    # assign maximum ratio to failures
+    max_ratio = maximum(filter(!isnan, all_df[!, :ratios]))
+    all_df[isnan.(all_df[!, metric]), :ratios] .= max_ratio
     sort!(all_df, :ratios)
 
     nsolvers = 2
     npts = nrow(all_df)
 
-    # for metric in [:solve_time, :iters]
     plot(xlim = (0, log10(maximum(all_df[!, :ratios])) + 1), ylim = (0, 1))
     subdf = all_df[all_df[!, feature] .== s1, :]
-    plot!(log10.(all_df[!, :ratios]), [sum(subdf[!, :ratios] .<= ti) ./ npts * nsolvers for ti in all_df[!, :ratios]], label = s1, t = :steppre)
+    plot!(log10.(all_df[!, :ratios]), [sum(subdf[!, :ratios] .<= ti) ./ npts * nsolvers for ti in all_df[!, :ratios]], label = string(s1), t = :steppre)
 
     subdf = all_df[all_df[!, feature] .== s2, :]
-    plot!(log10.(all_df[!, :ratios]), [sum(subdf[!, :ratios] .<= ti) ./ npts * nsolvers for ti in all_df[!, :ratios]], label = s2, t = :steppre)
+    plot!(log10.(all_df[!, :ratios]), [sum(subdf[!, :ratios] .<= ti) ./ npts * nsolvers for ti in all_df[!, :ratios]], label = string(s2), t = :steppre)
     xaxis!("logratio")
-    title!(string(feature) * " / " * string(metric) * " pp")
+    plot_name = string(feature) * " & " * string(metric) * " pp"
+    title!(plot_name)
+
+    png(plot_name)
 
     return
 end
-# perf_prof()
+
+for feature in [:stepper, :use_curve_search, :use_corr], metric in [:solve_time, :iters]
+    perf_prof(feature = feature, metric = metric)
+end
 
 # anything else that can get plotted in latex
 function make_csv()
@@ -145,3 +137,37 @@ function make_csv()
     CSV.write(joinpath(output_folder, "df_long.csv"), df_agg)
     return
 end
+
+
+# comb_df = filter(:stepper => isequal("Hypatia.Solvers.CombinedStepper{Float64}"), dropmissing(all_df))
+# pc_df = filter(t -> t.stepper == "Hypatia.Solvers.PredOrCentStepper{Float64}" && t.use_curve_search == true, dropmissing(all_df))
+# comb_times = (comb_df[!, :solve_time])
+# pc_times = (pc_df[!, :solve_time])
+#
+# plot(comb_times, seriestype=:stephist)
+# plot!(pc_times, seriestype=:stephist)
+
+# missig instances
+# all_df = CSV.read(bench_file, DataFrame)
+# insts1 = filter(t -> t.stepper == "Hypatia.Solvers.PredOrCentStepper{Float64}" && t.use_curve_search == true, dropmissing(all_df))
+# insts2 = filter(t -> t.stepper == "Hypatia.Solvers.CombinedStepper{Float64}" && t.shift == 1, dropmissing(all_df))
+# @show setdiff(unique(insts2[!, :inst_data]), unique(insts1[!, :inst_data]))
+
+# boxplots
+# using StatsPlots
+# all_df = CSV.read(bench_file, DataFrame)
+# transform!(all_df, [:inst_data, :extender] => ((x, y) -> x .* y) => :inst_key)
+# all_df = combine(groupby(all_df, :inst_key), names(all_df), :status => (x -> all(isequal.("Optimal", x)) || all(isequal.("Infeasible", x))) => :all_conv)
+# filter!(t -> t.all_conv == true, all_df)
+# all_df = combine(groupby(all_df, :inst_key), names(all_df), :solve_time => sum => :time_sum)
+# sort!(all_df, order(:time_sum, rev = true))
+# all_df = all_df[1:(4 * 20), :]
+# select!(all_df,
+#     [:inst_data, :extender] => ((x, y) -> x .* y) => :k,
+#     [:stepper, :use_corr, :use_curve_search] => ((a, b, c) -> a .* "_" .* string.(b) .* "_" .* string.(c)) => :stepper,
+#     :solve_time => ByRow(log10) => :log_time,
+#     # :solve_time,
+#     )
+# timings = unstack(all_df, :stepper, :log_time)
+# # timings = unstack(all_df, :stepper, :solve_time)
+# boxplot(["pc_00" "pc_01" "pc_11" "comb"], Matrix(timings[:, 2:end]), leg = false)
