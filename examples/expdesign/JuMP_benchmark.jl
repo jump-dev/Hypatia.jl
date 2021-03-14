7dafbc6e30fd8e80257e9e35cb6d1bf021496a40
Lea Kapelevich <leakapelevich@gmail.com>
2021-03-14 17:23:13 -0400

    undo benchmark related changes

--- examples/expdesign/JuMP_benchmark.jl
+++ examples/expdesign/JuMP_benchmark.jl
@@ -1,8 +1,7 @@
 
 expdesign_insts = [
     [(q, 2q, 2q, 5, use_logdet, !use_logdet, false) for q in vcat(3, 25:25:300)] # includes compile run
-    for use_logdet in (false,)
-    # for use_logdet in (false, true)
+    for use_logdet in (false, true)
     ]
 
 insts = Dict()
