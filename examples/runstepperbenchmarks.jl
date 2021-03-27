50308f0ebca9c08e41b9b1b6823302f5549a43f2
-v <-v>
2021-03-26 21:29:39 -0400

    move options out of testaux

--- examples/runstepperbenchmarks.jl
+++ examples/runstepperbenchmarks.jl
@@ -6,7 +6,81 @@ killall julia; ~/julia/julia examples/runstepperbenchmarks.jl &> ~/bench/bench.t
 =#
 
 examples_dir = joinpath(@__DIR__, "../examples")
-(model_types, native_example_names, JuMP_example_names, perf, test_instances) = include(joinpath(examples_dir, "testaux.jl"))
+(perf, test_instances) = include(joinpath(examples_dir, "testaux.jl"))
+
+# path to write results DataFrame to CSV, if any
+results_path = joinpath(homedir(), "bench", "bench.csv")
+# results_path = nothing
+
+# default options to solvers
+default_options = (
+    # verbose = false,
+    verbose = true,
+    default_tol_relax = 10,
+    stepper = Solvers.CombinedStepper{Float64}(),
+    # stepper = Solvers.PredOrCentStepper{Float64}(),
+    iter_limit = 1000,
+    )
+
+    # instance sets and real types to run and corresponding time limits (seconds)
+    instance_sets = [
+        ("minimal", Float64, 60),
+        # ("minimal", Float32, 60),
+        # ("minimal", BigFloat, 60),
+        # ("fast", Float64, 60),
+        # ("slow", Float64, 120),
+        ]
+
+# types of models to run and corresponding options and example names
+model_types = [
+    "native",
+    "JuMP",
+    ]
+
+# list of names of native examples to run
+native_example_names = [
+    "densityest",
+    # "envelope",
+    # "expdesign",
+    # "linearopt",
+    # "matrixcompletion",
+    # "matrixregression",
+    # "maxvolume",
+    # "polymin",
+    # "portfolio",
+    # "sparsepca",
+    ]
+
+# list of names of JuMP examples to run
+JuMP_example_names = [
+    # "centralpolymat",
+    # "conditionnum",
+    # "contraction",
+    # "densityest",
+    # "envelope",
+    # "expdesign",
+    # "lotkavolterra",
+    # "lyapunovstability",
+    # "matrixcompletion",
+    # "matrixquadratic",
+    # "matrixregression",
+    # "maxvolume",
+    # "muconvexity",
+    # "nearestpsd",
+    # "polymin",
+    # "polynorm",
+    # "portfolio",
+    # "nearestpolymat",
+    # "regionofattr",
+    # "robustgeomprog",
+    # "secondorderpoly",
+    # "semidefinitepoly",
+    # "shapeconregr",
+    # "signomialmin",
+    # "stabilitynumber",
+    ]
+
+isnothing(results_path) || CSV.write(results_path, perf)
 
 predorcent = Solvers.PredOrCentStepper{Float64}
 combined = Solvers.CombinedStepper{Float64}
@@ -23,7 +97,7 @@ steppers = [
     @testset "$ex_name" for ex_name in eval(Symbol(mod_type, "_example_names"))
         include(joinpath(examples_dir, ex_name, mod_type * ".jl"))
         (ex_type, ex_insts) = include(joinpath(examples_dir, ex_name, mod_type * "_test.jl"))
-        test_instances(steppers, instance_sets, ex_insts, ex_type, perf)
+        test_instances(mod_type, steppers, instance_sets, ex_insts, ex_type, default_options, results_path)
     end
 end
 end
--- examples/testaux.jl
+++ examples/testaux.jl
@@ -11,85 +11,6 @@ examples_dir = joinpath(@__DIR__, "../examples")
 include(joinpath(examples_dir, "common_JuMP.jl"))
 include(joinpath(examples_dir, "common_native.jl"))
 
-# path to write results DataFrame to CSV, if any
-results_path = joinpath(homedir(), "bench", "bench.csv")
-# results_path = nothing
-
-# default options to solvers
-default_options = (
-    verbose = false,
-    # verbose = true,
-    default_tol_relax = 10,
-    stepper = Solvers.CombinedStepper{Float64}(),
-    # stepper = Solvers.PredOrCentStepper{Float64}(),
-    iter_limit = 1000,
-    )
-
-use_curve_search(::Solvers.Stepper) = true
-use_curve_search(stepper::Solvers.PredOrCentStepper) = stepper.use_curve_search
-use_correction(::Solvers.Stepper) = true
-use_correction(stepper::Solvers.PredOrCentStepper) = stepper.use_correction
-shift(::Solvers.Stepper) = 0
-shift(stepper::Solvers.CombinedStepper) = stepper.shift_alpha_sched
-
-# instance sets and real types to run and corresponding time limits (seconds)
-instance_sets = [
-    ("minimal", Float64, 60),
-    # ("minimal", Float32, 60),
-    # ("minimal", BigFloat, 60),
-    # ("fast", Float64, 60),
-    # ("slow", Float64, 120),
-    ]
-
-# types of models to run and corresponding options and example names
-model_types = [
-    "native",
-    "JuMP",
-    ]
-
-# list of names of native examples to run
-native_example_names = [
-    "densityest",
-    # "envelope",
-    # "expdesign",
-    # "linearopt",
-    # "matrixcompletion",
-    # "matrixregression",
-    # "maxvolume",
-    # "polymin",
-    # "portfolio",
-    # "sparsepca",
-    ]
-
-# list of names of JuMP examples to run
-JuMP_example_names = [
-    # "centralpolymat",
-    # "conditionnum",
-    # "contraction",
-    # "densityest",
-    # "envelope",
-    # "expdesign",
-    # "lotkavolterra",
-    # "lyapunovstability",
-    # "matrixcompletion",
-    # "matrixquadratic",
-    # "matrixregression",
-    # "maxvolume",
-    # "muconvexity",
-    # "nearestpsd",
-    # "polymin",
-    # "polynorm",
-    # "portfolio",
-    # "nearestpolymat",
-    # "regionofattr",
-    # "robustgeomprog",
-    # "secondorderpoly",
-    # "semidefinitepoly",
-    # "shapeconregr",
-    # "signomialmin",
-    # "stabilitynumber",
-    ]
-
 perf = DataFrames.DataFrame(
     example = String[],
     inst_set = String[],
@@ -133,10 +54,14 @@ perf = DataFrames.DataFrame(
     total_time = Float64[],
     )
 
-isnothing(results_path) || CSV.write(results_path, perf)
+use_curve_search(::Solvers.Stepper) = true
+use_curve_search(stepper::Solvers.PredOrCentStepper) = stepper.use_curve_search
+use_correction(::Solvers.Stepper) = true
+use_correction(stepper::Solvers.PredOrCentStepper) = stepper.use_correction
+shift(::Solvers.Stepper) = 0
+shift(stepper::Solvers.CombinedStepper) = stepper.shift_alpha_sched
 
-
-function test_instances(steppers, instance_sets, ex_insts, ex_type, perf)
+function test_instances(mod_type, steppers, instance_sets, ex_insts, ex_type, default_options, results_path)
     for stepper in steppers, (inst_set, real_T, time_limit) in instance_sets
         haskey(ex_insts, inst_set) || continue
         inst_subset = ex_insts[inst_set]
@@ -163,4 +88,4 @@ function test_instances(steppers, instance_sets, ex_insts, ex_type, perf)
     return
 end
 
-return (model_types, native_example_names, JuMP_example_names, perf, test_instances)
+return (perf, test_instances)
