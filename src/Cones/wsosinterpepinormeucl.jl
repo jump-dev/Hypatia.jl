1c64a4a385af72caedf067be549c3238145f8420
Lea Kapelevich <leakapelevich@gmail.com>
2021-03-31 16:46:19 -0400

    sketch out logic

--- src/Cones/wsosinterpepinormeucl.jl
+++ src/Cones/wsosinterpepinormeucl.jl
@@ -175,6 +175,20 @@ end
 
 is_dual_feas(cone::WSOSInterpEpiNormEucl) = true
 
+# block version of arrow(L * L') where L is half an arrow
+function arrow_outer_prod!(vect::Vector{T}, mat::Matrix{T}, U::Int, R::Int) where T
+    @views for u in 1:U
+        vect[u] += sum(abs2, mat[:, u])
+        idx = U + u
+        for r in 2:R
+            vect[idx] += 2 * dot(mat[:, idx], mat[:, u])
+            vect[u] += sum(abs2, mat[:, idx])
+            idx += U
+        end
+    end
+    return vect
+end
+
 function update_grad(cone::WSOSInterpEpiNormEucl{T}) where T
     @assert cone.is_feas
     U = cone.U
@@ -184,6 +198,7 @@ function update_grad(cone::WSOSInterpEpiNormEucl{T}) where T
     grad = cone.grad
 
     grad .= 0
+    g_try = copy(grad)
     @inbounds for k in eachindex(cone.Ps)
         Pk = cone.Ps[k]
         Λ11LiP = cone.Λ11LiP[k]
@@ -210,7 +225,45 @@ function update_grad(cone::WSOSInterpEpiNormEucl{T}) where T
                 idx += U
             end
         end
+
+        # L = size(Pk, 2)
+        # point_views = cone.point_views
+        # lambda_full = zeros(L * R, L * R)
+        # lambda_full[1:L, 1:L] = Pk' * Diagonal(point_views[1]) * Pk
+        # for r in 2:R
+        #     lambda_full[block_idxs(L, r), block_idxs(L, r)] = lambda_full[1:L, 1:L]
+        #     lambda_full[block_idxs(L, r), 1:L] = lambda_full[1:L, block_idxs(L, r)] = Pk' * Diagonal(point_views[r]) * Pk
+        # end
+        #
+        #
+        # lambda_inv_half = zeros(L * R, L * R)
+        # lambda_inv_half[1:L, 1:L] = cholesky(inv(matfact[k])).U
+        # for r in 2:R
+        #     lambda_inv_half[1:L, block_idxs(L, r)] = -inv(lambda_inv_half[1:L, 1:L])' * inv(matfact[k]) * lambda_full[1:L, block_idxs(L, r)] * inv(Λfact[k])
+        #     lambda_inv_half[block_idxs(L, r), block_idxs(L, r)] = cholesky(inv(Λfact[k])).U
+        #     # lambda_inv_half[block_idxs(L, r), 1:L] = -inv(Λfact[k]) * lambda_full[block_idxs(L, r), 1:L] * inv(matfact[k]) * inv(lambda_inv_half[1:L, 1:L])'
+        #     # lambda_inv_half[block_idxs(L, r), block_idxs(L, r)] = cholesky(inv(Λfact[k])).L
+        # end
+        # # lambda_inv_half = matfact[k].L \ lambda_inv_half
+        # # @show R
+        # @show (lambda_inv_half' * lambda_inv_half) ./ inv(lambda_full)
+        #
+        # lam_half_P = lambda_inv_half * kron(I(R), Pk')
+        # @views for u in 1:U
+        #     g_try[u] -= sum(abs2, lam_half_P[:, u])
+        #     idx = U + u
+        #     for r in 2:R
+        #         g_try[idx] -= 2 * dot(lam_half_P[:, idx], lam_half_P[:, u])
+        #         g_try[u] -= sum(abs2, lam_half_P[:, idx])
+        #         idx += U
+        #     end
+        # end
+        # g_try[1:U] += (R - 2) * diag(Pk * inv(Λfact[k]) * Pk')
+
+
+
     end
+    # @show g_try ./ cone.grad
 
     cone.grad_updated = true
     return cone.grad
@@ -299,6 +352,8 @@ function correction(cone::WSOSInterpEpiNormEucl, primal_dir::AbstractVector)
     corr .= 0
     R = cone.R
     U = cone.U
+    c2 = zero(corr)
+    # @show R
 
     @inbounds for pk in eachindex(cone.Ps)
         @views mul!(cone.tempLU[pk], cone.Λ11LiP[pk], Diagonal(primal_dir[1:U]))
@@ -309,19 +364,19 @@ function correction(cone::WSOSInterpEpiNormEucl, primal_dir::AbstractVector)
     end
     @. @views corr[1:U] *= 2 - R
 
-    @inbounds for pk in eachindex(cone.Ps)
-        L = size(cone.Ps[pk], 2)
-        ΛLiP_edge = cone.ΛLiPs_edge[pk]
-        matLiP = cone.matLiP[pk]
-        PΛiP = cone.PΛiPs[pk]
-        Λ11LiP = cone.Λ11LiP[pk]
-        scaled_row = cone.tempLU_vec[pk]
-        scaled_col = cone.tempLU_vec2[pk]
+    @inbounds for k in eachindex(cone.Ps)
+        L = size(cone.Ps[k], 2)
+        ΛLiP_edge = cone.ΛLiPs_edge[k]
+        matLiP = cone.matLiP[k]
+        PΛiP = cone.PΛiPs[k]
+        Λ11LiP = cone.Λ11LiP[k]
+        scaled_row = cone.tempLU_vec[k]
+        scaled_col = cone.tempLU_vec2[k]
 
         # get ΛLiP * D * PΛiP where D is diagonalized primal_dir scattered in an arrow and ΛLiP is half an arrow
         # ΛLiP * D is an arrow matrix but row edge doesn't equal column edge
-        @views scaled_diag = mul!(cone.tempLU[pk], Λ11LiP, Diagonal(primal_dir[1:U]))
-        @views scaled_pt = mul!(cone.tempLU2[pk], matLiP, Diagonal(primal_dir[1:U]))
+        @views scaled_diag = mul!(cone.tempLU[k], Λ11LiP, Diagonal(primal_dir[1:U]))
+        @views scaled_pt = mul!(cone.tempLU2[k], matLiP, Diagonal(primal_dir[1:U]))
         @views for r in 2:R
             mul!(scaled_pt, ΛLiP_edge[r - 1], Diagonal(primal_dir[block_idxs(U, r)]), true, true)
             mul!(scaled_row[r - 1], matLiP, Diagonal(primal_dir[block_idxs(U, r)]))
@@ -329,7 +384,7 @@ function correction(cone::WSOSInterpEpiNormEucl, primal_dir::AbstractVector)
             mul!(scaled_col[r - 1], Λ11LiP, Diagonal(primal_dir[block_idxs(U, r)]))
         end
 
-        corr_half = cone.tempLRUR[pk]
+        corr_half = cone.tempLRUR[k]
         corr_half .= 0
         @views for c in 1:R
             mul!(corr_half[1:L, block_idxs(U, c)], scaled_pt, PΛiP[1:U, block_idxs(U, c)])
@@ -340,17 +395,163 @@ function correction(cone::WSOSInterpEpiNormEucl, primal_dir::AbstractVector)
             end
         end
 
+        arrow_outer_prod!(corr, corr_half, U, R)
+
+
+
+
+        Pk = cone.Ps[k]
+        Δ = zeros(L * R, L * R)
+        Δ[1:L, 1:L] = Pk' * Diagonal(primal_dir[1:U]) * Pk
+        for r in 2:R
+            Δ[block_idxs(L, r), block_idxs(L, r)] = Δ[1:L, 1:L]
+            Δ[block_idxs(L, r), 1:L] = Δ[1:L, block_idxs(L, r)] = Pk' * Diagonal(primal_dir[block_idxs(U, r)]) * Pk
+        end
+
+        L = size(Pk, 2)
+        point_views = cone.point_views
+        lambda_full = zeros(L * R, L * R)
+        lambda_full[1:L, 1:L] = Pk' * Diagonal(point_views[1]) * Pk
+        for r in 2:R
+            lambda_full[block_idxs(L, r), block_idxs(L, r)] = lambda_full[1:L, 1:L]
+            lambda_full[block_idxs(L, r), 1:L] = lambda_full[1:L, block_idxs(L, r)] = Pk' * Diagonal(point_views[r]) * Pk
+        end
+
+        lambda_inv_half = zeros(L * R, L * R)
+        lambda_inv_half[1:L, 1:L] = cholesky(inv(cone.matfact[k])).U
+        for r in 2:R
+            lambda_inv_half[1:L, block_idxs(L, r)] = -inv(lambda_inv_half[1:L, 1:L])' * inv(cone.matfact[k]) * lambda_full[1:L, block_idxs(L, r)] * inv(cone.Λfact[k])
+            lambda_inv_half[block_idxs(L, r), block_idxs(L, r)] = cholesky(inv(cone.Λfact[k])).U
+        end
+
+        chalf = lambda_inv_half * Δ * lambda_inv_half' * lambda_inv_half * kron(I(R), Pk')
+
         @views for u in 1:U
-            corr[u] += sum(abs2, corr_half[:, u])
+            c2[u] += sum(abs2, chalf[:, u])
             idx = U + u
             for r in 2:R
-                corr[idx] += 2 * dot(corr_half[:, idx], corr_half[:, u])
-                corr[u] += sum(abs2, corr_half[:, idx])
+                c2[idx] += 2 * dot(chalf[:, idx], chalf[:, u])
+                c2[u] += sum(abs2, chalf[:, idx])
                 idx += U
             end
         end
+        L11 = lambda_full[1:L, 1:L]
+        Δ11 = Δ[1:L, 1:L]
+        c2[1:U] -= (R - 2) * diag(Pk * inv(L11) * Δ11 * inv(L11) * Δ11 * inv(L11) * Pk')
+
+        At = lambda_inv_half[1:L, 1:L]
+        Bt = lambda_inv_half[1:L, (L + 1):end]
+        Ct = lambda_inv_half[(L + 1):2L, (L + 1):2L]
+        X = Δ[1:L, 1:L]
+        Y = Δ[(L + 1):end, 1:L]
+        Z = Δ[(L + 1):2L, (L + 1):2L]
+        CtP = Ct * Pk'
+
+        # lambda_inv_half * Δ * lambda_inv_half' is arrow
+        LP = zeros(L * R, U * R)
+        LΔL = zeros(L * R, L * R)
+        BYA = Bt * Y * At'
+        LΔL[1:L, 1:L] = At * X * At' + BYA + BYA' + sum(Bt[:, block_idxs(L, r)] * Z * Bt[:, block_idxs(L, r)]' for r in 1:(R - 1))
+        LP[1:L, 1:U] = lambda_inv_half[1:L, 1:L] * Pk'
+        for r in 2:R
+            # s * L^3
+            LΔL[block_idxs(L, r), 1:L] = Ct * (Y[block_idxs(L, r - 1), :] * At' + Z * Bt[:, block_idxs(L, r - 1)]')
+            LΔL[1:L, block_idxs(L, r)] = LΔL[block_idxs(L, r), 1:L]'
+            LΔL[block_idxs(L, r), block_idxs(L, r)] = Ct * Z * Ct'
+            #
+            # s * L^2*U
+            LP[1:L, block_idxs(U, r)] = lambda_inv_half[1:L, block_idxs(L, r)] * Pk'
+            LP[block_idxs(L, r), block_idxs(U, r)] = CtP
+        end
+
+        # s^2 * L^2 * U
+        chalf2 = zeros(L * R, L * R)
+        # chalf[1:L, 1:U] = LΔL[1:L, :] * LP[:, 1:L]
+        chalf2 = LΔL[:, 1:L] * LP[1:L, :]
+        for r in 2:R
+            chalf2[1:L, block_idxs(U, r)] = LΔL[1:L, 1:L] * LP[1:L, block_idxs(U, r)] + LΔL[1:L, block_idxs(L, r)] * LP[block_idxs(L, r), block_idxs(U, r)]
+            chalf2[block_idxs(L, r), block_idxs(U, r)] = LΔL[block_idxs(L, r), block_idxs(L, r)] * LP[block_idxs(L, r), block_idxs(U, r)]
+        end
+
+
+        # @show LΔL
+        # @show LΔL ≈ (lambda_inv_half * Δ * lambda_inv_half')
+        # @show LΔL * LP ≈ chalf
+        @show R
+        @show norm(chalf2 - chalf, Inf), norm(chalf, Inf)
+
+        # @show norm(LΔL - (lambda_inv_half * Δ * lambda_inv_half'))
+
+
+        #
+        # lambda_full = zeros(L * R, L * R)
+        # lambda_full[1:L, 1:L] = Pk' * Diagonal(cone.point_views[1]) * Pk
+        # for r in 2:R
+        #     # s t^2 U
+        #     lambda_full[block_idxs(L, r), block_idxs(L, r)] = lambda_full[1:L, 1:L]
+        #     lambda_full[block_idxs(L, r), 1:L] = lambda_full[1:L, block_idxs(L, r)] = Pk' * Diagonal(cone.point_views[r]) * Pk
+        # end
+        #
+        # ΛLiP_edge = cone.ΛLiPs_edge[k]
+        # matLiP = cone.matLiP[k]
+        # LiP = zeros(L * R, U * R)
+        # Li = zeros(L * R, L * R)
+        # LiP[1:L, 1:U] .= Λ11LiP
+        #
+        # Li[1:L, 1:L] .= inv(cone.Λfact[k].L)
+        # for r in 2:R
+        #     LiP[block_idxs(L, r), block_idxs(U, r)] .= matLiP
+        #     LiP[block_idxs(L, r), 1:U] .= ΛLiP_edge[r - 1]
+        #
+        #     Li[block_idxs(L, r), block_idxs(L, r)] .= inv(cone.matfact[k].L)
+        #     Li[block_idxs(L, r), 1:L] .= -cone.matfact[k].L \ (cone.ΛLi_Λ[k][r - 1]' * inv(cone.Λfact[k].L))
+        # end
+        #
+        # lam_sqrt = cholesky(Hermitian(lambda_full)).L
+        #
+        # # @show (Li' * Li) ./ inv(lambda_full)
+        #  # @show (kron(I(R), Pk) * Li' * Li * kron(I(R), Pk')) ./ (kron(I(R), Pk) * inv(lambda_full) * kron(I(R), Pk'))
+        # # @show Li * kron(I(R), Pk') ≈ LiP
+        # @show (LiP' * LiP) ./ (kron(I(R), Pk) * inv(lambda_full) * kron(I(R), Pk'))
+        #
+        # # chalf = Li * Δ * Li' * LiP
+        # chalf = inv(lam_sqrt) * Δ * inv(lambda_full) * kron(I(R), Pk')
+        # # arrow_outer_prod!(c2, chalf, U, R)
+        #
+        # L11 = lambda_full[1:L, 1:L]
+        # Δ11 = Δ[1:L, 1:L]
+        #
+        # @views for u in 1:U
+        #     c2[u] += sum(abs2, chalf[:, u])
+        #     idx = U + u
+        #     for r in 2:R
+        #         c2[idx] += 2 * dot(chalf[:, idx], chalf[:, u])
+        #         c2[u] += sum(abs2, chalf[:, idx])
+        #         idx += U
+        #     end
+        # end
+        #
+        #
+        # c2[1:U] -= (R - 2) * diag(Pk * inv(L11) * Δ11 * inv(L11) * Δ11 * inv(L11) * Pk')
+        #
+        # # bigthing = kron(I(R), Pk) * inv(lambda_full) * Δ * inv(lambda_full) * Δ * inv(lambda_full) * kron(I(R), Pk')
+        # # function lamadj(vec, mat)
+        # #     idx = 1
+        # #     vec[1:U] += sum(diag(mat[block_idxs(U, r), block_idxs(U, r)]) for r in 1:R)
+        # #     for r in 2:R
+        # #         vec[block_idxs(U, r)] += 2 * diag(mat[block_idxs(U, r), 1:U])
+        # #     end
+        # #     return vec
+        # # end
+        # # lamadj(c2, bigthing)
+        # # L11 = lambda_full[1:L, 1:L]
+        # # Δ11 = Δ[1:L, 1:L]
+        # # c2[1:U] -= (R - 2) * diag(Pk * inv(L11) * Δ11 * inv(L11) * Δ11 * inv(L11) * Pk')
+        #
+        # # @show chalf
 
     end
+    @show c2 ./ corr
 
     return corr
 end
