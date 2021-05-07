45e55259ca27f91e30e72f9e50eb1d3f1d801fc6
Chris Coey <coey.chris@gmail.com>
2021-05-07 14:08:27 +1000

    add inv function

--- src/Cones/epipersepspectral/sepspectralfun.jl
+++ src/Cones/epipersepspectral/sepspectralfun.jl
@@ -1,5 +1,5 @@
 #=
-suitable univariate functions defined on ℝ₊₊ with matrix monotone derivative, and associated oracles
+suitable univariate convex functions defined on ℝ₊₊ and associated oracles
 - h_val evaluates (sum of) h
 - h_conj_dom_pos is true if domain of convex conjugate of h is ℝ₊₊, otherwise domain is ℝ
 - h_conj evaluates the convex conjugate of (sum of) h
@@ -9,11 +9,27 @@ suitable univariate functions defined on ℝ₊₊ with matrix monotone derivati
 TODO derive central initial points for get_initial_point and return correct real type
 =#
 
+# inverse: x^-1
+struct InvSSF <: SepSpectralFun end
+
+h_val(xs::Vector, ::InvSSF) = sum(inv, xs)
+h_conj_dom_pos(::InvSSF) = true
+h_conj(xs::Vector, ::InvSSF) = -2 * sum(sqrt, xs)
+
+h_der1(ds::Vector, xs::Vector, ::InvSSF) = (@. ds = -xs^-2)
+h_der2(ds::Vector, xs::Vector, ::InvSSF) = (@. ds = 2 * xs^-3)
+h_der3(ds::Vector, xs::Vector, ::InvSSF) = (@. ds = -6 * xs^-4)
+
+function get_initial_point(d::Int, ::InvSSF)
+    # TODO initial central point
+    return (2d, 1, 1)
+end
+
+
 # negative logarithm: -log(x)
 struct NegLogSSF <: SepSpectralFun end
 
 h_val(xs::Vector, ::NegLogSSF) = -sum(log, xs)
-# h_conj_dom(xs::Vector, ::NegLogSSF) = all(>(eps(eltype(xs))), xs)
 h_conj_dom_pos(::NegLogSSF) = true
 h_conj(xs::Vector, ::NegLogSSF) = -length(xs) - sum(log, xs)
 
@@ -22,7 +38,7 @@ h_der2(ds::Vector, xs::Vector, ::NegLogSSF) = (@. ds = xs^-2)
 h_der3(ds::Vector, xs::Vector, ::NegLogSSF) = (@. ds = -2 * xs^-3)
 
 function get_initial_point(d::Int, ::NegLogSSF)
-    # @warn("not initial central point") # TODO
+    # TODO initial central point
     return (1, 1, 1)
 end
 
@@ -31,7 +47,6 @@ end
 struct NegEntropySSF <: SepSpectralFun end
 
 h_val(xs::Vector, ::NegEntropySSF) = sum(x * log(x) for x in xs)
-# h_conj_dom(xs::Vector, ::NegEntropySSF) = true
 h_conj_dom_pos(::NegEntropySSF) = false
 h_conj(xs::Vector, ::NegEntropySSF) = sum(exp(-x - 1) for x in xs)
 
@@ -40,7 +55,7 @@ h_der2(ds::Vector, xs::Vector, ::NegEntropySSF) = (@. ds = inv(xs))
 h_der3(ds::Vector, xs::Vector, ::NegEntropySSF) = (@. ds = -xs^-2)
 
 function get_initial_point(d::Int, ::NegEntropySSF)
-    # @warn("not initial central point") # TODO
+    # TODO initial central point
     return (1, 1, 1)
 end
 
@@ -54,7 +69,6 @@ struct Power12SSF <: SepSpectralFun
 end
 
 h_val(xs::Vector, h::Power12SSF) = sum(x^h.p for x in xs)
-# h_conj_dom(xs::Vector, h::Power12SSF) = true
 h_conj_dom_pos(::Power12SSF) = false
 function h_conj(xs::Vector, h::Power12SSF)
     p = h.p
@@ -84,6 +98,6 @@ function h_der3(ds::Vector, xs::Vector, h::Power12SSF)
 end
 
 function get_initial_point(d::Int, h::Power12SSF)
-    # @warn("not initial central point") # TODO
+    # TODO initial central point
     return (2d, 1, 1)
 end
