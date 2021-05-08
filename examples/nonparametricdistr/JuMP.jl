1cd84ff770a4834a90e74936fff5fdb38d94f820
Chris Coey <coey.chris@gmail.com>
2021-05-08 15:35:42 +1000

    comment barrier tests; make nonparam dist ex better

--- examples/nonparametricdistr/JuMP.jl
+++ examples/nonparametricdistr/JuMP.jl
@@ -5,13 +5,13 @@ satisfying some prior information (expressed using convex constraints)
 
 adapted from Boyd and Vandenberghe, "Convex Optimization", section 7.2
 
-p ∈ ℝᵈ is the probability variable
-minimize    f(p)                    (note: enforces p ≥ 0)
-subject to  Σᵢ pᵢ = 1               (probability distribution)
-            gⱼ(Aⱼ p + aⱼ) ≤ kⱼ ∀j   (prior info as convex constraints)
-            B p = b                 (prior info as equalities)
-            C p ≤ c                 (prior info as inequalities)
-where f and gⱼ are convex spectral functions
+p ∈ ℝᵈ is the probability variable, scaled by d (to keep each pᵢ close to 1)
+minimize    f(p)            (note: enforces p ≥ 0)
+subject to  Σᵢ pᵢ = d       (probability distribution, scaled by d)
+            gⱼ(p) ≤ kⱼ ∀j   (prior info as convex constraints)
+            B p = b         (prior info as equalities)
+            C p ≤ c         (prior info as inequalities)
+where f and gⱼ are different convex spectral functions
 =#
 
 struct NonparametricDistrJuMP{T <: Real} <: ExampleInstanceJuMP{T}
@@ -20,44 +20,39 @@ end
 
 function build(inst::NonparametricDistrJuMP{T}) where {T <: Float64}
     d = inst.d
-    @assert d >= 1
+    @assert d >= 2
     p0 = rand(T, d)
-    p0 ./= sum(p0)
+    p0 .*= d / sum(p0)
 
-    sep_spectral_funs = [
+    fg_funs = Random.shuffle!([
         Cones.InvSSF(),
         Cones.NegLogSSF(),
         Cones.NegEntropySSF(),
         Cones.Power12SSF(1.5),
-        ]
+        ])
 
     model = JuMP.Model()
     JuMP.@variable(model, p[1:d])
-    JuMP.@constraint(model, sum(p) == 1)
+    JuMP.@constraint(model, sum(p) == d)
 
     # convex objective
-    f = rand(sep_spectral_funs)
-    f_cone = Hypatia.EpiPerSepSpectralCone{T}(f, Cones.VectorCSqr{T}, d)
+    f_cone = Hypatia.EpiPerSepSpectralCone{T}(fg_funs[1], Cones.VectorCSqr{T}, d)
     JuMP.@variable(model, epi)
     JuMP.@objective(model, Min, epi)
     JuMP.@constraint(model, vcat(epi, 1, p) in f_cone)
 
     # convex prior constraints
-    for g in sep_spectral_funs
-        A = randn(T, d, d)
-        a0 = 1 .+ T(0.1) * randn(T, d)
-        a = a0 - A * p0
-        k = Cones.h_val(a0, g)
+    for g in fg_funs[2:end]
         g_cone = Hypatia.EpiPerSepSpectralCone{T}(g, Cones.VectorCSqr{T}, d)
-        JuMP.@constraint(model, vcat(k, 1, A * p + a) in g_cone)
+        k = Cones.h_val(p0, g)
+        JuMP.@constraint(model, vcat(k, 1, p) in g_cone)
     end
 
     # linear prior constraints
-    lin_dim = round(Int, sqrt(d - 1))
-    B = randn(T, lin_dim, d)
+    B = randn(T, round(Int, sqrt(d - 1)), d)
     b = B * p0
     JuMP.@constraint(model, B * p .== b)
-    C = randn(T, lin_dim, d)
+    C = randn(T, round(Int, log(d - 1)), d)
     c = C * p0
     JuMP.@constraint(model, C * p .<= c)
 
