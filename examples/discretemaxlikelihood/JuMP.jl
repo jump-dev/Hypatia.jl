9466b632faaeec6e882f461ca49d896dff44492d
Lea Kapelevich <leakapelevich@gmail.com>
2021-05-24 18:31:15 -0400

    add example with powermean cone (#719)
    * add example with powermean cone
    
    * remove nonneg restriction
    
    * fix spelling

---
+++ examples/discretemaxlikelihood/JuMP.jl
@@ -0,0 +1,30 @@
+#=
+maximize likelihood of d observations at discrete points appearing with random
+frequencies, subject to probability vector not being too far from a uniform
+prior
+=#
+
+struct DiscreteMaxLikelihood{T <: Real} <: ExampleInstanceJuMP{T}
+    d::Int
+end
+
+function build(inst::DiscreteMaxLikelihood{T}) where {T <: Float64}
+    d = inst.d
+    @assert d >= 2
+    freq = rand(1:(2 * d), d)
+
+    model = JuMP.Model()
+    JuMP.@variables(model, begin
+        p[1:d]
+        prodp
+    end)
+    JuMP.@constraints(model, begin
+        vcat(prodp, p) in Hypatia.HypoPowerMeanCone{T}(freq / sum(freq))
+        vcat(inv(d), inv(d), p) in Hypatia.EpiPerSepSpectralCone{T}(
+            Cones.NegEntropySSF(), Cones.VectorCSqr{T}, d)
+        sum(p) == 1
+    end)
+    JuMP.@objective(model, Max, prodp)
+
+    return model
+end
