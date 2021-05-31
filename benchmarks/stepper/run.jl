63a4b48a9d7ece69fc1acefcd4f2e66cff72e798
Chris Coey <coey.chris@gmail.com>
2021-05-31 21:02:51 +1000

    update run script

--- benchmarks/stepper/run.jl
+++ benchmarks/stepper/run.jl
@@ -31,7 +31,17 @@ default_options = (
 # stepper option sets to run
 porc = Solvers.PredOrCentStepper{Float64}
 comb = Solvers.CombinedStepper{Float64}
+
+sypy_options = (;
+    # stepper options
+    use_adjustment = false, use_curve_search = false,
+    max_cent_steps = 8, pred_prox_bound = 0.0332, use_pred_sum_prox = true,
+    # searcher options
+    min_prox = 0.0, prox_bound = 0.2844, use_sum_prox = true,
+    alpha_sched = [0.9999 * 0.7^i for i in 0:8])
+
 stepper_options = [
+    "sypy" => porc(; sypy_options...),
     "basic" => porc(use_adjustment = false, use_curve_search = false),
     "toa" => porc(use_adjustment = true, use_curve_search = false),
     "curve" => porc(use_adjustment = true, use_curve_search = true),
