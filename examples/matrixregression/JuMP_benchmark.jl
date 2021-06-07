b112b3bc8099a8aefc8485d842970dce3f82a690
Chris C <chriscoey@users.noreply.github.com>
2021-06-07 17:01:56 +1000

    adjust mat regr benchmark sizes

--- examples/matrixregression/JuMP_benchmark.jl
+++ examples/matrixregression/JuMP_benchmark.jl
@@ -1,7 +1,7 @@
 
 matrixregression_insts = [
     vcat((4, 3), [(n, m) for n in ns]) # includes compile run
-    for (m, ns) in ((15, vcat(50:50:150, 250:250:3000)),
+    for (m, ns) in ((15, vcat(50:50:150, 250, 500:500:3000)),
         (30, vcat(50:50:150, 250:250:1500)))
     ]
 
