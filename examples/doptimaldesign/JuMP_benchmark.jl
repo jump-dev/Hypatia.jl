6b68c549eb8c2c4c6926efc7fcb2813d5d6ef265
Chris C <chriscoey@users.noreply.github.com>
2021-06-10 16:24:45 +1000

    improve memory usage in dopt example; only run rootdet with SEP (#739)

--- examples/doptimaldesign/JuMP_benchmark.jl
+++ examples/doptimaldesign/JuMP_benchmark.jl
@@ -1,12 +1,13 @@
 
-doptimaldesign_insts = [
+doptimaldesign_insts(use_logdet::Bool) = [
     [(q, 2q, 2q, 5, use_logdet, !use_logdet, false)
-    for q in vcat(3, 50:50:600)] # includes compile run
-    for use_logdet in (false, true)
+    for q in vcat(3, 50:50:200, 300:100:1000)] # includes compile run
     ]
 
 insts = OrderedDict()
-insts["nat"] = (nothing, doptimaldesign_insts)
-insts["extEP"] = (:ExpPSD, doptimaldesign_insts)
-insts["extSEP"] = (:SOCExpPSD, doptimaldesign_insts)
+rootdet_insts = doptimaldesign_insts(false)
+all_insts = vcat(rootdet_insts, doptimaldesign_insts(true))
+insts["nat"] = (nothing, all_insts)
+insts["extEP"] = (:ExpPSD, all_insts)
+insts["extSEP"] = (:SOCExpPSD, rootdet_insts)
 return (DOptimalDesignJuMP, insts)
