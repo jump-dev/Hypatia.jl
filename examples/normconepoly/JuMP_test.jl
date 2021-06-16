60007fd9233773be0b6ca993db3d8bc891b8d733
Lea Kapelevich <leakapelevich@gmail.com>
2021-06-16 10:17:44 -0400

    remove instances that take 0 iterations

--- examples/normconepoly/JuMP_test.jl
+++ examples/normconepoly/JuMP_test.jl
@@ -22,5 +22,16 @@ insts["fast"] = [
     ((:polys8, 2, false, false),),
     ((:polys9, 2, false, false),),
     ]
-insts["various"] = insts["fast"]
+insts["various"] = [
+    ((:polys2, 2, true, true),),
+    ((:polys3, 2, true, true),),
+    ((:polys4, 4, true, true),),
+    ((:polys7, 2, false, true),),
+    ((:polys8, 2, false, true),),
+    ((:polys2, 2, true, false),),
+    ((:polys3, 2, true, false),),
+    ((:polys4, 4, true, false),),
+    ((:polys7, 2, false, false),),
+    ((:polys8, 2, false, false),),
+    ]
 return (NormConePoly, insts)
