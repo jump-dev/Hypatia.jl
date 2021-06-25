514206ab62d2bc4bc35b4fc77307834a2cf895b5
Chris Coey <coey.chris@gmail.com>
2021-06-25 23:22:18 +1000

    fix cone test

--- test/cone.jl
+++ test/cone.jl
@@ -43,8 +43,8 @@ function test_oracles(
     Cones.load_dual_point(cone, dual_point)
     @test Cones.is_dual_feas(cone)
     @test cone.dual_point == dual_point
-    @test Cones.get_proxsqr(cone, one(T), false) <= 1 # max proximity
-    @test Cones.get_proxsqr(cone, one(T), true) <= dim # sum proximity
+    @test Cones.get_proxsqr(cone, one(T), true) <= 1 # max proximity
+    @test Cones.get_proxsqr(cone, one(T), false) <= dim # sum proximity
 
     prod_vec = zero(point)
     @test Cones.hess_prod!(prod_vec, point, cone) ≈ dual_point atol=tol rtol=tol
@@ -228,7 +228,7 @@ function show_time_alloc(
     end
 
     println("get_proxsqr")
-    @time Cones.get_proxsqr(cone, one(T), false)
+    @time Cones.get_proxsqr(cone, one(T), true)
 
     return
 end
