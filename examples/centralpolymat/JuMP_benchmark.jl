fa2712675c403cfb85d6188d3cd9b1fe03d5a571
Lea Kapelevich <leakapelevich@gmail.com>
2021-07-11 10:03:36 -0400

    fix instance specifications

--- examples/centralpolymat/JuMP_benchmark.jl
+++ examples/centralpolymat/JuMP_benchmark.jl
@@ -2,7 +2,7 @@
 # size is symmetric wrt n and d
 centralpolymat_nds = [
     (1, 2),
-    (1, 10), (1, 20), (1, 30),
+    (1, 50), (1, 100), (1, 200),
     (2, 4), (2, 8), (2, 16),
     (3, 2), (3, 4), (3, 8),
     (4, 4), (4, 5),
@@ -13,11 +13,10 @@ centralpolymat_insts(ext::MatSpecExt) = [
 
 insts = OrderedDict()
 insts["nat"] = (nothing, vcat(
-    centralpolymat_insts(MatNeg2Sqrt()),
-    centralpolymat_insts(MatNegExp1()), centralpolymat_insts(MatPower12Conj(1.5)),
+    centralpolymat_insts(MatNegExp1()),
+    centralpolymat_insts(MatPower12Conj(1.5)),
     ))
 insts["ext"] = (nothing, vcat(
-    centralpolymat_insts(MatNeg2SqrtEigOrd()),
     centralpolymat_insts(MatNegExp1EigOrd()),
     centralpolymat_insts(MatPower12ConjEigOrd(1.5)),
     ))
--- examples/nonparametricdistr/JuMP_benchmark.jl
+++ examples/nonparametricdistr/JuMP_benchmark.jl
@@ -1,6 +1,6 @@
 
 nonparametricdistr_ds = vcat(10, 2000:2000:18000) # includes compile run
 insts = OrderedDict()
-insts["nat"] = (nothing, [[(d, VecPower12(1.5)) for d in nonparametricdistr_ds]])
-insts["ext"] = (nothing, [[(d, VecPower12EF(1.5)) for d in nonparametricdistr_ds]])
+insts["nat"] = (nothing, [[(d, [VecPower12(1.5)]) for d in nonparametricdistr_ds]])
+insts["ext"] = (nothing, [[(d, [VecPower12EF(1.5)]) for d in nonparametricdistr_ds]])
 return (NonparametricDistrJuMP, insts)
