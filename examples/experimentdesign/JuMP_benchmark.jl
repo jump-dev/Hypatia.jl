86a675d3321045520a60864def83d5f09dd4264f
Chris C <chriscoey@users.noreply.github.com>
2021-07-18 15:09:42 +1000

    adjust size

--- examples/experimentdesign/JuMP_benchmark.jl
+++ examples/experimentdesign/JuMP_benchmark.jl
@@ -1,7 +1,7 @@
 
 experimentdesign_insts(ext::MatSpecExt) = [
     [(d, ext)
-    for d in vcat(3, 25:25:100, 200:100:500)] # includes compile run
+    for d in vcat(3, 25:25:100, 200:100:400)] # includes compile run
     ]
 
 insts = OrderedDict()
