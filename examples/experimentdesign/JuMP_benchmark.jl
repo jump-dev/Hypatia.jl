aa36f728f53b6c8a1bb780721f98847151be72c5
Chris Coey <coey.chris@gmail.com>
2021-10-12 13:55:57 -0400

    update benchmarks

--- examples/experimentdesign/JuMP_benchmark.jl
+++ examples/experimentdesign/JuMP_benchmark.jl
@@ -1,24 +1,20 @@
 
 experimentdesign_insts(ext::MatSpecExt) = [
     [(d, ext)
-    for d in vcat(3, 25:25:100, 200:100:400)] # includes compile run
+    for d in vcat(3, 25:25:100, 150, 200:100:700)] # includes compile run
     ]
 
 insts = OrderedDict()
 insts["nat"] = (nothing, vcat(
     experimentdesign_insts(MatNegGeom()),
+    experimentdesign_insts(MatLogdetCone()),
     experimentdesign_insts(MatNegSqrt()),
-    experimentdesign_insts(MatNegSqrtConj()),
     experimentdesign_insts(MatNegPower01(1/3)),
-    experimentdesign_insts(MatNegPower01Conj(1/3)),
-    experimentdesign_insts(MatPower12(1.5)),
     ))
 insts["ext"] = (nothing, vcat(
     experimentdesign_insts(MatNegGeomEFExp()),
+    experimentdesign_insts(MatNegLogDirect()),
     experimentdesign_insts(MatNegSqrtEigOrd()),
-    experimentdesign_insts(MatNegSqrtConjDirect()),
     experimentdesign_insts(MatNegPower01EigOrd(1/3)),
-    experimentdesign_insts(MatNegPower01ConjEigOrd(1/3)),
-    experimentdesign_insts(MatPower12EigOrd(1.5)),
     ))
 return (ExperimentDesignJuMP, insts)
--- examples/nonparametricdistr/JuMP_benchmark.jl
+++ examples/nonparametricdistr/JuMP_benchmark.jl
@@ -1,18 +1,21 @@
 
 nonparametricdistr_insts(ext::VecSpecExt) = [
     [(d, ext)
-    for d in vcat(10, 500, 1000, 2500, 5000:5000:25000)] # includes compile run
+    for d in vcat(10, 500, 1000, 2500, 5000:5000:30000)] # includes compile run
     ]
 
 insts = OrderedDict()
 insts["nat"] = (nothing, vcat(
     nonparametricdistr_insts(VecNegGeom()),
+    nonparametricdistr_insts(VecLogCone()),
     nonparametricdistr_insts(VecNegLog()),
     nonparametricdistr_insts(VecNegEntropy()),
+    nonparametricdistr_insts(VecNegSqrtConj()),
     ))
 insts["vecext"] = (nothing, vcat(
     nonparametricdistr_insts(VecNegGeomEFExp()),
     nonparametricdistr_insts(VecNegLogEF()),
     nonparametricdistr_insts(VecNegEntropyEF()),
+    nonparametricdistr_insts(VecNegSqrtConjEF()),
     ))
 return (NonparametricDistrJuMP, insts)
