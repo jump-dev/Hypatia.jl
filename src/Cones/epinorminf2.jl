0a3dc0e7f81f310a3585eb6b577b9781f853f570
Chris Coey <coey.chris@gmail.com>
2021-11-07 17:58:53 -0500

    further simplify everything

--- src/Cones/epinorminf2.jl
+++ src/Cones/epinorminf2.jl
@@ -21,24 +21,16 @@ mutable struct EpiNormInf{T <: Real, R <: RealOrComplex{T}} <: Cone{T}
     grad_updated::Bool
     hess_updated::Bool
     inv_hess_updated::Bool
-    hess_aux_updated::Bool
+    inv_hess_aux_updated::Bool
     is_feas::Bool
     hess::Symmetric{T, SparseMatrixCSC{T, Int}}
     inv_hess::Symmetric{T, Matrix{T}}
 
     w::Vector{R}
-    s::Vector{T}
-    V::Vector{R}
-    z::Vector{T}
-    zi::Vector{T}
-    uzi::Vector{T}
-    szi::Vector{T}
-
-    huu::T
-    huw::Vector{T}
-    hiuui::T
-    hiuw::Vector{T}
-    hiww #?
+    zh::Vector{T}
+    cu::T
+    u2ti::Vector{T}
+    zti1::T
 
     w1::Vector{R}
     w2::Vector{R}
@@ -60,7 +52,7 @@ mutable struct EpiNormInf{T <: Real, R <: RealOrComplex{T}} <: Cone{T}
 end
 
 reset_data(cone::EpiNormInf) = (cone.feas_updated = cone.grad_updated =
-    cone.hess_updated = cone.inv_hess_updated = cone.hess_aux_updated = false)
+    cone.hess_updated = cone.inv_hess_updated = cone.inv_hess_aux_updated = false)
 
 use_sqrt_hess_oracles(::Int, cone::EpiNormInf) = false
 
@@ -69,16 +61,8 @@ function setup_extra_data!(
     ) where {T <: Real, R <: RealOrComplex{T}}
     d = cone.d
     cone.w = zeros(R, d)
-    cone.s = zeros(T, d)
-    cone.z = zeros(T, d)
-    cone.zi = zeros(T, d)
-    cone.uzi = zeros(T, d)
-    cone.szi = zeros(T, d)
-    cone.V = zeros(R, d)
-    cone.huw = zeros(T, d)
-    cone.hiuw = zeros(T, d)
-    # cone.hiww = zeros(T, d, d)
-
+    cone.zh = zeros(T, d)
+    cone.u2ti = zeros(T, d)
     cone.w1 = zeros(R, d)
     cone.w2 = zeros(R, d)
     cone.s1 = zeros(T, d)
@@ -121,64 +105,24 @@ function is_dual_feas(cone::EpiNormInf{T}) where T
     return false
 end
 
-function update_grad(cone::EpiNormInf)
+function update_grad(cone::EpiNormInf{T}) where T
     @assert cone.is_feas
     u = cone.point[1]
     w = cone.w
-    z = cone.z
-    uzi = cone.uzi
+    zh = cone.zh
     w1 = cone.w1
     g = cone.grad
 
+    cone.cu = (cone.d - 1) / u
     u2 = abs2(u)
-    @. z = u2 - abs2(w)
-    @. uzi = 2 * u / z
+    @. zh = T(0.5) * (u2 - abs2(w))
 
-    g[1] = -sum(uzi) + (cone.d - 1) / u
-    @. w1 = 2 * w / z
+    g[1] = cone.cu - sum(u / zh[i] for i in 1:cone.d)
+    @. w1 = w / zh
     @views vec_copyto!(g[2:end], w1)
 
     cone.grad_updated = true
     return cone.grad
-end
-
-function update_hess_aux(cone::EpiNormInf)
-    @assert !cone.hess_aux_updated
-    @assert cone.grad_updated
-    u = cone.point[1]
-    w = cone.w
-    V = cone.V
-    s = cone.s
-    z = cone.z
-    zi = cone.zi
-    uzi = cone.uzi
-    szi = cone.szi
-    d = cone.d
-    # hiww = cone.hiww
-
-    @. s = abs(w)
-    @. V = w / s
-    @. zi = inv(z)
-    @. szi = 2 * s / z
-    u2 = abs2(u)
-    cu = (cone.d - 1) / u2
-    t = u2 .+ abs2.(s) # TODO
-    cone.huu = 2 * sum(t[i] / z[i] * zi[i] for i in 1:d) - cu
-    @. cone.huw = -uzi * szi
-    cone.hiuui = 2 * sum(inv, t) - cu
-    @. cone.hiuw = 2 * u * s ./ t
-
-    # for j in 1:d
-    #     s_j = s[j]
-    #     z_j = z[j]
-    #     for i in 1:(j - 1)
-    #         dij = u2 + s[i] * s_j
-    #         hiww[i, j] = z[i] / (2 * dij) * z_j
-    #     end
-    #     hiww[j, j] = z_j / (2 * t[j]) * z_j
-    # end
-
-    cone.hess_aux_updated = true
 end
 
 function hess_prod!(
@@ -186,45 +130,43 @@ function hess_prod!(
     arr::AbstractVecOrMat,
     cone::EpiNormInf,
     )
-    cone.hess_aux_updated || update_hess_aux(cone)
-    V = cone.V
+    @assert cone.grad_updated
+    d = cone.d
     u = cone.point[1]
     w = cone.w
-    s = cone.s
-    z = cone.z
-    # TODO delete not using
-    zi = cone.zi
-    uzi = cone.uzi
-    szi = cone.szi
-    huw = cone.huw
-    huu = cone.huu
+    zh = cone.zh
+    cu = cone.cu
     r = cone.w1
+    w2 = cone.w2
+    s1 = cone.s1
 
-    simszi = zero(s)
-    HW = zero(w)
-    Vszi = 2 * conj.(w) ./ z
-
-# TODO don't use huu (for all spectral cones)
-
-    # TODO try to do not column by column, like old code
     @inbounds for j in 1:size(prod, 2)
         p = arr[1, j]
         @views vec_copyto!(r, arr[2:end, j])
 
+        @. s1 = (real(conj(w) * r) - u * p) / zh
 
-        @. simszi = real(Vszi * r)
-        prod[1, j] = huu * p - dot(uzi, simszi)
-        @. HW = 2 * (r + (simszi - p * uzi) * w) / z
+        prod[1, j] = -sum((p + u * s1[i]) / zh[i] for i in 1:d) - cu * p / u
 
-
-
-        # prod[1, j] =
-
-
-        @views vec_copyto!(prod[2:end, j], HW)
+        @. w2 = (r + s1 * w) / zh
+        @views vec_copyto!(prod[2:end, j], w2)
     end
 
     return prod
+end
+
+function update_inv_hess_aux(cone::EpiNormInf)
+    @assert !cone.inv_hess_aux_updated
+    @assert cone.grad_updated
+    zh = cone.zh
+    th = cone.s1
+
+    u2 = abs2(cone.point[1])
+    @. th = u2 - zh
+    @. cone.u2ti = u2 / th
+    @inbounds cone.zti1 = 1 + sum(zh[i] / th[i] for i in 1:cone.d)
+
+    cone.inv_hess_aux_updated = true
 end
 
 function inv_hess_prod!(
@@ -232,49 +174,37 @@ function inv_hess_prod!(
     arr::AbstractVecOrMat,
     cone::EpiNormInf,
     )
-    cone.hess_aux_updated || update_hess_aux(cone)
-    V = cone.V
+    cone.inv_hess_aux_updated || update_inv_hess_aux(cone)
     u = cone.point[1]
     w = cone.w
-    s = cone.s
-    z = cone.z
-    # TODO delete not using
-    zi = cone.zi
-    uzi = cone.uzi
-    szi = cone.szi
-    hiuui = cone.hiuui
+    zh = cone.zh
+    zti1 = cone.zti1
+    u2ti = cone.u2ti
     r = cone.w1
+    w2 = cone.w2
+    s1 = cone.s1
 
-    u2 = abs2(u)
-    t = u2 .+ abs2.(s)
-
-    simsz = zero(s)
-    HiW = zero(w)
-    wti = w ./ t
-    T = eltype(z)
-
-    # TODO try to do not column by column, like old code
     @inbounds for j in 1:size(prod, 2)
         p = arr[1, j]
         @views vec_copyto!(r, arr[2:end, j])
-        @. simsz = real(conj(wti) * r)
 
-        c1 = (p + 2 * u * sum(simsz)) / hiuui
-        prod[1, j] = c1
+        @. s1 = real(conj(w) * r)
 
-        dconst = 2 * u * c1
-        @. HiW = dconst * wti + z * (T(0.5) * r - simsz * w)
-        @views vec_copyto!(prod[2:end, j], HiW)
+        c1 = (u * p + dot(u2ti, s1)) / zti1
+        prod[1, j] = c1 * u
+
+        @. w2 = zh * r + ((c1 - s1) * u2ti + s1) * w
+        @views vec_copyto!(prod[2:end, j], w2)
     end
 
     return prod
 end
 
-function dder3(cone::EpiNormInf, dir::AbstractVector)
-    cone.hess_aux_updated || update_hess_aux(cone)
+function dder3(cone::EpiNormInf{T}, dir::AbstractVector{T}) where T
+    @assert cone.grad_updated
     u = cone.point[1]
     w = cone.w
-    z = cone.z
+    zh = cone.zh
     dder3 = cone.dder3
     r = cone.w1
     s1 = cone.s1
@@ -283,13 +213,13 @@ function dder3(cone::EpiNormInf, dir::AbstractVector)
     p = dir[1]
     @views vec_copyto!(r, dir[2:end])
 
-    @. s1 = -2 * (u * p - real(conj(w) * r)) / z
-    @. s2 = (abs2(r) - abs2(p)) / z + abs2(s1)
+    @. s1 = (u * p - real(conj(w) * r)) / zh
+    @. s2 = T(0.5) * (abs2(p) - abs2(r)) / zh - abs2(s1)
 
-    dder3[1] = 2 * sum((p * s1[i] + u * s2[i]) / z[i] for i in 1:cone.d) -
-        (cone.d - 1) / u * abs2(p / u)
+    @inbounds c1 = sum((p * s1[i] + u * s2[i]) / zh[i] for i in 1:cone.d)
+    dder3[1] = -c1 - cone.cu * abs2(p / u)
 
-    @. cone.w2 = -2 * (s1 * r + s2 * w) / z
+    @. cone.w2 = (s1 * r + s2 * w) / zh
     @views vec_copyto!(dder3[2:end], cone.w2)
 
     return dder3
