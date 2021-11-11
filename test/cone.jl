6b8187cc1b2a62b5cfcb473f112ecb1f7efbf662
Chris Coey <coey.chris@gmail.com>
2021-11-11 13:32:49 -0500

    reimplement infinity norm cone oracles

--- test/cone.jl
+++ test/cone.jl
@@ -150,13 +150,17 @@ function test_barrier(
 
     fd_hess_dir = ForwardDiff.gradient(s -> ForwardDiff.derivative(t ->
         barrier_dir(s, t), 0), TFD_point)
+
     @test Cones.hess(cone) * dir ≈ fd_hess_dir atol=tol rtol=tol
+    @test Cones.inv_hess(cone) * fd_hess_dir ≈ dir atol=tol rtol=tol
     prod_vec = zero(dir)
     @test Cones.hess_prod!(prod_vec, dir, cone) ≈ fd_hess_dir atol=tol rtol=tol
+    @test Cones.inv_hess_prod!(prod_vec, fd_hess_dir, cone) ≈ dir atol=tol rtol=tol
 
     if Cones.use_dder3(cone)
         fd_third_dir = ForwardDiff.gradient(s2 -> ForwardDiff.derivative(s ->
             ForwardDiff.derivative(t -> barrier_dir(s2, t), s), 0), TFD_point)
+
         @test -2 * Cones.dder3(cone, dir) ≈ fd_third_dir atol=tol rtol=tol
     end
 
