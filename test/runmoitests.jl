ddde0307b01f5fed4f19b1d307dd05aeaeb78d76
Chris Coey <coey.chris@gmail.com>
2021-12-03 13:46:16 -0500

    try moi tests with bigfloat

--- test/runmoitests.jl
+++ test/runmoitests.jl
@@ -10,29 +10,33 @@ include(joinpath(@__DIR__, "moicones.jl"))
 
 @testset "MathOptInterface wrapper tests" begin
 
-@testset "MOI cone tests" begin
-    println("starting MOI wrapper cone tests")
-    real_types = [
-        Float64,
-        # Float32,
-        BigFloat,
-        ]
-    for T in real_types
-        @testset "$T" begin
-            println(T, " ...")
-            test_moi_cones(T)
-        end
-    end
-end
+# @testset "MOI cone tests" begin
+#     println("starting MOI wrapper cone tests")
+#     real_types = [
+#         Float64,
+#         # Float32,
+#         BigFloat,
+#         ]
+#     for T in real_types
+#         @testset "$T" begin
+#             println(T, " ...")
+#             test_moi_cones(T)
+#         end
+#     end
+# end
 
-@testset "MOI.Test tests" begin
-    println("\nstarting MOI.Test tests")
-    # TODO test other real types
-    T = Float64
+# real types, tolerances, and tests to include for MOI.Test tests
+test_T = [
+    (Float64, 2 * sqrt(sqrt(eps())), 4, String[]),
+    (BigFloat, 2 * eps(BigFloat)^0.2, 1, String["test_conic"]), # TODO add test_linear
+]
+
+@testset "MOI.Test tests: $T" for (T, tol_test, tol_relax, includes) in test_T
+    println("\nstarting MOI.Test tests: $T")
     model = MOI.Bridges.full_bridge_optimizer(
         MOI.Utilities.CachingOptimizer(
             MOI.Utilities.UniversalFallback(MOI.Utilities.Model{T}()),
-            Hypatia.Optimizer{T}(; default_tol_relax = 4),
+            Hypatia.Optimizer{T}(; default_tol_relax = tol_relax),
         ),
         T,
     )
@@ -41,8 +45,8 @@ end
         model,
         MOI.Test.Config(
             T,
-            atol = 2 * sqrt(sqrt(eps(T))),
-            rtol = 2 * sqrt(sqrt(eps(T))),
+            atol = tol_test,
+            rtol = tol_test,
             exclude = Any[
                 MOI.ConstraintBasisStatus,
                 MOI.VariableBasisStatus,
@@ -50,10 +54,9 @@ end
                 MOI.SolverVersion,
             ],
         ),
-        # include = String["test_conic"],
+        include = includes,
         exclude = String[
-            # TODO(odow): unexpected failure. But this is probably in the bridge
-            # layer, not Hypatia.
+            # TODO(odow): unexpected failure, probably in the bridge layer
             "test_model_UpperBoundAlreadySet",
             "test_model_LowerBoundAlreadySet",
         ],
